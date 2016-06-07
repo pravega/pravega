fb3f18a60d28a87569226d451f53769439a6eed7
Andrei Paduroiu <andrei.paduroiu@emc.com>
2016-06-07 10:42:00 -0700

    Fixed bug in ServiceStarter where it was not initializing the Container Manager, thus no containers were usable.

--- logservice/server/src/main/java/com/emc/logservice/server/service/StreamSegmentService.java
+++ logservice/server/src/main/java/com/emc/logservice/server/service/StreamSegmentService.java
@@ -46,7 +46,9 @@ public class StreamSegmentService implements StreamSegmentStore {
 
     @Override
     public CompletableFuture<Long> append(String streamSegmentName, byte[] data, AppendContext appendContext, Duration timeout) {
-        return getContainer(streamSegmentName).thenCompose(container -> container.append(streamSegmentName, data, appendContext, timeout));
+        CompletableFuture<Long> result = getContainer(streamSegmentName).thenCompose(container -> container.append(streamSegmentName, data, appendContext, timeout));
+        //result.whenComplete((r, ex) -> System.out.println(String.format("Append to %s (%d bytes) complete: Result %d, Error %s.", streamSegmentName, data.length, r, ex)));
+        return result;
     }
 
     @Override
--- logservice/serverhost/src/main/java/com/emc/logservice/serverhost/ServiceStarter.java
+++ logservice/serverhost/src/main/java/com/emc/logservice/serverhost/ServiceStarter.java
@@ -5,16 +5,21 @@ import com.emc.logservice.server.mocks.InMemoryServiceBuilder;
 import com.emc.logservice.server.service.ServiceBuilder;
 import com.emc.logservice.serverhost.handler.LogServiceConnectionListener;
 
+import java.time.Duration;
+
 /**
  * Starts the Log Service.
  */
 public final class ServiceStarter {
     private static final int Port = 12345;
     private static final int ContainerCount = 1;
+    private static final Duration InitializeTimeout = Duration.ofSeconds(30);
 
     public static void main(String[] args) {
         System.out.println("Initializing ServiceBuilder ...");
         try (ServiceBuilder serviceBuilder = new InMemoryServiceBuilder(ContainerCount)) {
+            System.out.println("Initializing Container Manager ...");
+            serviceBuilder.getContainerManager().initialize(InitializeTimeout).join();
             System.out.println("Creating StreamSegmentService ...");
             StreamSegmentStore service = serviceBuilder.createStreamSegmentService();
             System.out.println("Starting LogServiceConnectionListener ...");
