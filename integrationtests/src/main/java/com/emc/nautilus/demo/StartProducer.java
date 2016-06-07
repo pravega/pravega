af3563837e674172dfc8944ffdd6233a4c7d9084
Tom <tom.kaitchuck@emc.com>
2016-06-07 14:42:32 -0700

    Change producer as discussed

--- integrationtests/src/main/java/com/emc/nautilus/demo/StartProducer.java
+++ integrationtests/src/main/java/com/emc/nautilus/demo/StartProducer.java
@@ -9,20 +9,20 @@ import com.emc.nautilus.streaming.impl.SingleSegmentStreamManagerImpl;
 import lombok.Cleanup;
 
 public class StartProducer {
-	
-	public static final int PORT = 12345;
 
 	public static void main(String[] args) {
 		String endpoint = "localhost";
-		String streamName = "abc";
+		int port = 12345;
+		String scope = "Scope1";
+		String streamName = "Stream1";
 		String testString = "Hello world: ";
 
-		SingleSegmentStreamManagerImpl streamManager = new SingleSegmentStreamManagerImpl(endpoint, PORT, "Scope1");
+		SingleSegmentStreamManagerImpl streamManager = new SingleSegmentStreamManagerImpl(endpoint, port, scope);
 		Stream stream = streamManager.createStream(streamName, null);
 		@Cleanup
 		Producer<String> producer = stream.createProducer(new JavaSerializer<>(), new ProducerConfig(null));
-		for (int i=0;i<1000;i++) {
-			producer.publish("RoutingKey", testString+i+"\n");
+		for (int i=0;i<100000;i++) {
+			producer.publish(null, testString+i+"\n");
 		}
 		producer.flush();
 	}
