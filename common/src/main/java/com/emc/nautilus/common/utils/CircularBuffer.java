b1b323e2ab52b889c2f95493b61cb98a2dc3efb0
Tom <tom.kaitchuck@emc.com>
2016-07-29 18:12:36 -0700

    Add tests for circularBuffer and fix bugs found.

--- common/src/main/java/com/emc/nautilus/common/utils/CircularBuffer.java
+++ common/src/main/java/com/emc/nautilus/common/utils/CircularBuffer.java
@@ -1,11 +1,11 @@
 /**
  * Licensed to the Apache Software Foundation (ASF) under one
- * or more contributor license agreements.  See the NOTICE file
+ * or more contributor license agreements. See the NOTICE file
  * distributed with this work for additional information
- * regarding copyright ownership.  The ASF licenses this file
+ * regarding copyright ownership. The ASF licenses this file
  * to you under the Apache License, Version 2.0 (the
  * "License"); you may not use this file except in compliance
- * with the License.  You may obtain a copy of the License at
+ * with the License. You may obtain a copy of the License at
  * <p>
  * http://www.apache.org/licenses/LICENSE-2.0
  * <p>
@@ -24,7 +24,8 @@ import lombok.Getter;
 /**
  * Convenience class wrapping byteBuffer to provide a circular buffer. This works by maintaining
  * two byte buffers backed by the same array. The position of the readBuffer corresponds to the
- * point up to which data has been read. The position of the writeBuffer corresponds to the point up to
+ * point up to which data has been read. The position of the writeBuffer corresponds to the point up
+ * to
  * which data has been written. Each buffer's limit is either the end or the buffer or the position
  * of the other, depending on where the data has wrapped.
  */
@@ -53,21 +54,25 @@ public class CircularBuffer {
      */
     public int read(ByteBuffer toFill) {
         int origionalPos = toFill.position();
-        readHelper(toFill);
-        if (readBuffer.hasRemaining() && toFill.hasRemaining()) {
+        while (dataAvailable() > 0 && toFill.hasRemaining()) {
             readHelper(toFill);
         }
         return toFill.position() - origionalPos;
     }
 
     private void readHelper(ByteBuffer toFill) {
+        int readLimit = readBuffer.limit();
         int toRead = Math.min(toFill.remaining(), readBuffer.remaining());
-        readBuffer.limit(toRead);
+        readBuffer.limit(readBuffer.position() + toRead);
         toFill.put(readBuffer);
-        if (readBuffer.position() <= fillBuffer.position()) {
+        readBuffer.limit(readLimit);
+        if (readBuffer.position() == capacity) {
+            readBuffer.position(0);
             readBuffer.limit(fillBuffer.position());
-        } else {
-            readBuffer.limit(readBuffer.capacity());
+            fillBuffer.limit(capacity);
+        }
+        if (fillBuffer.position() < readBuffer.position()) {
+            fillBuffer.limit(readBuffer.position());
         }
     }
 
@@ -76,26 +81,30 @@ public class CircularBuffer {
      */
     public int fill(ByteBuffer fillFrom) {
         int origionalPos = fillFrom.position();
-        fillHelper(fillFrom);
-        if (fillBuffer.hasRemaining() && fillFrom.remaining() > 0) {
+        while (capacityAvailable() > 0 && fillFrom.hasRemaining()) {
             fillHelper(fillFrom);
         }
         return fillFrom.position() - origionalPos;
     }
 
     private void fillHelper(ByteBuffer fillFrom) {
+        int fillLimit = fillBuffer.limit();
         int toAdd = Math.min(fillFrom.remaining(), fillBuffer.remaining());
-        fillBuffer.limit(toAdd);
+        fillBuffer.limit(fillBuffer.position() + toAdd);
 
         int limit = fillFrom.limit();
         fillFrom.limit(fillFrom.position() + toAdd);
         fillBuffer.put(fillFrom);
         fillFrom.limit(limit);
+        fillBuffer.limit(fillLimit);
 
-        if (fillBuffer.position() <= readBuffer.position()) {
+        if (fillBuffer.position() == capacity) {
+            fillBuffer.position(0);
             fillBuffer.limit(readBuffer.position());
-        } else {
-            fillBuffer.limit(fillBuffer.capacity());
+            readBuffer.limit(capacity);
+        }
+        if (readBuffer.position() < fillBuffer.position()) {
+            readBuffer.limit(fillBuffer.position());
         }
     }
 
@@ -103,14 +112,30 @@ public class CircularBuffer {
      * @return the number of bytes that can be read
      */
     public int dataAvailable() {
-        if (readBuffer.position() <= fillBuffer.position()) {
-            return fillBuffer.position() - readBuffer.position();
+        if (readBuffer.position() < fillBuffer.position()) {
+            return readBuffer.remaining();
+        } else if (readBuffer.position() > fillBuffer.position()) {
+            return capacity - fillBuffer.remaining();
         } else {
-            return readBuffer.capacity() - readBuffer.position() + fillBuffer.position();
+            if (readBuffer.hasRemaining()) {
+                return readBuffer.remaining() + fillBuffer.position();
+            } else {
+                return 0;
+            }
         }
     }
 
     public int capacityAvailable() {
-        return capacity - dataAvailable();
+        if (fillBuffer.position() < readBuffer.position()) {
+            return fillBuffer.remaining();
+        } else if (fillBuffer.position() > readBuffer.position()) {
+            return capacity - readBuffer.remaining();
+        } else {
+            if (fillBuffer.hasRemaining()) {
+                return fillBuffer.remaining() + readBuffer.position();
+            } else {
+                return 0;
+            }
+        }
     }
 }
