b77bab9cd753a8b1389585ba35f76e299a050962
Tom <tom.kaitchuck@emc.com>
2016-08-02 17:07:34 -0700

    Fix leftover imports

--- integrationtests/src/test/java/testsuites/TestAppendPath.java
+++ integrationtests/src/test/java/testsuites/TestAppendPath.java
@@ -4,10 +4,10 @@ import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 
+import com.emc.nautilus.stream.segment.impl.SegmentOutputStreamTest;
+import com.emc.pravega.common.netty.AppendEncodeDecodeTest;
 import com.emc.pravega.integrationtests.AppendTest;
 import com.emc.pravega.service.server.host.handler.AppendProcessorTest;
-import com.emc.nautilus.common.netty.AppendEncodeDecodeTest;
-import com.emc.nautilus.stream.segment.impl.SegmentOutputStreamTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({AppendEncodeDecodeTest.class, AppendProcessorTest.class, AppendTest.class, SegmentOutputStreamTest.class})
