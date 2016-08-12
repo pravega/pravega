ab249cfc10026636a699de1aa5eb6dd02bb7385d
Sandeep <sandeep.shridhar@emc.com>
2016-08-12 04:38:31 +0000

    1. removed multiple build.gradle.; All the configuration is in the top level build.gradle 2. org.apache needs to be part of the checkstyle import-control.xml for thrift related libs. 3. Ensured that the checkstyle check gradle tasks is working fine.

--- controller/client/src/main/java/com/emc/pravega/controller/client/Client.java
+++ controller/client/src/main/java/com/emc/pravega/controller/client/Client.java
@@ -1,3 +1,21 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p/>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p/>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.emc.pravega.controller.client;
 
 import com.emc.pravega.controller.api.EchoService;
@@ -10,7 +28,7 @@ import org.apache.thrift.protocol.TBinaryProtocol;
 import org.apache.thrift.protocol.TProtocol;
 
 public class Client {
-    public static void main(String [] args) {
+    public static void main(String[] args) {
 
         if (args.length != 1) {
             System.out.println("Please enter 'simple' or 'secure'");
@@ -22,8 +40,7 @@ public class Client {
             if (args[0].contains("simple")) {
                 transport = new TSocket("localhost", 9090);
                 transport.open();
-            }
-            else {
+            } else {
         /*
          * Similar to the server, you can use the parameters to setup client parameters or
          * use the default settings. On the client side, you will need a TrustStore which
@@ -39,7 +56,7 @@ public class Client {
                 transport = TSSLTransportFactory.getClientSocket("localhost", 9091, 0, params);
             }
 
-            TProtocol protocol = new  TBinaryProtocol(transport);
+            TProtocol protocol = new TBinaryProtocol(transport);
             EchoService.Client client = new EchoService.Client(protocol);
 
             String result = client.echo("test string");
