45d43089a3f309347471dd9a82f2e9c8774fb0c5
bhargav gulavani <bhargav.gulavani@emc.com>
2016-09-11 05:01:37 -0700

    javadocs, well-formed positions

--- clients/streaming/src/main/java/com/emc/pravega/stream/Position.java
+++ clients/streaming/src/main/java/com/emc/pravega/stream/Position.java
@@ -25,16 +25,6 @@ import com.emc.pravega.stream.impl.PositionImpl;
  * A position in a stream. Used to restore to a consumer particular point in a stream.
  * Note that this is serializable so that it can be written to an external datastore.
  *
- * A position has two components
- * 1. ownedSegments -- segments that can be read currently. Each ownedSegment also has an offset indicating the
- * point until which events have been read from that segment. Completely read segments have offset of -1.
- * 2. futureOwnedSegments -- segments that can be read after one of the currently read segment is completely read. Each
- * segment in this set has exactly one previous segment that belongs to the set ownedSegments.
- *
- * Well-formed position object. A position is called well-formed iff the following hold.
- * 1. for each segment s in futureOwnedSegment, s.previous belongs to ownedSegments and s.previous.offset != -1
- * 2. for each segment s in ownedSegment, s.previous does not belongs to ownedSegments
- *
  */
 public interface Position extends Serializable {
     
