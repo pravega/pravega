1ecde18dcfaf7ade2a4474f71532d148598e8593
bhargav gulavani <bhargav.gulavani@emc.com>
2016-10-02 03:25:04 -0700

    Splitting functions into smaller ones to enable easy future lifting

--- controller/server/src/main/java/com/emc/pravega/controller/store/stream/AbstractStreamMetadataStore.java
+++ controller/server/src/main/java/com/emc/pravega/controller/store/stream/AbstractStreamMetadataStore.java
@@ -24,6 +24,7 @@ import java.util.AbstractMap;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
 import java.util.Map;
 import java.util.Set;
@@ -36,9 +37,6 @@ import java.util.stream.Collectors;
 public abstract class AbstractStreamMetadataStore implements StreamMetadataStore {
 
     public abstract Stream getStream(String name);
-
-    @Override
-    public abstract boolean createStream(String name, StreamConfiguration configuration);
 
     @Override
     public boolean updateConfiguration(String name, StreamConfiguration configuration) {
@@ -65,18 +63,7 @@ public abstract class AbstractStreamMetadataStore implements StreamMetadataStore
     public SegmentFutures getActiveSegments(String name, long timestamp) {
         Stream stream = getStream(name);
         List<Integer> activeSegments = stream.getActiveSegments(timestamp);
-        List<Integer> currentSegments = new ArrayList<>();
-        Map<Integer, Integer> futureSegments = new HashMap<>();
-        int i = 0;
-        while (i < activeSegments.size()) {
-            int number = activeSegments.get(i);
-            currentSegments.add(number);
-            // futures is set to all the successors of segment that have this segment as the only predecessor
-            getDefaultFutures(stream, number).stream()
-                    .forEach(x -> futureSegments.put(x, number));
-            i++;
-        }
-        return new SegmentFutures(currentSegments, futureSegments);
+        return constructSegmentFutures(stream, activeSegments);
     }
 
     @Override
@@ -86,50 +73,17 @@ public abstract class AbstractStreamMetadataStore implements StreamMetadataStore
 
         Stream stream = getStream(name);
 
-        // append completed segment set with implicitly completed segments
+        Set<Integer> current = new HashSet<>();
         positions.forEach(position ->
-                position.getCurrent().forEach(number ->
-                        stream.getPredecessors(number).stream().forEach(completedSegments::add)));
+                position.getCurrent().forEach(current::add));
 
-        // successors of completed segments are interesting, which means
-        // some of them may become current, and
-        // some of them may become future
-        Set<Integer> successors = completedSegments.stream().flatMap(x -> stream.getSuccessors(x).stream()).collect(Collectors.toSet());
+        Set<Integer> implicitCompletedSegments = getImplicitCompletedSegments(stream, completedSegments, current);
 
-        // a successor that has
-        // 1. all its predecessors completed, and
-        // 2. it is not completed yet, and
-        // 3. it is not current in any of the positions,
-        // shall become current and be added to some position
-        List<Integer> newCurrents = successors.stream().filter(x ->
-                        // 1. all its predecessors completed, and
-                        stream.getPredecessors(x).stream().allMatch(completedSegments::contains)
-                                // 2. it is not completed yet, and
-                                && !completedSegments.contains(x)
-                                // 3. it is not current in any of the positions
-                                && positions.stream().allMatch(z -> !z.getCurrent().contains(x))
-        ).collect(Collectors.toList());
+        Set<Integer> successors = getSuccessors(stream, implicitCompletedSegments);
 
-        Map<Integer, List<Integer>> newFutures = new HashMap<>();
-        successors.stream().forEach(
-                x -> {
-                    // if x is not completed
-                    if (!completedSegments.contains(x)) {
-                        // number of predecessors not completed == 1
-                        List<Integer> filtered = stream.getPredecessors(x).stream().filter(y -> !completedSegments.contains(y)).collect(Collectors.toList());
-                        if (filtered.size() == 1) {
-                            Integer pendingPredecessor = filtered.get(0);
-                            if (newFutures.containsKey(pendingPredecessor)) {
-                                newFutures.get(pendingPredecessor).add(x);
-                            } else {
-                                List<Integer> list = new ArrayList<>();
-                                list.add(x);
-                                newFutures.put(pendingPredecessor, list);
-                            }
-                        }
-                    }
-                }
-        );
+        List<Integer> newCurrents = getNewCurrents(stream, successors, implicitCompletedSegments, positions);
+
+        Map<Integer, List<Integer>> newFutures = getNewFutures(stream, successors, implicitCompletedSegments);
 
         return divideSegments(stream, newCurrents, newFutures, positions);
     }
@@ -137,6 +91,16 @@ public abstract class AbstractStreamMetadataStore implements StreamMetadataStore
     @Override
     public List<Segment> scale(String name, List<Integer> sealedSegments, List<AbstractMap.SimpleEntry<Double, Double>> newRanges, long scaleTimestamp) {
         return getStream(name).scale(sealedSegments, newRanges, scaleTimestamp);
+    }
+
+    private SegmentFutures constructSegmentFutures(Stream stream, List<Integer> activeSegments) {
+        Map<Integer, Integer> futureSegments = new HashMap<>();
+        for (Integer number: activeSegments) {
+            for (Integer future : getDefaultFutures(stream, number)) {
+                futureSegments.put(future, number);
+            }
+        }
+        return new SegmentFutures(activeSegments, futureSegments);
     }
 
     /**
@@ -150,6 +114,65 @@ public abstract class AbstractStreamMetadataStore implements StreamMetadataStore
         return stream.getSuccessors(number).stream()
                 .filter(x -> stream.getPredecessors(x).size() == 1)
                 .collect(Collectors.toList());
+    }
+
+    private Set<Integer> getImplicitCompletedSegments(Stream stream, Set<Integer> completedSegments, Set<Integer> current) {
+        // append completed segment set with implicitly completed segments
+        current.stream().forEach(number ->
+                stream.getPredecessors(number).stream().forEach(completedSegments::add));
+        return completedSegments;
+    }
+
+    private Set<Integer> getSuccessors(Stream stream, Set<Integer> completedSegments) {
+        // successors of completed segments are interesting, which means
+        // some of them may become current, and
+        // some of them may become future
+        //Set<Integer> successors = completedSegments.stream().flatMap(x -> stream.getSuccessors(x).stream()).collect(Collectors.toSet());
+        return completedSegments.stream().flatMap(x -> stream.getSuccessors(x).stream()).collect(Collectors.toSet());
+    }
+
+    private List<Integer> getNewCurrents(Stream stream, Set<Integer> successors, Set<Integer> completedSegments, List<SegmentFutures> positions) {
+        // a successor that has
+        // 1. it is not completed yet, and
+        // 2. it is not current in any of the positions,
+        // 3. all its predecessors completed, and
+        // shall become current and be added to some position
+        List<Integer> newCurrents = successors.stream().filter(x ->
+                        // 1. it is not completed yet, and
+                        !completedSegments.contains(x)
+                                // 2. it is not current in any of the positions
+                                && positions.stream().allMatch(z -> !z.getCurrent().contains(x))
+                                // 1. all its predecessors completed, and
+                                && stream.getPredecessors(x).stream().allMatch(completedSegments::contains)
+        ).collect(Collectors.toList());
+        return newCurrents;
+    }
+
+    private Map<Integer, List<Integer>> getNewFutures(Stream stream, Set<Integer> successors, Set<Integer> completedSegments) {
+
+        List<Integer> subset = successors.stream().filter(x -> !completedSegments.contains(x)).collect(Collectors.toList());
+
+        List<List<Integer>> preds =
+                subset.stream()
+                        .map(number -> stream.getPredecessors(number).stream()
+                                .filter(y -> !completedSegments.contains(y)).collect(Collectors.toList()))
+                        .collect(Collectors.toList());
+
+        Map<Integer, List<Integer>> map = new HashMap<>();
+        for (int i = 0; i < preds.size(); i++) {
+            List<Integer> filtered = preds.get(i);
+            if (filtered.size() == 1) {
+                Integer pendingPredecessor = filtered.get(0);
+                if (map.containsKey(pendingPredecessor)) {
+                    map.get(pendingPredecessor).add(subset.get(i));
+                } else {
+                    List<Integer> list = new ArrayList<>();
+                    list.add(subset.get(i));
+                    map.put(pendingPredecessor, list);
+                }
+            }
+        }
+        return map;
     }
 
     /**
