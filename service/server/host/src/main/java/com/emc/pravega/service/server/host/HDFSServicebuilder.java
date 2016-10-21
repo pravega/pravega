5006549d53cd793ff26d5adcc3e5f10755e39808
arvindkandhare <arvind.kandhare@emc.com>
2016-10-21 14:08:19 -0700

    Travis fix

--- service/server/host/src/main/java/com/emc/pravega/service/server/host/HDFSServicebuilder.java
+++ service/server/host/src/main/java/com/emc/pravega/service/server/host/HDFSServicebuilder.java
@@ -19,11 +19,9 @@
 package com.emc.pravega.service.server.host;
 
 import com.emc.pravega.service.server.store.ServiceBuilderConfig;
-import com.emc.pravega.service.storage.DurableDataLogFactory;
+import com.emc.pravega.service.storage.StorageFactory;
 import com.emc.pravega.service.storage.impl.hdfs.HDFSStorageConfig;
 import com.emc.pravega.service.storage.impl.hdfs.HDFSStorageFactory;
-import com.emc.pravega.service.storage.StorageFactory;
-import com.emc.pravega.service.storage.mocks.InMemoryDurableDataLogFactory;
 
 public class HDFSServicebuilder extends DistributedLogServiceBuilder {
     public HDFSServicebuilder(ServiceBuilderConfig config) {
