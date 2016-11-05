b2faf80cbed6cbf8b0f70e8c98a513829c1195bf
arvindkandhare <arvind.kandhare@emc.com>
2016-11-04 18:37:53 -0700

    Fixing the read side of things.

--- service/server/src/main/java/com/emc/pravega/service/server/containers/StreamSegmentMapper.java
+++ service/server/src/main/java/com/emc/pravega/service/server/containers/StreamSegmentMapper.java
@@ -272,7 +272,8 @@ public class StreamSegmentMapper {
     private void assignStreamSegmentId(String streamSegmentName, Duration timeout) {
         TimeoutTimer timer = new TimeoutTimer(timeout);
         this.storage
-                .getStreamSegmentInfo(streamSegmentName, timer.getRemaining())
+                .acquireLockForSegment(streamSegmentName)
+                .thenCompose(() -> this.storage.getStreamSegmentInfo(streamSegmentName, timer.getRemaining()))
                 .thenCompose(streamInfo -> persistInDurableLog(streamInfo, timer.getRemaining()))
                 .exceptionally(ex -> {
                     failAssignment(streamSegmentName, ex);
