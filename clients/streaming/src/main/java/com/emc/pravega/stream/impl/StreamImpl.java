78c151a7f84f15adbb8e04e7ec16a2355f51d13e
Flavio Junqueira (fpj) <flavio.junqueira@emc.com>
2016-11-24 15:40:32 +0000

    Missing change

--- clients/streaming/src/main/java/com/emc/pravega/stream/impl/StreamImpl.java
+++ clients/streaming/src/main/java/com/emc/pravega/stream/impl/StreamImpl.java
@@ -89,7 +89,7 @@ public class StreamImpl implements Stream {
     }
 
     @Override
-    public String getQualifiedName() {
+    public String getScopedName() {
         StringBuffer sb = new StringBuffer();
         if (scope != null) {
             sb.append(scope);
