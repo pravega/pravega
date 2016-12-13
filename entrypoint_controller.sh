fc68080de05711522dc8b559f1912d28cc489f57
Abhijeet-Jadhav <Abhijeet.Jadhav@emc.com>
2016-12-12 20:49:41 -0800

    modified entrypoint of controller

--- entrypoint_controller.sh
+++ entrypoint_controller.sh
@@ -1,3 +1,11 @@
 #!/bin/bash
-sed -i 's/'localhost:2181'/'$zkCluster'/'  /opt/controller/server/conf/application.conf
+grep -iq 'zk {' /opt/controller/server/conf/application.conf
+if [ "$?" == "1" ]
+then 
+     echo "ZK config not found"
+     exit
+else
+     sed -i 's/url = "localhost:2181"/url = "'$zkCluster'"/' /opt/controller/server/conf/application.conf
+fi
+
 /opt/controller/server/bin/server
