9152b3d5b22dfbe3743d5df6c1d08f319d507dc5
Abhijeet-Jadhav <Abhijeet.Jadhav@emc.com>
2017-01-09 04:33:00 -0800

    Contract folder added

---
+++ controller/server/src/main/java/com/emc/pravega/controller/server/rest/contract/StreamConfig.java
@@ -0,0 +1,44 @@
+/**
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ * <p>
+ * http://www.apache.org/licenses/LICENSE-2.0
+ * <p>
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.emc.pravega.controller.server.rest.contract;
+
+import com.emc.pravega.stream.RetentionPolicy;
+import com.emc.pravega.stream.ScalingPolicy;
+import com.emc.pravega.stream.StreamConfiguration;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.core.MediaType;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@Produces(MediaType.APPLICATION_JSON)
+@Consumes(MediaType.APPLICATION_JSON)
+public class StreamConfig implements StreamConfiguration {
+
+    private String scope;
+    private String name;
+    private ScalingPolicy scalingPolicy;
+    private RetentionPolicy retentionPolicy;
+
+}
