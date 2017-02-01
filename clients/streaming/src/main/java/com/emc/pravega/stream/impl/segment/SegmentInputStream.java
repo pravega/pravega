19c3c660e065128f1d41e9073e71ebc371636a26
Bayar Volodya <bayarsaikhan.volodya@emc.com>
2017-02-01 12:26:50 -0800

    new line after before @param

--- clients/streaming/src/main/java/com/emc/pravega/stream/impl/segment/SegmentInputStream.java
+++ clients/streaming/src/main/java/com/emc/pravega/stream/impl/segment/SegmentInputStream.java
@@ -39,6 +39,7 @@ public abstract class SegmentInputStream implements AutoCloseable {
     /**
      * Gets the current offset. (Passing this to setOffset in the future will reset reads to the
      * current position in the segment.)
+     * 
      * @return The current offset.
      */
     public abstract long getOffset();
@@ -46,6 +47,7 @@ public abstract class SegmentInputStream implements AutoCloseable {
     /**
      * Returns the length of the current segment. i.e. calling setOffset with the result of this
      * method followed by read would result in read blocking until more data is written.
+     * 
      * @return The length of the current segment.
      */
     public abstract long fetchCurrentStreamLength();
