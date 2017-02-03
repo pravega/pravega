5f7d556ffc5f901035a92a4cd3de0890eec4788d
root <root@lviprbig250151.lss.emc.com>
2017-02-03 02:55:39 +0000

    Fixed issue with scripts

--- perftest/cleanup_zk.sh
+++ perftest/cleanup_zk.sh
@@ -1,4 +1,4 @@
-cat << EOF | ./distributedlog-service/bin/dlog zkshell 10.249.250.151
+cat << EOF | ~/distributedlog-service/bin/dlog zkshell 10.249.250.151
 deleteall /cluster
 deleteall /messaging
 deleteall /streams
