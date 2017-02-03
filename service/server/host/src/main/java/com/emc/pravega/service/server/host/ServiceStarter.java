c1395680f6268496c03089e5b3c19d65bf5e1664
arvindkandhare <arvind.kandhare@emc.com>
2017-02-02 16:03:03 -0800

    Feature single process deploy (#313)
    Created a new class that instantiates the following components in a single process (for testing purposes):
    - HDFS
    - booies
    - A single host
    - A single controller

--- service/server/host/src/main/java/com/emc/pravega/service/server/host/ServiceStarter.java
+++ service/server/host/src/main/java/com/emc/pravega/service/server/host/ServiceStarter.java
@@ -62,14 +62,14 @@ public final class ServiceStarter {
 
     //region Constructor
 
-    private ServiceStarter(ServiceBuilderConfig config) {
+    public ServiceStarter(ServiceBuilderConfig config) {
         this.builderConfig = config;
         this.serviceConfig = this.builderConfig.getConfig(ServiceConfig::new);
         Options opt = new Options();
-        opt.distributedLog = false;
-        opt.hdfs = false;
+        opt.distributedLog = true;
+        opt.hdfs = true;
         opt.rocksDb = true;
-        opt.zkSegmentManager = false;
+        opt.zkSegmentManager = true;
         this.serviceBuilder = createServiceBuilder(opt);
     }
 
@@ -98,7 +98,7 @@ public final class ServiceStarter {
 
     //region Service Operation
 
-    private void start() {
+    public void start() {
         Exceptions.checkNotClosed(this.closed, this);
 
         LoggerContext context = (LoggerContext) LoggerFactory.getILoggerFactory();
@@ -121,7 +121,7 @@ public final class ServiceStarter {
         log.info("StreamSegmentService started.");
     }
 
-    private void shutdown() {
+    public void shutdown() {
         if (!this.closed) {
             this.serviceBuilder.close();
             log.info("StreamSegmentService shut down.");
