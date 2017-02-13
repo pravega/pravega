d805f927e6e8301a9988e9982afb7e39e6f2cafc
Flavio Junqueira (fpj) <flavio.junqueira@emc.com>
2017-02-13 15:06:07 +0000

    Renaming SegmentReader to SegmentEventReader

--- clients/streaming/src/main/java/com/emc/pravega/stream/impl/ClientFactoryImpl.java
+++ clients/streaming/src/main/java/com/emc/pravega/stream/impl/ClientFactoryImpl.java
@@ -135,7 +135,7 @@ public class ClientFactoryImpl implements ClientFactory {
         private final AtomicInteger counter = new AtomicInteger(0);
 
         @Override
-        public SegmentReader<T> nextSegment(List<SegmentReader<T>> segments) {
+        public SegmentEventReader<T> nextSegment(List<SegmentEventReader<T>> segments) {
             int count = counter.incrementAndGet();
             return segments.get(count % segments.size());
         }
