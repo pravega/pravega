c8682df06fc2321714779281f6d478776b3b267d
abhijeet-jadhav <Abhijeet.Jadhav@emc.com>
2017-02-20 21:21:03 -0800

    new header

--- controller/server/src/main/java/com/emc/pravega/controller/store/stream/ZKStream.java
+++ controller/server/src/main/java/com/emc/pravega/controller/store/stream/ZKStream.java
@@ -30,6 +30,7 @@ import java.util.concurrent.CompletableFuture;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.SerializationUtils;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.utils.ZKPaths;
@@ -44,6 +45,7 @@ import org.apache.zookeeper.data.Stat;
  * It may cache files read from the store for its lifetime.
  * This shall reduce store round trips for answering queries, thus making them efficient.
  */
+@Slf4j
 class ZKStream extends PersistentStreamBase<Integer> {
     private static final String SCOPE_PATH = "/store/%s";
     private static final String STREAM_PATH = SCOPE_PATH + "/%s";
@@ -454,6 +456,7 @@ class ZKStream extends PersistentStreamBase<Integer> {
                         }
                     } else {
                         //path does not exist indicates Stream is not present
+                        log.error("Failed to write data. path {}", path);
                         throw new StreamNotFoundException(extractStreamName(path));
                     }
                 }) // load into cache after writing the data
