0fb2f59505be00dd3f897dabd481db6a0873106a
Suri <suresh.krishnappa@gmail.com>
2017-02-24 07:42:04 -0800

    compile fix

--- controller/server/src/main/java/com/emc/pravega/controller/store/stream/AbstractStreamMetadataStore.java
+++ controller/server/src/main/java/com/emc/pravega/controller/store/stream/AbstractStreamMetadataStore.java
@@ -22,7 +22,12 @@ import com.google.common.cache.CacheLoader;
 import com.google.common.cache.LoadingCache;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.*;
+import java.util.AbstractMap;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 import java.util.stream.Collectors;
