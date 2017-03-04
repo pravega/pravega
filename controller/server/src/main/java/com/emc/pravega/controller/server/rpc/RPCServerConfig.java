870fa4adcdf57b86d557a7258ce557073dad3477
bhargav gulavani <bhargav.gulavani@emc.com>
2017-03-04 09:49:08 -0800

    Precondition checks to RPCServerConfig constructor

--- controller/server/src/main/java/com/emc/pravega/controller/server/rpc/RPCServerConfig.java
+++ controller/server/src/main/java/com/emc/pravega/controller/server/rpc/RPCServerConfig.java
@@ -3,6 +3,7 @@
  */
 package com.emc.pravega.controller.server.rpc;
 
+import com.google.common.base.Preconditions;
 import lombok.Builder;
 import lombok.Data;
 
@@ -10,10 +11,22 @@ import lombok.Data;
  * Thrift RPC server config.
  */
 @Data
-@Builder
 public class RPCServerConfig {
-    int port;
-    int workerThreadCount;
-    int selectorThreadCount;
-    int maxReadBufferBytes;
+    private final int port;
+    private final int workerThreadCount;
+    private final int selectorThreadCount;
+    private final int maxReadBufferBytes;
+
+    @Builder
+    public RPCServerConfig(int port, int workerThreadCount, int selectorThreadCount, int maxReadBufferBytes) {
+        Preconditions.checkArgument(port > 0, "Invalid port.");
+        Preconditions.checkArgument(workerThreadCount > 0, "Invalid workerThreadCount.");
+        Preconditions.checkArgument(selectorThreadCount > 0, "Invalid selectorThreadCount.");
+        Preconditions.checkArgument(maxReadBufferBytes > 0, "Invalid maxReadBufferBytes.");
+
+        this.port = port;
+        this.workerThreadCount = workerThreadCount;
+        this.selectorThreadCount = selectorThreadCount;
+        this.maxReadBufferBytes = maxReadBufferBytes;
+    }
 }
