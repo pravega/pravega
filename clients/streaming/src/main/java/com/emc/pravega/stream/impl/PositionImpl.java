d5dd5037678271ef04573f1084aa7aef65830b5b
Tom Kaitchuck <Tom.Kaitchuck@emc.com>
2017-03-08 10:36:12 -0800

    Reduce visibility of positionImpl

--- clients/streaming/src/main/java/com/emc/pravega/stream/impl/PositionImpl.java
+++ clients/streaming/src/main/java/com/emc/pravega/stream/impl/PositionImpl.java
@@ -28,7 +28,7 @@ public class PositionImpl extends PositionInternal {
      *
      * @param ownedSegments Current segments that the position refers to.
      */
-    public PositionImpl(Map<Segment, Long> ownedSegments) {
+    PositionImpl(Map<Segment, Long> ownedSegments) {
         this.ownedSegments = new HashMap<>(ownedSegments);
     }
 
