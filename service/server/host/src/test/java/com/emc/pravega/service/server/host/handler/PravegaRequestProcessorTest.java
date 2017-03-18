281b8b8a92f7bd1984b45d637f986a7e7080f291
Flavio Junqueira (fpj) <flavio.junqueira@emc.com>
2017-03-18 18:09:44 +0000

    Initializing metrics in PravegaRequestProcessorTest to fix test failure

--- service/server/host/src/test/java/com/emc/pravega/service/server/host/handler/PravegaRequestProcessorTest.java
+++ service/server/host/src/test/java/com/emc/pravega/service/server/host/handler/PravegaRequestProcessorTest.java
@@ -6,6 +6,8 @@
 package com.emc.pravega.service.server.host.handler;
 
 import com.emc.pravega.common.concurrent.FutureHelpers;
+import com.emc.pravega.common.metrics.MetricsConfig;
+import com.emc.pravega.common.metrics.MetricsProvider;
 import com.emc.pravega.common.metrics.OpStatsData;
 import com.emc.pravega.common.netty.WireCommands.CreateSegment;
 import com.emc.pravega.common.netty.WireCommands.DeleteSegment;
@@ -55,6 +57,12 @@ import static org.mockito.Mockito.when;
 
 @Slf4j
 public class PravegaRequestProcessorTest {
+    static {
+        Properties properties = new Properties();
+
+        properties.setProperty("metrics." + MetricsConfig.ENABLE_STATISTICS, "true");
+        MetricsProvider.initialize(new MetricsConfig(properties));
+    }
 
     @Data
     private static class TestReadResult implements ReadResult {
