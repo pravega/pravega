fd84a0949e972127b3c99fb8fcbf46e223145e15
arvindkandhare <arvind.kandhare@emc.com>
2017-03-30 12:31:27 -0700

    Test failure.

--- service/server/src/test/java/com/emc/pravega/service/server/store/ServiceConfigTests.java
+++ service/server/src/test/java/com/emc/pravega/service/server/store/ServiceConfigTests.java
@@ -14,31 +14,34 @@ public class ServiceConfigTests {
     public void testListeningAndPublicIPAndPort() {
         // When the published IP and port is not specified, it should default to listening IP and port
         ServiceConfig cfg1 = ServiceConfig.builder()
+                .with(ServiceConfig.CONTAINER_COUNT,1)
                 .with(ServiceConfig.LISTENING_IP_ADDRESS, "myhost")
                 .with(ServiceConfig.LISTENING_PORT, 4000)
                 .build();
-        Assert.assertTrue("Published IP and port should default to listerning IP and port",
+        Assert.assertTrue("Published IP and port should default to listening IP and port",
                 cfg1.getListeningIPAddress().equals(cfg1.getListeningIPAddress())
                         && cfg1.getListeningPort() == cfg1.getPublishedPort());
         // Published IP not defined but port is different as compared to listening port
         ServiceConfig cfg2 = ServiceConfig.builder()
+                .with(ServiceConfig.CONTAINER_COUNT,1)
                 .with(ServiceConfig.LISTENING_IP_ADDRESS, "myhost")
                 .with(ServiceConfig.PUBLISHED_IP_ADDRESS, "myhost1")
                 .with(ServiceConfig.LISTENING_PORT, 4000)
                 .build();
-        Assert.assertTrue("Published IP and port should default to listerning IP and port",
-                !cfg2.getListeningIPAddress().equals(cfg2.getListeningIPAddress())
+        Assert.assertTrue("Published IP should default to listening IP even when ports are different",
+                !cfg2.getListeningIPAddress().equals(cfg2.getPublishedIPAddress())
                         && cfg2.getListeningPort() == cfg2.getPublishedPort());
         //Both published IP and port are defined and are different than listening IP and port
         ServiceConfig cfg3 = ServiceConfig.builder()
+                .with(ServiceConfig.CONTAINER_COUNT,1)
                 .with(ServiceConfig.LISTENING_IP_ADDRESS, "myhost")
                 .with(ServiceConfig.PUBLISHED_IP_ADDRESS, "myhost1")
                 .with(ServiceConfig.LISTENING_PORT, 4000)
                 .with(ServiceConfig.PUBLISHED_PORT, 5000)
                 .build();
-        Assert.assertTrue("Published IP and port should default to listerning IP and port",
-                !cfg2.getListeningIPAddress().equals(cfg2.getListeningIPAddress())
-                        && cfg2.getListeningPort() != cfg2.getPublishedPort());
+        Assert.assertTrue("When specified publishing IP and port should differ from listening IP and port",
+                !cfg3.getListeningIPAddress().equals(cfg3.getPublishedIPAddress())
+                        && cfg3.getListeningPort() != cfg3.getPublishedPort());
     }
 
 }
