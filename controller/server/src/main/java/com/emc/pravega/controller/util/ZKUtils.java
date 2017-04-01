9f9396992a4437126ec5fbbbdf02c619c5550440
bhargav gulavani <bhargav.gulavani@emc.com>
2017-04-01 11:21:56 -0700

    Test cases

--- controller/server/src/main/java/com/emc/pravega/controller/util/ZKUtils.java
+++ controller/server/src/main/java/com/emc/pravega/controller/util/ZKUtils.java
@@ -27,28 +27,6 @@ public final class ZKUtils {
      *
      * @param client                The curator client to access zookeeper.
      * @param basePath              The znode path string.
-     * @throws RuntimeException     If checking or creating path on zookeeper fails.
-     */
-    public static void createPathIfNotExists(final CuratorFramework client, final String basePath) {
-        Preconditions.checkNotNull(client, "client");
-        Preconditions.checkNotNull(basePath, "basePath");
-
-        try {
-            if (client.checkExists().forPath(basePath) == null) {
-                client.create().creatingParentsIfNeeded().forPath(basePath);
-            }
-        } catch (KeeperException.NodeExistsException e) {
-            log.debug("Path exists {}, ignoring exception", basePath);
-        } catch (Exception e) {
-            throw new RuntimeException("Exception while creating znode: " + basePath, e);
-        }
-    }
-
-    /**
-     * Creates the znode if is doesn't already exist in zookeeper.
-     *
-     * @param client                The curator client to access zookeeper.
-     * @param basePath              The znode path string.
      * @param initData              Initialize the znode using the supplied data if not already created.
      * @throws RuntimeException     If checking or creating path on zookeeper fails.
      */
