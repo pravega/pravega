e803fd294895545b811ab2203d19d9ba6040e6bd
shivesh ranjan <shivesh.ranjan@emc.com>
2017-04-02 07:22:56 +0000

    checkstyle

--- controller/server/src/test/java/com/emc/pravega/controller/store/stream/InMemoryStreamTest.java
+++ controller/server/src/test/java/com/emc/pravega/controller/store/stream/InMemoryStreamTest.java
@@ -20,7 +20,6 @@ import java.util.List;
 import java.util.Optional;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Collectors;
