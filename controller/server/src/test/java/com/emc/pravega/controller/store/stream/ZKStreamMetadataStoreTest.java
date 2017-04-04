c594d5b7f4468da23eba0645510a5f52c0bfb83d
Suri <suresh.krishnappa@emc.com>
2017-04-04 13:26:33 +0530

    Fix for listStream failure. Also enabled some disabled test and added test timeouts

--- controller/server/src/test/java/com/emc/pravega/controller/store/stream/ZKStreamMetadataStoreTest.java
+++ controller/server/src/test/java/com/emc/pravega/controller/store/stream/ZKStreamMetadataStoreTest.java
@@ -5,12 +5,18 @@
  */
 package com.emc.pravega.controller.store.stream;
 
+import com.emc.pravega.controller.store.stream.tables.State;
+import com.emc.pravega.stream.StreamConfiguration;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
 import org.apache.curator.retry.RetryOneTime;
 import org.apache.curator.test.TestingServer;
+import org.junit.Test;
 
 import java.io.IOException;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
 
 /**
  * Zookeeper based stream metadata store tests.
@@ -33,5 +39,21 @@ public class ZKStreamMetadataStoreTest extends StreamMetadataStoreTest {
     public void cleanupTaskStore() throws IOException {
         cli.close();
         zkServer.close();
+    }
+
+    @Test
+    public void listStreamsWithInactiveStream() throws Exception {
+        // list stream in scope
+        store.createScope("Scope").get();
+        store.createStream("Scope", stream1, configuration1, System.currentTimeMillis(), null, executor).get();
+        store.setState("Scope", stream1, State.ACTIVE, null, executor).get();
+
+        store.createStream("Scope", stream2, configuration2, System.currentTimeMillis(), null, executor).get();
+        store.setState("Scope", stream1, State.CREATING, null, executor).get();
+
+        List<StreamConfiguration> streamInScope = store.listStreamsInScope("Scope").get();
+        assertEquals("List streams in scope", 2, streamInScope.size());
+        assertEquals("List streams in scope", stream1, streamInScope.get(0).getStreamName());
+        assertEquals("List streams in scope", stream2, streamInScope.get(1).getStreamName());
     }
 }
