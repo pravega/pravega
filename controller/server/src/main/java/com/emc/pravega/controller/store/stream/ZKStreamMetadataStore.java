1caedaa52e57cf16b8c8117b404d9879f0b5061b
Suri <suresh.krishnappa@emc.com>
2017-04-04 15:25:54 +0530

    compile fix

--- controller/server/src/main/java/com/emc/pravega/controller/store/stream/ZKStreamMetadataStore.java
+++ controller/server/src/main/java/com/emc/pravega/controller/store/stream/ZKStreamMetadataStore.java
@@ -3,7 +3,6 @@
  */
 package com.emc.pravega.controller.store.stream;
 
-import com.emc.pravega.common.concurrent.FutureHelpers;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.curator.framework.CuratorFramework;
 
