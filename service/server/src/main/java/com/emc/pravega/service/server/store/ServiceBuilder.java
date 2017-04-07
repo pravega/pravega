078c91d83a602751fcb7b2834369e810bd375236
arvindkandhare <arvind.kandhare@emc.com>
2017-04-07 08:53:49 -0700

    Using a different class for in memory storage for single node.

--- service/server/src/main/java/com/emc/pravega/service/server/store/ServiceBuilder.java
+++ service/server/src/main/java/com/emc/pravega/service/server/store/ServiceBuilder.java
@@ -27,6 +27,7 @@ import com.emc.pravega.service.storage.StorageFactory;
 import com.emc.pravega.service.storage.mocks.InMemoryCacheFactory;
 import com.emc.pravega.service.storage.mocks.InMemoryDurableDataLogFactory;
 import com.emc.pravega.service.storage.mocks.InMemoryStorageFactory;
+import com.emc.pravega.service.storage.mocks.SinglenodeInMemoryStorageFactory;
 import com.google.common.base.Preconditions;
 import com.google.common.util.concurrent.ThreadFactoryBuilder;
 import java.util.concurrent.CompletableFuture;
@@ -334,7 +335,7 @@ public final class ServiceBuilder implements AutoCloseable {
         return serviceBuilder.withCacheFactory(setup -> new InMemoryCacheFactory())
                              .withContainerManager(setup -> new LocalSegmentContainerManager(
                                      setup.getContainerRegistry(), setup.getSegmentToContainerMapper()))
-                             .withStorageFactory(setup -> new InMemoryStorageFactory(setup.getExecutor()))
+                             .withStorageFactory(setup -> new SinglenodeInMemoryStorageFactory(setup.getExecutor()))
                              .withDataLogFactory(setup -> new InMemoryDurableDataLogFactory(setup.getExecutor()))
                              .withStreamSegmentStore(setup -> new StreamSegmentService(setup.getContainerRegistry(),
                                      setup.getSegmentToContainerMapper()));
--- service/storage/src/main/java/com/emc/pravega/service/storage/mocks/SinglenodeInMemoryStorageFactory.java
+++ service/storage/src/main/java/com/emc/pravega/service/storage/mocks/SinglenodeInMemoryStorageFactory.java
@@ -16,18 +16,21 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import lombok.RequiredArgsConstructor;
 
 /**
- * In-Memory mock for StorageFactory. Contents is destroyed when object is garbage collected.
+ * In-Memory mock for StorageFactory used for single node cluster.
+ * Contents is destroyed when object is garbage
+ * collected.
  */
-public class InMemoryStorageFactory implements StorageFactory {
-    private final InMemoryStorage baseStorage;
+public class SinglenodeInMemoryStorageFactory implements StorageFactory {
+    private final ScheduledExecutorService executor;
 
-    public InMemoryStorageFactory(ScheduledExecutorService executor) {
-        this.baseStorage = new InMemoryStorage(executor);
+
+    public SinglenodeInMemoryStorageFactory(ScheduledExecutorService executor) {
+        this.executor = executor;
     }
 
     @Override
     public TruncateableStorage createStorageAdapter() {
-        return new FencedWrapper(this.baseStorage);
+        return new FencedWrapper( new InMemoryStorage(executor));
     }
 
     //region FencedWrapper
