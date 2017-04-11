eed68b6fc328f4e868c8021dbb76f466091a67a0
bhargav gulavani <bhargav.gulavani@emc.com>
2017-04-11 08:31:31 -0700

    Checkstyle

--- systemtests/tests/src/test/java/com/emc/pravega/ControllerFailoverTest.java
+++ systemtests/tests/src/test/java/com/emc/pravega/ControllerFailoverTest.java
@@ -33,7 +33,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.CompletableFuture;
 
 /**
