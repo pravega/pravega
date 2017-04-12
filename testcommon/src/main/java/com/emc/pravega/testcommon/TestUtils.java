f51b1da974444b3a68bce4d243983ab61b4c107b
Abhijeet Jadhav <Abhijeet.Jadhav@emc.com>
2017-04-12 18:15:06 +0530

    Issue 1009: Fix getAvailableListenPort (#1010)
    Issue 1009: fix getAvailableListenPort

--- testcommon/src/main/java/com/emc/pravega/testcommon/TestUtils.java
+++ testcommon/src/main/java/com/emc/pravega/testcommon/TestUtils.java
@@ -7,12 +7,17 @@ import lombok.extern.slf4j.Slf4j;
 
 import java.io.IOException;
 import java.net.ServerSocket;
+import java.util.concurrent.atomic.AtomicInteger;
 
 /**
  * Utility class for Tests.
  */
 @Slf4j
 public class TestUtils {
+    // Linux uses ports from range 32768 - 61000.
+    private static final int BASE_PORT = 32768;
+    private static final int MAX_PORT_COUNT = 28232;
+    private static final AtomicInteger NEXT_PORT = new AtomicInteger(1);
 
     /**
      * A helper method to get a random free port.
@@ -20,16 +25,17 @@ public class TestUtils {
      * @return free port.
      */
     public static int getAvailableListenPort() {
-        ServerSocket serverSocket = null;
-        int freePort;
-        try {
-            serverSocket = new ServerSocket(0);
-            freePort = serverSocket.getLocalPort();
-            serverSocket.close();
-            return freePort;
-        } catch (IOException e) {
-            log.error("Could not find free port");
-            return -1;
+        for (int i = 0; i < MAX_PORT_COUNT; i++) {
+            int candidatePort = BASE_PORT + NEXT_PORT.getAndIncrement() % MAX_PORT_COUNT;
+            try {
+                ServerSocket serverSocket = new ServerSocket(candidatePort);
+                serverSocket.close();
+                return candidatePort;
+            } catch (IOException e) {
+                // Do nothing. Try another port.
+            }
         }
+        throw new IllegalStateException(
+                String.format("Could not assign port in range %d - %d", BASE_PORT, MAX_PORT_COUNT + BASE_PORT));
     }
 }
