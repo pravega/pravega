2df6a5dd5f92503c82da837f665f177d198b0716
abhijeet-jadhav <Abhijeet.Jadhav@emc.com>
2017-04-12 13:21:25 +0530

    PR feedback

--- testcommon/src/main/java/com/emc/pravega/testcommon/TestUtils.java
+++ testcommon/src/main/java/com/emc/pravega/testcommon/TestUtils.java
@@ -14,8 +14,9 @@ import java.util.concurrent.atomic.AtomicInteger;
  */
 @Slf4j
 public class TestUtils {
-    private static final int BASE_PORT = 11221;
-    private static final int MAX_PORT_COUNT = 41546;
+    // Linux uses ports from range 32768 - 61000.
+    private static final int BASE_PORT = 32768;
+    private static final int MAX_PORT_COUNT = 28232;
     private static final AtomicInteger NEXT_PORT = new AtomicInteger(1);
 
     /**
@@ -23,9 +24,9 @@ public class TestUtils {
      *
      * @return free port.
      */
-    public synchronized static int getAvailableListenPort() {
-        int candidatePort = BASE_PORT + NEXT_PORT.getAndIncrement() % MAX_PORT_COUNT;
+    public static int getAvailableListenPort() {
         for (int i = 0; i < MAX_PORT_COUNT; i++) {
+            int candidatePort = BASE_PORT + NEXT_PORT.getAndIncrement() % MAX_PORT_COUNT;
             if (candidatePort > BASE_PORT + MAX_PORT_COUNT) {
                 candidatePort = BASE_PORT;
             }
@@ -34,7 +35,7 @@ public class TestUtils {
                 serverSocket.close();
                 return candidatePort;
             } catch (IOException e) {
-                candidatePort++;
+                // Do nothing. Try another port.
             }
         }
         throw new IllegalStateException(
