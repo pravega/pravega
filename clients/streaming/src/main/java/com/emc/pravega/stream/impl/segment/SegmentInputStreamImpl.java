282151eb11b705f8fae410b91e95f7222b48a24a
Tom Kaitchuck <Tom.Kaitchuck@emc.com>
2017-04-14 10:58:32 -0700

    Replace <= with == when checking buffer length.

--- clients/streaming/src/main/java/com/emc/pravega/stream/impl/segment/SegmentInputStreamImpl.java
+++ clients/streaming/src/main/java/com/emc/pravega/stream/impl/segment/SegmentInputStreamImpl.java
@@ -101,7 +101,7 @@ class SegmentInputStreamImpl implements SegmentInputStream {
     public ByteBuffer read(long timeout) throws EndOfSegmentException {
         log.trace("Read called at offset {}", offset);
         fillBuffer();
-        if (buffer.dataAvailable() <= 0 && receivedEndOfSegment) {
+        if (buffer.dataAvailable() == 0 && receivedEndOfSegment) {
             throw new EndOfSegmentException();
         }
         if (buffer.dataAvailable() < TYPE_PLUS_LENGTH_SIZE) {
