8f9110fe9c6bd6bf5823c8191367969c89aa5415
Tom Kaitchuck <Tom.Kaitchuck@emc.com>
2017-05-04 14:59:11 -0700

    Add missing file

--- test/integration/src/test/java/io/pravega/test/integration/testsuites/TestAppendPath.java
+++ test/integration/src/test/java/io/pravega/test/integration/testsuites/TestAppendPath.java
@@ -15,14 +15,13 @@
  */
 package io.pravega.test.integration.testsuites;
 
+import io.pravega.client.segment.impl.SegmentOutputStreamTest;
+import io.pravega.service.server.host.handler.AppendProcessorTest;
+import io.pravega.shared.protocol.netty.AppendEncodeDecodeTest;
+import io.pravega.test.integration.AppendTest;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
-
-import io.pravega.shared.protocol.netty.AppendEncodeDecodeTest;
-import io.pravega.test.integration.AppendTest;
-import io.pravega.service.server.host.handler.AppendProcessorTest;
-import io.pravega.client.stream.impl.segment.SegmentOutputStreamTest;
 
 @RunWith(Suite.class)
 @SuiteClasses({ AppendEncodeDecodeTest.class, AppendProcessorTest.class, AppendTest.class, SegmentOutputStreamTest.class })
