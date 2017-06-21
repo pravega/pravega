333fcabc0a3479076e1b1d6ecacf17f5cfb5382b
arvindkandhare <arvind.kandhare@emc.com>
2017-06-21 15:57:36 -0700

    Consolidated SegmentStore tests and added comment WRT concat performance.

--- segmentstore/server/host/src/test/java/io/pravega/segmentstore/server/host/SegmentStoreFileSystemIntegrationTest.java
+++ segmentstore/server/host/src/test/java/io/pravega/segmentstore/server/host/SegmentStoreFileSystemIntegrationTest.java
@@ -17,23 +17,15 @@ import io.pravega.segmentstore.storage.Storage;
 import io.pravega.segmentstore.storage.StorageFactory;
 import io.pravega.segmentstore.storage.impl.bookkeeper.BookKeeperConfig;
 import io.pravega.segmentstore.storage.impl.bookkeeper.BookKeeperLogFactory;
-import io.pravega.segmentstore.storage.impl.bookkeeper.BookKeeperServiceRunner;
 import io.pravega.segmentstore.storage.impl.filesystem.FileSystemStorageConfig;
 import io.pravega.segmentstore.storage.impl.filesystem.FileSystemStorageFactory;
 import io.pravega.segmentstore.storage.impl.rocksdb.RocksDBCacheFactory;
 import io.pravega.segmentstore.storage.impl.rocksdb.RocksDBConfig;
-import io.pravega.test.common.TestUtils;
-import lombok.val;
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.junit.After;
-import org.junit.Before;
-
 import java.io.File;
 import java.nio.file.Files;
-import java.util.ArrayList;
 import java.util.concurrent.atomic.AtomicReference;
+import org.junit.After;
+import org.junit.Before;
 
 /**
  * End-to-end tests for SegmentStore, with integrated Storage and DurableDataLog.
