bd3101056e3879e72508e22c86e8ab56b10ac459
Tom Kaitchuck <Tom.Kaitchuck@emc.com>
2017-06-26 19:14:10 -0700

    Whitespace

--- test/system/src/test/java/io/pravega/test/system/AbstractScaleTests.java
+++ test/system/src/test/java/io/pravega/test/system/AbstractScaleTests.java
@@ -31,14 +31,14 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 abstract class AbstractScaleTests {
     protected final static String SCOPE = "testAutoScale" + new Random().nextInt(Integer.MAX_VALUE);
-    @Getter(lazy = true, onMethod=@__({@SuppressFBWarnings}))
+    @Getter(lazy = true, onMethod = @__({ @SuppressFBWarnings }))
     private final URI controllerURI = createControllerURI();
-    @Getter(lazy = true, onMethod=@__({@SuppressFBWarnings}))
+    @Getter(lazy = true, onMethod = @__({ @SuppressFBWarnings }))
     private final ConnectionFactory connectionFactory = new ConnectionFactoryImpl(false);
-    @Getter(lazy = true, onMethod=@__({@SuppressFBWarnings}))
+    @Getter(lazy = true, onMethod = @__({ @SuppressFBWarnings }))
     private final ClientFactory clientFactory = new ClientFactoryImpl(SCOPE,
             new ControllerImpl(getControllerURI(), getConnectionFactory()), getConnectionFactory());
-    @Getter(lazy = true, onMethod=@__({@SuppressFBWarnings}))
+    @Getter(lazy = true, onMethod = @__({ @SuppressFBWarnings }))
     private final ControllerImpl controller = new ControllerImpl(getControllerURI(), getConnectionFactory());
 
     private URI createControllerURI() {
