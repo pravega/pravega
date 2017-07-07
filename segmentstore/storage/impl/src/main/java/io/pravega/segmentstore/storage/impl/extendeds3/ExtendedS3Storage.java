a4bf98f1b0313fec5981455ab77a0d940c3b1c99
arvindkandhare <arvind.kandhare@emc.com>
2017-07-07 12:47:43 -0700

    Comment explaining how concat is implemented using multi part copy.

--- segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/extendeds3/ExtendedS3Storage.java
+++ segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/extendeds3/ExtendedS3Storage.java
@@ -310,6 +310,12 @@ public class ExtendedS3Storage implements Storage {
         return null;
     }
 
+    /**
+     * The concat is implemented using extended S3 implementation of multipart copy API. Please see here for more details on multipart copy.
+     * http://docs.aws.amazon.com/AmazonS3/latest/dev/CopyingObjctsUsingLLJavaMPUapi.html
+     * The multipart copy is an atomic operation. We schedule two parts and commit the atomically using completeMultiPartUpload call.
+     * The first part is the destination object and the second part is the source segment.
+     */
     private Void syncConcat(SegmentHandle targetHandle, long offset, String sourceSegment) throws StreamSegmentNotExistsException {
         Preconditions.checkArgument(!targetHandle.isReadOnly(), "target handle must not be read-only.");
         long traceId = LoggerHelpers.traceEnter(log, "concat", targetHandle.getSegmentName(), offset, sourceSegment);
