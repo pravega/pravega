f04bc713f0b4a619eda22eb5ef744955be51f9ef
arvindkandhare <arvind.kandhare@emc.com>
2017-07-13 14:04:35 -0700

    Verifying metrics in unit tests for FileSystemStorageTest specific operations.

--- test/system/src/main/java/io/pravega/test/system/framework/services/PravegaSegmentStoreService.java
+++ test/system/src/main/java/io/pravega/test/system/framework/services/PravegaSegmentStoreService.java
@@ -128,19 +128,19 @@ public class PravegaSegmentStoreService extends MarathonBasedService {
         String zk = zkUri.getHost() + ":" + ZKSERVICE_ZKPORT;
 
         //System properties to configure SS service.
-        String hostSystemProperties = setSystemProperty("pravegaservice.zkURL", zk) +
-                setSystemProperty("bookkeeper.zkAddress", zk) +
-                setSystemProperty("hdfs.hdfsUrl", "hdfs.marathon.containerip.dcos.thisdcos.directory:8020") +
-                setSystemProperty("autoScale.muteInSeconds", "120") +
+        String hostSystemProperties = setSystemProperty("autoScale.muteInSeconds", "120") +
                 setSystemProperty("autoScale.cooldownInSeconds", "120") +
                 setSystemProperty("autoScale.cacheExpiryInSeconds", "120") +
                 setSystemProperty("autoScale.cacheCleanUpInSeconds", "120") +
-                setSystemProperty("autoScale.controllerUri", conUri.toString()) +
                 setSystemProperty("log.level", "DEBUG") +
-                setSystemProperty("curator-default-session-timeout", String.valueOf(30 * 1000)) +
-                setSystemProperty("pravegaservice.storageImplementation", "HDFS");
+                setSystemProperty("curator-default-session-timeout", String.valueOf(30 * 1000));
 
         Map<String, String> map = new HashMap<>();
+        map.put("ZK_URL", zk);
+        map.put("BK_ZK_URL", zk);
+        map.put("HDFS_URL", "hdfs.marathon.containerip.dcos.thisdcos.directory:8020");
+        map.put("CONTROLLER_URI", conUri.toString());
+        map.put("TIER2_STORAGE", "HDFS");
         map.put("PRAVEGA_SEGMENTSTORE_OPTS", hostSystemProperties);
         app.setEnv(map);
         app.setArgs(Arrays.asList("segmentstore"));
