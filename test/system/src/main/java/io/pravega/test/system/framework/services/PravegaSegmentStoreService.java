8f3086a33f4079bf59917e36bc53cc30f7c41c79
arvindkandhare <arvind.kandhare@emc.com>
2017-07-18 00:30:10 -0700

    Issue 1580:  System tests should follow the same path through entrypoint.sh as production deployment (#1581)
    * Sets the configuration through environment variables as the script entrypoint.sh uses in 
    production deployment.
    
    * Overrides defaults using PRAVEGA_SEGMENTSTORE_OPTS as required specifically for system 
    tests.

--- test/system/src/main/java/io/pravega/test/system/framework/services/PravegaSegmentStoreService.java
+++ test/system/src/main/java/io/pravega/test/system/framework/services/PravegaSegmentStoreService.java
@@ -127,20 +127,22 @@ public class PravegaSegmentStoreService extends MarathonBasedService {
         //set env
         String zk = zkUri.getHost() + ":" + ZKSERVICE_ZKPORT;
 
-        //System properties to configure SS service.
-        String hostSystemProperties = setSystemProperty("pravegaservice.zkURL", zk) +
-                setSystemProperty("bookkeeper.zkAddress", zk) +
-                setSystemProperty("hdfs.hdfsUrl", "hdfs.marathon.containerip.dcos.thisdcos.directory:8020") +
-                setSystemProperty("autoScale.muteInSeconds", "120") +
+        //Environment variables to configure SS service.
+        Map<String, String> map = new HashMap<>();
+        map.put("ZK_URL", zk);
+        map.put("BK_ZK_URL", zk);
+        map.put("HDFS_URL", "hdfs.marathon.containerip.dcos.thisdcos.directory:8020");
+        map.put("CONTROLLER_URL", conUri.toString());
+        map.put("TIER2_STORAGE", "HDFS");
+
+        //Properties set to override defaults for system tests
+        String hostSystemProperties = setSystemProperty("autoScale.muteInSeconds", "120") +
                 setSystemProperty("autoScale.cooldownInSeconds", "120") +
                 setSystemProperty("autoScale.cacheExpiryInSeconds", "120") +
                 setSystemProperty("autoScale.cacheCleanUpInSeconds", "120") +
-                setSystemProperty("autoScale.controllerUri", conUri.toString()) +
                 setSystemProperty("log.level", "DEBUG") +
-                setSystemProperty("curator-default-session-timeout", String.valueOf(30 * 1000)) +
-                setSystemProperty("pravegaservice.storageImplementation", "HDFS");
+                setSystemProperty("curator-default-session-timeout", String.valueOf(30 * 1000));
 
-        Map<String, String> map = new HashMap<>();
         map.put("PRAVEGA_SEGMENTSTORE_OPTS", hostSystemProperties);
         app.setEnv(map);
         app.setArgs(Arrays.asList("segmentstore"));
