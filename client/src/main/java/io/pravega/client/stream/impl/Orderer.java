f252d770d3ab53c2b412404e6d60dcdc68786f08
Tom Kaitchuck <tkaitchuck@users.noreply.github.com>
2017-07-17 23:46:47 -0700

    Issue 1596: Fix use of negative in modulo operation. (#1599)
    * Fixes use of negative in modulo operation client Orderer.

--- client/src/main/java/io/pravega/client/stream/impl/Orderer.java
+++ client/src/main/java/io/pravega/client/stream/impl/Orderer.java
@@ -9,17 +9,27 @@
  */
 package io.pravega.client.stream.impl;
 
+import com.google.common.annotations.VisibleForTesting;
 import io.pravega.client.segment.impl.SegmentInputStream;
+import io.pravega.common.MathHelpers;
 import java.util.List;
 import java.util.concurrent.atomic.AtomicInteger;
+import lombok.RequiredArgsConstructor;
 
 /**
  * Used to select which event should go next when consuming from multiple segments.
  *
  */
+@RequiredArgsConstructor
 public class Orderer {
     private final AtomicInteger counter = new AtomicInteger(0);
 
+    @VisibleForTesting
+    Orderer(int initialCount) {
+        this();
+        counter.set(initialCount);
+    }
+    
     /**
      * Given a list of segments this reader owns, (which contain their positions) returns the one that should
      * be read from next. This is done in way to minimize blocking and ensure fairness.
@@ -27,18 +37,18 @@ public class Orderer {
      * @param segments The logs to get the next reader for.
      * @return A segment that this reader should read from next.
      */
-    SegmentInputStream nextSegment(List<SegmentInputStream> segments) {
+    <T extends SegmentInputStream> T nextSegment(List<T> segments) {
         if (segments.isEmpty()) {
             return null;
         }
         for (int i = 0; i < segments.size(); i++) {
-            SegmentInputStream inputStream = segments.get(counter.incrementAndGet() % segments.size());
+            T inputStream = segments.get(MathHelpers.abs(counter.incrementAndGet()) % segments.size());
             if (inputStream.canReadWithoutBlocking()) {
                 return inputStream;
             } else {
                 inputStream.fillBuffer();
             }
         }
-        return segments.get(counter.incrementAndGet() % segments.size());
+        return segments.get(MathHelpers.abs(counter.incrementAndGet()) % segments.size());
     }
 }
