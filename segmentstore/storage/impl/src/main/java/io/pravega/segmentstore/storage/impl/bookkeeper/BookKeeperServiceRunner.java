5cd0794b5d1d3a56b99b7fb5d9aac1ad0c73437a
Andrei Paduroiu <andrei.paduroiu@emc.com>
2017-08-02 16:13:18 -0700

    checkstyle fix

--- segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperServiceRunner.java
+++ segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperServiceRunner.java
@@ -76,7 +76,7 @@ public class BookKeeperServiceRunner implements AutoCloseable {
     }
 
     @SneakyThrows
-    private void cleanupOnShutdown(){
+    private void cleanupOnShutdown() {
         close();
     }
 
