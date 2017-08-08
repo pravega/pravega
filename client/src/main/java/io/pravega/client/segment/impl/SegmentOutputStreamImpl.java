65975f556e8fad6d25103b34fc9386ad580d73b7
Tom Kaitchuck <Tom.Kaitchuck@emc.com>
2017-08-08 09:54:35 -0700

    Remove nested call to runReleaserAndAwait.

--- client/src/main/java/io/pravega/client/segment/impl/SegmentOutputStreamImpl.java
+++ client/src/main/java/io/pravega/client/segment/impl/SegmentOutputStreamImpl.java
@@ -398,8 +398,11 @@ class SegmentOutputStreamImpl implements SegmentOutputStream {
         if (state.isAlreadySealed()) {
             throw new SegmentSealedException(this.segmentName);
         }
+        if (state.getConnection() == null) {
+            reconnect();
+        }
         CompletableFuture<ClientConnection> future =  new CompletableFuture<>();
-        state.setupConnection.runReleaserAndAwait(this::reconnect, future);
+        state.setupConnection.await(future);
         return future;
     }
     
