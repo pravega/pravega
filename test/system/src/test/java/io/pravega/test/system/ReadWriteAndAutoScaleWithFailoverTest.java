c49797f58cd24047a0b54572a817a892c3d47835
Tom Kaitchuck <Tom.Kaitchuck@emc.com>
2017-08-09 17:48:07 -0700

    Reduce log noise in System tests on shutdown.

--- test/system/src/test/java/io/pravega/test/system/ReadWriteAndAutoScaleWithFailoverTest.java
+++ test/system/src/test/java/io/pravega/test/system/ReadWriteAndAutoScaleWithFailoverTest.java
@@ -98,6 +98,8 @@ public class ReadWriteAndAutoScaleWithFailoverTest extends AbstractFailoverTests
 
     @After
     public void tearDown() {
+        testState.stopReadFlag.set(true);
+        testState.stopWriteFlag.set(true);
         controllerInstance.scaleService(1, true);
         segmentStoreInstance.scaleService(1, true);
         executorService.shutdownNow();
