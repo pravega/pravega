253e35c92214696107977b9b89dcf943a1d7b849
Paduroiu, Andrei <Andrei.Paduroiu@emc.com>
2017-08-16 22:21:47 -0700

    reverted unintended changes

--- segmentstore/server/src/test/java/io/pravega/segmentstore/server/containers/StreamSegmentContainerMetadataTests.java
+++ segmentstore/server/src/test/java/io/pravega/segmentstore/server/containers/StreamSegmentContainerMetadataTests.java
@@ -27,7 +27,9 @@ import java.util.function.Function;
 import java.util.stream.Collectors;
 import lombok.val;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 /**
  * Unit tests for StreamSegmentContainerMetadata class.
@@ -37,8 +39,8 @@ public class StreamSegmentContainerMetadataTests {
     private static final int SEGMENT_COUNT = 100;
     private static final int TRANSACTIONS_PER_SEGMENT_COUNT = 2;
 
-    //    @Rule
-    //    public Timeout globalTimeout = Timeout.seconds(10);
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(10);
 
     /**
      * Tests SequenceNumber-related operations.
