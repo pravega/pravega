3a74a7a34b53e43dabac9eff03b21f83499b7499
Tom Kaitchuck <Tom.Kaitchuck@emc.com>
2017-08-23 14:53:48 -0700

    Javadoc

--- client/src/main/java/io/pravega/client/segment/impl/SegmentInputStreamImpl.java
+++ client/src/main/java/io/pravega/client/segment/impl/SegmentInputStreamImpl.java
@@ -179,7 +179,7 @@ class SegmentInputStreamImpl implements SegmentInputStream {
     }
 
     /**
-     * @return If there is enough room for another request, and we aren't already waiting on one
+     * Issues a request if there is enough room for another request, and we aren't already waiting on one
      */
     private void issueRequestIfNeeded() {
         if (!receivedEndOfSegment && buffer.capacityAvailable() > readLength) {
