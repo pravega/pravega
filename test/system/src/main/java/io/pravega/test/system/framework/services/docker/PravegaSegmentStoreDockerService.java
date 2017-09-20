427bc3c2e879126b31717176ae04aaef67f1ca81
root <Vijayalakshmi.Veerubhotla@emc.com>
2017-09-20 11:27:04 -0700

    resolving build errors

--- test/system/src/main/java/io/pravega/test/system/framework/services/docker/PravegaSegmentStoreDockerService.java
+++ test/system/src/main/java/io/pravega/test/system/framework/services/docker/PravegaSegmentStoreDockerService.java
@@ -29,7 +29,6 @@ import java.net.URI;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.Map;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.TimeoutException;
--- test/system/src/test/java/io/pravega/test/system/ControllerFailoverTest.java
+++ test/system/src/test/java/io/pravega/test/system/ControllerFailoverTest.java
@@ -30,7 +30,7 @@ import io.pravega.test.system.framework.services.marathon.BookkeeperService;
 import io.pravega.test.system.framework.services.marathon.PravegaControllerService;
 import io.pravega.test.system.framework.services.marathon.PravegaSegmentStoreService;
 import io.pravega.test.system.framework.services.Service;
-import io.pravega.test.system.framework.services.ZookeeperService;
+import io.pravega.test.system.framework.services.marathon.ZookeeperService;
 import java.net.URI;
 import java.net.URISyntaxException;
 import java.util.Collections;
