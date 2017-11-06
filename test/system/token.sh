827a7bcc81c31bed155431ae2021902283a7c9a0
Prabha <Vijayalakshmi.Veerubhotla@emc.com>
2017-11-06 17:28:41 +0530

    correcting sepll typo
    Signed-off-by: Prabha <Vijayalakshmi.Veerubhotla@emc.com>

--- test/system/token.sh
+++ test/system/token.sh
@@ -1,6 +1,6 @@
 To add a worker to this swarm, run the following command:
 
     docker swarm join \
-    --token SWMTKN-1-3vj1r6q8bwl218mie09h80nlbvxkecazl75dwge4glvzoy4i16-d8ca7qveslw04y34xqktgmk7b \
-    172.16.18.20:2377
+    --token SWMTKN-1-2kbxue3fgjvtqmupwzq0gcyoe3p6cb0lsa3y9ch827h972f3q9-1t2d19zk4ulwqpcyrjxwuvcfb \
+    172.16.11.20:2377
 
