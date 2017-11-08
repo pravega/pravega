cfd551304a6efd0dbae28d97edb6452f4c6680cd
shrids <sandeep.shridhar@emc.com>
2017-11-08 13:35:24 +0530

    Undo changes to EndToEndTxnWithScaleTest
    Signed-off-by: shrids <sandeep.shridhar@emc.com>

--- test/integration/src/test/java/io/pravega/test/integration/endtoendtest/EndToEndTxnWithScaleTest.java
+++ test/integration/src/test/java/io/pravega/test/integration/endtoendtest/EndToEndTxnWithScaleTest.java
@@ -98,7 +98,7 @@ public class EndToEndTxnWithScaleTest {
         zkTestServer.close();
     }
 
-    @Test
+    @Test(timeout = 10000)
     public void testTxnWithScale() throws Exception {
         StreamConfiguration config = StreamConfiguration.builder()
                                                         .scope("test")
@@ -117,10 +117,7 @@ public class EndToEndTxnWithScaleTest {
                 EventWriterConfig.builder().transactionTimeoutScaleGracePeriod(10000).transactionTimeoutTime(10000).build());
         Transaction<String> transaction = test.beginTxn();
         transaction.writeEvent("0", "txntest1");
-        Stream stream1 = new StreamImpl("test1", "test");
-        Void pingResult = controller.pingTransaction(stream1, transaction.getTxnId(), 5000L).get();
         transaction.commit();
-
 
         // scale
         Stream stream = new StreamImpl("test", "test");
