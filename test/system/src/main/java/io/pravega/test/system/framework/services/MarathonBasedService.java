33ad4618ca2fd7052718151bdaf01a138331528f
shrids <sandeep.shridhar@emc.com>
2017-11-13 14:10:42 +0530

    Ensure dockerImageRepository is read from system property.
    Signed-off-by: shrids <sandeep.shridhar@emc.com>

--- test/system/src/main/java/io/pravega/test/system/framework/services/MarathonBasedService.java
+++ test/system/src/main/java/io/pravega/test/system/framework/services/MarathonBasedService.java
@@ -47,8 +47,7 @@ public abstract class MarathonBasedService implements Service {
 
     static final int ZKSERVICE_ZKPORT = 2181;
     static final String CONTAINER_TYPE = "MESOS";
-    //TODO: Revert this change once the jenkins job is reconfigured.
-    static final String IMAGE_PATH = "devops-repo.isus.emc.com:8116";
+    static final String IMAGE_PATH = System.getProperty("dockerImageRegistry");
     static final String PRAVEGA_VERSION = System.getProperty("imageVersion");
     private static final String TCP = "tcp://";
     final String id;
