42871fe90c7c0524c4ea910c05e2d58f901057a8
shrids <sandeep.shridhar@emc.com>
2017-11-13 16:35:03 +0530

    Manually change the repository since the jenkins job is not yet updated.
    Signed-off-by: shrids <sandeep.shridhar@emc.com>

--- test/system/src/main/java/io/pravega/test/system/framework/services/MarathonBasedService.java
+++ test/system/src/main/java/io/pravega/test/system/framework/services/MarathonBasedService.java
@@ -47,7 +47,7 @@ public abstract class MarathonBasedService implements Service {
 
     static final int ZKSERVICE_ZKPORT = 2181;
     static final String CONTAINER_TYPE = "MESOS";
-    static final String IMAGE_PATH = System.getProperty("dockerImageRegistry");
+    static final String IMAGE_PATH = "devops-repo.isus.emc.com:8116";
     static final String PRAVEGA_VERSION = System.getProperty("imageVersion");
     private static final String TCP = "tcp://";
     final String id;
