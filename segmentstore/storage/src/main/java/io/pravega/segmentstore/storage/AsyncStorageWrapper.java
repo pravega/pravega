3163e4616f3664ea027619e435d52f74b4ede06e
Andrei Paduroiu <andrei.paduroiu@emc.com>
2017-12-18 17:34:48 -0800

    Fixed some bugs in AsyncStorageWrapper and wrote a number of unit tests for it.
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>

--- segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/AsyncStorageWrapper.java
+++ segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/AsyncStorageWrapper.java
@@ -9,6 +9,7 @@
  */
 package io.pravega.segmentstore.storage;
 
+import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Preconditions;
 import io.pravega.common.Exceptions;
 import io.pravega.common.function.RunnableWithException;
@@ -25,11 +26,16 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.atomic.AtomicBoolean;
 import javax.annotation.concurrent.GuardedBy;
 import javax.annotation.concurrent.ThreadSafe;
+import lombok.RequiredArgsConstructor;
 import lombok.SneakyThrows;
 import lombok.val;
 
 /**
  * Wrapper for a SyncStorage implementation that executes all operations asynchronously in a Thread Pool.
+ *
+ * Instances of this class guarantee that no two operations on the same Segment can execute concurrently in the same
+ * instance. Different Segments are not affected.
+ *
  */
 @ThreadSafe
 public class AsyncStorageWrapper implements Storage {
@@ -37,8 +43,10 @@ public class AsyncStorageWrapper implements Storage {
 
     private final SyncStorage syncStorage;
     private final Executor executor;
-    @GuardedBy("lastOperation")
-    private final HashMap<String, CompletableFuture<?>> lastOperation;
+    @GuardedBy("lastTasks")
+    private final HashMap<String, RunningTask> lastTasks;
+    @GuardedBy("lastTasks")
+    private int currentTaskId;
     private final AtomicBoolean closed;
 
     //endregion
@@ -54,7 +62,7 @@ public class AsyncStorageWrapper implements Storage {
     public AsyncStorageWrapper(SyncStorage syncStorage, Executor executor) {
         this.syncStorage = Preconditions.checkNotNull(syncStorage, "syncStorage");
         this.executor = Preconditions.checkNotNull(executor, "executor");
-        this.lastOperation = new HashMap<>();
+        this.lastTasks = new HashMap<>();
         this.closed = new AtomicBoolean();
     }
 
@@ -143,30 +151,46 @@ public class AsyncStorageWrapper implements Storage {
     //region Helpers
 
     /**
-     * Executes the given Supplier asynchronously and returns a CompletableFuture that will be completed with the result.
+     * Gets a value representing the number of segments that currently have at least an ongoing task running.
+     */
+    @VisibleForTesting
+    int getSegmentWithOngoingOperationsCount() {
+        synchronized (this.lastTasks) {
+            return this.lastTasks.size();
+        }
+    }
+
+    /**
+     * Executes the given Callable asynchronously and returns a CompletableFuture that will be completed with the result.
+     * @param operation    The Callable to execute.
+     * @param segmentNames The names of the Segments involved in this operation (for sequencing purposes).
      */
     private <R> CompletableFuture<R> supplyAsync(Callable<R> operation, String... segmentNames) {
         Exceptions.checkNotClosed(this.closed.get(), this);
         CompletableFuture<R> result;
-        synchronized (this.lastOperation) {
+        synchronized (this.lastTasks) {
             // Collect all futures this is dependent on.
             val futures = Arrays.stream(segmentNames)
-                                .map(this.lastOperation::get)
-                                .filter(Objects::nonNull)
-                                .toArray(CompletableFuture[]::new);
+                    .map(this.lastTasks::get)
+                    .filter(Objects::nonNull)
+                    .map(t -> t.task)
+                    .toArray(CompletableFuture[]::new);
 
+            int taskId = this.currentTaskId++;
             if (futures.length == 0) {
-                // Nothing to depend on.
-                result = CompletableFuture.supplyAsync(() -> execute(operation, segmentNames), this.executor);
+                // Nothing to depend on - free to execute now.
+                result = CompletableFuture.supplyAsync(() -> execute(operation, taskId, segmentNames), this.executor);
             } else {
                 // We need to wait on these futures to complete before executing ours.
                 result = CompletableFuture.allOf(futures)
-                                          .handleAsync((r, ex) -> execute(operation, segmentNames), this.executor);
+                        .handleAsync((r, ex) -> execute(operation, taskId, segmentNames), this.executor);
             }
 
-            // Update the last operation for each involved segment to be this.
-            Arrays.stream(segmentNames)
-                  .forEach(s -> this.lastOperation.put(s, result));
+            // Update the last task for each involved segment to be this so that future tasks can be properly sequenced.
+            RunningTask t = new RunningTask(taskId, result);
+            for (String s : segmentNames) {
+                this.lastTasks.put(s, t);
+            }
         }
 
         return result;
@@ -175,6 +199,8 @@ public class AsyncStorageWrapper implements Storage {
     /**
      * Executes the given RunnableWithException asynchronously and returns a CompletableFuture that will be completed
      * when the Runnable completes.
+     * @param operation    The RunnableWithException to execute.
+     * @param segmentNames The names of the Segments involved in this operation (for sequencing purposes).
      */
     private CompletableFuture<Void> runAsync(RunnableWithException operation, String... segmentNames) {
         return supplyAsync(() -> {
@@ -183,28 +209,46 @@ public class AsyncStorageWrapper implements Storage {
         }, segmentNames);
     }
 
+    /**
+     * Executes the given Callable synchronously and invokes cleanup when done.
+     *
+     * @param operation    The Callable to execute.
+     * @param taskId       The id of the current task to be used for cleanup purposes.
+     * @param segmentNames The names of the Segments involved in this operation (for sequencing purposes).
+     */
     @SneakyThrows(Exception.class)
-    private <R> R execute(Callable<R> callable, String[] segmentNames) {
+    private <R> R execute(Callable<R> operation, int taskId, String[] segmentNames) {
         try {
-            return callable.call();
+            return operation.call();
         } finally {
-            cleanupIfNeeded(segmentNames);
+            cleanupIfNeeded(taskId, segmentNames);
         }
     }
 
-    private void cleanupIfNeeded(String[] segmentNames) {
-        synchronized (this.lastOperation) {
-            // Cleanup those tasks that have no more dependents registered.
+    private void cleanupIfNeeded(int taskId, String[] segmentNames) {
+        synchronized (this.lastTasks) {
             ArrayList<String> toRemove = new ArrayList<>();
             for (String s : segmentNames) {
-                val f = this.lastOperation.get(s);
-                if (f != null && f.getNumberOfDependents() == 0) {
+                // A segment entry can be safely cleaned up if the last registered task has the same id as the one
+                // we got in this method (that means no more tasks have been added).
+                val task = this.lastTasks.get(s);
+                if (task != null && task.taskId == taskId) {
                     toRemove.add(s);
                 }
             }
 
-            toRemove.forEach(this.lastOperation::remove);
+            toRemove.forEach(this.lastTasks::remove);
         }
+    }
+
+    //endregion
+
+    //region RunningTask
+
+    @RequiredArgsConstructor
+    private static class RunningTask {
+        private final int taskId;
+        private final CompletableFuture<?> task;
     }
 
     //endregion
--- segmentstore/storage/src/test/java/io/pravega/segmentstore/storage/AsyncStorageWrapperTests.java
+++ segmentstore/storage/src/test/java/io/pravega/segmentstore/storage/AsyncStorageWrapperTests.java
@@ -9,31 +9,51 @@
  */
 package io.pravega.segmentstore.storage;
 
+import io.pravega.common.Exceptions;
+import io.pravega.common.util.ReusableLatch;
 import io.pravega.segmentstore.contracts.SegmentProperties;
-import io.pravega.segmentstore.contracts.StreamSegmentException;
 import io.pravega.segmentstore.contracts.StreamSegmentInformation;
 import io.pravega.segmentstore.storage.mocks.InMemoryStorage;
+import io.pravega.test.common.AssertExtensions;
 import io.pravega.test.common.ThreadPooledTestSuite;
 import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.time.Duration;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.BiConsumer;
 import java.util.function.BiFunction;
+import lombok.Cleanup;
 import lombok.RequiredArgsConstructor;
 import lombok.val;
 import org.junit.Assert;
+import org.junit.Rule;
 import org.junit.Test;
+import org.junit.rules.Timeout;
 
 /**
  * Unit tests for AsyncStorageWrapper class.
+ *
+ * How these tests work:
+ * - A TestStorage is created that invokes a common function with two arguments: operation code and segment name.
+ * - Each test has two ReusableLatch maps: one for when an operation was invoked, and one when the operation should complete.
+ * - In order to detect when an operation begins, the test awaits the "invoked" latch for that operation.
+ * - In order to finalize an operation, the test releases the "waitOn" latch for that operation.
+ * - The tests (based on the scenario) will do all sort of checks in between (and after) these steps to verify the class
+ * works as expected.
  */
 public class AsyncStorageWrapperTests extends ThreadPooledTestSuite {
-
-    private static final int TIMEOUT_MILLIS = 1000;
+    private static final int LOCK_TIMEOUT_MILLIS = 50;
+    private static final int TIMEOUT_MILLIS = LOCK_TIMEOUT_MILLIS * 100;
     private static final Duration TIMEOUT = Duration.ofMillis(TIMEOUT_MILLIS);
 
-    // TODO: add global timeout
+    @Rule
+    public Timeout globalTimeout = Timeout.millis(TIMEOUT_MILLIS);
 
     @Override
     protected int getThreadPoolSize() {
@@ -55,6 +75,7 @@ public class AsyncStorageWrapperTests extends ThreadPooledTestSuite {
             return toReturn.get();
         });
 
+        @Cleanup
         val s = new AsyncStorageWrapper(innerStorage, executorService());
 
         // Create
@@ -152,9 +173,189 @@ public class AsyncStorageWrapperTests extends ThreadPooledTestSuite {
         s.truncate(handle, 0, TIMEOUT).join();
     }
 
+    /**
+     * Tests basic same-segment concurrency for simple operations. Since all operations use the same sequencing mechanism
+     * it suffices to test using two arbitrary operations instead of every possible pair.
+     */
     @Test
-    public void testConcurrency() {
-        // TODO: write this
+    public void testConcurrencySameSegment() throws Exception {
+        final String segmentName = "Segment";
+        final String op1 = TestStorage.CREATE;
+        final String op2 = TestStorage.DELETE;
+
+        // Create a set of latches that can be used to detect when an operation was invoked and when to release it.
+        val invoked = new HashMap<String, ReusableLatch>();
+        val waitOn = new HashMap<String, ReusableLatch>();
+        invoked.put(op1, new ReusableLatch());
+        invoked.put(op2, new ReusableLatch());
+        waitOn.put(op1, new ReusableLatch());
+        waitOn.put(op2, new ReusableLatch());
+
+        val innerStorage = new TestStorage((operation, segment) -> {
+            invoked.get(operation).release();
+            Exceptions.handleInterrupted(() -> waitOn.get(operation).await());
+            return null;
+        });
+
+        @Cleanup
+        val s = new AsyncStorageWrapper(innerStorage, executorService());
+
+        // Begin executing one operation (Create) and wait for it properly "acquire" the lock.
+        val futures = new ArrayList<CompletableFuture<?>>();
+        futures.add(s.create(segmentName, TIMEOUT));
+        invoked.get(op1).await(LOCK_TIMEOUT_MILLIS);
+        Assert.assertEquals("Unexpected number of active segments.", 1, s.getSegmentWithOngoingOperationsCount());
+
+        // Begin executing a second operation (Delete) and verify that it hasn't started within a reasonable amount of time.
+        futures.add(s.delete(InMemoryStorage.newHandle(segmentName, false), TIMEOUT));
+        AssertExtensions.assertThrows(
+                "Second operation was invoked while the first one was still running.",
+                () -> invoked.get(op2).await(LOCK_TIMEOUT_MILLIS),
+                ex -> ex instanceof TimeoutException);
+
+        // Complete the first operation and await the second operation to begin executing, then release it too.
+        waitOn.get(op1).release();
+        invoked.get(op2).await(LOCK_TIMEOUT_MILLIS);
+        Assert.assertEquals("Unexpected number of active segments.", 1, s.getSegmentWithOngoingOperationsCount());
+        waitOn.get(op2).release();
+
+        // Wait for both operations to complete. This will re-throw any exceptions that may have occurred.
+        allOf(futures).get(TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+        Assert.assertEquals("Unexpected final number of active segments.", 0, s.getSegmentWithOngoingOperationsCount());
+    }
+
+    /**
+     * Tests the fact that different segment do not interfere (block) with each other for concurrent operations.
+     */
+    @Test
+    public void testConcurrencyDifferentSegment() throws Exception {
+        final String segment1 = "Segment1";
+        final String segment2 = "Segment2";
+
+        // Create a set of latches that can be used to detect when an operation was invoked and when to release it.
+        val invoked = new HashMap<String, ReusableLatch>();
+        val waitOn = new HashMap<String, ReusableLatch>();
+        invoked.put(segment1, new ReusableLatch());
+        invoked.put(segment2, new ReusableLatch());
+        waitOn.put(segment1, new ReusableLatch());
+        waitOn.put(segment2, new ReusableLatch());
+
+        val innerStorage = new TestStorage((operation, segment) -> {
+            invoked.get(segment).release();
+            Exceptions.handleInterrupted(() -> waitOn.get(segment).await());
+            return null;
+        });
+
+        @Cleanup
+        val s = new AsyncStorageWrapper(innerStorage, executorService());
+
+        // Begin executing one create.
+        val futures = new ArrayList<CompletableFuture<?>>();
+        futures.add(s.create(segment1, TIMEOUT));
+        invoked.get(segment1).await(LOCK_TIMEOUT_MILLIS);
+        Assert.assertEquals("Unexpected number of active segments.", 1, s.getSegmentWithOngoingOperationsCount());
+
+        // Begin executing the second create and verify it is not blocked by the first one.
+        futures.add(s.create(segment2, TIMEOUT));
+        invoked.get(segment2).await(LOCK_TIMEOUT_MILLIS);
+        Assert.assertEquals("Unexpected number of active segments.", 2, s.getSegmentWithOngoingOperationsCount());
+
+        // Complete the first operation and await the second operation to begin executing, then release it too.
+        waitOn.get(segment1).release();
+        waitOn.get(segment2).release();
+
+        // Wait for both operations to complete. This will re-throw any exceptions that may have occurred.
+        allOf(futures).get(TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+        Assert.assertEquals("Unexpected final number of active segments.", 0, s.getSegmentWithOngoingOperationsCount());
+    }
+
+    /**
+     * Tests the segment-based concurrency when concat is involved. In particular, that a concat() will wait for any pending
+     * operations on each involved segment and that any subsequent operation on any of those segments will be queued up.
+     */
+    @Test
+    public void testConcurrencyConcat() throws Exception {
+        final String segment1 = "Segment1";
+        final String segment2 = "Segment2";
+        final BiFunction<String, String, String> joiner = (op, segment) -> op + "|" + segment;
+        final String createSegment1Key = joiner.apply(TestStorage.CREATE, segment1);
+        final String createSegment2Key = joiner.apply(TestStorage.CREATE, segment2);
+        final String concatKey = joiner.apply(TestStorage.CONCAT, segment1 + "|" + segment2);
+        final String writeSegment1Key = joiner.apply(TestStorage.WRITE, segment1);
+        final String writeSegment2Key = joiner.apply(TestStorage.WRITE, segment2);
+
+        // Create a set of latches that can be used to detect when an operation was invoked and when to release it.
+        val invoked = new HashMap<String, ReusableLatch>();
+        val waitOn = new HashMap<String, ReusableLatch>();
+        invoked.put(createSegment1Key, new ReusableLatch());
+        invoked.put(createSegment2Key, new ReusableLatch());
+        invoked.put(concatKey, new ReusableLatch());
+        invoked.put(writeSegment1Key, new ReusableLatch());
+        invoked.put(writeSegment2Key, new ReusableLatch());
+
+        waitOn.put(createSegment1Key, new ReusableLatch());
+        waitOn.put(createSegment2Key, new ReusableLatch());
+        waitOn.put(concatKey, new ReusableLatch());
+        waitOn.put(writeSegment1Key, new ReusableLatch());
+        waitOn.put(writeSegment2Key, new ReusableLatch());
+
+        val innerStorage = new TestStorage((operation, segment) -> {
+            invoked.get(joiner.apply(operation, segment)).release();
+            Exceptions.handleInterrupted(() -> waitOn.get(joiner.apply(operation, segment)).await());
+            return null;
+        });
+
+        @Cleanup
+        val s = new AsyncStorageWrapper(innerStorage, executorService());
+
+        // Issue two Create operations with the two segments and wait for both of them to be running.
+        val futures = new ArrayList<CompletableFuture<?>>();
+        futures.add(s.create(segment1, TIMEOUT));
+        futures.add(s.create(segment2, TIMEOUT));
+        invoked.get(createSegment1Key).await(LOCK_TIMEOUT_MILLIS);
+        invoked.get(createSegment2Key).await(LOCK_TIMEOUT_MILLIS);
+        Assert.assertEquals("Unexpected number of active segments.", 2, s.getSegmentWithOngoingOperationsCount());
+
+        // Initiate the concat and complete one of the original operations, and verify the concat did not start.
+        futures.add(s.concat(InMemoryStorage.newHandle(segment1, false), 0, segment2, TIMEOUT));
+        waitOn.get(createSegment1Key).release();
+        AssertExtensions.assertThrows(
+                "Concat was invoked while the at least one of the creates was running.",
+                () -> invoked.get(concatKey).await(LOCK_TIMEOUT_MILLIS),
+                ex -> ex instanceof TimeoutException);
+
+        // Finish up the "source" create and verify the concat is released.
+        waitOn.get(createSegment2Key).release();
+        invoked.get(concatKey).await(TIMEOUT_MILLIS);
+
+        // Add more operations after the concat and verify they are queued up (that they haven't started).
+        futures.add(s.write(InMemoryStorage.newHandle(segment1, false), 0, new ByteArrayInputStream(new byte[0]), 0, TIMEOUT));
+        futures.add(s.write(InMemoryStorage.newHandle(segment2, false), 0, new ByteArrayInputStream(new byte[0]), 0, TIMEOUT));
+        AssertExtensions.assertThrows(
+                "Write(target) was invoked while concat was running",
+                () -> invoked.get(writeSegment1Key).await(LOCK_TIMEOUT_MILLIS),
+                ex -> ex instanceof TimeoutException);
+
+        AssertExtensions.assertThrows(
+                "Write(source) was invoked while concat was running",
+                () -> invoked.get(writeSegment2Key).await(LOCK_TIMEOUT_MILLIS),
+                ex -> ex instanceof TimeoutException);
+        Assert.assertEquals("Unexpected number of active segments.", 2, s.getSegmentWithOngoingOperationsCount());
+
+        // Finish up the concat and verify the two writes are released.
+        waitOn.get(concatKey).release();
+        invoked.get(writeSegment1Key).await(LOCK_TIMEOUT_MILLIS);
+        invoked.get(writeSegment2Key).await(LOCK_TIMEOUT_MILLIS);
+        waitOn.get(writeSegment1Key).release();
+        waitOn.get(writeSegment2Key).release();
+
+        allOf(futures).get(TIMEOUT_MILLIS, TimeUnit.MILLISECONDS);
+        Assert.assertEquals("Unexpected number of active segments.", 0, s.getSegmentWithOngoingOperationsCount());
+
+    }
+
+    private CompletableFuture<Void> allOf(Collection<CompletableFuture<?>> futures) {
+        return CompletableFuture.allOf(futures.toArray(new CompletableFuture[futures.size()]));
     }
 
     //region TestStorage
@@ -175,17 +376,17 @@ public class AsyncStorageWrapperTests extends ThreadPooledTestSuite {
         private final BiFunction<String, String, Object> methodInvoked;
 
         @Override
-        public SegmentHandle openRead(String streamSegmentName) throws StreamSegmentException {
+        public SegmentHandle openRead(String streamSegmentName) {
             return (SegmentHandle) this.methodInvoked.apply(OPEN_READ, streamSegmentName);
         }
 
         @Override
-        public int read(SegmentHandle handle, long offset, byte[] buffer, int bufferOffset, int length) throws StreamSegmentException {
+        public int read(SegmentHandle handle, long offset, byte[] buffer, int bufferOffset, int length) {
             return (Integer) this.methodInvoked.apply(READ, handle.getSegmentName());
         }
 
         @Override
-        public SegmentProperties getStreamSegmentInfo(String streamSegmentName) throws StreamSegmentException {
+        public SegmentProperties getStreamSegmentInfo(String streamSegmentName) {
             return (SegmentProperties) this.methodInvoked.apply(GET_INFO, streamSegmentName);
         }
 
@@ -195,44 +396,44 @@ public class AsyncStorageWrapperTests extends ThreadPooledTestSuite {
         }
 
         @Override
-        public SegmentHandle openWrite(String streamSegmentName) throws StreamSegmentException {
+        public SegmentHandle openWrite(String streamSegmentName) {
             return (SegmentHandle) this.methodInvoked.apply(OPEN_WRITE, streamSegmentName);
         }
 
         @Override
-        public SegmentProperties create(String streamSegmentName) throws StreamSegmentException {
+        public SegmentProperties create(String streamSegmentName) {
             return (SegmentProperties) this.methodInvoked.apply(CREATE, streamSegmentName);
         }
 
         @Override
-        public void delete(SegmentHandle handle) throws StreamSegmentException {
+        public void delete(SegmentHandle handle) {
             this.methodInvoked.apply(DELETE, handle.getSegmentName());
         }
 
         @Override
-        public void write(SegmentHandle handle, long offset, InputStream data, int length) throws StreamSegmentException {
+        public void write(SegmentHandle handle, long offset, InputStream data, int length) {
             this.methodInvoked.apply(WRITE, handle.getSegmentName());
         }
 
         @Override
-        public void seal(SegmentHandle handle) throws StreamSegmentException {
+        public void seal(SegmentHandle handle) {
             this.methodInvoked.apply(SEAL, handle.getSegmentName());
         }
 
         @Override
-        public void concat(SegmentHandle targetHandle, long offset, String sourceSegment) throws StreamSegmentException {
+        public void concat(SegmentHandle targetHandle, long offset, String sourceSegment) {
             this.methodInvoked.apply(CONCAT, targetHandle.getSegmentName() + "|" + sourceSegment);
         }
 
         @Override
-        public void truncate(SegmentHandle handle, long offset) throws StreamSegmentException {
+        public void truncate(SegmentHandle handle, long offset) {
             this.methodInvoked.apply(TRUNCATE, handle.getSegmentName());
         }
 
         // region Unimplemented methods
 
         @Override
-        public void unseal(SegmentHandle handle) throws StreamSegmentException {
+        public void unseal(SegmentHandle handle) {
         }
 
         @Override
