7e4a6f0aec754cb1c7eb41df51f5b018ff71fa24
arvindkandhare <arvind.kandhare@emc.com>
2018-01-05 14:56:46 -0800

    CR comments.
    Signed-off-by: arvindkandhare <arvind.kandhare@emc.com>

--- standalone/src/test/java/io/pravega/local/InProcPravegaClusterTest.java
+++ standalone/src/test/java/io/pravega/local/InProcPravegaClusterTest.java
@@ -62,12 +62,13 @@ public class InProcPravegaClusterTest {
         String streamName = "Stream";
         int numSegments = 10;
 
+        System.setProperty("io.pravega.auth.enabled", String.valueOf(authEnabled));
+        System.setProperty("io.pravega.auth.certFile",  "../config/cert.pem");
+
         @Cleanup
         StreamManager streamManager = StreamManager.create(URI.create(
                 localPravega.getInProcPravegaCluster().getControllerURI()),
-                new PravegaDefaultCredentials("1111_aaaa", "admin"),
-                authEnabled,
-                "../config/cert.pem");
+                new PravegaDefaultCredentials("1111_aaaa", "admin"));
 
         streamManager.createScope(scope);
         Assert.assertTrue("Stream creation is not successful ",
