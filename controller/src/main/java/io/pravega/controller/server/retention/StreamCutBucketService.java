73ea151d95284e0c04cd0c62a84521ecdc75f778
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2018-01-26 07:56:32 +0000

    else block
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- controller/src/main/java/io/pravega/controller/server/retention/StreamCutBucketService.java
+++ controller/src/main/java/io/pravega/controller/server/retention/StreamCutBucketService.java
@@ -152,6 +152,8 @@ public class StreamCutBucketService extends AbstractService implements BucketCha
                     notifyStopped();
                 }
             });
+        } else {
+            notifyStopped();
         }
     }
 
