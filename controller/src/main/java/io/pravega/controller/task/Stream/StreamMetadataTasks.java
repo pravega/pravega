2535396e19379b1e722677a2bac894fe69d2d0d6
shivesh ranjan <13659022+shiveshr@users.noreply.github.com>
2018-02-16 15:06:41 +0530

    Issue 2312: Create node with Ephemeral mode in ZkStoreHelper.createEphemeralZNode mode (#2315)
    * ZkStoreHelper.createEphemeral node to actually
    create ephemeral node
    
    * Use map.computeIfAbsent instead of putIfAbsent
    in StreamCutBucketService while adding new stream.
    
    * Adds more info and debug logging for retention workflow
    
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- controller/src/main/java/io/pravega/controller/task/Stream/StreamMetadataTasks.java
+++ controller/src/main/java/io/pravega/controller/task/Stream/StreamMetadataTasks.java
@@ -216,7 +216,10 @@ public class StreamMetadataTasks extends TaskBase {
         if (latestCut == null || recordingTime - latestCut.getRecordingTime() > RETENTION_FREQUENCY_IN_MINUTES) {
             return generateStreamCut(scope, stream, context)
                     .thenCompose(newRecord -> streamMetadataStore.addStreamCutToRetentionSet(scope, stream, newRecord, context, executor)
-                        .thenApply(x -> newRecord));
+                        .thenApply(x -> {
+                            log.debug("New streamCut generated for stream {}/{}", scope, stream);
+                            return newRecord;
+                        }));
         } else {
             return  CompletableFuture.completedFuture(null);
         }
@@ -331,7 +334,10 @@ public class StreamMetadataTasks extends TaskBase {
                                 // 3. start truncation by updating the metadata
                                 .thenCompose(x -> streamMetadataStore.startTruncation(scope, stream, streamCut,
                                         context, executor))
-                                .thenApply(x -> true);
+                                .thenApply(x -> {
+                                    log.debug("Started truncation request for stream {}/{}", scope, stream);
+                                    return true;
+                                });
                     } else {
                         log.warn("Another truncation in progress for {}/{}", scope, stream);
                         return CompletableFuture.completedFuture(false);
