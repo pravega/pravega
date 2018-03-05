3f24e79bf7eec0fc44de03b4f41c5fb340f2af63
arvindkandhare <arvind.kandhare@emc.com>
2018-03-04 16:25:30 -0800

    Enabling TLS for inproc BK server for test.
    Signed-off-by: Arvind Kandhare <arvind.kandhare@emc.com>

--- segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperServiceRunner.java
+++ segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperServiceRunner.java
@@ -215,13 +215,15 @@ public class BookKeeperServiceRunner implements AutoCloseable {
         conf.setAllowLoopback(true);
         conf.setJournalAdaptiveGroupWrites(false);
         conf.setZkLedgersRootPath(ledgersPath);
-        conf.setTLSProvider("OpenSSL");
-        conf.setTLSProviderFactoryClass("org.apache.bookkeeper.tls.TLSContextFactory");
-        conf.setTLSKeyStore("../../../config/bookie.keystore.jks");
-        conf.setTLSKeyStorePasswordPath("../../../config/bookie.keystore.jks.passwd");
-        conf.setTLSTrustStore("../../../config/bookie.truststore.jks");
-        conf.setTLSTrustStore("../../../config/bookie.truststore.jks.passwd");
+        if (this.secureBK) {
+            conf.setTLSProvider("OpenSSL");
+            conf.setTLSProviderFactoryClass("org.apache.bookkeeper.tls.TLSContextFactory");
+            conf.setTLSKeyStore("../../../config/bookie.keystore.jks");
+            conf.setTLSKeyStorePasswordPath("../../../config/bookie.keystore.jks.passwd");
+            conf.setTLSTrustStore("../../../config/bookie.truststore.jks");
+            conf.setTLSTrustStore("../../../config/bookie.truststore.jks.passwd");
 
+        }
         log.info("Starting Bookie at port " + bkPort);
         val bs = new BookieServer(conf);
         bs.start();
