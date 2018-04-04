0880cbad4ff6b9a1eabd2c0e18205ac76463af28
Tom Kaitchuck <tom.kaitchuck@emc.com>
2018-04-04 10:26:25 -0700

    Centralize RandomStringUtils use in common to suppress warnings.
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

---
+++ common/src/main/java/io/pravega/common/lang/RandomStringUtils.java
@@ -0,0 +1,45 @@
+/**
+ * Copyright (c) 2017 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.common.lang;
+
+public class RandomStringUtils {
+
+    
+    /**
+     * <p>Creates a random string whose length is the number of characters
+     * specified.</p>
+     *
+     * <p>Characters will be chosen from the set of Latin alphabetic
+     * characters (a-z, A-Z) and the digits 0-9.</p>
+     *
+     * @param count  the length of random string to create
+     * @return the random string
+     */
+    @SuppressWarnings("deprecation")
+    public static String randomAlphanumeric(final int count) {
+        return org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric(count);
+    }
+    
+    /**
+     * <p>Creates a random string whose length is the number of characters
+     * specified.</p>
+     *
+     * <p>Characters will be chosen from the set of Latin alphabetic
+     * characters (a-z, A-Z).</p>
+     *
+     * @param count  the length of random string to create
+     * @return the random string
+     */
+    @SuppressWarnings("deprecation")
+    public static String randomAlphabetic(final int count) {
+        return org.apache.commons.lang3.RandomStringUtils.randomAlphabetic(count);
+    }
+    
+}
