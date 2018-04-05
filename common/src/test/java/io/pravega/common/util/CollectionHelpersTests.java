5d1ac73281bbb10f471042dedac4c8b7ac0ec15e
Andrei Paduroiu <andrei.paduroiu@emc.com>
2018-04-05 15:53:36 -0700

    Unit tests for the new methods in CollectionHelpers.
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>

--- common/src/test/java/io/pravega/common/util/CollectionHelpersTests.java
+++ common/src/test/java/io/pravega/common/util/CollectionHelpersTests.java
@@ -9,8 +9,10 @@
  */
 package io.pravega.common.util;
 
+import com.google.common.collect.Sets;
 import io.pravega.test.common.AssertExtensions;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
@@ -63,6 +65,44 @@ public class CollectionHelpersTests {
             val filterResult = CollectionHelpers.filterOut(collection, toExclude);
             AssertExtensions.assertContainsSameElements("Unexpected result from filterOut for i = " + i, expectedResult, filterResult);
         }
+    }
+
+    /**
+     * Tests the joinSets() method.
+     */
+    @Test
+    public void testJoinSets() {
+        AssertExtensions.<Integer>assertContainsSameElements("Empty set.", Collections.emptySet(),
+                CollectionHelpers.joinSets(Collections.emptySet(), Collections.emptySet()));
+        AssertExtensions.assertContainsSameElements("Empty+non-empty.", Sets.newHashSet(1, 2, 3),
+                CollectionHelpers.joinSets(Collections.emptySet(), Sets.newHashSet(1, 2, 3)));
+        AssertExtensions.assertContainsSameElements("Non-empty+empty.", Sets.newHashSet(1, 2, 3),
+                CollectionHelpers.joinSets(Sets.newHashSet(1, 2, 3), Collections.emptySet()));
+        AssertExtensions.assertContainsSameElements("Non-empty+non-empty.", Sets.newHashSet(1, 2, 3),
+                CollectionHelpers.joinSets(Sets.newHashSet(1, 3), Sets.newHashSet(2)));
+        AssertExtensions.assertContainsSameElements("Non-empty+non-empty(duplicates).", Arrays.asList(1, 2, 2, 3),
+                CollectionHelpers.joinSets(Sets.newHashSet(1, 2), Sets.newHashSet(2, 3)));
+    }
+
+    /**
+     * Tests the joinCollections() method.
+     */
+    @Test
+    public void testJoinCollections() {
+        AssertExtensions.assertContainsSameElements("Empty set.", Collections.<Integer>emptySet(),
+                CollectionHelpers.joinCollections(Collections.<Integer>emptySet(), i -> i, Collections.<Integer>emptySet(), i -> i));
+        AssertExtensions.assertContainsSameElements("Empty+non-empty.", Arrays.asList(1, 2, 3),
+                CollectionHelpers.joinCollections(Collections.<Integer>emptySet(), i -> i, Arrays.asList("1", "2", "3"), Integer::parseInt));
+        AssertExtensions.assertContainsSameElements("Non-empty+empty.", Arrays.asList(1, 2, 3),
+                CollectionHelpers.joinCollections(Arrays.asList("1", "2", "3"), Integer::parseInt, Collections.<Integer>emptySet(), i -> i));
+        AssertExtensions.assertContainsSameElements("Non-empty+non-empty.", Arrays.asList(1, 2, 3),
+                CollectionHelpers.joinCollections(Arrays.asList("1", "3"), Integer::parseInt, Arrays.asList("2"), Integer::parseInt));
+        val c = CollectionHelpers.joinCollections(Arrays.asList("1", "2"), Integer::parseInt, Arrays.asList("2", "3"), Integer::parseInt);
+        AssertExtensions.assertContainsSameElements("Non-empty+non-empty(duplicates).", Arrays.asList(1, 2, 2, 3), c);
+
+        // Now test the iterator.
+        val copy = new ArrayList<Integer>(c);
+        AssertExtensions.assertContainsSameElements("Non-empty+non-empty(duplicates, copy).", c, copy);
     }
 
     private Collection<Integer> createCollection(int from, int upTo) {
