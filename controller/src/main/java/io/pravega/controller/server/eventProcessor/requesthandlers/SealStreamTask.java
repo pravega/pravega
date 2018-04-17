9338880d7155186a8df3e2bbeea3c94666674a72
shivesh ranjan <13659022+shiveshr@users.noreply.github.com>
2018-04-17 16:48:43 +0530

    Issue 2474: Seal Stream Task should also handle data not found exception on transactions to be aborted  (#2479)
    * Bug fix uncovered by system tests.
    
    * Handles missing exception in seal stream task.
    
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- controller/src/main/java/io/pravega/controller/server/eventProcessor/requesthandlers/SealStreamTask.java
+++ controller/src/main/java/io/pravega/controller/server/eventProcessor/requesthandlers/SealStreamTask.java
@@ -23,7 +23,6 @@ import io.pravega.controller.task.Stream.StreamTransactionMetadataTasks;
 import io.pravega.shared.controller.event.SealStreamEvent;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Collectors;
@@ -116,17 +115,28 @@ public class SealStreamTask implements StreamTask<SealStreamEvent> {
                                         .exceptionally(e -> {
                                             Throwable cause = Exceptions.unwrap(e);
                                             if (cause instanceof StoreException.IllegalStateException ||
-                                                    cause instanceof StoreException.WriteConflictException) {
+                                                    cause instanceof StoreException.WriteConflictException ||
+                                                    cause instanceof StoreException.DataNotFoundException) {
                                                 // IllegalStateException : The transaction is already in the process of being
                                                 // completed. Ignore
                                                 // WriteConflictException : Another thread is updating the transaction record.
                                                 // ignore. We will effectively retry cleaning up the transaction if it is not
                                                 // already being aborted.
-                                                return null;
+                                                // DataNotFoundException: If transaction metadata is cleaned up after reading list
+                                                // of active segments
+                                                log.debug("A known exception thrown during seal stream while trying to abort transaction " +
+                                                        "on stream {}/{}", scope, stream, cause);
                                             } else {
                                                 // throw the original exception
-                                                throw new CompletionException(e);
+                                                // Note: we can ignore this error because if there are transactions found on a stream,
+                                                // seal stream reposts the event back into request stream.
+                                                // So in subsequent iteration it will reattempt to abort all active transactions.
+                                                // This is a valid course of action because it is important to understand that
+                                                // all transactions are completable (either via abort of commit).
+                                                log.warn("Exception thrown during seal stream while trying to abort transaction " +
+                                                        "on stream {}/{}", scope, stream, cause);
                                             }
+                                            return null;
                                         }));
                             } else {
                                 voidCompletableFuture = CompletableFuture.completedFuture(null);
