7780d87df5d42aec609e79bb0171500f9341948e
arvindkandhare <arvind.kandhare@emc.com>
2018-05-09 12:24:49 -0700

    Dropping the test as discussed. This is covered in the standalone test.
    Signed-off-by: arvindkandhare <arvind.kandhare@emc.com>

--- segmentstore/server/host/src/test/java/io/pravega/segmentstore/server/host/stat/AutoScaleProcessorTest.java
+++ segmentstore/server/host/src/test/java/io/pravega/segmentstore/server/host/stat/AutoScaleProcessorTest.java
@@ -22,7 +22,6 @@ import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
 import org.apache.commons.lang3.tuple.ImmutablePair;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import static org.junit.Assert.assertNull;
@@ -75,10 +74,6 @@ public class AutoScaleProcessorTest extends ThreadPooledTestSuite {
             }
         });
 
-        System.setProperty("pravega.client.auth.method", "Default");
-        System.setProperty("pravega.client.auth.userName", "admin");
-        System.setProperty("pravega.client.auth.passwd", "passwd");
-
         AutoScaleProcessor monitor = new AutoScaleProcessor(writer,
                 AutoScalerConfig.builder().with(AutoScalerConfig.MUTE_IN_SECONDS, 0)
                         .with(AutoScalerConfig.COOLDOWN_IN_SECONDS, 0)
@@ -117,13 +112,6 @@ public class AutoScaleProcessorTest extends ThreadPooledTestSuite {
         assertTrue(Futures.await(result2));
         assertTrue(Futures.await(result3));
         assertTrue(Futures.await(result4));
-    }
-
-    @Ignore
-    public void scaleTestWithAuth() {
-        authEnabled = true;
-        this.scaleTest();
-        authEnabled = false;
     }
 
     @Test(timeout = 10000)
