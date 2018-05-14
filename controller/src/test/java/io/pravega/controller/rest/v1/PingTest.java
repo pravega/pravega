67e042fa56779c02bbf4996f458d5c2c2ae82cf1
arvindkandhare <arvind.kandhare@emc.com>
2018-05-14 13:45:06 -0700

    Unit tests for secure connection.
    Signed-off-by: arvindkandhare <arvind.kandhare@emc.com>

--- controller/src/test/java/io/pravega/controller/rest/v1/PingTest.java
+++ controller/src/test/java/io/pravega/controller/rest/v1/PingTest.java
@@ -17,9 +17,11 @@ import io.pravega.controller.server.rest.RESTServerConfig;
 import io.pravega.controller.server.rest.impl.RESTServerConfigImpl;
 import io.pravega.test.common.TestUtils;
 import java.util.concurrent.TimeUnit;
+import javax.net.ssl.SSLContext;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.core.Response;
+import org.glassfish.jersey.SslConfigurator;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Rule;
@@ -32,7 +34,7 @@ import static org.mockito.Mockito.mock;
 /**
  * Test for ping API.
  */
-public class PingTest {
+public abstract class PingTest {
 
     //Ensure each test completes within 30 seconds.
     @Rule
@@ -45,14 +47,18 @@ public class PingTest {
     @Before
     public void setup() {
         ControllerService mockControllerService = mock(ControllerService.class);
-        serverConfig = RESTServerConfigImpl.builder().host("localhost").port(TestUtils.getAvailableListenPort())
-                .build();
+        serverConfig = getServerConfig();
         restServer = new RESTServer(null, mockControllerService, null, serverConfig,
                 new ConnectionFactoryImpl(ClientConfig.builder().build()));
         restServer.startAsync();
         restServer.awaitRunning();
-        client = ClientBuilder.newClient();
+        client = createJerseyClient();
     }
+
+    protected abstract Client createJerseyClient();
+
+    abstract RESTServerConfig getServerConfig();
+
 
     @After
     public void tearDown() {
@@ -63,8 +69,57 @@ public class PingTest {
 
     @Test
     public void test() {
-        String streamResourceURI = "http://localhost:" + serverConfig.getPort() + "/ping";
+        String streamResourceURI = getURLScheme() + "://localhost:" + serverConfig.getPort() + "/ping";
         Response response = client.target(streamResourceURI).request().buildGet().invoke();
         assertEquals(200, response.getStatus());
+    }
+
+    protected abstract String getURLScheme();
+
+    public static class DirectPingTest extends PingTest {
+
+        @Override
+        protected Client createJerseyClient() {
+            return ClientBuilder.newClient();
+        }
+
+        @Override
+        RESTServerConfig getServerConfig() {
+            return RESTServerConfigImpl.builder().host("localhost").port(TestUtils.getAvailableListenPort())
+                                .build();
+        }
+
+        @Override
+        protected String getURLScheme() {
+            return "http";
+        }
+    }
+
+    public static class SecurePingTest extends PingTest {
+
+        @Override
+        protected Client createJerseyClient() {
+            SslConfigurator sslConfig = SslConfigurator.newInstance()
+                                                       .trustStoreFile("/Users/kandha/IdeaProjects/pravega/config/bookie.truststore.jks");
+
+            SSLContext sslContext = sslConfig.createSSLContext();
+            return ClientBuilder.newBuilder().sslContext(sslContext)
+                                .hostnameVerifier((s1, s2) -> true)
+                                .build();
+        }
+
+        @Override
+        RESTServerConfig getServerConfig() {
+            return RESTServerConfigImpl.builder().host("localhost").port(TestUtils.getAvailableListenPort())
+                                       .enableTls(true)
+                                       .keyFilePath("/Users/kandha/IdeaProjects/pravega/config/bookie.keystore.jks")
+                                       .keyFilePasswordPath("/Users/kandha/IdeaProjects/pravega/config/bookie.keystore.jks.passwd")
+                                       .build();
+        }
+
+        @Override
+        protected String getURLScheme() {
+            return "https";
+        }
     }
 }
