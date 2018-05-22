f2d6b422486cdcd3a31becbe2c2369a3ecf86cdf
Raúl Gracia <raul.gracia@emc.com>
2018-05-22 11:57:22 +0200

    Issue 2540: Removed extra new line in imports.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- test/system/src/test/java/io/pravega/test/system/StreamCutsTest.java
+++ test/system/src/test/java/io/pravega/test/system/StreamCutsTest.java
@@ -53,7 +53,6 @@ import org.junit.Rule;
 import org.junit.Test;
 import org.junit.rules.Timeout;
 import org.junit.runner.RunWith;
-
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
