8b080ee3647e015ac4218164e95e10000c140768
Tom Kaitchuck <tom.kaitchuck@emc.com>
2018-06-07 15:35:43 -0700

    Increase default buffer size to 1mb.
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- client/src/main/java/io/pravega/client/segment/impl/SegmentInputStreamImpl.java
+++ client/src/main/java/io/pravega/client/segment/impl/SegmentInputStreamImpl.java
@@ -37,7 +37,7 @@ import static io.pravega.client.segment.impl.EndOfSegmentException.ErrorType.END
 class SegmentInputStreamImpl implements SegmentInputStream {
     private static final int DEFAULT_READ_LENGTH = 64 * 1024;
     private static final long UNBOUNDED_END_OFFSET = Long.MAX_VALUE;
-    static final int DEFAULT_BUFFER_SIZE = 2 * SegmentInputStreamImpl.DEFAULT_READ_LENGTH;
+    static final int DEFAULT_BUFFER_SIZE = 1024 * 1024;
 
     private final AsyncSegmentInputStream asyncInput;
     private final int readLength;
