ef024b7c4ed58db25162455e4d30ec63260be57e
Raúl Gracia <raul.gracia@emc.com>
2018-07-12 14:25:43 +0200

    Issue 2289: Leave only Clock cache setting.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/rocksdb/RocksDBCache.java
+++ segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/rocksdb/RocksDBCache.java
@@ -239,8 +239,8 @@ class RocksDBCache implements Cache {
     private Options createDatabaseOptions() {
         BlockBasedTableConfig tableFormatConfig = new BlockBasedTableConfig()
                 .setBlockCacheSize(readCacheSizeMB * 1024L * 1024L)
-                .setBlockSize(cacheBlockSizeKB * 1024L)
-                .setCacheIndexAndFilterBlocks(true);
+                .setBlockSize(cacheBlockSizeKB * 1024L);
+                //.setCacheIndexAndFilterBlocks(true);
                 //.setNoBlockCache(true);
 
         return new Options()
@@ -249,12 +249,12 @@ class RocksDBCache implements Cache {
                 .setWalDir(Paths.get(this.dbDir, DB_WRITE_AHEAD_LOG_DIR).toString())
                 .setWalTtlSeconds(0)
                 .setWalSizeLimitMB(MAX_WRITE_AHEAD_LOG_SIZE_MB)
-                .setWriteBufferSize(writeBufferSizeMB * 1024L * 1024L)
-                .setMaxWriteBufferNumber(MAX_WRITE_BUFFER_NUMBER)
-                .setMinWriteBufferNumberToMerge(MIN_WRITE_BUFFER_NUMBER_TO_MERGE)
-                .setTableFormatConfig(tableFormatConfig)
-                .setOptimizeFiltersForHits(true)
-                .setUseDirectReads(true)
+                //.setWriteBufferSize(writeBufferSizeMB * 1024L * 1024L)
+                //.setMaxWriteBufferNumber(MAX_WRITE_BUFFER_NUMBER)
+                //.setMinWriteBufferNumberToMerge(MIN_WRITE_BUFFER_NUMBER_TO_MERGE)
+                //.setTableFormatConfig(tableFormatConfig)
+                //.setOptimizeFiltersForHits(true)
+                //.setUseDirectReads(true)
                 .setRowCache(new ClockCache(readCacheSizeMB * 1024L * 1024L, -1, true));
     }
 
