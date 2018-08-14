a38ebc8c72e5ea47d31031618960b97043ca0d29
Raúl Gracia <raul.gracia@emc.com>
2018-08-14 19:56:29 +0200

    Issue 2715: Fixed class modifier.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- test/system/src/test/java/io/pravega/test/system/AbstractSystemTest.java
+++ test/system/src/test/java/io/pravega/test/system/AbstractSystemTest.java
@@ -22,7 +22,7 @@ import lombok.extern.slf4j.Slf4j;
  * Abstract class containing utilities to initialize Pravega services that are necessary in most system tests.
  */
 @Slf4j
-public class AbstractSystemTest {
+abstract class AbstractSystemTest {
 
     static URI startZookeeperInstance() {
         Service zkService = Utils.createZookeeperService();
