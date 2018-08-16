06bac2103d1cbe5ca362274067ade30531383379
Andrei Paduroiu <andrei.paduroiu@emc.com>
2018-08-16 14:06:16 -0700

    Fixed a bug
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>

--- common/src/main/java/io/pravega/common/util/btree/BTreeIndex.java
+++ common/src/main/java/io/pravega/common/util/btree/BTreeIndex.java
@@ -580,8 +580,13 @@ public class BTreeIndex {
      * @return A CompletableFuture with a PageWrapper for the sought page.
      */
     private CompletableFuture<PageWrapper> locatePage(ByteArraySegment key, PageCollection pageCollection, TimeoutTimer timer) {
+        // Verify the sought key has the expected length.
         Preconditions.checkArgument(key.getLength() == this.leafPageConfig.getKeyLength(), "Invalid key length.");
-        Preconditions.checkArgument(pageCollection.getIndexLength() == this.state.get().length, "Unexpected length.");
+
+        // Sanity check that our pageCollection has a somewhat correct view of the data index state. It is OK for it to
+        // think the index length is less than what it actually is (since a concurrent update may have increased it), but
+        // it is not a good sign if it thinks it's longer than the actual length.
+        Preconditions.checkArgument(pageCollection.getIndexLength() <= this.state.get().length, "Unexpected PageCollection.IndexLength.");
 
         if (this.state.get().rootPageOffset == PagePointer.NO_OFFSET && pageCollection.getCount() == 0) {
             // No data. Return an empty (leaf) page, which will serve as the root for now.
