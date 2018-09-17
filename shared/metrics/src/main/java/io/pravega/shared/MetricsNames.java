d229d3a124f366c0873899ba818e42a416f3457d
Flavio Junqueira (fpj) <flavio.junqueira@emc.com>
2018-09-17 15:28:49 +0200

    Added a description before the class definition.
    Signed-off-by: Flavio Junqueira (fpj) <flavio.junqueira@emc.com>

--- shared/metrics/src/main/java/io/pravega/shared/MetricsNames.java
+++ shared/metrics/src/main/java/io/pravega/shared/MetricsNames.java
@@ -9,15 +9,35 @@
  */
 package io.pravega.shared;
 
+
+
+/*
+ * Class that defines all names used for metrics that Pravega exports. All metrics have a prefix that indicates
+ * the component that is exporting it. We use a second level to indicate the sub-component (e.g., storage) or
+ * abstraction (e.g., transactions) the metric is related to.
+ *
+ * The current prefix being used are:
+ * - segmentstore.segment: metrics for individual segments
+ * - segmentstore.storage: metrics related to our long-term storage (Tier 2)
+ * - segmentstore.bookkeeper: metrics related to bookkeeper (Tier 1)
+ * - segmentstore.container: metrics for segment containers
+ * - segmentstore.cache: cache-related metrics (RocksDB)
+ * - controller.stream: metrics for operations on streams (e.g., number of streams created)
+ * - controller.segments: metrics about segments, per stream (e.g., count, splits, merges)
+ * - controller.transactions: metrics related to transactions (e.g., created, committed, aborted)
+ * - controller.retention: metrics related to data retention, per stream (e.g., frequency, size of truncated data)
+ */
+
+
 public final class MetricsNames {
     // Metrics in Segment Store Service
     // Segment-related stats
-    public static final String SEGMENT_CREATE_LATENCY = "segmentstore.segment_create_latency_ms"; // Timer
-    public static final String SEGMENT_READ_LATENCY = "segmentstore.segment_read_latency_ms";     // Timer
-    public static final String SEGMENT_WRITE_LATENCY = "segmentstore.segment_write_latency_ms";   // Timer
-    public static final String SEGMENT_READ_BYTES = "segmentstore.segment_read_bytes";            // Dynamic Counter
-    public static final String SEGMENT_WRITE_BYTES = "segmentstore.segment_write_bytes";          // Dynamic Counter
-    public static final String SEGMENT_WRITE_EVENTS = "segmentstore.segment_write_events";        // Dynamic Counter
+    public static final String SEGMENT_CREATE_LATENCY = "segmentstore.segment.create_latency_ms"; // Timer
+    public static final String SEGMENT_READ_LATENCY = "segmentstore.segment.read_latency_ms";     // Timer
+    public static final String SEGMENT_WRITE_LATENCY = "segmentstore.segment.write_latency_ms";   // Timer
+    public static final String SEGMENT_READ_BYTES = "segmentstore.segment.read_bytes";            // Dynamic Counter
+    public static final String SEGMENT_WRITE_BYTES = "segmentstore.segment.write_bytes";          // Dynamic Counter
+    public static final String SEGMENT_WRITE_EVENTS = "segmentstore.segment.write_events";        // Dynamic Counter
 
     //storage stats
     public static final String STORAGE_READ_LATENCY = "segmentstore.storage.read_latency_ms";   // Timer
@@ -26,8 +46,10 @@ public final class MetricsNames {
     public static final String STORAGE_WRITE_BYTES = "segmentstore.storage.write_bytes";        // Counter
 
     //Cache (RocksDB) stats
-    public static final String CACHE_INSERT_LATENCY = "segmentstore.cache_insert_latency";
-    public static final String CACHE_GET_LATENCY = "segment.store.cache_get_latency";
+    public static final String CACHE_INSERT_LATENCY = "segmentstore.cache.insert_latency";
+    public static final String CACHE_GET_LATENCY = "segment.store.cache.get_latency";
+    public static final String CACHE_TOTAL_SIZE_BYTES = "segmentstore.cache.size_bytes";
+    public static final String CACHE_GENERATION_SPREAD = "segmentstore.cache.gen";
 
     //DurableDataLog (Tier1) stats
     public static final String BK_TOTAL_WRITE_LATENCY = "segmentstore.bookkeeper.total_write_latency"; // Including Queue
@@ -62,33 +84,31 @@ public final class MetricsNames {
     public static final String OPERATION_LOG_SIZE = "segmentstore.operation.log_size";
     public static final String ACTIVE_SEGMENT_COUNT = "segmentstore.active_segments";
 
-    // General metrics
-    public static final String CACHE_TOTAL_SIZE_BYTES = "segmentstore.cache_size_bytes";
-    public static final String CACHE_GENERATION_SPREAD = "segmentstore.cache_gen";
-    public static final String THREAD_POOL_QUEUE_SIZE = "segmentstore.thread_pool_queue_size";
-    public static final String THREAD_POOL_ACTIVE_THREADS = "segmentstore.thread_pool_active_threads";
+    // Thread pool metrics
+    public static final String THREAD_POOL_QUEUE_SIZE = "segmentstore.thread_pool.queue_size";
+    public static final String THREAD_POOL_ACTIVE_THREADS = "segmentstore.thread_pool.active_threads";
 
     // Metrics in Controller
     // Stream request counts (Static)
-    public static final String CREATE_STREAM = "controller.stream_created";    // Histogram
-    public static final String SEAL_STREAM = "controller.stream_sealed";       // Histogram
-    public static final String DELETE_STREAM = "controller.stream_deleted";    // Histogram
+    public static final String CREATE_STREAM = "controller.stream.created";    // Histogram
+    public static final String SEAL_STREAM = "controller.stream.sealed";       // Histogram
+    public static final String DELETE_STREAM = "controller.stream.deleted";    // Histogram
 
     // Transaction request Operations (Dynamic)
-    public static final String CREATE_TRANSACTION = "controller.transactions_created";   // Dynamic Counter
-    public static final String COMMIT_TRANSACTION = "controller.transactions_committed"; // Dynamic Counter
-    public static final String ABORT_TRANSACTION = "controller.transactions_aborted";    // Dynamic Counter
-    public static final String OPEN_TRANSACTIONS = "controller.transactions_opened";     // Dynamic Gauge
-    public static final String TIMEDOUT_TRANSACTIONS = "controller.transactions_timedout";     // Dynamic Counter
+    public static final String CREATE_TRANSACTION = "controller.transactions.created";   // Dynamic Counter
+    public static final String COMMIT_TRANSACTION = "controller.transactions.committed"; // Dynamic Counter
+    public static final String ABORT_TRANSACTION = "controller.transactions.aborted";    // Dynamic Counter
+    public static final String OPEN_TRANSACTIONS = "controller.transactions.opened";     // Dynamic Gauge
+    public static final String TIMEDOUT_TRANSACTIONS = "controller.transactions.timedout";     // Dynamic Counter
 
     // Stream segment counts (Dynamic)
-    public static final String SEGMENTS_COUNT = "controller.segments_count";   // Dynamic Gauge
-    public static final String SEGMENTS_SPLITS = "controller.segment_splits"; // Dynamic Counter
-    public static final String SEGMENTS_MERGES = "controller.segment_merges"; // Dynamic Counter
+    public static final String SEGMENTS_COUNT = "controller.segments.count";   // Dynamic Gauge
+    public static final String SEGMENTS_SPLITS = "controller.segment.splits"; // Dynamic Counter
+    public static final String SEGMENTS_MERGES = "controller.segment.merges"; // Dynamic Counter
 
     // Stream retention operations (Dynamic)
-    public static final String RETENTION_FREQUENCY = "controller.retention_frequency";   // Dynamic Counter
-    public static final String TRUNCATED_SIZE = "controller.truncated_size"; // Dynamic Counter
+    public static final String RETENTION_FREQUENCY = "controller.retention.frequency";   // Dynamic Counter
+    public static final String TRUNCATED_SIZE = "controller.retention.truncated_size"; // Dynamic Counter
 
     private static String escapeSpecialChar(String name) {
         return name.replace('/', '.').replace(':', '.').replace('|', '.').replaceAll("\\s+", "_");
