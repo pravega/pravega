78dc79d36b0af0b5b017946db007b113d9379ba5
Andrei Paduroiu <andrei.paduroiu@emc.com>
2018-09-25 10:55:54 -0700

    Updated javadoc
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>

--- segmentstore/server/src/main/java/io/pravega/segmentstore/server/WriterSegmentProcessor.java
+++ segmentstore/server/src/main/java/io/pravega/segmentstore/server/WriterSegmentProcessor.java
@@ -9,6 +9,7 @@
  */
 package io.pravega.segmentstore.server;
 
+import io.pravega.segmentstore.server.logs.operations.Operation;
 import java.time.Duration;
 import java.util.concurrent.CompletableFuture;
 
@@ -21,24 +22,25 @@ public interface WriterSegmentProcessor extends AutoCloseable {
     void close();
 
     /**
-     * Gets a value indicating whether the SegmentAggregator is closed (for any kind of operations).
+     * Gets a value indicating whether the Segment Processor is closed (for any kind of operations).
      */
     boolean isClosed();
 
     /**
      * Gets the SequenceNumber of the first operation that is not fully committed to Storage.
+     * See {@link Operation#getSequenceNumber} for a definition of Sequence Number.
      */
     long getLowestUncommittedSequenceNumber();
 
     /**
-     * Gets a value indicating whether a call to flush() is required given the current state of this Segment Processor.
+     * Gets a value indicating whether a call to {@link #flush} is required given the current state of this Segment Processor.
      */
     boolean mustFlush();
 
     /**
-     * Adds the given SegmentOperation to the Processor.
+     * Adds the given {@link SegmentOperation} to the Processor.
      *
-     * @param operation the Operation to add.
+     * @param operation the SegmentOperation to add.
      * @throws DataCorruptionException  If the validation of the given Operation indicates a possible data corruption in
      *                                  the code (offset gaps, out-of-order operations, etc.)
      * @throws IllegalArgumentException If the validation of the given Operation indicates a possible non-corrupting bug
