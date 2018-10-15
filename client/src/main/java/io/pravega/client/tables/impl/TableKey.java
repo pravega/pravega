33e957959970546a0a03ebf7ff51e3a6d4eefd64
Andrei Paduroiu <andreipaduroiu@users.noreply.github.com>
2018-10-15 12:45:55 -0700

    Issue 2871: Tables Contracts (Server) (#2955)
    * Adds contract definitions for Tables:
       - Segment Store
       - Client: TableSegment only
    
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>

---
+++ client/src/main/java/io/pravega/client/tables/impl/TableKey.java
@@ -0,0 +1,27 @@
+/**
+ * Copyright (c) 2017 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.client.tables.impl;
+
+/**
+ * A Table Key with a Version.
+ *
+ * @param <KeyT> Type of the Key.
+ */
+interface TableKey<KeyT> {
+    /**
+     * The Key.
+     */
+    KeyT getKey();
+
+    /**
+     * The Version. If null, any updates for this Key will be unconditional.
+     */
+    KeyVersion getVersion();
+}
