b3896819628eb611f36d37c402c93d0e3b2011d1
Tom Kaitchuck <tom.kaitchuck@emc.com>
2018-10-15 17:57:39 -0700

    Header
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- common/src/test/java/io/pravega/common/util/ByteBufferUtilsTests.java
+++ common/src/test/java/io/pravega/common/util/ByteBufferUtilsTests.java
@@ -1,3 +1,12 @@
+/**
+ * Copyright (c) 2018 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
 package io.pravega.common.util;
 
 import java.nio.ByteBuffer;
