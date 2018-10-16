027873eb0f5a0448f30a4feb6b2b99166f643000
Andrei Paduroiu <andrei.paduroiu@emc.com>
2018-10-16 15:38:05 -0700

    Fixed Javadoc
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>

--- segmentstore/server/src/main/java/io/pravega/segmentstore/server/SegmentContainer.java
+++ segmentstore/server/src/main/java/io/pravega/segmentstore/server/SegmentContainer.java
@@ -29,14 +29,42 @@ public interface SegmentContainer extends StreamSegmentStore, Container {
     Collection<SegmentProperties> getActiveSegments();
 
     /**
-     * Returns a DirectSegmentAccess object that can be used for operating on a particular StreamSegment directly. The
-     * result of this call should only be used for processing a single request (and discarded afterwards) and not be cached
-     * for long-term access (i.e., between multiple requests).
+     * Returns a {@link DirectSegmentAccess} object that can be used for operating on a particular StreamSegment directly.
+     * The result of this call should only be used for processing a single external request (i.e., coming from over the
+     * network) and be discarded afterwards. It should not be cached for long-term access or shared between multiple
+     * external requests.
      *
      * If the resulting {@link DirectSegmentAccess} object is cached for more than one request, there is a chance that
      * the underlying Segment may have been evicted from memory. Attempting to use this object at that time will result
-     * in a {@link MetadataUpdateException} being thrown for every method call on it. Consider re-invoking this method to
-     * obtain a new {@link DirectSegmentAccess} instance that can be used for future access.
+     * in a {@link MetadataUpdateException} being thrown for every method call on it. As such, employing such a pattern
+     * is strongly discouraged; consider re-invoking this method to obtain a new {@link DirectSegmentAccess} instance
+     * every time a new external request is received.
+     *
+     * Note that it is also possible, though highly unlikely, that the Segment may be evicted from memory from the time
+     * this method is invoked until it is needed (even if the calls happen in rapid succession). Even if the initial call
+     * does "touch" the Segment (and should prevent it from being evicted in the near future), it is possible that either
+     * due to extreme memory pressure or other thread pool contention (in which case the DirectSegmentAccess consumers
+     * may be invoked much later) that the Segment may be evicted from memory before it can be used.
+     *
+     * Example
+     * <pre>
+     * {@code
+     * CompletableFuture<Long> truncateAppendSeal(String segmentName, byte[] data, Duration timeout) {
+     *     SegmentContainer container = getSegmentContainer(segmentName);
+     *     TimeoutTimer timer = new TimeoutTimer(timeout);
+     *     // Get the reference to DirectSegmentAccess here.
+     *     return container.forSegment(segmentName, timer.getRemaining())
+     *                     .thenCompose(segment -> {
+     *                         // Use the DirectSegmentAccess instance repeatedly for multiple Segment operations.
+     *                         SegmentProperties properties = segment.getInfo();
+     *                         return segment.truncate(properties.getLength(), timer.getRemaining())
+     *                                       .thenCompose(ignored -> segment.append(data, null, timer.getRemaining()))
+     *                                       .thenCompose(ignored -> segment.seal(timer.getRemaining()));
+     *                      });
+     *     // Discard the DirectSegmentAccess instance (i.e., release its references so the GC can reclaim it).
+     *     }
+     * }
+     * </pre>
      *
      * @param streamSegmentName The name of the StreamSegment to get for.
      * @param timeout           Timeout for the operation.
