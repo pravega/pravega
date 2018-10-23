e3276326648f669550a174dfe0860a52e4f74bf8
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2018-10-23 04:47:12 -0700

    PR comment
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- controller/src/main/java/io/pravega/controller/store/stream/records/SealedSegmentsMapShard.java
+++ controller/src/main/java/io/pravega/controller/store/stream/records/SealedSegmentsMapShard.java
@@ -49,22 +49,28 @@ public class SealedSegmentsMapShard {
 
     private final int shardNumber;
     /**
-     * We maintain a map of Sealed segments with size at the time of sealing. This map can grow very large, O(number of segments)
-     * and hence we will store this in multiple shards. Each shard should ideally contain limited number of entries and we should
-     * be able to compute the shard just by looking at segmentId. 
-     * A segmentId is composed of two parts, creationEpoch and segmentNumber. Which gives us two logical choices for sharding. 
-     * If we were to use segmentNumber to identify shard for a segment, then each shard can contain arbitrarily large number of
-     * entries because with rolling transactions, there can be many duplicate segments created per segment. 
-     * If we use creationEpoch, then we can still have unbounded number of entries because there is no limit on number of segments
-     * created in an epoch. However, typically with auto-scale, this number would be fairly small for each epoch. 
-     * However, with rolling transactions, all segments would be recreated in an epoch and this could be arbitrarily large.
-     * But with limiting the number of epochs and taking a view that about 1000 segments per epoch on average, we can still support
-     * million segments in each shard with a shard size of 1000. 
-     * 
-     * So we will have each shard contains segments from a range of epochs in batches of 1000. 
-     * So to get sealed segment record -> extract segment epoch from segment id. compute the shard by dividing segment number by SHARD_SIZE.
-     * Fetch the size from the shard map.
-     */
+     * We maintain a map of Sealed segments with size at the time of sealing. This map can grow very large, 
+     * O(number of segments), and hence we will store this in multiple shards. Each shard should ideally
+     *  contain a limited number of entries, and we should be able to compute the shard just by looking at
+     *  segmentId. A segmentId is composed of two parts, creationEpoch and segmentNumber, which gives
+     * us two logical choices for sharding. 
+     *
+     * If we were to use segmentNumber to identify shard for a segment, then each shard can contain an
+     * arbitrarily large number of entries because with rolling transactions, there can be many duplicate
+     * segments. If we use creationEpoch, then we can still have an unbounded number of entries because
+     * there is no limit on the number of segments created in an epoch. However, it is expected with auto-scale
+     * that this number is small for each epoch. 
+     *
+     * Rolling transactions adds a level of complexity. All segments corresponding to a transaction are recreated
+     * in an epoch and number this could be arbitrarily large. We do not foresee transactions in the vast majority
+     * of the cases rolling for multiple consecutive epochs, though. As such, assuming a small number of epochs
+     * for a rolling transaction and about 1000 segments per epoch on average as an educated guess, we can still 
+     * support million segments in each shard with a shard size of 1000. 
+     *
+     * Each shard consequently contains segments from a range of epochs in batches of 1000. 
+     * To get a sealed segment record -> extract segment epoch from segment id, and compute the shard
+     * by taking the modulo of the segment number by the SHARD_SIZE. Fetch the size from the shard map.
+     **/
     private final Map<Long, Long> sealedSegmentsSizeMap;
 
     SealedSegmentsMapShard(int shardNumber, Map<Long, Long> sealedSegmentsSizeMap) {
