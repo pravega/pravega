3ce79ceaffa7e7881bb801bbbdd011a579ee3c2e
Andrei Paduroiu <andrei.paduroiu@emc.com>
2018-10-25 14:05:02 -0700

    Merge remote branch `master`.
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>

--- common/src/test/java/io/pravega/common/util/CollectionHelpersTests.java
+++ common/src/test/java/io/pravega/common/util/CollectionHelpersTests.java
@@ -18,10 +18,7 @@ import java.util.Collection;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.List;
-import java.util.Map;
-import java.util.Random;
 import java.util.concurrent.atomic.AtomicInteger;
-
 import lombok.Data;
 import lombok.val;
 import org.junit.Assert;
@@ -50,41 +47,6 @@ public class CollectionHelpersTests {
             }
             // Add an element.
             list.add(maxSearchElement);
-        }
-    }
-
-    /**
-     * Tests the binarySearch() method on a IndexedMap.
-     */
-    @Test
-    public void testBinarySearchSortedMap() {
-        int maxSize = 100;
-        int skip = 3;
-        val m = new TestIndexedMap();
-        val allValues = new HashMap<Integer, String>();
-        val r = new Random(0);
-        for (int size = 0; size < maxSize; size++) {
-            // Generate search keys.
-            int maxSearchElement = (m.getCount() + 1) * skip;
-            val searchKeys = new ArrayList<Integer>();
-            for (int i = 0; i < size; i += skip) {
-                searchKeys.add(r.nextInt(size * 2));
-            }
-
-            val expectedValues = new HashMap<Integer, String>();
-            searchKeys.stream()
-                      .filter(allValues::containsKey)
-                      .forEach(k -> expectedValues.put(k, allValues.get(k)));
-
-            val actualValues = new HashMap<Integer, String>();
-            val anythingFound = CollectionHelpers.binarySearch(m, searchKeys, actualValues);
-
-            Assert.assertEquals("Unexpected return value for size " + size, expectedValues.size() > 0, anythingFound);
-            AssertExtensions.assertMapEquals("Unexpected result contents for size " + size, expectedValues, actualValues);
-
-            // Add new data.
-            m.add(maxSearchElement, Integer.toString(maxSearchElement));
-            allValues.put(maxSearchElement, Integer.toString(maxSearchElement));
         }
     }
 
@@ -170,29 +132,6 @@ public class CollectionHelpersTests {
         }
 
         return collection;
-    }
-
-    private static class TestIndexedMap implements IndexedMap<Integer, String> {
-        private final ArrayList<Map.Entry<Integer, String>> entries = new ArrayList<>();
-
-        void add(int key, String value) {
-            this.entries.add(new AbstractMap.SimpleImmutableEntry<>(key, value));
-        }
-
-        @Override
-        public int getCount() {
-            return this.entries.size();
-        }
-
-        @Override
-        public Integer getKey(int position) {
-            return this.entries.get(position).getKey();
-        }
-
-        @Override
-        public String getValue(int position) {
-            return this.entries.get(position).getValue();
-        }
     }
 
     @Data
