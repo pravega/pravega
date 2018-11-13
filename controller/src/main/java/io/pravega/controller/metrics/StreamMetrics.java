a8a36f8617212d72be82149fb0e70d56d9533d6c
Raúl Gracia <raul.gracia@emc.com>
2018-11-13 16:48:37 +0100

    Issue 1957: Added missing comments.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- controller/src/main/java/io/pravega/controller/metrics/StreamMetrics.java
+++ controller/src/main/java/io/pravega/controller/metrics/StreamMetrics.java
@@ -52,8 +52,8 @@ public final class StreamMetrics implements AutoCloseable {
     private final OpStatsLogger truncateStreamLatency = STATS_LOGGER.createStats(TRUNCATE_STREAM_LATENCY);
 
     /**
-     * This method increments the global counter of created Streams in the system, initializes stream-specific metrics
-     * and reports the latency of the operation.
+     * This method increments the global and Stream-specific counters of Stream creations, initializes other
+     * stream-specific metrics and reports the latency of the operation.
      *
      * @param scope             Scope.
      * @param streamName        Name of the Stream.
@@ -83,8 +83,8 @@ public final class StreamMetrics implements AutoCloseable {
     }
 
     /**
-     * This method increments the counter of deleted Streams in the system, freezes stream-specific metrics and
-     * reports the latency of the successful deleteStream operation.
+     * This method increments the global and Stream-specific counters of Stream deletions and reports the latency of
+     * the operation.
      *
      * @param scope         Scope.
      * @param streamName    Name of the Stream.
@@ -108,6 +108,14 @@ public final class StreamMetrics implements AutoCloseable {
         DYNAMIC_LOGGER.incCounterValue(nameFromStream(DELETE_STREAM_FAILED, scope, streamName), 1);
     }
 
+    /**
+     * This method increments the global and Stream-specific counters of seal Stream operations, set the number of open
+     * Transactions to 0, and reports the latency of the operation.
+     *
+     * @param scope         Scope.
+     * @param streamName    Name of the Stream.
+     * @param latency       Latency of the sealStream operation.
+     */
     public void sealStream(String scope, String streamName, Duration latency) {
         DYNAMIC_LOGGER.incCounterValue(SEAL_STREAM, 1);
         DYNAMIC_LOGGER.incCounterValue(nameFromStream(SEAL_STREAM, scope, streamName), 1); // Streams can be re-created
@@ -116,8 +124,8 @@ public final class StreamMetrics implements AutoCloseable {
     }
 
     /**
-     * This method increments the counter of failed Stream seal operations in the system as well as the failed
-     * seal attempts for this specific Stream.
+     * This method increments the counter of failed Stream seal operations in the system as well as the failed seal
+     * attempts for this specific Stream.
      *
      * @param scope         Scope.
      * @param streamName    Name of the Stream.
@@ -127,6 +135,14 @@ public final class StreamMetrics implements AutoCloseable {
         DYNAMIC_LOGGER.incCounterValue(nameFromStream(SEAL_STREAM_FAILED, scope, streamName), 1);
     }
 
+    /**
+     * This method increments the global and Stream-specific counters of Stream updates and reports the latency of the
+     * operation.
+     *
+     * @param scope         Scope.
+     * @param streamName    Name of the Stream.
+     * @param latency       Latency of the updateStream operation.
+     */
     public void updateStream(String scope, String streamName, Duration latency) {
         DYNAMIC_LOGGER.incCounterValue(UPDATE_STREAM, 1);
         DYNAMIC_LOGGER.incCounterValue(nameFromStream(UPDATE_STREAM, scope, streamName), 1);
@@ -134,8 +150,8 @@ public final class StreamMetrics implements AutoCloseable {
     }
 
     /**
-     * This method increments the counter of failed Stream seal operations in the system as well as the failed
-     * seal attempts for this specific Stream.
+     * This method increments the counter of failed Stream update operations in the system as well as the failed
+     * update attempts for this specific Stream.
      *
      * @param scope         Scope.
      * @param streamName    Name of the Stream.
@@ -145,27 +161,65 @@ public final class StreamMetrics implements AutoCloseable {
         DYNAMIC_LOGGER.incCounterValue(nameFromStream(UPDATE_STREAM_FAILED, scope, streamName), 1);
     }
 
+    /**
+     * This method increments the global and Stream-specific counters of Stream truncations and reports the latency of
+     * the operation.
+     *
+     * @param scope         Scope.
+     * @param streamName    Name of the Stream.
+     * @param latency       Latency of the truncateStream operation.
+     */
     public void truncateStream(String scope, String streamName, Duration latency) {
         DYNAMIC_LOGGER.incCounterValue(TRUNCATE_STREAM, 1);
         DYNAMIC_LOGGER.incCounterValue(nameFromStream(TRUNCATE_STREAM, scope, streamName), 1);
         truncateStreamLatency.reportSuccessEvent(latency);
     }
 
+    /**
+     * This method increments the counter of failed Stream truncate operations in the system as well as the failed
+     * truncate attempts for this specific Stream.
+     *
+     * @param scope         Scope.
+     * @param streamName    Name of the Stream.
+     */
     public void truncateStreamFailed(String scope, String streamName) {
         DYNAMIC_LOGGER.incCounterValue(TRUNCATE_STREAM_FAILED, 1);
         DYNAMIC_LOGGER.incCounterValue(nameFromStream(TRUNCATE_STREAM_FAILED, scope, streamName), 1);
     }
 
+    /**
+     * This method increments the Stream-specific counter of retention operations.
+     *
+     * @param scope         Scope.
+     * @param streamName    Name of the Stream.
+     */
     public void retention(String scope, String streamName) {
         DYNAMIC_LOGGER.recordMeterEvents(nameFromStream(RETENTION_FREQUENCY, scope, streamName), 1);
     }
 
-    public void reportActiveSegments(String scope, String streamName, int minNumSegments) {
-        DYNAMIC_LOGGER.reportGaugeValue(nameFromStream(SEGMENTS_COUNT, scope, streamName), minNumSegments);
+    /**
+     *
+     * @param scope         Scope.
+     * @param streamName    Name of the Stream.
+     * @param numSegments   Number of active segments in this Stream.
+     */
+    public void reportActiveSegments(String scope, String streamName, int numSegments) {
+        DYNAMIC_LOGGER.reportGaugeValue(nameFromStream(SEGMENTS_COUNT, scope, streamName), numSegments);
     }
 
+    /**
+     * Reports the number of segment splits and merges related to a particular scale operation on a Stream. Both global
+     * and Stream-specific counters are updated.
+     *
+     * @param scope         Scope.
+     * @param streamName    Name of the Stream.
+     * @param splits        Number of segment splits in the scale operation.
+     * @param merges        Number of segment merges in the scale operation.
+     */
     public void reportSegmentSplitsAndMerges(String scope, String streamName, long splits, long merges) {
+        DYNAMIC_LOGGER.updateCounterValue(SEGMENTS_SPLITS, splits);
         DYNAMIC_LOGGER.updateCounterValue(nameFromStream(SEGMENTS_SPLITS, scope, streamName), splits);
+        DYNAMIC_LOGGER.updateCounterValue(SEGMENTS_MERGES, merges);
         DYNAMIC_LOGGER.updateCounterValue(nameFromStream(SEGMENTS_MERGES, scope, streamName), merges);
     }
 
