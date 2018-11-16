063522e1a921f597a529b16853a54b7e8720cd1d
Sandeep <sandeep.shridhar@emc.com>
2018-11-16 12:52:15 -0500

    Add K8 based sequential executor.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

---
+++ test/system/src/main/java/io/pravega/test/system/framework/services/kubernetes/K8Sequential.java
@@ -0,0 +1,83 @@
+/**
+ * Copyright (c) 2017 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.test.system.framework.services.kubernetes;
+
+import com.google.common.collect.ImmutableMap;
+import io.kubernetes.client.models.V1Pod;
+import io.kubernetes.client.models.V1PodBuilder;
+import io.pravega.test.system.framework.TestExecutor;
+import io.pravega.test.system.framework.kubernetes.K8Client;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang.NotImplementedException;
+
+import java.lang.reflect.Method;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionException;
+
+@Slf4j
+public class K8Sequential implements TestExecutor {
+
+    private static final String NAMESPACE = "default";
+
+    @Override
+    public CompletableFuture<Void> startTestExecution(Method testMethod) {
+        String className = testMethod.getDeclaringClass().getName();
+        String methodName = testMethod.getName();
+        String podName = className.toLowerCase().replace(".", "-") + "-" + methodName.toLowerCase();
+        log.info("Start execution of test {}#{} on the K8 Cluster", className, methodName);
+
+        K8Client client = new K8Client();
+        V1Pod pod = getTestPod(className, methodName, podName.toLowerCase());
+
+        CompletableFuture<Void> testFuture = client.deployPod(NAMESPACE, pod)
+                                                   .thenCompose(v -> client.waitUntilPodCompletes(NAMESPACE, podName))
+                                                   .handle((s, t) -> {
+                                                       if (t == null) {
+                                                           log.info("Test execution completed with status {}", s);
+                                                           if (s.getExitCode() != 0) {
+                                                               log.error("Test {}#{} failed. Details: {}", className, methodName, s);
+                                                               throw new AssertionError(methodName + " test failed.");
+                                                           } else {
+                                                               return null;
+                                                           }
+                                                       } else {
+                                                           throw new CompletionException("Error while invoking the test " + podName, t);
+                                                       }
+                                                   });
+
+        testFuture.thenRun(client::close); // close the client.
+        return testFuture;
+    }
+
+    private V1Pod getTestPod(String className, String methodName, String podName) {
+        String repoUrl = System.getProperty("repoUrl");
+        String testVersion = System.getProperty("testVersion");
+        return new V1PodBuilder()
+                .withNewMetadata().withName(podName).withLabels(ImmutableMap.of("POD_NAME", podName)).endMetadata()
+                .withNewSpec().addNewContainer()
+                .withName(podName) // container name is same as that of the pod.
+                .withImage("openjdk:8-jre-alpine")
+                .withImagePullPolicy("IfNotPresent")
+                .withCommand("/bin/sh")
+                .withArgs("-c",
+                          "wget " + repoUrl + "/io/pravega/pravega-test-system/" + testVersion + "/pravega-test-system-" + testVersion +".jar && "
+                                  + "echo \"download of system test jar complete\" && "
+                                  + "java -DexecType=K8 -cp ./pravega-test-system-" + testVersion + ".jar io.pravega.test.system.SingleJUnitTestRunner "
+                                  + className + "#" +methodName /*+ " > server.log 2>&1 */ + "; exit $?")
+                .endContainer()
+                .withRestartPolicy("Never")
+                .endSpec().build();
+    }
+
+    @Override
+    public void stopTestExecution() {
+        throw new NotImplementedException("Not implemented for Kubernetes based tests");
+    }
+}
