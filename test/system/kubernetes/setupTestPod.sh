7ca2cabd5e71ddd6bdb5b75fe8b4d737c7ca1a95
Sandeep <sandeep.shridhar@emc.com>
2018-12-06 08:56:23 -0500

    Simplify kubernetes based framework by eliminating publish step.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

---
+++ test/system/kubernetes/setupTestPod.sh
@@ -0,0 +1,79 @@
+#!/usr/bin/env bash
+
+function program_is_installed {
+  # set to 1 initially
+  local return_=1
+  # set to 0 if not found
+  type $1 >/dev/null 2>&1 || { local return_=0; }
+  # return value
+  echo "$return_"
+}
+
+# verify if kubectl is present.
+if [ $(program_is_installed kubectl) == 0 ]; then
+  echo "kubectl is not present"
+  exit 1
+else
+  echo "kubectl is present"
+fi
+
+echo "Logging the details of Kubernetes cluster"
+kubectl cluster-info
+
+if [ $? -ne 0 ]; then
+   echo "Kubectl is not configured correctly, please configure kubectl before starting tests."
+   exit 1
+fi
+
+#create a dynamic PVC
+cat <<EOF | kubectl create -f -
+kind: PersistentVolumeClaim
+apiVersion: v1
+metadata:
+  name: task-pv-claim
+spec:
+  accessModes:
+    - ReadWriteOnce
+  resources:
+    requests:
+      storage: 1Gi
+EOF
+
+#create a pod.
+cat <<EOF | kubectl create -f -
+kind: Pod
+apiVersion: v1
+metadata:
+  name: task-pv-pod
+spec:
+  volumes:
+    - name: task-pv-storage
+      persistentVolumeClaim:
+       claimName: task-pv-claim
+  containers:
+    - name: task-pv-container
+      image: openjdk:8-jre-alpine
+      command: ["/bin/sh"]
+      args: ["-c", "sleep 60000"]
+      volumeMounts:
+        - mountPath: "/data"
+          name: task-pv-storage
+EOF
+kubectl wait --for=condition=Ready pod/task-pv-pod
+
+checksum="$(kubectl exec task-pv-pod md5sum '/data/test-collection.jar' | awk '{ print $1 }')"
+echo "Checksum of test artifact on the pod $checksum"
+
+expectedCheckSum="$(md5sum './build/libs/test-collection.jar' | awk '{ print $1 }' )"
+echo "Checksum of the local test artifact $expectedCheckSum"
+
+if [ "$checksum" == "$expectedCheckSum" ]; then
+  echo "Checksum match, no need to copy the test jar to Kubernetes cluster"
+else
+  echo "Copying test artifact to cluster, (this will take a couple of minutes)..."
+  kubectl cp ./build/libs/test-collection.jar task-pv-pod:/data
+fi
+
+#delete the pod that was created.
+echo "Deleting pod task-pv-pod that was used to copy the test artifacts"
+kubectl delete po task-pv-pod --now
