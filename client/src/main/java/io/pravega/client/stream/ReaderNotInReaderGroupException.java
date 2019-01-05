58e79c21c96d2006406429a9f0e1239cd1fb9335
Tom Kaitchuck <tom.kaitchuck@emc.com>
2019-01-04 16:28:50 -0800

    PR feedback.
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- client/src/main/java/io/pravega/client/stream/ReaderNotInReaderGroupException.java
+++ client/src/main/java/io/pravega/client/stream/ReaderNotInReaderGroupException.java
@@ -11,15 +11,19 @@ package io.pravega.client.stream;
 
 import lombok.Getter;
 
+/**
+ * Read was invoked on a reader that the reader group does not consider a member.
+ * This likely means the reader was declared dead by invoking {@link ReaderGroup#readerOffline(String, Position)}
+ */
 public class ReaderNotInReaderGroupException extends Exception {
 
     private static final long serialVersionUID = 1L;
 
     @Getter
     private final String readerId;
-    
+ 
     public ReaderNotInReaderGroupException(String readerId) {
-        super("Reader: "+readerId);
+        super("Reader: " + readerId);
         this.readerId = readerId;
     }
 }
