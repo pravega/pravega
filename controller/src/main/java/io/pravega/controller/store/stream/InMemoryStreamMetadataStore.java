5025c5d0e5b1408612a04c8c3f80bf1b3a113b41
Raúl Gracia <raul.gracia@emc.com>
2019-01-21 15:23:04 +0100

    Cherry pick PR 3272.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- controller/src/main/java/io/pravega/controller/store/stream/InMemoryStreamMetadataStore.java
+++ controller/src/main/java/io/pravega/controller/store/stream/InMemoryStreamMetadataStore.java
@@ -348,6 +348,11 @@ class InMemoryStreamMetadataStore extends AbstractStreamMetadataStore {
         return CompletableFuture.completedFuture(null);
     }
 
+    @Override
+    public CompletableFuture<Void> createBucketsRoot() {
+        return CompletableFuture.completedFuture(null);
+    }
+
     private String scopedStreamName(final String scopeName, final String streamName) {
         return new StringBuilder(scopeName).append("/").append(streamName).toString();
     }
