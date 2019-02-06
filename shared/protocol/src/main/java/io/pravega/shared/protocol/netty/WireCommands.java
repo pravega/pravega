8ae2f4f2056aeb9c199de64218c04925605107b3
Andrei Paduroiu <andreipaduroiu@users.noreply.github.com>
2019-02-06 12:54:48 -0800

    Issue 3325: AutoScaleProcessor fixes (#3331)
    * `SegmentAggregates`: simplifies and makes it thread-safe.
    * Unifies `ScaleType` and `WireProtocol.CreateSegment.ScaleType`
    to be compatible.
    * Moves `ScaleType` to shared
    * `SegmentStatsFactory` renames to `AutoScaleMonitor`  
    * Also properly closing resources and using a single thread pool
    * Other fixes related to the touched classes.
    * Fixing `InlineExecutor` and `ThreadPooledTestSuite` to automatically
    cancel ongoing scheduled tasks when shutting down
    
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>

--- shared/protocol/src/main/java/io/pravega/shared/protocol/netty/WireCommands.java
+++ shared/protocol/src/main/java/io/pravega/shared/protocol/netty/WireCommands.java
@@ -14,6 +14,7 @@ import io.netty.buffer.ByteBuf;
 import io.netty.buffer.ByteBufInputStream;
 import io.netty.buffer.Unpooled;
 import io.netty.handler.codec.CorruptedFrameException;
+import io.pravega.shared.segment.ScaleType;
 import java.io.DataInput;
 import java.io.DataOutput;
 import java.io.IOException;
@@ -994,9 +995,9 @@ public final class WireCommands {
 
     @Data
     public static final class CreateSegment implements Request, WireCommand {
-        public static final byte NO_SCALE = (byte) 0;
-        public static final byte IN_KBYTES_PER_SEC = (byte) 1;
-        public static final byte IN_EVENTS_PER_SEC = (byte) 2;
+        public static final byte NO_SCALE = ScaleType.NoScaling.getValue();
+        public static final byte IN_KBYTES_PER_SEC = ScaleType.Throughput.getValue();
+        public static final byte IN_EVENTS_PER_SEC = ScaleType.EventRate.getValue();
 
         final WireCommandType type = WireCommandType.CREATE_SEGMENT;
         final long requestId;
---
+++ shared/protocol/src/main/java/io/pravega/shared/segment/ScaleType.java
@@ -0,0 +1,55 @@
+/**
+ * Copyright (c) 2017 Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+
+package io.pravega.shared.segment;
+
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+
+/**
+ * Defines Scale Types for Segments.
+ */
+@RequiredArgsConstructor(access = AccessLevel.PRIVATE)
+public enum ScaleType {
+    /**
+     * No scaling.
+     */
+    NoScaling((byte) 0),
+    /**
+     * Scale based on the rate in bytes.
+     */
+    Throughput((byte) 1),
+    /**
+     * Scale based on the rate in events.
+     */
+    EventRate((byte) 2);
+
+    @Getter
+    private final byte value;
+
+    /**
+     * Gets the {@link ScaleType} that matches the given id.
+     *
+     * @param value The Id to match.
+     * @return the {@link ScaleType}.
+     */
+    public static ScaleType fromValue(byte value) {
+        if (value == NoScaling.getValue()) {
+            return NoScaling;
+        } else if (value == Throughput.getValue()) {
+            return Throughput;
+        } else if (value == EventRate.getValue()) {
+            return EventRate;
+        } else {
+            throw new IllegalArgumentException("Unsupported Scale Type id " + value);
+        }
+    }
+}
