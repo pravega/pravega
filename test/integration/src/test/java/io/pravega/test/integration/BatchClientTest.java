7e518f1f96c2238fc45ebc976a66ecf29e2748c9
huide9 <huide9@gmail.com>
2019-02-23 08:45:50 -0800

    missing @Cleanup
    Signed-off-by: huide9 <huide9@gmail.com>

--- test/integration/src/test/java/io/pravega/test/integration/BatchClientTest.java
+++ test/integration/src/test/java/io/pravega/test/integration/BatchClientTest.java
@@ -165,6 +165,7 @@ public class BatchClientTest {
         createTestStreamWithEvents(clientFactory);
 
         ClientConfig clientConfig = ClientConfig.builder().controllerURI(controllerUri).build();
+	@Cleanup
         ConnectionFactory connectionFactory = new ConnectionFactoryImpl(clientConfig);
         @Cleanup
         BatchClientFactory batchClient = BatchClientFactory.withScope(SCOPE, clientConfig, connectionFactory);
@@ -190,6 +191,7 @@ public class BatchClientTest {
         createTestStreamWithEvents(clientFactory);
 
         ClientConfig clientConfig = ClientConfig.builder().controllerURI(controllerUri).build();
+	@Cleanup
         ConnectionFactory connectionFactory = new ConnectionFactoryImpl(clientConfig);
         @Cleanup
         BatchClientFactory batchClient = BatchClientFactory.withScope(SCOPE, clientConfig, connectionFactory);
