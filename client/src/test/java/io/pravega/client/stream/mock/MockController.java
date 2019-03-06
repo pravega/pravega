eb1491e718205b7469c47daa86176212b9772f85
Tom Kaitchuck <tom.kaitchuck@emc.com>
2019-03-05 16:50:26 -0800

    Add stubs for mock and local controller.
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- client/src/test/java/io/pravega/client/stream/mock/MockController.java
+++ client/src/test/java/io/pravega/client/stream/mock/MockController.java
@@ -16,6 +16,7 @@ import io.pravega.client.netty.impl.Flow;
 import io.pravega.client.netty.impl.ClientConnection;
 import io.pravega.client.netty.impl.ConnectionFactory;
 import io.pravega.client.segment.impl.Segment;
+import io.pravega.client.stream.Position;
 import io.pravega.client.stream.ScalingPolicy;
 import io.pravega.client.stream.Stream;
 import io.pravega.client.stream.StreamConfiguration;
@@ -518,6 +519,17 @@ public class MockController implements Controller {
     @Override
     public CompletableFuture<String> getOrRefreshDelegationTokenFor(String scope, String streamName) {
         return CompletableFuture.completedFuture("");
+    }
+
+    @Override
+    public CompletableFuture<Void> noteTimestampFromWriter(String writer, Stream stream, long timestamp,
+                                                           Position lastWrittenPosition) {
+        return CompletableFuture.completedFuture(null);
+    }
+
+    @Override
+    public CompletableFuture<Void> writerShutdown(String writerId, Stream stream) {
+        return CompletableFuture.completedFuture(null);
     }
 }
 
