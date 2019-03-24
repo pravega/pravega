73a5dfbd406fcde3605683c8905bc7ec2447e14a
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2019-03-24 06:31:52 -0700

    reverting import consolidation
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- controller/src/test/java/io/pravega/controller/eventProcessor/impl/SerializedRequestHandlerTest.java
+++ controller/src/test/java/io/pravega/controller/eventProcessor/impl/SerializedRequestHandlerTest.java
@@ -31,7 +31,10 @@ import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.stream.Collectors;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+import static org.junit.Assert.assertTrue;
 
 public class SerializedRequestHandlerTest extends ThreadPooledTestSuite {
 
