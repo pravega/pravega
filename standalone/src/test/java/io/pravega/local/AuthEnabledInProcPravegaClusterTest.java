0fb13db91515c09c3b3210c496f25da1221c2592
Ravi Sharda <ravi.sharda@emc.com>
2019-04-24 22:10:30 +0530

    Issue 3643: Fix reliability issues of standalone mode tests (#3656)
    Improve the standalone mode/in-proc cluster tests so that:
    - They are more likely to catch regression issues, and
    - They do not block builds for too long.
    
    Signed-off-by: Ravi Sharda <ravi.sharda@emc.com>

--- standalone/src/test/java/io/pravega/local/AuthEnabledInProcPravegaClusterTest.java
+++ standalone/src/test/java/io/pravega/local/AuthEnabledInProcPravegaClusterTest.java
@@ -9,37 +9,22 @@
  */
 package io.pravega.local;
 
+import io.grpc.StatusRuntimeException;
 import io.pravega.client.ClientConfig;
-import io.pravega.client.ClientFactory;
-import io.pravega.client.admin.ReaderGroupManager;
 import io.pravega.client.admin.StreamManager;
-
-import io.pravega.client.stream.EventStreamWriter;
-import io.pravega.client.stream.ReinitializationRequiredException;
-import io.pravega.client.stream.ScalingPolicy;
-import io.pravega.client.stream.StreamConfiguration;
-import io.pravega.client.stream.impl.JavaSerializer;
-import io.pravega.client.stream.EventWriterConfig;
-import io.pravega.client.stream.ReaderGroupConfig;
-import io.pravega.client.stream.Stream;
-import io.pravega.client.stream.EventStreamReader;
-import io.pravega.client.stream.ReaderConfig;
 import io.pravega.client.stream.impl.DefaultCredentials;
 import io.pravega.test.common.AssertExtensions;
 import java.net.URI;
-import java.util.UUID;
-
 import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.commons.lang3.exception.ExceptionUtils;
 import org.junit.After;
-import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.assertEquals;
-
 /**
- * Integration tests for auth enabled in-process standalone cluster.
+ * This class contains tests for auth enabled in-process standalone cluster. It inherits the test methods defined
+ * in the parent class.
  */
 @Slf4j
 public class AuthEnabledInProcPravegaClusterTest extends InProcPravegaClusterTest {
@@ -52,96 +37,37 @@ public class AuthEnabledInProcPravegaClusterTest extends InProcPravegaClusterTes
     }
 
     @Override
-    public void createTestStream() {
-        // This test from the base is unnecessary in this class as that and more is already being
-        // tested in another test method.
+    String scopeName() {
+        return "AuthTestScope";
+    }
+
+    @Override
+    String streamName() {
+        return "AuthTestStream";
+    }
+
+    @Override
+    String eventMessage() {
+        return "Test message on the plaintext channel with auth credentials";
+    }
+
+    @Override
+    ClientConfig prepareValidClientConfig() {
+        return ClientConfig.builder()
+                .controllerURI(URI.create(localPravega.getInProcPravegaCluster().getControllerURI()))
+                .credentials(new DefaultCredentials("1111_aaaa", "admin"))
+                .build();
     }
 
     /**
-     * Compares reads and writes to verify that an in-process Pravega cluster runs properly with
-     * with valid auth (authentication and authorization) configuration.
+     * This test verifies that create stream fails when the client config is invalid.
      *
-     * Note:
-     * Strictly speaking, this test is really an "integration test" and is a little time consuming. For now, its
-     * intended to also run as a unit test, but it could be moved to an integration test suite if and when necessary.
-     *
+     * Note: The timeout being used for the test is kept rather large so that there is ample time for the expected
+     * exception to be raised even in case of abnormal delays in test environments.
      */
-    @Test(timeout = 50000)
-    public void testWriteAndReadEventsWhenConfigIsProper() throws ReinitializationRequiredException {
-        String scope = "org.example.auth";
-        String streamName = "stream1";
-        int numSegments = 1;
-
-        ClientConfig clientConfig = ClientConfig.builder()
-                .credentials(new DefaultCredentials("1111_aaaa", "admin"))
-                .controllerURI(URI.create(localPravega.getInProcPravegaCluster().getControllerURI()))
-                .build();
-        log.info("Done creating client config");
-
-        @Cleanup
-        StreamManager streamManager = StreamManager.create(clientConfig);
-        log.info("Created a stream manager");
-
-        streamManager.createScope(scope);
-        log.info("Created a scope [{}]", scope);
-
-        streamManager.createStream(scope, streamName, StreamConfiguration.builder()
-                .scalingPolicy(ScalingPolicy.fixed(numSegments))
-                .build());
-        log.info("Created a stream with name [{}]", streamName);
-
-        @Cleanup
-        ClientFactory clientFactory = ClientFactory.withScope(scope, clientConfig);
-
-        @Cleanup
-        EventStreamWriter<String> writer = clientFactory.createEventWriter(streamName,
-                new JavaSerializer<String>(),
-                EventWriterConfig.builder().build());
-        log.info("Got a writer");
-
-        String writeEvent1 = "This is event 1";
-        writer.writeEvent(writeEvent1);
-        log.info("Done writing event [{}]", writeEvent1);
-
-        String writeEvent2 = "This is event 2";
-        writer.writeEvent(writeEvent2);
-        log.info("Done writing event [{}]", writeEvent2);
-
-        // Now, read the events from the stream.
-
-        String readerGroup = UUID.randomUUID().toString().replace("-", "");
-        ReaderGroupConfig readerGroupConfig = ReaderGroupConfig.builder()
-                .stream(Stream.of(scope, streamName))
-                .disableAutomaticCheckpoints()
-                .build();
-
-        @Cleanup
-        ReaderGroupManager readerGroupManager = ReaderGroupManager.withScope(scope, clientConfig);
-        readerGroupManager.createReaderGroup(readerGroup, readerGroupConfig);
-
-        @Cleanup
-        EventStreamReader<String> reader = clientFactory.createReader(
-                "readerId", readerGroup,
-                new JavaSerializer<String>(), ReaderConfig.builder().build());
-
-        // Keeping the read timeout large so that there is ample time for reading the event even in
-        // case of abnormal delays in test environments.
-        String readEvent1 = reader.readNextEvent(10000).getEvent();
-        log.info("Done reading event [{}]", readEvent1);
-
-        String readEvent2 = reader.readNextEvent(10000).getEvent();
-        log.info("Done reading event [{}]", readEvent2);
-
-        assertEquals(writeEvent1, readEvent1);
-        assertEquals(writeEvent2, readEvent2);
-    }
-
-    @Test
-    public void testCreateStreamFailsWhenCredentialsAreInvalid() {
-        Assert.assertNotNull("Pravega not initialized", localPravega);
-        String scope = "Scope";
-
-        ClientConfig clientConfig = ClientConfig.builder()
+    @Test(timeout = 300000)
+    public void testCreateStreamFailsWithInvalidClientConfig() {
+       ClientConfig clientConfig = ClientConfig.builder()
                 .credentials(new DefaultCredentials("", ""))
                 .controllerURI(URI.create(localPravega.getInProcPravegaCluster().getControllerURI()))
                 .build();
@@ -149,8 +75,19 @@ public class AuthEnabledInProcPravegaClusterTest extends InProcPravegaClusterTes
         @Cleanup
         StreamManager streamManager = StreamManager.create(clientConfig);
 
-        AssertExtensions.assertThrows(RuntimeException.class,
-                () -> streamManager.createScope(scope));
+        AssertExtensions.assertThrows("Auth exception did not occur.",
+                () -> streamManager.createScope(scopeName()),
+                e -> hasAuthExceptionAsRootCause(e));
+    }
+
+    private boolean hasAuthExceptionAsRootCause(Throwable e) {
+        Throwable innermostException = ExceptionUtils.getRootCause(e);
+
+        // Depending on an exception message for determining whether the given exception represents auth failure
+        // is not a good thing to do, but we have no other choice here because auth failures are represented as the
+        // overly general io.grpc.StatusRuntimeException.
+        return innermostException instanceof StatusRuntimeException &&
+             innermostException.getMessage().toUpperCase().contains("UNAUTHENTICATED");
     }
 
     @After
