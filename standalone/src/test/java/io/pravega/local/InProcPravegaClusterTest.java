afcf17f17c5426e0b4bac6bbdde369dd34ccc890
Ravi Sharda <ravi.sharda@emc.com>
2019-04-24 22:10:30 +0530

    Issue 3643: Fix reliability issues of standalone mode tests (#3656)
    Improve the standalone mode/in-proc cluster tests so that:
    - They are more likely to catch regression issues, and
    - They do not block builds for too long.
    
    Signed-off-by: Ravi Sharda <ravi.sharda@emc.com>

--- standalone/src/test/java/io/pravega/local/InProcPravegaClusterTest.java
+++ standalone/src/test/java/io/pravega/local/InProcPravegaClusterTest.java
@@ -11,16 +11,24 @@ package io.pravega.local;
 
 import io.pravega.client.ClientConfig;
 import io.pravega.client.EventStreamClientFactory;
+import io.pravega.client.admin.ReaderGroupManager;
 import io.pravega.client.admin.StreamManager;
+import io.pravega.client.stream.EventStreamReader;
 import io.pravega.client.stream.EventStreamWriter;
 import io.pravega.client.stream.EventWriterConfig;
+import io.pravega.client.stream.ReaderConfig;
+import io.pravega.client.stream.ReaderGroupConfig;
+import io.pravega.client.stream.ReinitializationRequiredException;
 import io.pravega.client.stream.ScalingPolicy;
+import io.pravega.client.stream.Stream;
 import io.pravega.client.stream.StreamConfiguration;
-import io.pravega.client.stream.impl.DefaultCredentials;
 import io.pravega.client.stream.impl.JavaSerializer;
 import io.pravega.segmentstore.server.store.ServiceBuilderConfig;
 import io.pravega.test.common.TestUtils;
 import java.net.URI;
+import java.util.UUID;
+import java.util.concurrent.ExecutionException;
+
 import lombok.Cleanup;
 import lombok.extern.slf4j.Slf4j;
 import org.junit.After;
@@ -28,11 +36,18 @@ import org.junit.Assert;
 import org.junit.Before;
 import org.junit.Test;
 
+import static io.pravega.local.LocalPravegaEmulator.LocalPravegaEmulatorBuilder;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 /**
- * Unit tests for standalone
+ * This class contains tests for in-process standalone cluster. It also configures and runs standalone mode cluster
+ * with appropriate configuration for itself as well as for sub-classes.
  */
 @Slf4j
 public class InProcPravegaClusterTest {
+
     boolean restEnabled = true;
     boolean authEnabled = false;
     boolean tlsEnabled = false;
@@ -46,65 +61,128 @@ public class InProcPravegaClusterTest {
                 .build();
         SingleNodeConfig conf = config.getConfig(SingleNodeConfig::builder);
 
-        localPravega = LocalPravegaEmulator.builder()
+        LocalPravegaEmulatorBuilder emulatorBuilder = LocalPravegaEmulator.builder()
                 .controllerPort(TestUtils.getAvailableListenPort())
                 .segmentStorePort(TestUtils.getAvailableListenPort())
                 .zkPort(TestUtils.getAvailableListenPort())
                 .restServerPort(TestUtils.getAvailableListenPort())
                 .enableRestServer(restEnabled)
                 .enableAuth(authEnabled)
-                .enableTls(tlsEnabled)
-                .certFile("../config/cert.pem")
-                .keyFile("../config/key.pem")
-                .jksKeyFile("../config/standalone.keystore.jks")
-                .jksTrustFile("../config/standalone.truststore.jks")
-                .keyPasswordFile("../config/standalone.keystore.jks.passwd")
-                .passwdFile("../config/passwd")
-                .userName("admin")
-                .passwd("1111_aaaa")
-                .build();
+                .enableTls(tlsEnabled);
 
+        // Since the server is being built right here, avoiding delegating these conditions to subclasses via factory
+        // methods. This is so that it is easy to see the difference in server configs all in one place. This is also
+        // unlike the ClientConfig preparation which is being delegated to factory methods to make their preparation
+        // explicit in the respective test classes.
+
+        if (authEnabled) {
+            emulatorBuilder.passwdFile("../config/passwd")
+                    .userName("admin")
+                    .passwd("1111_aaaa");
+        }
+        if (tlsEnabled) {
+            emulatorBuilder.certFile("../config/cert.pem")
+                    .keyFile("../config/key.pem")
+                    .jksKeyFile("../config/standalone.keystore.jks")
+                    .jksTrustFile("../config/standalone.truststore.jks")
+                    .keyPasswordFile("../config/standalone.keystore.jks.passwd");
+        }
+
+        localPravega = emulatorBuilder.build();
         localPravega.start();
     }
 
-    /**
-     * Create the test stream.
-     *
-     * @throws Exception on any errors.
-     */
-    @Test
-    public void createTestStream()
-            throws Exception {
-        Assert.assertNotNull("Pravega not initialized", localPravega);
-        String scope = "Scope";
-        String streamName = "Stream";
-        int numSegments = 10;
+    //region Factory methods. These should be overridden in subclasses.
 
-        ClientConfig clientConfig = ClientConfig.builder()
-                                                .controllerURI(URI.create(localPravega.getInProcPravegaCluster().getControllerURI()))
-                                                .credentials(new DefaultCredentials("1111_aaaa", "admin"))
-                                                .trustStore("../config/cert.pem")
-                                                .validateHostName(false)
-                                                .build();
+    String scopeName() {
+        return "TestScope";
+    }
+
+    String streamName() {
+        return "TestStream";
+    }
+
+    String eventMessage() {
+        return "Test message on the plaintext channel";
+    }
+
+    ClientConfig prepareValidClientConfig() {
+        return ClientConfig.builder()
+                .controllerURI(URI.create(localPravega.getInProcPravegaCluster().getControllerURI()))
+                .build();
+    }
+
+    //endregion
+
+    /**
+     * Compares reads and writes to verify that an in-process Pravega cluster responds properly with
+     * with valid client configuration.
+     *
+     * Note:
+     * Strictly speaking, this test is really an "integration test" and is a little time consuming. For now, its
+     * intended to also run as a unit test, but it could be moved to an integration test suite if and when necessary.
+     *
+     */
+    @Test(timeout = 50000)
+    public void testWriteAndReadEventWithValidClientConfig() throws ExecutionException,
+            InterruptedException, ReinitializationRequiredException {
+
+        String scope = scopeName();
+        String streamName = streamName();
+        int numSegments = 1;
+        String message = eventMessage();
+
+        ClientConfig clientConfig = prepareValidClientConfig();
+
         @Cleanup
         StreamManager streamManager = StreamManager.create(clientConfig);
+        assertNotNull(streamManager);
 
-        streamManager.createScope(scope);
-        Assert.assertTrue("Stream creation is not successful ",
-                streamManager.createStream(scope, streamName, StreamConfiguration.builder()
-                                   .scalingPolicy(ScalingPolicy.fixed(numSegments))
-                                   .build()));
-        log.info("Created stream: " + streamName);
+        boolean isScopeCreated = streamManager.createScope(scope);
+        assertTrue("Failed to create scope", isScopeCreated);
 
+        boolean isStreamCreated = streamManager.createStream(scope, streamName, StreamConfiguration.builder()
+                .scalingPolicy(ScalingPolicy.fixed(numSegments))
+                .build());
+        Assert.assertTrue("Failed to create the stream ", isStreamCreated);
+
+        @Cleanup
         EventStreamClientFactory clientFactory = EventStreamClientFactory.withScope(scope, clientConfig);
+
+        // Write an event to the stream.
+
+        @Cleanup
         EventStreamWriter<String> writer = clientFactory.createEventWriter(streamName,
                 new JavaSerializer<String>(),
                 EventWriterConfig.builder().build());
-        log.info("Created writer for stream: " + streamName);
+        writer.writeEvent(message).get();
+        log.debug("Done writing message '{}' to stream '{} / {}'", message, scope, streamName);
 
-        writer.writeEvent("hello").get();
-        log.info("Wrote data to the stream");
+        // Now, read the event from the stream.
+
+        String readerGroup = UUID.randomUUID().toString().replace("-", "");
+        ReaderGroupConfig readerGroupConfig = ReaderGroupConfig.builder()
+                .stream(Stream.of(scope, streamName))
+                .disableAutomaticCheckpoints()
+                .build();
+
+        @Cleanup
+        ReaderGroupManager readerGroupManager = ReaderGroupManager.withScope(scope, clientConfig);
+        readerGroupManager.createReaderGroup(readerGroup, readerGroupConfig);
+
+        @Cleanup
+        EventStreamReader<String> reader = clientFactory.createReader(
+                "readerId", readerGroup,
+                new JavaSerializer<String>(), ReaderConfig.builder().build());
+
+        // Keeping the read timeout large so that there is ample time for reading the event even in
+        // case of abnormal delays in test environments.
+        String readMessage = reader.readNextEvent(10000).getEvent();
+        log.info("Done reading event [{}]", readMessage);
+
+        assertEquals(message, readMessage);
     }
+
 
     @After
     public void tearDown() throws Exception {
