48e92be74269d5df9cc007a5fc1d0726a35bfee9
Raúl Gracia <raul.gracia@emc.com>
2019-05-02 16:34:25 +0200

    Issue 3477: Fixes on EventProcessors during Controller graceful restart (#3481)
    * Allows EventProcessorCell i) to close its readers at
    the position of the last processed event, and ii) to
    handle HostStoreException derived from a ZK session
    expiration.
    
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- client/src/main/java/io/pravega/client/stream/EventStreamReader.java
+++ client/src/main/java/io/pravega/client/stream/EventStreamReader.java
@@ -71,4 +71,13 @@ public interface EventStreamReader<T> extends AutoCloseable {
      */
     @Override
     void close();
+
+    /**
+     * Close the reader at a specific position. No further actions may be performed. If this reader is part of
+     * a {@link ReaderGroup}, this will automatically invoke {@link ReaderGroup#readerOffline(String, Position)} with
+     * the supplied position.
+     *
+     * @param position {@link Position} to use while reporting readerOffline on the {@link ReaderGroup}.
+     */
+    void closeAt(Position position);
 }
