647f7fc9e50d751e81d82807c126927cc55b4f8f
Keshava Munegowda <keshava.munegowda@dell.com>
2019-06-03 21:54:04 +0530

    Fix the checkstyle errors
    Signed-off-by: Keshava Munegowda <keshava.munegowda@dell.com>

--- shared/protocol/src/test/java/io/pravega/shared/protocol/netty/AppendEncodeDecodeTest.java
+++ shared/protocol/src/test/java/io/pravega/shared/protocol/netty/AppendEncodeDecodeTest.java
@@ -25,7 +25,6 @@ import java.util.Arrays;
 import java.util.List;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.function.Function;
 
 import lombok.Cleanup;
 import lombok.RequiredArgsConstructor;
