b5ab3d4226197a309846469990495c90c9df3819
Ravi Sharda <ravi.sharda@emc.com>
2019-06-07 04:07:20 -0700

    Update comments.
    Signed-off-by: Ravi Sharda <ravi.sharda@emc.com>

--- segmentstore/server/host/src/main/java/io/pravega/segmentstore/server/host/delegationtoken/DelegationTokenVerifier.java
+++ segmentstore/server/host/src/main/java/io/pravega/segmentstore/server/host/delegationtoken/DelegationTokenVerifier.java
@@ -19,15 +19,32 @@ import io.pravega.auth.TokenExpiredException;
  * This interface represents the code on segment store side that verifies the delegation token.
  */
 public interface DelegationTokenVerifier {
+
     /**
-     * Verifies whether the token represents access to a given resource to the level expected.
-     * @param resource       The resource for which access is to be verified.
-     * @param token          The delegation token.
-     * @param expectedLevel  Maximum expected access to the given resoure after this verification.
-     * @return               true if the delegation token allows access to the resource for the expected level.
+     * Determines whether the given {@code token} is valid for the specified {@code expectedLevel} of access on the
+     * given {@code resource}.
+     *
+     * @param resource       the resource for which access is desired.
+     * @param token          the access/delegation token.
+     * @param expectedLevel  maximum expected access to the given {@code resource}.
+     * @return               {@code true} if token is valid for accessing the resource, otherwise {@code false}.
      */
     boolean isTokenValid(String resource, String token, AuthHandler.Permissions expectedLevel);
 
+
+    /**
+     * Ensures that the given {@code token} represents specified {@code expectedLevel} of access on the
+     * given {@code resource}. It returns normally if the token grants the access, and throws exceptions otherwise.
+     *
+     * @param resource                the resource for which access is desired.
+     * @param token                   the access/delegation token.
+     * @param expectedLevel           maximum expected access to the given {@code resource}.
+     * @throws TokenExpiredException  if {@code token} has expired
+     * @throws InvalidTokenException  if {@code token} is invalid
+     * @throws InvalidClaimException  if {@code token} does not contain the claim representing
+     *                                {@code expectedLevel} of access
+     * @throws TokenException         in any other failure scenarios.
+     */
     void verifyToken(String resource, String token, AuthHandler.Permissions expectedLevel)
             throws TokenExpiredException, InvalidTokenException, InvalidClaimException, TokenException;
 }
