20c8f6cab23f706e20d24efc7a0ab724a4fb2505
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2019-06-06 23:11:15 -0700

    checkstyle
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- controller/src/test/java/io/pravega/controller/fault/ControllerClusterListenerTest.java
+++ controller/src/test/java/io/pravega/controller/fault/ControllerClusterListenerTest.java
@@ -62,7 +62,6 @@ import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
-import static org.mockito.Mockito.when;
 
 /**
  * ControllerClusterListener tests.
