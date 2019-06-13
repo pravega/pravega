44dec7c0d95b2ecea8b4ceb4959a091160aab1c1
Sandeep <sandeep.shridhar@emc.com>
2019-06-13 21:51:49 +0530

    Issue 3425: Ensure the Sealed segment writer is removed after all inflight events are resent to the newer segment writers. (#3901)
    * Ensures SegmentOutputStream#flush of a sealed segment throws a SegmentSealedException in the case of a zero inflight events.
    
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>
    Signed-off-by: Flavio Junqueira (fpj) <flavio.junqueira@emc.com>

--- client/src/test/java/io/pravega/client/stream/impl/SegmentSelectorTest.java
+++ client/src/test/java/io/pravega/client/stream/impl/SegmentSelectorTest.java
@@ -22,6 +22,9 @@ import io.pravega.common.util.RetriesExhaustedException;
 import java.nio.ByteBuffer;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 import java.util.TreeMap;
 import java.util.concurrent.CompletableFuture;
 import java.util.function.Consumer;
@@ -31,7 +34,9 @@ import org.junit.Test;
 import org.mockito.ArgumentMatchers;
 import org.mockito.Mockito;
 
+import static io.pravega.shared.segment.StreamSegmentNameUtils.computeSegmentId;
 import static io.pravega.test.common.AssertExtensions.assertFutureThrows;
+import static java.util.Collections.singletonList;
 import static org.junit.Assert.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
@@ -72,7 +77,7 @@ public class SegmentSelectorTest {
         }
     }
 
-    private void addNewSegment(TreeMap<Double, SegmentWithRange> segments, int number, double low, double high) {
+    private void addNewSegment(TreeMap<Double, SegmentWithRange> segments, long number, double low, double high) {
         segments.put(high, new SegmentWithRange(new Segment(scope, streamName, number), low, high));
     }
 
@@ -215,6 +220,68 @@ public class SegmentSelectorTest {
 
         assertEquals(Collections.emptyList(), selector.refreshSegmentEventWritersUponSealed(segment0, segmentSealedCallback));
         assertFutureThrows("Writer Future", writerFuture, t -> t instanceof ControllerFailureException);
+    }
+
+    @Test
+    public void testSegmentRefreshOnSealed() {
+        final Segment segment0 = new Segment(scope, streamName, 0);
+        final Segment segment1 = new Segment(scope, streamName, computeSegmentId(1, 1));
+        final Segment segment2 = new Segment(scope, streamName, computeSegmentId(2, 1));
+        final CompletableFuture<Void> writerFuture = new CompletableFuture<>();
+        final PendingEvent pendingEvent = PendingEvent.withHeader("0", ByteBuffer.wrap("e".getBytes()), writerFuture);
+
+        TreeMap<Double, SegmentWithRange> segmentBeforeScale = new TreeMap<>();
+        addNewSegment(segmentBeforeScale, 0, 0.0, 1.0);
+        StreamSegments streamSegmentsBeforeScale = new StreamSegments(segmentBeforeScale, "");
+
+        Map<SegmentWithRange, List<Long>> newRange = new HashMap<>();
+        newRange.put(new SegmentWithRange(segment1, 0.0, 0.5), ImmutableList.of(segment0.getSegmentId()));
+        newRange.put(new SegmentWithRange(segment2, 0.5, 1.0), ImmutableList.of(segment0.getSegmentId()));
+        StreamSegmentsWithPredecessors segmentsWithPredecessors = new StreamSegmentsWithPredecessors(newRange, "");
+
+        // Setup Mock.
+        SegmentOutputStream s0Writer = Mockito.mock(SegmentOutputStream.class);
+        SegmentOutputStream s1Writer = Mockito.mock(SegmentOutputStream.class);
+        SegmentOutputStream s2Writer = Mockito.mock(SegmentOutputStream.class);
+        SegmentOutputStreamFactory factory = Mockito.mock(SegmentOutputStreamFactory.class);
+        Controller controller = Mockito.mock(Controller.class);
+
+        when(s0Writer.getUnackedEventsOnSeal()).thenReturn(ImmutableList.of(pendingEvent));
+        when(factory.createOutputStreamForSegment(eq(segment0), ArgumentMatchers.<Consumer<Segment>>any(), any(EventWriterConfig.class), anyString()))
+                .thenReturn(s0Writer);
+        when(factory.createOutputStreamForSegment(eq(segment1), ArgumentMatchers.<Consumer<Segment>>any(), any(EventWriterConfig.class), anyString()))
+                .thenReturn(s1Writer);
+        when(factory.createOutputStreamForSegment(eq(segment2), ArgumentMatchers.<Consumer<Segment>>any(), any(EventWriterConfig.class), anyString()))
+                .thenReturn(s2Writer);
+        // get current segments returns segment 0
+        when(controller.getCurrentSegments(scope, streamName))
+                .thenReturn(CompletableFuture.completedFuture(streamSegmentsBeforeScale));
+        when(controller.getSuccessors(segment0))
+                .thenAnswer(i -> {
+                    CompletableFuture<StreamSegmentsWithPredecessors> result = new CompletableFuture<>();
+                    result.complete(segmentsWithPredecessors);
+                    return result;
+                });
+
+        SegmentSelector selector = new SegmentSelector(new StreamImpl(scope, streamName), controller, factory, config);
+        //trigger refresh
+        selector.refreshSegmentEventWriters(segmentSealedCallback);
+        // only segment 0 writer is present.
+        assertEquals(singletonList(s0Writer), selector.getWriters());
+
+        // trigger a referesh of writers due to segment 0 being sealed.
+        List<PendingEvent> pendingEvents = selector.refreshSegmentEventWritersUponSealed(segment0, segmentSealedCallback);
+        // one pending event is returned by segment0
+        assertEquals(singletonList(pendingEvent), pendingEvents);
+        // the current number of writers is 3, it includes the writer to segment 0.
+        List<SegmentOutputStream> writers = selector.getWriters();
+        assertEquals(3, writers.size());
+        assertTrue(writers.contains(s0Writer));
+        assertTrue(writers.contains(s1Writer));
+        assertTrue(writers.contains(s2Writer));
+        // remove segment 0, this is done post resending the pending events.
+        selector.removeSegmentWriter(segment0);
+        assertFalse(selector.getWriters().contains(s0Writer));
     }
 
 }
