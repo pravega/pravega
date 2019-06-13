ba345f981b978ccc6940ba2bd6f3eac24ec6b6c5
Ravi Sharda <ravi.sharda@emc.com>
2019-06-13 02:25:55 -0700

    Add an integration test to verify client receives error when token is expired.
    Signed-off-by: Ravi Sharda <ravi.sharda@emc.com>

--- test/integration/src/test/java/io/pravega/test/integration/DelegationTokenTest.java
+++ test/integration/src/test/java/io/pravega/test/integration/DelegationTokenTest.java
@@ -9,6 +9,8 @@
  */
 package io.pravega.test.integration;
 
+import io.pravega.auth.TokenException;
+import io.pravega.auth.TokenExpiredException;
 import io.pravega.client.ClientConfig;
 import io.pravega.client.EventStreamClientFactory;
 import io.pravega.client.admin.StreamManager;
@@ -26,6 +28,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.junit.Assert;
 import org.junit.Test;
 
+import static io.pravega.test.common.AssertExtensions.assertThrows;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertTrue;
 
@@ -36,11 +39,28 @@ import static org.junit.Assert.assertTrue;
 @Slf4j
 public class DelegationTokenTest {
 
-    @Test(timeout = 50000)
-    public void testWriteSucceedsWhenTokenIsNotExpired() throws ExecutionException, InterruptedException {
-        // By keeping the ttl high (200 seconds), we are trying to ensure that that the token lives for sufficient time
-        // for this test to complete while the token is still alive.
-        writeAnEvent(200);
+    /*
+     * Note: No normal happy path integration tests were added here, as those scenarios are already covered in
+     * other tests elsewhere which do that anyway (using the default token TTL and valid auth credentials).
+     */
+
+    @Test(timeout = 20000)
+    public void testWriteSucceedsWhenTokenTtlIsMinusOne() throws ExecutionException, InterruptedException {
+        // A Token TTL -1 indicates to the Controller to not set any expiry on the delegation token.
+        writeAnEvent(-1);
+    }
+
+    @Test(timeout = 30000)
+    public void testWriteFailsWhenTokenExpires() {
+        // To ensure the token is certainly expired when it reaches the segment store, we are setting Controller TTL
+        // as 0, so that the token expiry is set as the same time as the time it is issued in the Controller.
+        assertThrows("Token expiration didn't cause write failure.",
+                () -> writeAnEvent(0),
+                e -> isDueToTokenExpiredException(e));
+    }
+
+    private boolean isDueToTokenExpiredException(Throwable e) {
+        return e instanceof TokenException && e.getMessage().contains(TokenExpiredException.class.getName());
     }
 
     private void writeAnEvent(int tokenTtlInSeconds) throws ExecutionException, InterruptedException {
@@ -76,11 +96,14 @@ public class DelegationTokenTest {
             @Cleanup
             EventStreamClientFactory clientFactory = EventStreamClientFactory.withScope(scope, clientConfig);
 
-            @Cleanup
+            //@Cleanup
             EventStreamWriter<String> writer = clientFactory.createEventWriter(streamName,
                     new JavaSerializer<String>(),
                     EventWriterConfig.builder().build());
+
+            // Note: A TokenException is thrown here if token verification fails on the server.
             writer.writeEvent(message).get();
+
             log.debug("Done writing message '{}' to stream '{} / {}'", message, scope, streamName);
         } finally {
             pravegaCluster.close();
