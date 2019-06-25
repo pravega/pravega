8cfb0590293c007b7a8f198ad738dc71d5499543
Keshava Munegowda <keshava.gowda@gmail.com>
2019-06-26 02:40:55 +0530

    Issue 3774: Update the default max connection and Netty channel water marking for performance Improvement (#3959)
    * Updates the default max number of connections per segment store to 10 and the low and high watermarks to 640KB And 1MB, respectively.
    
    Signed-off-by: Keshava Munegowda <keshava.munegowda@dell.com>
    Signed-off-by: Flavio Junqueira (fpj) <flavio.junqueira@emc.com>

--- segmentstore/server/host/src/main/java/io/pravega/segmentstore/server/host/handler/AppendProcessor.java
+++ segmentstore/server/host/src/main/java/io/pravega/segmentstore/server/host/handler/AppendProcessor.java
@@ -72,8 +72,8 @@ public class AppendProcessor extends DelegatingRequestProcessor {
     //region Members
 
     static final Duration TIMEOUT = Duration.ofMinutes(1);
-    private static final int HIGH_WATER_MARK = 128 * 1024;
-    private static final int LOW_WATER_MARK = 64 * 1024;
+    private static final int HIGH_WATER_MARK = 1024 * 1024; // 1MB
+    private static final int LOW_WATER_MARK = 640 * 1024;  // 640KB
     private static final String EMPTY_STACK_TRACE = "";
     private final StreamSegmentStore store;
     private final ServerConnection connection;
