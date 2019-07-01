fb77ac67b0dc69701ae3e8b96c8c015a88ad9257
shivesh ranjan <13659022+shiveshr@users.noreply.github.com>
2019-07-02 01:38:52 +0530

    Issue 3442: Ensure znode store/streamsInScope is ignored when listing scopes for Zk based stream store (#3988)
    * Changes znode store/streamsInScope to store/_streamsInScope
    
    * Filters streamsInScope when listing scopes
    
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>
    Signed-off-by: Flavio Junqueira (fpj) <fpj@apache.org>

--- controller/src/main/java/io/pravega/controller/store/stream/ZKScope.java
+++ controller/src/main/java/io/pravega/controller/store/stream/ZKScope.java
@@ -39,9 +39,9 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class ZKScope implements Scope {
-
+    static final String STREAMS_IN_SCOPE = "_streamsinscope";
     private static final String SCOPE_PATH = "/store/%s";
-    private static final String STREAMS_IN_SCOPE_ROOT_PATH = "/store/streamsinscope/%s";
+    private static final String STREAMS_IN_SCOPE_ROOT_PATH = "/store/" + STREAMS_IN_SCOPE + "/%s";
     private static final String STREAMS_IN_SCOPE_ROOT_PATH_FORMAT = STREAMS_IN_SCOPE_ROOT_PATH + "/streams";
     private static final String COUNTER_PATH = STREAMS_IN_SCOPE_ROOT_PATH + "/counter";
     private static final Predicate<Throwable> DATA_NOT_FOUND_PREDICATE = e -> Exceptions.unwrap(e) instanceof StoreException.DataNotFoundException;
