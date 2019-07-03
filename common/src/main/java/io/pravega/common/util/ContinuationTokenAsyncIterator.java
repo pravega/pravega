9ee147d074489e13f2f767cb789cc1382ef60ee4
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2019-07-03 02:22:08 -0700

    checkstyle
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- common/src/main/java/io/pravega/common/util/ContinuationTokenAsyncIterator.java
+++ common/src/main/java/io/pravega/common/util/ContinuationTokenAsyncIterator.java
@@ -19,7 +19,6 @@ import java.util.Collection;
 import java.util.Map;
 import java.util.Queue;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.function.Function;
