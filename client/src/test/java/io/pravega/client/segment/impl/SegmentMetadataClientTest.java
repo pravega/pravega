7a3ba35cbf1c4dfa24eeed2f1e3effeedc947a89
Tom Kaitchuck <tom.kaitchuck@emc.com>
2019-07-09 15:22:38 -0700

    Disable connection pooling in tests
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- client/src/test/java/io/pravega/client/segment/impl/SegmentMetadataClientTest.java
+++ client/src/test/java/io/pravega/client/segment/impl/SegmentMetadataClientTest.java
@@ -11,7 +11,6 @@ package io.pravega.client.segment.impl;
 
 import io.pravega.client.netty.impl.ClientConnection;
 import io.pravega.client.netty.impl.ConnectionFactory;
-import io.pravega.client.netty.impl.Flow;
 import io.pravega.client.stream.mock.MockConnectionFactoryImpl;
 import io.pravega.client.stream.mock.MockController;
 import io.pravega.common.concurrent.Futures;
@@ -244,13 +243,13 @@ public class SegmentMetadataClientTest {
         ClientConnection connection1 = mock(ClientConnection.class);
         ClientConnection connection2 = mock(ClientConnection.class);
         AtomicReference<ReplyProcessor> processor = new AtomicReference<>();
-        Mockito.when(cf.establishConnection(Mockito.any(Flow.class), Mockito.eq(endpoint), Mockito.any()))
+        Mockito.when(cf.establishConnection(Mockito.eq(endpoint), Mockito.any()))
                .thenReturn(Futures.failedFuture(new ConnectionFailedException()))
                .thenReturn(CompletableFuture.completedFuture(connection1))
                .thenAnswer(new Answer<CompletableFuture<ClientConnection>>() {
                    @Override
                    public CompletableFuture<ClientConnection> answer(InvocationOnMock invocation) throws Throwable {
-                       processor.set(invocation.getArgument(2));
+                       processor.set(invocation.getArgument(1));
                        return CompletableFuture.completedFuture(connection2);
                    }
                });
@@ -284,11 +283,11 @@ public class SegmentMetadataClientTest {
         SegmentMetadataClientImpl client = new SegmentMetadataClientImpl(segment, controller, cf, "");
         InOrder order = Mockito.inOrder(connection1, connection2, cf);
         long length = client.fetchCurrentSegmentLength();
-        order.verify(cf, Mockito.times(2)).establishConnection(Mockito.any(Flow.class), Mockito.eq(endpoint), Mockito.any());
+        order.verify(cf, Mockito.times(2)).establishConnection(Mockito.eq(endpoint), Mockito.any());
         order.verify(connection1).sendAsync(Mockito.eq(new WireCommands.GetStreamSegmentInfo(requestIds.get(0), segment.getScopedName(), "")),
                                             Mockito.any(ClientConnection.CompletedCallback.class));
         order.verify(connection1).close();
-        order.verify(cf).establishConnection(Mockito.any(Flow.class), Mockito.eq(endpoint), Mockito.any());
+        order.verify(cf).establishConnection(Mockito.eq(endpoint), Mockito.any());
         order.verify(connection2).sendAsync(Mockito.eq(new WireCommands.GetStreamSegmentInfo(requestIds.get(1), segment.getScopedName(), "")),
                                             Mockito.any(ClientConnection.CompletedCallback.class));
         order.verifyNoMoreInteractions();
