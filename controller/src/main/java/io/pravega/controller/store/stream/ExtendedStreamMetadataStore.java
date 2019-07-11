89fc4aaf5c041888b82beaf9b7c2c6e860ec8cd4
anirudhkovuru <anirudhkovuru@gmail.com>
2019-07-11 01:49:42 -0700

    added docstrings for ExtendedStreamMetadataStore and for StreamStoreFactory

--- controller/src/main/java/io/pravega/controller/store/stream/ExtendedStreamMetadataStore.java
+++ controller/src/main/java/io/pravega/controller/store/stream/ExtendedStreamMetadataStore.java
@@ -15,14 +15,54 @@ import io.pravega.controller.store.stream.records.HistoryTimeSeriesRecord;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 
+/**
+ * Extended Stream Metadata.
+ */
 public interface ExtendedStreamMetadataStore extends StreamMetadataStore {
-    CompletableFuture<HistoryTimeSeriesRecord> getHistoryTimeSeriesRecord(final String scope, final String streamName,
-                                                                          final int epoch, final OperationContext context,
+
+    /**
+     * Method to get the HistoryTimeSeriesRecord, given the epoch to which it corresponds to.
+     *
+     * @param scope       stream scope.
+     * @param streamName  stream name.
+     * @param epoch       epoch.
+     * @param context     operation context.
+     * @param executor    callers executor.
+     * @return Completable future that, upon completion, holds the history record corresponding to request epoch.
+     */
+    CompletableFuture<HistoryTimeSeriesRecord> getHistoryTimeSeriesRecord(final String scope,
+                                                                          final String streamName,
+                                                                          final int epoch,
+                                                                          final OperationContext context,
                                                                           final Executor executor);
 
-    CompletableFuture<Boolean> checkSegmentSealed(final String scope, final String streamName, final long segmentId,
-                                                  final OperationContext context, final Executor executor);
+    /**
+     * Method to check if the given segment, whose ID is given, is sealed or not.
+     *
+     * @param scope       stream scope.
+     * @param streamName  stream name.
+     * @param segmentId   segment ID.
+     * @param context     operation context.
+     * @param executor    callers executor.
+     * @return Completable future that, upon completion, holds the boolean representing whether the segment is sealed or not.
+     */
+    CompletableFuture<Boolean> checkSegmentSealed(final String scope,
+                                                  final String streamName,
+                                                  final long segmentId,
+                                                  final OperationContext context,
+                                                  final Executor executor);
 
-    CompletableFuture<HistoryTimeSeries> getHistoryTimeSeriesChunkRecent(final String scope, final String streamName,
-                                                                         final OperationContext context, final Executor executor);
+    /**
+     * Method to get the most recent chunk of the HistoryTimeSeries.
+     *
+     * @param scope       stream scope.
+     * @param streamName  stream name.
+     * @param context     operation context.
+     * @param executor    callers executor.
+     * @return Completable future that, upon completion, holds the most recent HistoryTimeSeries chunk.
+     */
+    CompletableFuture<HistoryTimeSeries> getHistoryTimeSeriesChunkRecent(final String scope,
+                                                                         final String streamName,
+                                                                         final OperationContext context,
+                                                                         final Executor executor);
 }
--- controller/src/main/java/io/pravega/controller/store/stream/StreamStoreFactoryExtended.java
+++ controller/src/main/java/io/pravega/controller/store/stream/StreamStoreFactoryExtended.java
@@ -9,6 +9,7 @@
  */
 package io.pravega.controller.store.stream;
 
+import com.google.common.annotations.VisibleForTesting;
 import io.pravega.controller.server.SegmentHelper;
 import io.pravega.controller.server.rpc.auth.AuthHelper;
 import io.pravega.controller.store.client.StoreClient;
@@ -20,7 +21,7 @@ import java.util.concurrent.ScheduledExecutorService;
 
 public class StreamStoreFactoryExtended extends StreamStoreFactory {
     public static ExtendedStreamMetadataStore createStore(final StoreClient storeClient, final SegmentHelper segmentHelper,
-                                                                  final AuthHelper authHelper, final ScheduledExecutorService executor) {
+                                                          final AuthHelper authHelper, final ScheduledExecutorService executor) {
         switch (storeClient.getType()) {
             case InMemory:
                 return new InMemoryStreamMetadataStore(executor);
@@ -33,15 +34,18 @@ public class StreamStoreFactoryExtended extends StreamStoreFactory {
         }
     }
 
+    @VisibleForTesting
     public static ExtendedStreamMetadataStore createPravegaTablesStore(final SegmentHelper segmentHelper, final AuthHelper authHelper,
-                                                               final CuratorFramework client, final ScheduledExecutorService executor) {
+                                                                       final CuratorFramework client, final ScheduledExecutorService executor) {
         return new PravegaTablesStreamMetadataStore(segmentHelper, client, executor, authHelper);
     }
 
+    @VisibleForTesting
     public static ExtendedStreamMetadataStore createZKStore(final CuratorFramework client, final ScheduledExecutorService executor) {
         return new ZKStreamMetadataStore(client, executor);
     }
 
+    @VisibleForTesting
     public static ExtendedStreamMetadataStore createInMemoryStore(final Executor executor) {
         return new InMemoryStreamMetadataStore(executor);
     }
