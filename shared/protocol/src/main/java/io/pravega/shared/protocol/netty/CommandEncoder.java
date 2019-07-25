3028fd63866851e9d78d5ba943aa6e9c240bd935
Keshava Munegowda <keshava.munegowda@dell.com>
2019-07-25 18:11:50 +0530

    Covering Tom's Review comments.
    Signed-off-by: Keshava Munegowda <keshava.munegowda@dell.com>

--- shared/protocol/src/main/java/io/pravega/shared/protocol/netty/CommandEncoder.java
+++ shared/protocol/src/main/java/io/pravega/shared/protocol/netty/CommandEncoder.java
@@ -24,7 +24,6 @@ import java.util.AbstractMap.SimpleImmutableEntry;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
-import java.util.List;
 import java.util.ArrayList;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicLong;
@@ -48,8 +47,9 @@ import static io.pravega.shared.protocol.netty.WireCommands.TYPE_SIZE;
  * Data - Which is obtained by calling the serializer for the specific wire command.
  *
  * Most commands are that simple. For performance Appends however are handled differently.
- * Appends are written in blocks so that the server does not need to decode the contents of the
- * block.
+ *
+ * If the channel is free, then the Appends are written in blocks so that the server does
+ * not need to decode the contents of the block.
  * The block identifies which stream is appending so that each event does not have to and each each
  * event does not
  * have to be parsed individually. Events inside the block are encoded normally (with their Type and
@@ -60,6 +60,12 @@ import static io.pravega.shared.protocol.netty.WireCommands.TYPE_SIZE;
  * The AppendBlockEnd contains metadata about the block that was just appended so that it does not
  * need to be parsed out of individual messages. Notably this includes the event number of the last
  * event in the block, so that it can be acknowledged.
+ *
+ * if the channel is not free, then each append is enqueued to the session buffer.
+ * This session buffer gets flushed under following conditions
+ *      if the session buffer exceed the threshold (MAX_BLOCK_SIZE)
+ *      if the multiple sessions are present , then total session buffer exceed the threshold (MAX_DATA_SIZE)
+ *      if the total number of events exceeds the threshold (MAX_EVENTS)
  *
  */
 @NotThreadSafe
@@ -75,8 +81,9 @@ public class CommandEncoder extends MessageToByteEncoder<Object> {
     private UUID writerIdPerformingAppends;
     private int currentBlockSize;
     private int bytesLeftInBlock;
-    private final List<Session> pendingWrites = new ArrayList<>();
-    private long totalBytes = 0;
+    private final Map<UUID, Session> pendingWrites = new HashMap<>();
+    private long pendingBytes = 0;
+
 
     @RequiredArgsConstructor
     private final class Session {
@@ -84,48 +91,100 @@ public class CommandEncoder extends MessageToByteEncoder<Object> {
         private static final int MAX_EVENTS = 500;
         private final UUID id;
         private final long requestId;
-        private final ByteBuf data = buffer();
+        private final ByteBuf buffer = buffer();
         private long lastEventNumber = -1L;
         private int eventCount = 0;
 
-        private void recordAppend(Append append) {
+        /**
+         * Record the given Append to Session by tracking the last event number and number of events to write.
+         */
+        private void record(Append append) {
             lastEventNumber = append.getEventNumber();
             eventCount += append.getEventCount();
         }
 
-        private void append(ByteBuf buffer, ByteBuf out) {
-            if (buffer != null && buffer.readableBytes() > 0) {
-                totalBytes += buffer.readableBytes();
-                if (data.readableBytes() == 0) {
-                    pendingWrites.add(this);
-                }
-                data.writeBytes(buffer);
-                if (totalBytes > MAX_DATA_SIZE) {
+        /**
+         * Check if the session is empty or not.
+         *
+         * @return true if there are no pending Events to write; false otherwise.
+         */
+        private boolean isEmpty() {
+            return eventCount == 0;
+        }
+
+        /**
+         * Write the Append data to Session' buffer.
+         * session will be enqueued for the future writing to network channel.
+         * data will be appended to sessions buffer.
+         * if the session buffer size exceeds  MAX_BLOCK_SIZE then
+         * events are flushed to network channel buffer.
+         * if the total pending data size exceeds MAX_DATA_SIZE then
+         * all pending events are flushed to network channel buffer.
+         * if the pending events of session exceeds the MAX_EVENTS then
+         * all session's events are flushed to network channel buffer.
+         *
+         * @param data  data bytes.
+         * @param out   Network channel buffer.
+         */
+        private void write(ByteBuf data, ByteBuf out) {
+            pendingWrites.putIfAbsent(id, this);
+            if (data.readableBytes() > 0) {
+                pendingBytes += data.readableBytes();
+                buffer.writeBytes(data);
+                if (pendingBytes > MAX_DATA_SIZE) {
                     breakCurrentAppend(out);
                     flushAll(out);
-                } else if ((data.readableBytes() > MAX_BLOCK_SIZE) ||
-                        ((data.readableBytes() > 0) && (eventCount > MAX_EVENTS))) {
+                } else if (buffer.readableBytes() > MAX_BLOCK_SIZE) {
                     breakCurrentAppend(out);
                     flush(out);
                 }
             }
+            if (eventCount > MAX_EVENTS) {
+                breakCurrentAppend(out);
+                flush(out);
+            }
         }
 
-        private void flush(ByteBuf out) {
-            if (data.readableBytes() > 0) {
-                totalBytes -= data.readableBytes();
-                writeMessage(new AppendBlockEnd(id, 0, data, eventCount, lastEventNumber, requestId), out);
-                data.clear();
+       /**
+        * Write/flush session's buffered data to network channel.
+        * by enclosing the session buffer data into Append Block End.
+        *
+        * @param out   Network channel buffer.
+        */
+       private void flush(ByteBuf out) {
+            if (!isEmpty()) {
+                pendingBytes -= buffer.readableBytes();
+                writeMessage(new AppendBlockEnd(id, 0, buffer, eventCount, lastEventNumber, requestId), out);
+                pendingWrites.remove(id);
+                buffer.clear();
                 eventCount = 0;
             }
         }
+
+        /**
+         * Write/flush given data to network channel.
+         * by writing the Append block End containing the given/input data and indicating the size of previous
+         * append block data.
+         *
+         * @param out   Network channel buffer.
+         */
+        private void flush(int sizOfWholeEvents, ByteBuf data, ByteBuf out) {
+            writeMessage(new AppendBlockEnd(id, sizOfWholeEvents, data, eventCount, lastEventNumber, requestId), out);
+            eventCount = 0;
+        }
     }
 
+    /**
+     * Write/flush Multi session's buffered data to network channel buffer
+     * by iterating over all the pending sessions .
+     *
+     * @param out   Network Channel Buffer.
+     */
     private void flushAll(ByteBuf out) {
-        if (totalBytes > 0) {
-            pendingWrites.forEach(session -> session.flush(out));
-            pendingWrites.clear();
-            totalBytes = 0;
+        if (!pendingWrites.isEmpty()) {
+            ArrayList<Session> sessions = new ArrayList<>(pendingWrites.values());
+            sessions.forEach(session -> session.flush(out));
+            pendingBytes = 0;
         }
     }
 
@@ -145,25 +204,30 @@ public class CommandEncoder extends MessageToByteEncoder<Object> {
             }
 
             if (isChannelFree()) {
-                session.flush(out);
-                session.recordAppend(append);
-                if (startAppend(ctx, blockSizeSupplier, append, out)) {
-                    continueAppend(data, out);
+                if (session.isEmpty()) {
+                    session.record(append);
+                    if (startAppend(ctx, blockSizeSupplier, append, out)) {
+                        continueAppend(data, out);
+                    } else {
+                        completeAppend(data, out);
+                    }
                 } else {
-                    breakFromAppend(null, data, false, out);
+                    session.record(append);
+                    session.write(data, out);
+                    session.flush(out);
                 }
             } else {
-                session.recordAppend(append);
+                session.record(append);
                 if (isChannelOwner(append.getWriterId(), append.getSegment())) {
                     if (bytesLeftInBlock > data.readableBytes()) {
                         continueAppend(data, out);
                     } else {
                         ByteBuf dataInsideBlock = data.readSlice(bytesLeftInBlock);
-                        breakFromAppend(dataInsideBlock, data, true, out);
+                        completeAppend(dataInsideBlock, data, out);
                         flushAll(out);
                     }
                 } else {
-                      session.append(data, out);
+                      session.write(data, out);
                 }
             }
         } else if (msg instanceof SetupAppend) {
@@ -204,14 +268,38 @@ public class CommandEncoder extends MessageToByteEncoder<Object> {
         }
     }
 
+    /**
+     * Check if the network channel is idle or not.
+     *
+     * @return true if channel is not used by any writers; false otherwise.
+     */
     private boolean isChannelFree() {
         return writerIdPerformingAppends == null;
     }
 
+    /**
+     * Check/Confirm the Network channel Ownership.
+     * The Network channel ownership is associated with Writer ID and segment to which writes are performed.
+     *
+     * @param writerID  Writers UUID.
+     * @param segment   Segment.
+     * @return true if channel is used by the given WriterId and Segment; false otherwise.
+     */
     private boolean isChannelOwner(UUID writerID, String segment) {
         return writerID.equals(writerIdPerformingAppends) && segment.equals(segmentBeingAppendedTo);
     }
 
+    /**
+     * Start the Append.
+     * if the Append batch size tracker decides the block size
+     * which is more than event message size then Append will be initiated.
+     *
+     * @param ctx               ChannelHandlerContext.
+     * @param blockSizeSupplier Append batch size tracker.
+     * @param append            Append data.
+     * @param out               channel Buffer.
+     * @return true if the append is started; false otherwise.
+     */
     private boolean startAppend(ChannelHandlerContext ctx, AppendBatchSizeTracker blockSizeSupplier, Append append, ByteBuf out) {
         final int msgSize = append.getData().readableBytes();
         currentBlockSize = msgSize;
@@ -231,38 +319,58 @@ public class CommandEncoder extends MessageToByteEncoder<Object> {
         return false;
     }
 
+    /**
+     * Continue the ongoing append by writing bytes to channel buffer.
+     *
+     * @param data  data to write.
+     * @param out   channel Buffer.
+     */
     private void continueAppend(ByteBuf data, ByteBuf out) {
         bytesLeftInBlock -= data.readableBytes();
         out.writeBytes(data);
     }
 
-    private void breakFromAppend(ByteBuf data, ByteBuf pendingData,  boolean isAppend, ByteBuf out) {
-        if (isChannelFree()) {
-            return;
-        }
-        if (isAppend) {
-            if (data != null) {
-                writeMessage(new PartialEvent(data), out);
-            } else {
-                writeMessage(new Padding(bytesLeftInBlock), out);
-            }
-         }
+    /**
+     * Complete the ongoing append by writing the pending data as Append Block End to channel buffer.
+     *
+     * @param pendingData   data to write.
+     * @param out           channel Buffer.
+     */
+    private void completeAppend(ByteBuf pendingData, ByteBuf out) {
         Session session = setupSegments.get(new SimpleImmutableEntry<>(segmentBeingAppendedTo, writerIdPerformingAppends));
-        writeMessage(new AppendBlockEnd(session.id,
-                currentBlockSize - bytesLeftInBlock,
-                    pendingData,
-                    session.eventCount,
-                    session.lastEventNumber, session.requestId), out);
+        session.flush(currentBlockSize - bytesLeftInBlock, pendingData, out);
         bytesLeftInBlock = 0;
         currentBlockSize = 0;
-        session.eventCount = 0;
         tokenCounter.incrementAndGet();
         segmentBeingAppendedTo = null;
         writerIdPerformingAppends = null;
     }
 
+    /**
+     * Complete the ongoing append by writing the partial Event to ongoing append to channel buffer.
+     * and the remaining data as Append Block End to channel buffer.
+     *
+     * @param data          Partial Event.
+     * @param pendingData   data to write.
+     * @param out           channel Buffer.
+     */
+    private void completeAppend(ByteBuf data, ByteBuf pendingData, ByteBuf out) {
+        writeMessage(new PartialEvent(data), out);
+        completeAppend(pendingData, out);
+    }
+
+
+    /**
+     * Break the ongoing append by padding zero bytes to remaining block size followed by Append block End.
+     *
+     * @param out   channel Buffer.
+     */
     private void breakCurrentAppend(ByteBuf out) {
-        breakFromAppend(null, null, true, out);
+        if (isChannelFree()) {
+            return;
+        }
+        writeMessage(new Padding(bytesLeftInBlock), out);
+        completeAppend(null, out);
     }
 
     @SneakyThrows(IOException.class)
