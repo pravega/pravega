b5967d80dfdde0f6eebb238a10018f3652035523
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2019-08-12 07:34:21 -0700

    feature-watermarking-test
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

---
+++ test/integration/src/test/java/io/pravega/test/integration/WatermarkingTest.java
@@ -0,0 +1,245 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.test.integration;
+
+import io.pravega.client.ClientConfig;
+import io.pravega.client.EventStreamClientFactory;
+import io.pravega.client.SynchronizerClientFactory;
+import io.pravega.client.admin.ReaderGroupManager;
+import io.pravega.client.admin.StreamManager;
+import io.pravega.client.admin.impl.ReaderGroupManagerImpl;
+import io.pravega.client.netty.impl.ConnectionFactory;
+import io.pravega.client.netty.impl.ConnectionFactoryImpl;
+import io.pravega.client.segment.impl.Segment;
+import io.pravega.client.stream.EventRead;
+import io.pravega.client.stream.EventStreamReader;
+import io.pravega.client.stream.EventStreamWriter;
+import io.pravega.client.stream.EventWriterConfig;
+import io.pravega.client.stream.ReaderConfig;
+import io.pravega.client.stream.ReaderGroupConfig;
+import io.pravega.client.stream.ScalingPolicy;
+import io.pravega.client.stream.Stream;
+import io.pravega.client.stream.StreamConfiguration;
+import io.pravega.client.stream.StreamCut;
+import io.pravega.client.stream.impl.Controller;
+import io.pravega.client.stream.impl.JavaSerializer;
+import io.pravega.client.stream.impl.StreamCutImpl;
+import io.pravega.client.watermark.WatermarkSerializer;
+import io.pravega.common.concurrent.Futures;
+import io.pravega.segmentstore.contracts.StreamSegmentStore;
+import io.pravega.segmentstore.contracts.tables.TableStore;
+import io.pravega.segmentstore.server.host.handler.PravegaConnectionListener;
+import io.pravega.segmentstore.server.store.ServiceBuilder;
+import io.pravega.segmentstore.server.store.ServiceBuilderConfig;
+import io.pravega.shared.NameUtils;
+import io.pravega.shared.watermarks.Watermark;
+import io.pravega.test.common.TestUtils;
+import io.pravega.test.common.TestingServerStarter;
+import io.pravega.test.integration.demo.ControllerWrapper;
+import lombok.Cleanup;
+import org.apache.curator.test.TestingServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.net.URI;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertTrue;
+
+/**
+ * Collection of tests to validate controller bootstrap sequence.
+ */
+public class WatermarkingTest {
+
+    private final int controllerPort = TestUtils.getAvailableListenPort();
+    private final int servicePort = TestUtils.getAvailableListenPort();
+
+    private TestingServer zkTestServer;
+    private ControllerWrapper controllerWrapper;
+    private PravegaConnectionListener server;
+    private ServiceBuilder serviceBuilder;
+    private StreamSegmentStore store;
+    private TableStore tableStore;
+    private ScheduledExecutorService executorService;
+
+    @Before
+    public void setup() throws Exception {
+        final String serviceHost = "localhost";
+        final int containerCount = 4;
+
+        // 1. Start ZK
+        zkTestServer = new TestingServerStarter().start();
+
+        serviceBuilder = ServiceBuilder.newInMemoryBuilder(ServiceBuilderConfig.getDefaultConfig());
+        serviceBuilder.initialize();
+        store = serviceBuilder.createStreamSegmentService();
+        tableStore = serviceBuilder.createTableStoreService();
+
+        server = new PravegaConnectionListener(false, servicePort, store, tableStore);
+        server.startListening();
+
+        // 2. Start controller
+        controllerWrapper = new ControllerWrapper(zkTestServer.getConnectString(), false,
+                controllerPort, serviceHost, servicePort, containerCount);
+
+        controllerWrapper.awaitRunning();
+        executorService = Executors.newScheduledThreadPool(5);
+    }
+
+    @After
+    public void cleanup() throws Exception {
+        if (controllerWrapper != null) {
+            controllerWrapper.close();
+        }
+        if (server != null) {
+            server.close();
+        }
+        if (serviceBuilder != null) {
+            serviceBuilder.close();
+        }
+        if (zkTestServer != null) {
+            zkTestServer.close();
+        }
+        executorService.shutdown();
+    }
+
+    @Test(timeout = 60000)
+    public void watermarkTest() throws Exception {
+        Controller controller = controllerWrapper.getController();
+        String scope = "scope";
+        String stream = "stream";
+        StreamConfiguration config = StreamConfiguration.builder().scalingPolicy(ScalingPolicy.fixed(5)).build();
+
+        URI controllerUri = URI.create("tcp://localhost:" + controllerPort);
+        StreamManager streamManager = StreamManager.create(controllerUri);
+        streamManager.createScope(scope);
+        streamManager.createStream(scope, stream, config);
+        StreamConfiguration markConfig = StreamConfiguration.builder().scalingPolicy(ScalingPolicy.fixed(1)).build();
+        controller.createStream(scope, NameUtils.getMarkStreamForStream(stream), markConfig).join();
+
+        Stream streamObj = Stream.of(scope, stream);
+
+        // create 2 writers
+        ClientConfig clientConfig = ClientConfig.builder().controllerURI(controllerUri).build();
+        @Cleanup
+        EventStreamClientFactory clientFactory = EventStreamClientFactory.withScope(scope, clientConfig);
+        JavaSerializer<String> javaSerializer = new JavaSerializer<>();
+        @Cleanup
+        EventStreamWriter<String> writer1 = clientFactory.createEventWriter(stream, javaSerializer,
+                EventWriterConfig.builder().build());
+        @Cleanup
+        EventStreamWriter<String> writer2 = clientFactory.createEventWriter(stream, javaSerializer,
+                EventWriterConfig.builder().build());
+
+        AtomicBoolean stopFlag = new AtomicBoolean(false);
+        // write events
+        writeEvents(writer1, stopFlag);
+        writeEvents(writer2, stopFlag);
+
+        // scale the stream several times so that we get complex positions
+        scale(controller, streamObj, config);
+
+        @Cleanup
+        ConnectionFactory connectionFactory = new ConnectionFactoryImpl(clientConfig);
+        @Cleanup
+        SynchronizerClientFactory syncClientFactory = SynchronizerClientFactory.withScope(scope, clientConfig);
+        @Cleanup
+        ReaderGroupManager watermarkingRG = new ReaderGroupManagerImpl(scope, controller, syncClientFactory, connectionFactory);
+        String markRG = "watermarkingRG";
+        String markStream = NameUtils.getMarkStreamForStream(stream);
+        watermarkingRG.createReaderGroup(markRG, ReaderGroupConfig.builder().stream(Stream.of(scope, markStream)).build());
+
+        // create reader
+        @Cleanup
+        final EventStreamReader<Watermark> markReader = clientFactory.createReader("markreader",
+                markRG,
+                new WatermarkSerializer(),
+                ReaderConfig.builder().build());
+        EventRead<Watermark> eventRead = markReader.readNextEvent(30000L);
+        while (eventRead.getEvent() == null) {
+            eventRead = markReader.readNextEvent(30000L);
+        }
+        Watermark watermark = eventRead.getEvent();
+        stopFlag.set(true);
+
+        // read events from the stream
+        @Cleanup
+        ReaderGroupManager readerGroupManager = new ReaderGroupManagerImpl(scope, controller, syncClientFactory, connectionFactory);
+        String readerGroup = "rg";
+
+        long timeLow = watermark.getLowerTimeBound();
+        Map<Segment, Long> positionMap0 = watermark.getStreamCut()
+                                                   .entrySet().stream().collect(Collectors.toMap(x -> new Segment(scope, stream, x.getKey().getSegmentId()),
+                        Map.Entry::getValue));
+
+        StreamCut streamCutStart = new StreamCutImpl(streamObj, positionMap0);
+        Map<Stream, StreamCut> start = Collections.singletonMap(streamObj, streamCutStart);
+        
+        readerGroupManager.createReaderGroup(readerGroup, ReaderGroupConfig.builder().stream(streamObj)
+                                                                           .startingStreamCuts(start).build());
+
+        // create reader on the stream
+        @Cleanup
+        final EventStreamReader<String> reader = clientFactory.createReader("myreader",
+                readerGroup,
+                javaSerializer,
+                ReaderConfig.builder().build());
+        
+        // read events from the reader. 
+        // verify that events read belong to the bound
+        EventRead<String> event = reader.readNextEvent(10000L);
+        while (event.getEvent() != null) {
+            Long time = Long.parseLong(event.getEvent());
+            assertTrue(time >= timeLow);
+            event = reader.readNextEvent(10000L);
+        }
+    }
+
+    private void scale(Controller controller, Stream streamObj, StreamConfiguration configuration) throws InterruptedException {
+        // perform several scales
+        int numOfSegments = configuration.getScalingPolicy().getMinNumSegments();
+        double delta = 1.0 / numOfSegments;
+        for (long segmentNumber = 0; segmentNumber < numOfSegments - 1; segmentNumber++) {
+            Thread.sleep(5000L);
+            double rangeLow = segmentNumber * delta;
+            double rangeHigh = (segmentNumber + 1) * delta;
+            double rangeMid = (rangeHigh + rangeLow) / 2;
+
+            Map<Double, Double> map = new HashMap<>();
+            map.put(rangeLow, rangeMid);
+            map.put(rangeMid, rangeHigh);
+            controller.scaleStream(streamObj, Collections.singletonList(segmentNumber), map, executorService).getFuture().join();
+        }
+    }
+
+    private CompletableFuture<Void> writeEvents(EventStreamWriter<String> writer, AtomicBoolean stopFlag) {
+        AtomicInteger count = new AtomicInteger(0);
+        AtomicLong currentTime = new AtomicLong();
+        return Futures.loop(() -> !stopFlag.get(), () -> Futures.delayedFuture(() -> {
+            currentTime.set(System.currentTimeMillis());
+            return writer.writeEvent(count.toString(), Long.toString(currentTime.get()))
+                         .thenAccept(v -> {
+                             if (count.incrementAndGet() % 10 == 0) {
+                                 writer.noteTime(currentTime.get());
+                             }
+                         });
+        }, 1000L, executorService), executorService);
+    }
+}
---
+++ test/system/src/test/java/io/pravega/test/system/WatermarkingTest.java
@@ -0,0 +1,246 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.test.system;
+
+import io.pravega.client.ClientConfig;
+import io.pravega.client.EventStreamClientFactory;
+import io.pravega.client.SynchronizerClientFactory;
+import io.pravega.client.admin.ReaderGroupManager;
+import io.pravega.client.admin.StreamManager;
+import io.pravega.client.admin.impl.ReaderGroupManagerImpl;
+import io.pravega.client.netty.impl.ConnectionFactory;
+import io.pravega.client.netty.impl.ConnectionFactoryImpl;
+import io.pravega.client.segment.impl.Segment;
+import io.pravega.client.stream.EventRead;
+import io.pravega.client.stream.EventStreamReader;
+import io.pravega.client.stream.EventStreamWriter;
+import io.pravega.client.stream.EventWriterConfig;
+import io.pravega.client.stream.ReaderConfig;
+import io.pravega.client.stream.ReaderGroupConfig;
+import io.pravega.client.stream.RetentionPolicy;
+import io.pravega.client.stream.ScalingPolicy;
+import io.pravega.client.stream.Stream;
+import io.pravega.client.stream.StreamConfiguration;
+import io.pravega.client.stream.StreamCut;
+import io.pravega.client.stream.impl.ClientFactoryImpl;
+import io.pravega.client.stream.impl.Controller;
+import io.pravega.client.stream.impl.ControllerImpl;
+import io.pravega.client.stream.impl.ControllerImplConfig;
+import io.pravega.client.stream.impl.JavaSerializer;
+import io.pravega.client.stream.impl.StreamCutImpl;
+import io.pravega.client.watermark.WatermarkSerializer;
+import io.pravega.common.Exceptions;
+import io.pravega.common.concurrent.Futures;
+import io.pravega.common.hash.RandomFactory;
+import io.pravega.shared.NameUtils;
+import io.pravega.shared.watermarks.Watermark;
+import io.pravega.test.system.framework.Environment;
+import io.pravega.test.system.framework.SystemTestRunner;
+import io.pravega.test.system.framework.Utils;
+import io.pravega.test.system.framework.services.Service;
+import lombok.Cleanup;
+import lombok.extern.slf4j.Slf4j;
+import mesosphere.marathon.client.MarathonException;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.Timeout;
+import org.junit.runner.RunWith;
+
+import java.io.Serializable;
+import java.net.URI;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Map;
+import java.util.UUID;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+
+import static org.junit.Assert.assertTrue;
+
+@Slf4j
+@RunWith(SystemTestRunner.class)
+public class WatermarkingTest extends AbstractSystemTest {
+
+    private static final String STREAM = "testWatermarkingStream";
+    private static final String SCOPE = "testWatermarkingScope" + RandomFactory.create().nextInt(Integer.MAX_VALUE);
+    private static final String READER_GROUP = "testWatermarkingReaderGroup" + RandomFactory.create().nextInt(Integer.MAX_VALUE);
+
+    @Rule
+    public Timeout globalTimeout = Timeout.seconds(10 * 60);
+
+    private final ScalingPolicy scalingPolicy = ScalingPolicy.fixed(5);
+    private final StreamConfiguration config = StreamConfiguration.builder().scalingPolicy(scalingPolicy).build();
+    private URI controllerURI;
+    private StreamManager streamManager;
+    private final ScheduledExecutorService executorService = Executors.newScheduledThreadPool(5);
+
+    /**
+     * This is used to setup the various services required by the system test framework.
+     *
+     * @throws MarathonException    when error in setup
+     */
+    @Environment
+    public static void initialize() throws MarathonException {
+        URI zkUri = startZookeeperInstance();
+        startBookkeeperInstances(zkUri);
+        URI controllerUri = ensureControllerRunning(zkUri);
+        ensureSegmentStoreRunning(zkUri, controllerUri);
+    }
+
+    @Before
+    public void setup() {
+        Service conService = Utils.createPravegaControllerService(null);
+        List<URI> ctlURIs = conService.getServiceDetails();
+        controllerURI = ctlURIs.get(0);
+        streamManager = StreamManager.create(Utils.buildClientConfig(controllerURI));
+        assertTrue("Creating Scope", streamManager.createScope(SCOPE));
+        assertTrue("Creating stream", streamManager.createStream(SCOPE, STREAM, config));
+        assertTrue("Creating mark stream", streamManager.createStream(SCOPE, NameUtils.getMarkStreamForStream(STREAM), config));
+    }
+
+    @After
+    public void tearDown() {
+        streamManager.close();
+    }
+
+    @Test
+    public void watermarkingTests() throws Exception {
+        final ClientConfig clientConfig = Utils.buildClientConfig(controllerURI);
+        @Cleanup
+        ConnectionFactory connectionFactory = new ConnectionFactoryImpl(clientConfig);
+        ControllerImpl controller = new ControllerImpl(ControllerImplConfig.builder().clientConfig(clientConfig).build(),
+                                                       connectionFactory.getInternalExecutor());
+        // create 2 writers
+        @Cleanup
+        EventStreamClientFactory clientFactory = EventStreamClientFactory.withScope(SCOPE, clientConfig);
+        JavaSerializer<String> javaSerializer = new JavaSerializer<>();
+        @Cleanup
+        EventStreamWriter<String> writer1 = clientFactory.createEventWriter(STREAM, javaSerializer,
+                EventWriterConfig.builder().build());
+        @Cleanup
+        EventStreamWriter<String> writer2 = clientFactory.createEventWriter(STREAM, javaSerializer,
+                EventWriterConfig.builder().build());
+
+        AtomicBoolean stopFlag = new AtomicBoolean(false);
+        // write events
+        writeEvents(writer1, stopFlag);
+        writeEvents(writer2, stopFlag);
+
+        // scale the stream several times so that we get complex positions
+        Stream streamObj = Stream.of(SCOPE, STREAM);
+        scale(controller, streamObj);
+
+        @Cleanup
+        SynchronizerClientFactory syncClientFactory = SynchronizerClientFactory.withScope(SCOPE, clientConfig);
+        @Cleanup
+        ReaderGroupManager watermarkingRG = new ReaderGroupManagerImpl(SCOPE, controller, syncClientFactory, connectionFactory);
+        String markRG = "watermarkingRG";
+        String markStream = NameUtils.getMarkStreamForStream(STREAM);
+        watermarkingRG.createReaderGroup(markRG, ReaderGroupConfig.builder().stream(Stream.of(SCOPE, markStream)).build());
+
+        // create reader
+        @Cleanup
+        final EventStreamReader<Watermark> markReader = clientFactory.createReader("markreader",
+                markRG,
+                new WatermarkSerializer(),
+                ReaderConfig.builder().build());
+        List<Watermark> watermarks = new LinkedList<>();
+
+        while (watermarks.size() < 2) {
+            EventRead<Watermark> eventRead = markReader.readNextEvent(30000L);
+            if (eventRead.getEvent() != null) {
+                watermarks.add(eventRead.getEvent());
+            }
+        }
+        stopFlag.set(true);
+
+        long timeLow = watermarks.get(0).getLowerTimeBound();
+        Map<Segment, Long> positionMap0 = watermarks.get(0).getStreamCut()
+                                                    .entrySet().stream().collect(Collectors.toMap(x -> new Segment(SCOPE, STREAM, x.getKey().getSegmentId()),
+                        Map.Entry::getValue));
+
+        StreamCut streamCutStart = new StreamCutImpl(streamObj, positionMap0);
+        Map<Stream, StreamCut> start = Collections.singletonMap(streamObj, streamCutStart);
+
+        Map<Segment, Long> positionMap1 = watermarks.get(1).getStreamCut()
+                                                    .entrySet().stream().collect(Collectors.toMap(x -> new Segment(SCOPE, STREAM, x.getKey().getSegmentId()),
+                        Map.Entry::getValue));
+
+        StreamCut streamCutEnd = new StreamCutImpl(streamObj, positionMap0);
+        Map<Stream, StreamCut> end = Collections.singletonMap(streamObj, streamCutEnd);
+
+        @Cleanup
+        ReaderGroupManager readerGroupManager = new ReaderGroupManagerImpl(SCOPE, controller, syncClientFactory, connectionFactory);
+        String readerGroup = "rg";
+
+        readerGroupManager.createReaderGroup(readerGroup, ReaderGroupConfig.builder().stream(streamObj)
+                                                                           .startingStreamCuts(start)
+                                                                           .endingStreamCuts(end).build());
+
+        // create reader on the stream
+        @Cleanup
+        final EventStreamReader<String> reader = clientFactory.createReader("myreader",
+                readerGroup,
+                javaSerializer,
+                ReaderConfig.builder().build());
+
+        // read events from the reader. 
+        // verify that events read belong to the bound
+        EventRead<String> event = reader.readNextEvent(10000L);
+        while (event.getEvent() != null) {
+            Long time = Long.parseLong(event.getEvent());
+            assertTrue(time >= timeLow);
+            event = reader.readNextEvent(10000L);
+        }
+    }
+
+    private void scale(Controller controller, Stream streamObj) throws InterruptedException {
+        // perform several scales
+        int numOfSegments = config.getScalingPolicy().getMinNumSegments();
+        double delta = 1.0 / numOfSegments;
+        for (long segmentNumber = 0; segmentNumber < numOfSegments - 1; segmentNumber++) {
+            Thread.sleep(3000L);
+            double rangeLow = segmentNumber * delta;
+            double rangeHigh = (segmentNumber + 1) * delta;
+            double rangeMid = (rangeHigh + rangeLow) / 2;
+
+            Map<Double, Double> map = new HashMap<>();
+            map.put(rangeLow, rangeMid);
+            map.put(rangeMid, rangeHigh);
+            controller.scaleStream(streamObj, Collections.singletonList(segmentNumber), map, executorService).getFuture().join();
+        }
+    }
+
+    private CompletableFuture<Void> writeEvents(EventStreamWriter<String> writer, AtomicBoolean stopFlag) {
+        AtomicInteger count = new AtomicInteger(0);
+        AtomicLong currentTime = new AtomicLong();
+        return Futures.loop(() -> !stopFlag.get(), () -> Futures.delayedFuture(() -> {
+            currentTime.set(System.currentTimeMillis());
+            return writer.writeEvent(count.toString(), Long.toString(currentTime.get()))
+                         .thenAccept(v -> {
+                             if (count.incrementAndGet() % 10 == 0) {
+                                 writer.noteTime(currentTime.get());
+                             }
+                         });
+        }, 1000L, executorService), executorService);
+    }
+
+}
