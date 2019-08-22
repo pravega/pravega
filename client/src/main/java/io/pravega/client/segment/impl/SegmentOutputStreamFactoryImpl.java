5aed2bbb8a03b49765e7246f7cd7b41b3fd6804a
Keshava Munegowda <keshava.munegowda@dell.com>
2019-08-22 18:12:04 +0530

    Segment out stream hash map implementation
    Signed-off-by: Keshava Munegowda <keshava.munegowda@dell.com>

--- client/src/main/java/io/pravega/client/segment/impl/SegmentOutputStreamFactoryImpl.java
+++ client/src/main/java/io/pravega/client/segment/impl/SegmentOutputStreamFactoryImpl.java
@@ -18,6 +18,12 @@ import io.pravega.common.util.RetriesExhaustedException;
 import io.pravega.common.util.Retry;
 import io.pravega.common.util.Retry.RetryWithBackoff;
 import io.pravega.shared.segment.StreamSegmentNameUtils;
+
+import java.util.AbstractMap.SimpleImmutableEntry;
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Map;
+import java.util.HashMap;
 import java.util.UUID;
 import java.util.function.Consumer;
 import lombok.RequiredArgsConstructor;
@@ -31,6 +37,19 @@ public class SegmentOutputStreamFactoryImpl implements SegmentOutputStreamFactor
     private final Controller controller;
     private final ConnectionFactory cf;
     private final Consumer<Segment> nopSegmentSealedCallback = s -> log.error("Transaction segment: {} cannot be sealed", s);
+    private final Map<String, Map.Entry<SegmentOutputStreamImpl, SegmentSealedCallbacks>> setupSegments = new HashMap<>();
+
+    private  static class SegmentSealedCallbacks {
+        private final List<Consumer<Segment>> callbackList = new ArrayList<>();
+
+        public void add(Consumer<Segment> segmentSealedCallback) {
+            callbackList.add(segmentSealedCallback);
+        }
+
+        public void callBack(Segment segment) {
+            callbackList.forEach(s -> s.accept(segment));
+        }
+    }
 
     @Override
     public SegmentOutputStream createOutputStreamForTransaction(Segment segment, UUID txId, EventWriterConfig config,
@@ -42,13 +61,35 @@ public class SegmentOutputStreamFactoryImpl implements SegmentOutputStreamFactor
 
     @Override
     public SegmentOutputStream createOutputStreamForSegment(Segment segment, Consumer<Segment> segmentSealedCallback, EventWriterConfig config, String delegationToken) {
-        SegmentOutputStreamImpl result =
-                new SegmentOutputStreamImpl(segment.getScopedName(), config.isEnableConnectionPooling(), controller, cf, UUID.randomUUID(), segmentSealedCallback,
-                                            getRetryFromConfig(config), delegationToken);
-        try {
-            result.getConnection();
-        } catch (RetriesExhaustedException | SegmentSealedException | NoSuchSegmentException e) {
-            log.warn("Initial connection attempt failure. Suppressing.", e);
+        SegmentOutputStreamImpl result;
+        SegmentSealedCallbacks segmentSeal;
+
+        String key = segment.toString();
+        if (config.isEnableConnectionPooling()) {
+            final Map.Entry<SegmentOutputStreamImpl, SegmentSealedCallbacks> entry = setupSegments.get(key);
+            if (entry == null) {
+                segmentSeal = new SegmentSealedCallbacks();
+                result = new SegmentOutputStreamImpl(key, config.isEnableConnectionPooling(),
+                        controller, cf, UUID.randomUUID(), segmentSeal::callBack, getRetryFromConfig(config), delegationToken);
+                try {
+                    result.getConnection();
+                } catch (RetriesExhaustedException | SegmentSealedException | NoSuchSegmentException e) {
+                    log.warn("Initial connection attempt failure. Suppressing.", e);
+                }
+                setupSegments.put(key, new SimpleImmutableEntry<>(result, segmentSeal));
+            } else {
+                segmentSeal = setupSegments.get(key).getValue();
+                result = entry.getKey();
+            }
+            segmentSeal.add(segmentSealedCallback);
+        } else {
+            result = new SegmentOutputStreamImpl(key, config.isEnableConnectionPooling(), controller, cf,
+                    UUID.randomUUID(), segmentSealedCallback, getRetryFromConfig(config), delegationToken);
+            try {
+                result.getConnection();
+            } catch (RetriesExhaustedException | SegmentSealedException | NoSuchSegmentException e) {
+                log.warn("Initial connection attempt failure. Suppressing.", e);
+            }
         }
         return result;
     }
