7efa44f31b3beb9a6070fc1bbd40142e764b9ca5
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2019-09-05 00:35:07 -0700

    Expand imports
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- controller/src/test/java/io/pravega/controller/server/eventProcessor/RequestHandlersTest.java
+++ controller/src/test/java/io/pravega/controller/server/eventProcessor/RequestHandlersTest.java
@@ -78,7 +78,10 @@ import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertNotEquals;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyInt;
 import static org.mockito.ArgumentMatchers.anyLong;
