0b7a9181d011f50e57c8ebaea743217e35ab5fbe
Shivesh Ranjan <shivesh.ranjan@gmail.com>
2019-09-23 21:58:14 -0700

    PR comments
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- controller/src/main/java/io/pravega/controller/server/eventProcessor/requesthandlers/StreamRequestHandler.java
+++ controller/src/main/java/io/pravega/controller/server/eventProcessor/requesthandlers/StreamRequestHandler.java
@@ -65,11 +65,11 @@ public class StreamRequestHandler extends AbstractRequestProcessor<ControllerEve
 
     @Override
     public CompletableFuture<Void> processScaleOpRequest(ScaleOpEvent scaleOpEvent) {
-        log.info("processing scale request {} for stream {}/{}", scaleOpEvent.getRequestId(), scaleOpEvent.getScope(), scaleOpEvent.getStream());
+        log.info("Processing scale request {} for stream {}/{}", scaleOpEvent.getRequestId(), scaleOpEvent.getScope(), scaleOpEvent.getStream());
         return withCompletion(scaleOperationTask, scaleOpEvent, scaleOpEvent.getScope(), scaleOpEvent.getStream(),
                 OPERATION_NOT_ALLOWED_PREDICATE.or(e -> e instanceof EpochTransitionOperationExceptions.ConflictException))
                 .thenAccept(v -> {
-                    log.info("processing scale request {} for stream {}/{} complete", scaleOpEvent.getRequestId(), scaleOpEvent.getScope(), scaleOpEvent.getStream());
+                    log.info("Processing scale request {} for stream {}/{} complete", scaleOpEvent.getRequestId(), scaleOpEvent.getScope(), scaleOpEvent.getStream());
                 });
     }
 
@@ -79,7 +79,7 @@ public class StreamRequestHandler extends AbstractRequestProcessor<ControllerEve
         return withCompletion(updateStreamTask, updateStreamEvent, updateStreamEvent.getScope(), updateStreamEvent.getStream(),
                 OPERATION_NOT_ALLOWED_PREDICATE)
                 .thenAccept(v -> {
-                    log.info("Processing update request {} for stream {}/{}", updateStreamEvent.getRequestId(), updateStreamEvent.getScope(), updateStreamEvent.getStream());
+                    log.info("Processing update request {} for stream {}/{} complete", updateStreamEvent.getRequestId(), updateStreamEvent.getScope(), updateStreamEvent.getStream());
                 });
     }
 
@@ -89,7 +89,7 @@ public class StreamRequestHandler extends AbstractRequestProcessor<ControllerEve
         return withCompletion(truncateStreamTask, truncateStreamEvent, truncateStreamEvent.getScope(), truncateStreamEvent.getStream(),
                 OPERATION_NOT_ALLOWED_PREDICATE)
                 .thenAccept(v -> {
-                    log.info("Processing truncate request {} for stream {}/{}", truncateStreamEvent.getRequestId(), truncateStreamEvent.getScope(), truncateStreamEvent.getStream());
+                    log.info("Processing truncate request {} for stream {}/{} complete", truncateStreamEvent.getRequestId(), truncateStreamEvent.getScope(), truncateStreamEvent.getStream());
                 });
     }
 
@@ -99,7 +99,7 @@ public class StreamRequestHandler extends AbstractRequestProcessor<ControllerEve
         return withCompletion(sealStreamTask, sealStreamEvent, sealStreamEvent.getScope(), sealStreamEvent.getStream(),
                 OPERATION_NOT_ALLOWED_PREDICATE)
                 .thenAccept(v -> {
-                    log.info("Processing seal request {} for stream {}/{}", sealStreamEvent.getRequestId(), sealStreamEvent.getScope(), sealStreamEvent.getStream());
+                    log.info("Processing seal request {} for stream {}/{} complete", sealStreamEvent.getRequestId(), sealStreamEvent.getScope(), sealStreamEvent.getStream());
                 });
     }
 
@@ -109,7 +109,7 @@ public class StreamRequestHandler extends AbstractRequestProcessor<ControllerEve
         return withCompletion(deleteStreamTask, deleteStreamEvent, deleteStreamEvent.getScope(), deleteStreamEvent.getStream(),
                 OPERATION_NOT_ALLOWED_PREDICATE)
                 .thenAccept(v -> {
-                    log.info("Processing delete request {} for stream {}/{}", deleteStreamEvent.getRequestId(), deleteStreamEvent.getScope(), deleteStreamEvent.getStream());
+                    log.info("Processing delete request {} for stream {}/{} complete", deleteStreamEvent.getRequestId(), deleteStreamEvent.getScope(), deleteStreamEvent.getStream());
                 });
     }
 }
