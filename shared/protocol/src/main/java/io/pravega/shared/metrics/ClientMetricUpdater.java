057752a1e05e7a636f057c1f7fd1393d6e0cdd6f
Sandeep <sandeep.shridhar@emc.com>
2019-10-03 06:22:33 -0400

    Metric related files.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

---
+++ shared/protocol/src/main/java/io/pravega/shared/metrics/ClientMetricUpdater.java
@@ -0,0 +1,44 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.shared.metrics;
+
+import com.google.common.base.Preconditions;
+import io.pravega.common.concurrent.ExecutorServiceHelpers;
+import io.pravega.common.concurrent.MultiKeyLatestItemSequentialProcessor;
+import java.util.concurrent.ScheduledExecutorService;
+
+
+public class ClientMetricUpdater implements MetricNotifier {
+    private final MultiKeyLatestItemSequentialProcessor<String, Long> successProcessor;
+    private final MultiKeyLatestItemSequentialProcessor<String, Long> failureProcessor;
+
+    private final ScheduledExecutorService clientMetricExecutor = ExecutorServiceHelpers.newScheduledThreadPool(2, "clientMetrics");
+
+    public ClientMetricUpdater(final MetricListener metricListener) {
+        Preconditions.checkNotNull(metricListener);
+        this.successProcessor = new MultiKeyLatestItemSequentialProcessor<>(metricListener::reportSuccessValue, clientMetricExecutor);
+        this.failureProcessor = new MultiKeyLatestItemSequentialProcessor<>(metricListener::reportFailValue, clientMetricExecutor);
+    }
+
+    @Override
+    public void updateSuccessMetric(String metricName, long value) {
+        successProcessor.updateItem(metricName, value);
+    }
+
+    @Override
+    public void updateFailureMetric(String metricName, long value) {
+        failureProcessor.updateItem(metricName, value);
+    }
+
+    @Override
+    public void close() {
+        ExecutorServiceHelpers.shutdown(clientMetricExecutor);
+    }
+}
