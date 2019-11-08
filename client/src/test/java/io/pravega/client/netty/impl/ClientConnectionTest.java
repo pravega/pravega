b20dc800a1af79cf7ff94bd97cbc5f9ac36ee247
Tom Kaitchuck <tom.kaitchuck@emc.com>
2019-11-08 13:23:15 -0800

    Assert thread id doesn't change
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- client/src/test/java/io/pravega/client/netty/impl/ClientConnectionTest.java
+++ client/src/test/java/io/pravega/client/netty/impl/ClientConnectionTest.java
@@ -10,20 +10,27 @@
 package io.pravega.client.netty.impl;
 
 import io.netty.buffer.Unpooled;
+import io.netty.channel.ChannelDuplexHandler;
+import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
+import io.netty.channel.ChannelPromise;
 import io.netty.channel.embedded.EmbeddedChannel;
 import io.netty.handler.codec.LengthFieldBasedFrameDecoder;
 import io.pravega.shared.protocol.netty.Append;
 import io.pravega.shared.protocol.netty.AppendDecoder;
 import io.pravega.shared.protocol.netty.CommandDecoder;
 import io.pravega.shared.protocol.netty.CommandEncoder;
+import io.pravega.shared.protocol.netty.ConnectionFailedException;
 import io.pravega.shared.protocol.netty.ExceptionLoggingHandler;
 import io.pravega.shared.protocol.netty.FailingReplyProcessor;
 import io.pravega.shared.protocol.netty.WireCommands;
 import io.pravega.shared.protocol.netty.WireCommands.AuthTokenCheckFailed;
 import io.pravega.shared.protocol.netty.WireCommands.Event;
+import java.util.HashSet;
+import java.util.List;
 import java.util.Queue;
 import java.util.UUID;
+import java.util.Vector;
 import java.util.concurrent.atomic.AtomicBoolean;
 import lombok.Cleanup;
 import org.junit.Test;
@@ -31,29 +38,32 @@ import org.junit.Test;
 import static io.pravega.shared.protocol.netty.WireCommands.MAX_WIRECOMMAND_SIZE;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class ClientConnectionTest {
 
+    private static class ReplyProcessor extends FailingReplyProcessor {
+        AtomicBoolean falure = new AtomicBoolean(false);
+
+        @Override
+        public void processingFailure(Exception error) {
+            falure.set(true);
+        }
+
+        @Override
+        public void connectionDropped() {
+            falure.set(true);
+        }
+
+        @Override
+        public void authTokenCheckFailed(AuthTokenCheckFailed authTokenCheckFailed) {
+            falure.set(true);
+        }
+    }
+    
     @Test
     public void testConnectionSetup() throws Exception {
-        AtomicBoolean falure = new AtomicBoolean(false);
-        FailingReplyProcessor processor = new FailingReplyProcessor() {
-
-            @Override
-            public void processingFailure(Exception error) {
-                falure.set(true);
-            }
-
-            @Override
-            public void connectionDropped() {
-                falure.set(true);
-            }
-
-            @Override
-            public void authTokenCheckFailed(AuthTokenCheckFailed authTokenCheckFailed) {
-                falure.set(true);
-            }
-        };
+        ReplyProcessor processor = new ReplyProcessor(); 
         Flow flow = new Flow(10, 0);
         FlowHandler flowHandler = new FlowHandler("testConnection");
         @Cleanup
@@ -71,7 +81,7 @@ public class ClientConnectionTest {
         embeddedChannel.runPendingTasks();
         messages = embeddedChannel.outboundMessages();
         assertEquals(3, messages.size());
-        assertFalse(falure.get());
+        assertFalse(processor.falure.get());
     }
 
     static EmbeddedChannel createChannelWithContext(ChannelInboundHandlerAdapter handler) {
@@ -79,4 +89,46 @@ public class ClientConnectionTest {
                                    new LengthFieldBasedFrameDecoder(MAX_WIRECOMMAND_SIZE, 4, 4), new CommandDecoder(),
                                    new AppendDecoder(), handler);
     }
+    
+    @Test
+    public void testThread() throws ConnectionFailedException {
+        ReplyProcessor processor = new ReplyProcessor(); 
+        Flow flow = new Flow(10, 0);
+        FlowHandler flowHandler = new FlowHandler("testConnection");
+        byte[] payload = new byte[100];
+        @Cleanup
+        ClientConnection clientConnection = flowHandler.createFlow(flow, processor);
+        List<String> threadIds = new Vector<>();
+        ChannelDuplexHandler spy = new ChannelDuplexHandler() {
+            @Override
+            public void write(ChannelHandlerContext ctx, Object msg, ChannelPromise promise) throws Exception {
+                threadIds.add(Thread.currentThread().getName());
+                super.write(ctx, msg, promise);
+            }
+
+            @Override
+            public void flush(ChannelHandlerContext ctx) throws Exception {
+                threadIds.add(Thread.currentThread().getName());
+                super.flush(ctx);
+            }
+        };
+        EmbeddedChannel embeddedChannel = new EmbeddedChannel(spy, new CommandEncoder(null),
+                                                              new LengthFieldBasedFrameDecoder(MAX_WIRECOMMAND_SIZE, 4, 4), new CommandDecoder(),
+                                                              new AppendDecoder(), flowHandler);
+        clientConnection.send(new WireCommands.SetupAppend(1, new UUID(1, 2), "segment", ""));
+        for (int i = 0; i < 100; i++) {
+            clientConnection.send(new Append("segment", new UUID(1, 2), i, new Event(Unpooled.wrappedBuffer(payload)), 2));
+            embeddedChannel.runPendingTasks();
+        }
+        assertTrue(threadIds.size() > 100);
+        Queue<Object> messages = embeddedChannel.outboundMessages();
+        assertEquals(102, messages.size());
+        assertFalse(processor.falure.get());
+        HashSet<String> uniqueThreads = new HashSet<>();
+        for (String threadId : threadIds) {
+            uniqueThreads.add(threadId);
+        }
+        assertEquals(1, uniqueThreads.size());
+    }
+
 }
