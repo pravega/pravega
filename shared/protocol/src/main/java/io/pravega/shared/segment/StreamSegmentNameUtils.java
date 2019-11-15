63e91674a001f7860fcb00ee9fdfda3c950f0c8e
Andrey Kurilov <andrey.kurilov@emc.com>
2019-11-15 18:30:53 +0300

    optimize the updateSegmentTags method

--- shared/protocol/src/main/java/io/pravega/shared/segment/StreamSegmentNameUtils.java
+++ shared/protocol/src/main/java/io/pravega/shared/segment/StreamSegmentNameUtils.java
@@ -16,6 +16,8 @@ import io.pravega.shared.NameUtils;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.UUID;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Utility methods for StreamSegment Names.
@@ -416,29 +418,20 @@ public final class StreamSegmentNameUtils {
         return tags;
     }
 
-    private static String[] updateSegmentTags(String qualifiedSegmentName, String[] tags) {
-        String segmentBaseName = getSegmentBaseName(qualifiedSegmentName);
-        String[] tokens = segmentBaseName.split("[/]");
+    final static Pattern SEGMENT_TAGS_PATTERN =
+        Pattern.compile("((([\\w\\-\\\\.]+)/)?(([\\w\\-\\\\.]+)/))?(\\d+)(\\.#epoch\\.(\\d+))?");
 
-        int segmentIdIndex = (tokens.length == 1) ? 0 : (tokens.length) == 2 ? 1 : 2;
-        if (tokens[segmentIdIndex].contains(EPOCH_DELIMITER)) {
-            String[] segmentIdTokens = tokens[segmentIdIndex].split(EPOCH_DELIMITER);
-            tags[5] = segmentIdTokens[0];
-            tags[7] = segmentIdTokens[1];
-        } else {
-            tags[5] = tokens[segmentIdIndex];
-            tags[7] = "0";
-        }
-        if (tokens.length == 3) {
-            tags[1] = tokens[0];
-            tags[3] = tokens[1];
-        } else if (tokens.length == 1) {
-            tags[1] = TAG_DEFAULT;
-            tags[3] = TAG_DEFAULT;
-        } else {
-            tags[1] = TAG_DEFAULT;
-            tags[3] = tokens[0];
-        }
+    private static String[] updateSegmentTags(String qualifiedSegmentName, String[] tags) {
+        final String segmentBaseName = getSegmentBaseName(qualifiedSegmentName);
+        final Matcher m = SEGMENT_TAGS_PATTERN.matcher(segmentBaseName);
+        m.find();
+        final String scope = m.group(3);
+        tags[1] = scope == null ? TAG_DEFAULT : scope;
+        final String stream = m.group(5);
+        tags[3] = stream == null ? TAG_DEFAULT : stream;
+        tags[5] = m.group(6);
+        final String epoch = m.group(8);
+        tags[7] = epoch == null ? "0" : epoch;
         return tags;
     }
 
