0ba15e100b372c331e9a38a997d7e50591201283
Andrei Paduroiu <andrei.paduroiu@dell.com>
2019-12-13 17:11:39 -0800

    Unit tests for CacheUtilizationProvider.
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@dell.com>

--- segmentstore/server/src/main/java/io/pravega/segmentstore/server/CacheUtilizationProvider.java
+++ segmentstore/server/src/main/java/io/pravega/segmentstore/server/CacheUtilizationProvider.java
@@ -1,5 +1,5 @@
 /**
- * Copyright (c) 2017 Dell Inc., or its subsidiaries. All Rights Reserved.
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -43,7 +43,7 @@ public class CacheUtilizationProvider {
     /**
      * Creates a new instance of the {@link CacheUtilizationProvider} class.
      *
-     * @param policy              The {@link  CachePolicy} to use.
+     * @param policy              The {@link CachePolicy} to use.
      * @param getCacheStoredBytes A {@link Supplier} that, when invoked, returns the number of stored bytes in the cache.
      */
     CacheUtilizationProvider(@NonNull CachePolicy policy, @NonNull Supplier<Long> getCacheStoredBytes) {
