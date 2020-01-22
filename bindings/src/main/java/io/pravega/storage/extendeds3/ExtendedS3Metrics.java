ff59f435665d870d24aab3c69237e59bcffc1166
Sachin Joshi <sachin.joshi@emc.com>
2020-01-22 11:03:53 -0800

    Issue 4505: Add missing metrics ExtendedS3.
    Currently ExtendedS3Storage class does not publish any storage related metrics.
    This change adds missing metrics for ExtendedS3.
    
    Signed-off-by: Sachin Joshi <sachin.joshi@emc.com>

---
+++ bindings/src/main/java/io/pravega/storage/extendeds3/ExtendedS3Metrics.java
@@ -0,0 +1,36 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.storage.extendeds3;
+
+import io.pravega.shared.MetricsNames;
+import io.pravega.shared.metrics.Counter;
+import io.pravega.shared.metrics.MetricsProvider;
+import io.pravega.shared.metrics.OpStatsLogger;
+import io.pravega.shared.metrics.StatsLogger;
+
+/**
+ * Defines all Metrics used by the ExtendedS3Storage class.
+ */
+final class ExtendedS3Metrics {
+    private static final StatsLogger ECS_LOGGER = MetricsProvider.createStatsLogger("ecs");
+    static final OpStatsLogger READ_LATENCY = ECS_LOGGER.createStats(MetricsNames.STORAGE_READ_LATENCY);
+    static final OpStatsLogger WRITE_LATENCY = ECS_LOGGER.createStats(MetricsNames.STORAGE_WRITE_LATENCY);
+    static final OpStatsLogger CREATE_LATENCY = ECS_LOGGER.createStats(MetricsNames.STORAGE_CREATE_LATENCY);
+    static final OpStatsLogger DELETE_LATENCY = ECS_LOGGER.createStats(MetricsNames.STORAGE_DELETE_LATENCY);
+    static final OpStatsLogger CONCAT_LATENCY = ECS_LOGGER.createStats(MetricsNames.STORAGE_CONCAT_LATENCY);
+
+    static final Counter READ_BYTES = ECS_LOGGER.createCounter(MetricsNames.STORAGE_READ_BYTES);
+    static final Counter WRITE_BYTES = ECS_LOGGER.createCounter(MetricsNames.STORAGE_WRITE_BYTES);
+    static final Counter CONCAT_BYTES = ECS_LOGGER.createCounter(MetricsNames.STORAGE_CONCAT_BYTES);
+
+    static final Counter CREATE_COUNT = ECS_LOGGER.createCounter(MetricsNames.STORAGE_CREATE_COUNT);
+    static final Counter DELETE_COUNT = ECS_LOGGER.createCounter(MetricsNames.STORAGE_DELETE_COUNT);
+    static final Counter CONCAT_COUNT = ECS_LOGGER.createCounter(MetricsNames.STORAGE_CONCAT_COUNT);
+}
