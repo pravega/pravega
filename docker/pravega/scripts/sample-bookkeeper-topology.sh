8e223e69933e0c2122a4eaf33839cabb289dee7c
Raúl Gracia <raul.gracia@emc.com>
2020-01-22 19:48:33 +0100

    Refactor sample code.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- docker/pravega/scripts/sample-bookkeeper-topology.sh
+++ docker/pravega/scripts/sample-bookkeeper-topology.sh
@@ -1,27 +1,14 @@
 #!/usr/bin/env sh
-# Here's a bash example to show just how simple these scripts can be
-# Assuming we have flat network with everything on a single switch, we can fake a rack topology.
-# This could occur in a lab environment where we have limited nodes,like 2-8 physical machines on a unmanaged switch.
-# This may also apply to multiple virtual machines running on the same physical hardware.
-# The number of machines isn't important, but that we are trying to fake a network topology when there isn't one.
 #
-#       +----------+    +--------+
-#       |jobtracker|    |datanode|
-#       +----------+    +--------+
-#              \        /
-#  +--------+  +--------+  +--------+
-#  |datanode|--| switch |--|datanode|
-#  +--------+  +--------+  +--------+
-#              /        \
-#       +--------+    +--------+
-#       |datanode|    |namenode|
-#       +--------+    +--------+
+# Copyright (c) 2017 Dell Inc., or its subsidiaries. All Rights Reserved.
 #
-# With this network topology, we are treating each host as a rack.  This is being done by taking the last octet
-# in the datanode's IP and prepending it with the word '/rack-'.  The advantage for doing this is so HDFS
-# can create its 'off-rack' block copy.
-# 1) 'echo $@' will echo all ARGV values to xargs.
-# 2) 'xargs' will enforce that we print a single argv value per line
-# 3) 'awk' will split fields on dots and append the last field to the string '/rack-'. If awk
-#    fails to split on four dots, it will still print '/rack-' last field value
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+
+# In this exemple, we treat each host as a rack (for test-only purposes). This is being done by taking the last octet
+# in the datanode's IP and prepending it with the word '/datacenter/rack-'.
 echo $@ | xargs -n 1 | awk -F '.' '{print "/datacenter/rack-"$NF}'
