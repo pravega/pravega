09dab3a51fb9dba750d6b00dd6a9f152baf68159
Sachin Joshi <sachin.joshi@emc.com>
2020-01-22 16:39:39 -0800

    Issue 4505: Use name ExtendedS3 for stats logger.
    Signed-off-by: Sachin Joshi <sachin.joshi@emc.com>

--- bindings/src/main/java/io/pravega/storage/extendeds3/ExtendedS3Metrics.java
+++ bindings/src/main/java/io/pravega/storage/extendeds3/ExtendedS3Metrics.java
@@ -19,18 +19,18 @@ import io.pravega.shared.metrics.StatsLogger;
  * Defines all Metrics used by the ExtendedS3Storage class.
  */
 final class ExtendedS3Metrics {
-    private static final StatsLogger ECS_LOGGER = MetricsProvider.createStatsLogger("ExtendedS3");
-    static final OpStatsLogger READ_LATENCY = ECS_LOGGER.createStats(MetricsNames.STORAGE_READ_LATENCY);
-    static final OpStatsLogger WRITE_LATENCY = ECS_LOGGER.createStats(MetricsNames.STORAGE_WRITE_LATENCY);
-    static final OpStatsLogger CREATE_LATENCY = ECS_LOGGER.createStats(MetricsNames.STORAGE_CREATE_LATENCY);
-    static final OpStatsLogger DELETE_LATENCY = ECS_LOGGER.createStats(MetricsNames.STORAGE_DELETE_LATENCY);
-    static final OpStatsLogger CONCAT_LATENCY = ECS_LOGGER.createStats(MetricsNames.STORAGE_CONCAT_LATENCY);
+    private static final StatsLogger EXTENDED_S3_LOGGER = MetricsProvider.createStatsLogger("ExtendedS3");
+    static final OpStatsLogger READ_LATENCY = EXTENDED_S3_LOGGER.createStats(MetricsNames.STORAGE_READ_LATENCY);
+    static final OpStatsLogger WRITE_LATENCY = EXTENDED_S3_LOGGER.createStats(MetricsNames.STORAGE_WRITE_LATENCY);
+    static final OpStatsLogger CREATE_LATENCY = EXTENDED_S3_LOGGER.createStats(MetricsNames.STORAGE_CREATE_LATENCY);
+    static final OpStatsLogger DELETE_LATENCY = EXTENDED_S3_LOGGER.createStats(MetricsNames.STORAGE_DELETE_LATENCY);
+    static final OpStatsLogger CONCAT_LATENCY = EXTENDED_S3_LOGGER.createStats(MetricsNames.STORAGE_CONCAT_LATENCY);
 
-    static final Counter READ_BYTES = ECS_LOGGER.createCounter(MetricsNames.STORAGE_READ_BYTES);
-    static final Counter WRITE_BYTES = ECS_LOGGER.createCounter(MetricsNames.STORAGE_WRITE_BYTES);
-    static final Counter CONCAT_BYTES = ECS_LOGGER.createCounter(MetricsNames.STORAGE_CONCAT_BYTES);
+    static final Counter READ_BYTES = EXTENDED_S3_LOGGER.createCounter(MetricsNames.STORAGE_READ_BYTES);
+    static final Counter WRITE_BYTES = EXTENDED_S3_LOGGER.createCounter(MetricsNames.STORAGE_WRITE_BYTES);
+    static final Counter CONCAT_BYTES = EXTENDED_S3_LOGGER.createCounter(MetricsNames.STORAGE_CONCAT_BYTES);
 
-    static final Counter CREATE_COUNT = ECS_LOGGER.createCounter(MetricsNames.STORAGE_CREATE_COUNT);
-    static final Counter DELETE_COUNT = ECS_LOGGER.createCounter(MetricsNames.STORAGE_DELETE_COUNT);
-    static final Counter CONCAT_COUNT = ECS_LOGGER.createCounter(MetricsNames.STORAGE_CONCAT_COUNT);
+    static final Counter CREATE_COUNT = EXTENDED_S3_LOGGER.createCounter(MetricsNames.STORAGE_CREATE_COUNT);
+    static final Counter DELETE_COUNT = EXTENDED_S3_LOGGER.createCounter(MetricsNames.STORAGE_DELETE_COUNT);
+    static final Counter CONCAT_COUNT = EXTENDED_S3_LOGGER.createCounter(MetricsNames.STORAGE_CONCAT_COUNT);
 }
