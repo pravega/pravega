d0ced0435d1b3accb22969a4d9b5db1c34b3891d
root <ravi.rajamani@emc.com>
2020-01-27 20:56:33 -0800

    add test

--- test/testcommon/src/test/java/io/pravega/test/common/UtilityMethodsTest.java
+++ test/testcommon/src/test/java/io/pravega/test/common/UtilityMethodsTest.java
@@ -36,24 +36,24 @@ public class UtilityMethodsTest {
      * @throws InterruptedException Required for ExecutorService.invokeAll
      * @throws ExecutionException   Required for Futures.get()
      */
-//    @Test(timeout = 10000)
-//    public void getAvailableListenPortTest() throws InterruptedException, ExecutionException {
-//        final int threadCount = 5;
-//        Callable<Integer> task = () -> TestUtils.getAvailableListenPort();
-//        List<Callable<Integer>> tasks = Collections.nCopies(threadCount, task);
-//        @Cleanup("shutdownNow")
-//        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
-//        List<Future<Integer>> futures = executorService.invokeAll(tasks);
-//        Set<Integer> resultList = new HashSet<>(futures.size());
-//
-//        // Set cannot hold duplicate elements.
-//        // Add ports returned by getAvailableListenPort() to the set to verify they are unique.
-//        for (Future<Integer> future : futures) {
-//            int port = future.get();
-//            assertTrue(!resultList.contains(port));
-//            resultList.add(port);
-//        }
-//        assertEquals(threadCount, futures.size());
-//        executorService.shutdown();
-//    }
+    @Test(timeout = 10000)
+    public void getAvailableListenPortTest() throws InterruptedException, ExecutionException {
+        final int threadCount = 5;
+        Callable<Integer> task = () -> TestUtils.getAvailableListenPort();
+        List<Callable<Integer>> tasks = Collections.nCopies(threadCount, task);
+        @Cleanup("shutdownNow")
+        ExecutorService executorService = Executors.newFixedThreadPool(threadCount);
+        List<Future<Integer>> futures = executorService.invokeAll(tasks);
+        Set<Integer> resultList = new HashSet<>(futures.size());
+
+        // Set cannot hold duplicate elements.
+        // Add ports returned by getAvailableListenPort() to the set to verify they are unique.
+        for (Future<Integer> future : futures) {
+            int port = future.get();
+            assertTrue(!resultList.contains(port));
+            resultList.add(port);
+        }
+        assertEquals(threadCount, futures.size());
+        executorService.shutdown();
+    }
 }
