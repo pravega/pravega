8b9c69d765f9676b11969bb8e9d37c0f07c0edf3
shivesh ranjan <13659022+shiveshr@users.noreply.github.com>
2020-02-03 20:28:41 +0530

    Issue 4456: Consolidate get active txn calls during commit transactions for multiple transactions (#4499)
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>

--- controller/src/main/java/io/pravega/controller/store/stream/PravegaTablesStoreHelper.java
+++ controller/src/main/java/io/pravega/controller/store/stream/PravegaTablesStoreHelper.java
@@ -305,6 +305,45 @@ public class PravegaTablesStoreHelper {
     }
 
     /**
+     * Method to retrieve the value for a given key from a table. This method takes a deserialization function and deserializes
+     * the received byte[] using the supplied function.
+     * @param tableName tableName
+     * @param keys keys to read
+     * @param fromBytes deserialization function
+     * @param nonExistent entry to populate for non existent keys
+     * @param <T> Type of deserialized object
+     * @return CompletableFuture which when completed will have the versionedMetadata retrieved from the store.
+     */
+    public <T> CompletableFuture<List<VersionedMetadata<T>>> getEntries(String tableName, List<String> keys, 
+                                                                        Function<byte[], T> fromBytes, VersionedMetadata<T> nonExistent) {
+        log.trace("get entries called for : {} keys : {}", tableName, keys);
+        List<TableKey<byte[]>> tableKeys = keys.stream().map(key -> new TableKeyImpl<>(key.getBytes(Charsets.UTF_8), null))
+                                           .collect(Collectors.toList());
+        CompletableFuture<List<VersionedMetadata<T>>> result = new CompletableFuture<>();
+
+        String message = "get entry: key: %s table: %s";
+        withRetries(() -> segmentHelper.readTable(tableName, tableKeys, authToken.get(), RequestTag.NON_EXISTENT_ID),
+                () -> String.format(message, keys, tableName))
+                .thenApplyAsync(entries -> entries.stream().map(entry -> {
+                    if (entry.getKey().getVersion().equals(KeyVersion.NOT_EXISTS)) {
+                        return nonExistent;
+                    } else {
+                        return new VersionedMetadata<>(fromBytes.apply(entry.getValue()), 
+                                new Version.LongVersion(entry.getKey().getVersion().getSegmentVersion()));
+                    }
+                }).collect(Collectors.toList()), executor)
+                .whenCompleteAsync((r, e) -> {
+                   if (e != null) {
+                       result.completeExceptionally(e);
+                   } else {
+                       result.complete(r);
+                   }
+                }, executor);
+        
+        return result;
+    }
+
+    /**
      * Method to remove entry from the store.
      * @param tableName tableName
      * @param key key
