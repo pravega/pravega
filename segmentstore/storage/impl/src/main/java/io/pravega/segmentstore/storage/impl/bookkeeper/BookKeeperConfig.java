5ce0f67493045d2915b15f3fda49e82097dcc324
Raúl Gracia <raul.gracia@emc.com>
2020-02-14 15:55:17 +0100

    Issue 4558: Changed Default BK Digest type to CRC32C (#4559)
    Add configuration option for Bookkeeper digest type.
    Changed default Bookkeeper digest type to CRC32C.
    
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>
    Signed-off-by: Ranjan-Padhi <Ranjan_rashmi_Padhi@Dell.com>

--- segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperConfig.java
+++ segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperConfig.java
@@ -18,6 +18,7 @@ import java.nio.charset.StandardCharsets;
 import java.time.Duration;
 import java.util.Arrays;
 import lombok.Getter;
+import org.apache.bookkeeper.client.BookKeeper;
 
 /**
  * General configuration for BookKeeper Client.
@@ -48,6 +49,7 @@ public class BookKeeperConfig {
     public static final Property<Integer> BK_MIN_NUM_RACKS_PER_WRITE_QUORUM = Property.named("minNumRacksPerWriteQuorum", 2);
     public static final Property<String> BK_NETWORK_TOPOLOGY_SCRIPT_FILE_NAME = Property.named("networkTopologyScriptFileName",
             "/opt/pravega/scripts/sample-bookkeeper-topology.sh");
+    public static final Property<String> BK_DIGEST_TYPE = Property.named("digestType", BookKeeper.DigestType.CRC32C.name());
 
     public static final String COMPONENT_CODE = "bookkeeper";
     /**
@@ -172,6 +174,9 @@ public class BookKeeperConfig {
     @Getter
     private final String networkTopologyFileName;
 
+    @Getter
+    private final BookKeeper.DigestType digestType;
+
     //endregion
 
     //region Constructor
@@ -220,6 +225,8 @@ public class BookKeeperConfig {
         this.enforceMinNumRacksPerWriteQuorum = properties.getBoolean(BK_ENFORCE_MIN_NUM_RACKS_PER_WRITE);
         this.minNumRacksPerWriteQuorum = properties.getInt(BK_MIN_NUM_RACKS_PER_WRITE_QUORUM);
         this.networkTopologyFileName = properties.get(BK_NETWORK_TOPOLOGY_SCRIPT_FILE_NAME);
+
+        this.digestType = getDigestType(properties.get(BK_DIGEST_TYPE));
     }
 
     /**
@@ -236,6 +243,19 @@ public class BookKeeperConfig {
      */
     public static ConfigBuilder<BookKeeperConfig> builder() {
         return new ConfigBuilder<>(COMPONENT_CODE, BookKeeperConfig::new);
+    }
+
+    private BookKeeper.DigestType getDigestType(String digestType) {
+        if (digestType.equals(BookKeeper.DigestType.MAC.name())) {
+            return BookKeeper.DigestType.MAC;
+        } else if (digestType.equals(BookKeeper.DigestType.CRC32.name())) {
+            return BookKeeper.DigestType.CRC32;
+        } else if (digestType.equals(BookKeeper.DigestType.DUMMY.name())) {
+            return BookKeeper.DigestType.DUMMY;
+        } else {
+            // Default digest for performance reasons.
+            return BookKeeper.DigestType.CRC32C;
+        }
     }
 
     //endregion
