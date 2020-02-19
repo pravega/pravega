5c49cd0bfaf0c801799c8b7d6f4396d783d66226
Colin Hryniowski <co-jo@users.noreply.github.com>
2020-02-19 14:08:16 -0800

    Revert unintended formatting changes.
    Signed-off-by: co-jo <colin@CNENHRYNICL3C.corp.emc.com>

--- test/system/src/main/java/io/pravega/test/system/framework/kubernetes/K8sClient.java
+++ test/system/src/main/java/io/pravega/test/system/framework/kubernetes/K8sClient.java
@@ -1,5 +1,6 @@
 /**
  * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
@@ -7,6 +8,7 @@
  *     http://www.apache.org/licenses/LICENSE-2.0
  */
 package io.pravega.test.system.framework.kubernetes;
+
 import com.google.common.collect.ImmutableMap;
 import com.google.gson.JsonSyntaxException;
 import com.google.gson.reflect.TypeToken;
