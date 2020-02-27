7f4b3d8a73a8f5c8af3e01b1a95bbaae136d4ae6
Tom Kaitchuck <tom.kaitchuck@emc.com>
2020-02-26 22:23:41 -0800

    naming
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- client/src/main/java/io/pravega/client/stream/impl/EventStreamReaderImpl.java
+++ client/src/main/java/io/pravega/client/stream/impl/EventStreamReaderImpl.java
@@ -163,7 +163,7 @@ public class EventStreamReaderImpl<Type> implements EventStreamReader<Type> {
     private void blockFor(long timeoutMs) {
         Exceptions.handleInterrupted(() -> {
             @SuppressWarnings("unused")
-            boolean _aquired = segmentsWithData.tryAcquire(timeoutMs, TimeUnit.MILLISECONDS);
+            boolean aquired = segmentsWithData.tryAcquire(timeoutMs, TimeUnit.MILLISECONDS);
         });
     }
     
