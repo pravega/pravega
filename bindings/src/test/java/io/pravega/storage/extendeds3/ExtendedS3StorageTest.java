c8579b46ff9c8f2a1a7fb0c51fcf5d26c6ca2ad5
Sachin Jayant Joshi <44757683+sachin-j-joshi@users.noreply.github.com>
2020-03-10 10:03:53 -0700

    Issue 4601: (RollingStorage/ExtendedS3) S3 client consumes whole InputStream and causes java.lang.IllegalArgumentException (#4602) (#4604)
    RollingStorage.write sends in BoundedInputStream to storage adapters to prevent reading more data. S3 client normally consumes whole InputStream and causes java.lang.IllegalArgumentException.
    
    Also, S3 client mocks simulate this InvalidRange behavior to match how the real system behaves.
    
    Signed-off-by: Sachin Joshi <sachin.joshi@emc.com>

--- bindings/src/test/java/io/pravega/storage/extendeds3/ExtendedS3StorageTest.java
+++ bindings/src/test/java/io/pravega/storage/extendeds3/ExtendedS3StorageTest.java
@@ -15,6 +15,7 @@ import com.emc.object.s3.bean.ObjectKey;
 import com.emc.object.s3.jersey.S3JerseyClient;
 import com.emc.object.s3.request.DeleteObjectsRequest;
 import com.emc.object.util.ConfigUri;
+import io.pravega.common.io.BoundedInputStream;
 import io.pravega.common.util.ConfigBuilder;
 import io.pravega.common.util.Property;
 import io.pravega.segmentstore.contracts.SegmentProperties;
@@ -140,7 +141,9 @@ public class ExtendedS3StorageTest extends IdempotentStorageTestBase {
             String str = "0123456789";
             int totalBytesWritten = 0;
             for (int i = 1; i < 5; i++) {
-                storage.write(handleA, totalBytesWritten, new ByteArrayInputStream(str.getBytes()), i, null).join();
+                try (BoundedInputStream bis = new BoundedInputStream(new ByteArrayInputStream(str.getBytes()), i)) {
+                    storage.write(handleA, totalBytesWritten, bis, i, null).join();
+                }
                 totalBytesWritten += i;
                 assertEquals(totalBytesWritten, ExtendedS3Metrics.WRITE_BYTES.get());
                 assertEquals(i, ExtendedS3Metrics.WRITE_LATENCY.toOpStatsData().getNumSuccessfulEvents());
