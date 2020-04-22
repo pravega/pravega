4f7423c36b4ed8af5ac7e0cf00f5c8af5e08272a
kevinhan88 <kevinhan88@gmail.com>
2020-04-21 18:00:44 -0700

    Issue-4642: load TLS certificates from /etc/secret-volume/ca-bundle to be align with other TLS materials
    Signed-off-by: kevinhan88 <kevinhan88@gmail.com>

--- docker/pravega/scripts/common.sh
+++ docker/pravega/scripts/common.sh
@@ -36,7 +36,7 @@ add_system_property_ecs_config_uri() {
 
 # Add ECS certificates into Java truststore
 add_certs_into_truststore() {
-    CERTS=/etc/ssl/certs/java/ecs-certs/*
+    CERTS=/etc/secret-volume/ca-bundle/*
     for cert in $CERTS
     do
       yes | keytool -importcert -storepass changeit -file "${cert}" -keystore /etc/ssl/certs/java/cacerts || true
