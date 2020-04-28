79ab867e7ea9ae1e3525e50936a5853de5668a54
Andrei Paduroiu <andreipaduroiu@users.noreply.github.com>
2020-04-28 06:50:27 -0700

    Issue 4743: (SegmentStore) Infinite loop in StorageWriter if deleting a segment while flushing (#4744)
    Fixed a bug in SegmentAggregator where it was possible to enter an infinite loop if deleting a segment while flushing.
    
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- segmentstore/server/src/main/java/io/pravega/segmentstore/server/writer/SegmentAggregator.java
+++ segmentstore/server/src/main/java/io/pravega/segmentstore/server/writer/SegmentAggregator.java
@@ -644,6 +644,10 @@ class SegmentAggregator implements WriterSegmentProcessor, AutoCloseable {
      * Determines whether flushFully can continue given the current state of this SegmentAggregator.
      */
     private boolean canContinueFlushingFully() {
+        if (this.metadata.isDeleted()) {
+            return false; // No point in flushing if the segment has been deleted in the meantime.
+        }
+
         StorageOperation next = this.operations.getFirst();
         return isAppendOperation(next) || isTruncateOperation(next);
     }
