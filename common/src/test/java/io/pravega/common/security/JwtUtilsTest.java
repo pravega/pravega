370417348917555dd8d4d1f91da55a373c603a90
Ravi Sharda <ravi.sharda@emc.com>
2020-05-27 11:46:33 +0530

    Issue 4811: Replace JWT object-JSON mapping library (#4812)
    Signed-off-by: Ravi Sharda <ravi.sharda@emc.com>
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- common/src/test/java/io/pravega/common/security/JwtUtilsTest.java
+++ common/src/test/java/io/pravega/common/security/JwtUtilsTest.java
@@ -12,6 +12,9 @@ package io.pravega.common.security;
 import io.pravega.test.common.JwtBody;
 import org.junit.Test;
 
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotNull;
 import static org.junit.Assert.assertNull;
@@ -29,8 +32,9 @@ public class JwtUtilsTest {
         //        "aud": "segmentstore",
         //        "iat": 1516239022
         //     }
+        JwtBody jwtBody = JwtBody.builder().subject("1234567890").audience("segmentstore").issuedAtTime(1516239022L).build();
         String token = String.format("%s.%s.%s", "base64-encoded-header",
-                JwtBody.builder().subject("1234567890").audience("segmentstore").issuedAtTime(1516239022L).build(),
+                Base64.getEncoder().encodeToString(jwtBody.toString().getBytes(StandardCharsets.US_ASCII)),
                 "base64-encoded-signature");
 
         assertNull(JwtUtils.extractExpirationTime(token));
