030091263fa09533653512af5d69b4d858a9b520
Enrico Olivelli <eolivelli@gmail.com>
2020-06-04 17:52:08 +0200

    Issue 4531: Use new BookKeeper API and Upgrade to BookKeeper 4.9.2 (#4798)
    * Upgrades BookKeeper to 4.9.2 in Java code (not docker images)
    * Replaces usages of LedgerHandle with WriteHandle and ReadHandle.
    * Uses the new CompletableFuture based API.
    
    Signed-off-by: Enrico Olivelli <eolivelli@apache.org>
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperServiceRunner.java
+++ segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperServiceRunner.java
@@ -109,6 +109,7 @@ public class BookKeeperServiceRunner implements AutoCloseable {
         Preconditions.checkState(this.servers.get(0) != null, "Bookie already stopped.");
         val bk = this.servers.get(bookieIndex);
         bk.shutdown();
+        log.info("Bookie {} is stopping.", bookieIndex);
         this.servers.set(bookieIndex, null);
         log.info("Bookie {} stopped.", bookieIndex);
     }
@@ -122,8 +123,9 @@ public class BookKeeperServiceRunner implements AutoCloseable {
     public void startBookie(int bookieIndex) throws Exception {
         Preconditions.checkState(this.servers.size() > 0, "No Bookies initialized. Call startAll().");
         Preconditions.checkState(this.servers.get(0) == null, "Bookie already running.");
+        log.info("Bookie {} is starting.", bookieIndex);
         this.servers.set(bookieIndex, runBookie(this.bookiePorts.get(bookieIndex)));
-        log.info("Bookie {} stopped.", bookieIndex);
+        log.info("Bookie {} started.", bookieIndex);
     }
 
     /**
