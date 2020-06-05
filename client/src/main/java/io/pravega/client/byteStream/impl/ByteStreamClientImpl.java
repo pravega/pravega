5ea3e7aa66939005332262e4eba06e35fdc4f5e3
Sandeep <sandeep.shridhar@emc.com>
2020-06-05 19:29:39 +0530

    Fix junits.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

--- client/src/main/java/io/pravega/client/byteStream/impl/ByteStreamClientImpl.java
+++ client/src/main/java/io/pravega/client/byteStream/impl/ByteStreamClientImpl.java
@@ -75,8 +75,8 @@ public class ByteStreamClientImpl implements ByteStreamClientFactory {
     @Override
     public ByteStreamWriter createByteStreamWriter(String streamName) {
         StreamSegments segments = Futures.getThrowingException(controller.getCurrentSegments(scope, streamName));
-        Preconditions.checkArgument(segments.getNumberOfSegments() == 0, "Stream is sealed");
-        Preconditions.checkArgument(segments.getNumberOfSegments() == 1, "Stream is configured with more than one segment");
+        Preconditions.checkState(segments.getNumberOfSegments() > 0, "Stream is sealed");
+        Preconditions.checkState(segments.getNumberOfSegments() == 1, "Stream is configured with more than one segment");
         Segment segment = segments.getSegments().iterator().next();
         EventWriterConfig config = EventWriterConfig.builder().build();
         String delegationToken = segments.getDelegationToken();
