efa9e08b205c6c45631b5bd3b492514eeae36421
Ravi Sharda <ravi.sharda@emc.com>
2020-06-09 03:02:07 +0530

    Issue 4712: Make configuration keys consistent and uniform (#4713)
    Modifies configuration keys across the board.
    Supports backward compatibility, so that old keys/names can be migrated to new ones asynchronously. Old names continue to work. Also, old and new names can coexist.
    
    Signed-off-by: Ravi Sharda <ravi.sharda@emc.com>

--- segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/noop/StorageExtraConfig.java
+++ segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/noop/StorageExtraConfig.java
@@ -22,8 +22,8 @@ import lombok.extern.slf4j.Slf4j;
 @Slf4j
 public class StorageExtraConfig {
 
-    public static final Property<Boolean> STORAGE_NO_OP_MODE = Property.named("storageNoOpMode", false);
-    public static final Property<Integer> STORAGE_WRITE_NO_OP_LATENCY = Property.named("storageWriteNoOpLatencyMillis", 20);
+    public static final Property<Boolean> STORAGE_NO_OP_MODE = Property.named("noOp.mode.enable", false, "storageNoOpMode");
+    public static final Property<Integer> STORAGE_WRITE_NO_OP_LATENCY = Property.named("noOp.write.latency.milliseconds", 20, "storageWriteNoOpLatencyMillis");
     private static final String COMPONENT_CODE = "storageextra";
 
     /**
