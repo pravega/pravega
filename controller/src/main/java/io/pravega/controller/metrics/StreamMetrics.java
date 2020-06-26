48f8091d5b5ad98d2af7576a42610e1fb6f6bb70
Raúl Gracia <raul.gracia@emc.com>
2020-06-26 09:23:12 +0200

    Close scope latency metrics.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- controller/src/main/java/io/pravega/controller/metrics/StreamMetrics.java
+++ controller/src/main/java/io/pravega/controller/metrics/StreamMetrics.java
@@ -308,6 +308,8 @@ public final class StreamMetrics extends AbstractControllerMetrics {
             INSTANCE.get().sealStreamLatency.close();
             INSTANCE.get().updateStreamLatency.close();
             INSTANCE.get().truncateStreamLatency.close();
+            INSTANCE.get().createScopeLatency.close();
+            INSTANCE.get().deleteScopeLatency.close();
             INSTANCE.set(null);
         }
     }
