5abbfa01ef429b42ed2c9ca4d411bb47a503aec8
Andrei Paduroiu <andrei.paduroiu@emc.com>
2020-07-02 08:32:22 -0700

    Merge conflict fixes.
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>

--- controller/src/main/java/io/pravega/controller/metrics/StreamMetrics.java
+++ controller/src/main/java/io/pravega/controller/metrics/StreamMetrics.java
@@ -74,6 +74,8 @@ public final class StreamMetrics extends AbstractControllerMetrics {
         sealStreamLatency = STATS_LOGGER.createStats(SEAL_STREAM_LATENCY);
         updateStreamLatency = STATS_LOGGER.createStats(UPDATE_STREAM_LATENCY);
         truncateStreamLatency = STATS_LOGGER.createStats(TRUNCATE_STREAM_LATENCY);
+        createScopeLatency = STATS_LOGGER.createStats(CREATE_SCOPE_LATENCY);
+        deleteScopeLatency = STATS_LOGGER.createStats(DELETE_SCOPE_LATENCY);
         createKeyValueTableLatency = STATS_LOGGER.createStats(CREATE_KVTABLE_LATENCY);
         deleteKeyValueTableLatency = STATS_LOGGER.createStats(DELETE_KVTABLE_LATENCY);
     }
