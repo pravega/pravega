cc36fcd3d2c804b63f8bfd6c8ee6af3b6464ca18
Andrei Paduroiu <andrei.paduroiu@emc.com>
2020-07-02 08:32:22 -0700

    Merge conflict fixes.
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>
    Signed-off-by: SomeshJoshi19 <SomeshJoshi19@gmail.com>

--- controller/src/main/java/io/pravega/controller/metrics/StreamMetrics.java
+++ controller/src/main/java/io/pravega/controller/metrics/StreamMetrics.java
@@ -91,11 +91,16 @@ public final class StreamMetrics extends AbstractControllerMetrics {
         updateStreamLatency = STATS_LOGGER.createStats(UPDATE_STREAM_LATENCY);
         truncateStreamLatency = STATS_LOGGER.createStats(TRUNCATE_STREAM_LATENCY);
 <<<<<<< HEAD
+<<<<<<< HEAD
         createScopeLatency = STATS_LOGGER.createStats(CREATE_SCOPE_LATENCY);
         deleteScopeLatency = STATS_LOGGER.createStats(DELETE_SCOPE_LATENCY);
         createKeyValueTableLatency = STATS_LOGGER.createStats(CREATE_KVTABLE_LATENCY);
         deleteKeyValueTableLatency = STATS_LOGGER.createStats(DELETE_KVTABLE_LATENCY);
 =======
+=======
+        createScopeLatency = STATS_LOGGER.createStats(CREATE_SCOPE_LATENCY);
+        deleteScopeLatency = STATS_LOGGER.createStats(DELETE_SCOPE_LATENCY);
+>>>>>>> Merge conflict fixes.
         createKeyValueTableLatency = STATS_LOGGER.createStats(CREATE_KVTABLE_LATENCY);
 <<<<<<< HEAD
 >>>>>>> Issue 4796: (KeyValue Tables) CreateAPI for Key Value Tables (#4797)
