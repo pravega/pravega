97ca451c9bcd9703caa7b01dd03a5236c1f167bd
Andrei Paduroiu <andrei.paduroiu@emc.com>
2020-07-06 08:10:29 -0700

    Minor things.
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>
    Signed-off-by: Ranjan-Padhi <Ranjan_rashmi_Padhi@Dell.com>

--- controller/src/test/java/io/pravega/controller/server/v1/ZKControllerServiceImplTest.java
+++ controller/src/test/java/io/pravega/controller/server/v1/ZKControllerServiceImplTest.java
@@ -230,26 +230,30 @@ public class ZKControllerServiceImplTest extends ControllerServiceImplTest {
     }
 
     @Test
+    @Override
     public void createKeyValueTableTests() {
-        //TODO: Place holder till we fix the test
-        assertTrue(true);
+        // TODO: consider implementing ZK metadata support or removing altogether (https://github.com/pravega/pravega/issues/4922).
+        // Key-Value Tables are not implemented in ZK Metadata.
     }
 
     @Test
+    @Override
     public void getCurrentSegmentsKeyValueTableTest() {
-        //TODO: Place holder till we fix the test
-        assertTrue(true);
+        // TODO: consider implementing ZK metadata support or removing altogether (https://github.com/pravega/pravega/issues/4922).
+        // Key-Value Tables are not implemented in ZK Metadata.
     }
 
     @Test
+    @Override
     public void kvtablesInScopeTest() {
-        //TODO: Place holder till we fix the test
-        assertTrue(true);
+        // TODO: consider implementing ZK metadata support or removing altogether (https://github.com/pravega/pravega/issues/4922).
+        // Key-Value Tables are not implemented in ZK Metadata.
     }
 
     @Test
+    @Override
     public void deleteKeyValueTableTests() {
-        //TODO: Place holder till we fix the test
-        assertTrue(true);
+        // TODO: consider implementing ZK metadata support or removing altogether (https://github.com/pravega/pravega/issues/4922).
+        // Key-Value Tables are not implemented in ZK Metadata.
     }
 }
