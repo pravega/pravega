ca0cbdd6622eaebfc309e03cea489a563880bcba
Andrei Paduroiu <andreipaduroiu@users.noreply.github.com>
2020-07-08 07:12:00 -0700

    Issue 4569: Key Value Tables (#4758)
    Adds Key-Value Tables as a new top-level primitive.
    See https://github.com/pravega/pravega/wiki/PDP-39-Key-Value-Tables for details.
    
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>
    Signed-off-by: Prajakta Belgundi <prajakta.belgundi@emc.com>

---
+++ common/src/test/java/io/pravega/common/util/ByteArrayComparatorTests.java
@@ -0,0 +1,155 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.common.util;
+
+import java.util.AbstractMap;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Map;
+import java.util.function.BiFunction;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+import lombok.val;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Unit tests for the ByteArrayComparator class.
+ */
+public class ByteArrayComparatorTests {
+    private static final int COUNT = 2000;
+
+    /**
+     * Tests comparing raw byte arrays.
+     */
+    @Test
+    public void testCompareByteArray() {
+        val sortedData = generateSortedData();
+        test(sortedData, new ByteArrayComparator()::compare);
+    }
+
+    /**
+     * Tests comparing equal-length {@link ArrayView} instances.
+     */
+    @Test
+    public void testCompareArrayView() {
+        val sortedData = generateSortedData().stream().map(ByteArraySegment::new).collect(Collectors.toList());
+        test(sortedData, new ByteArrayComparator()::compare);
+    }
+
+    /**
+     * Tests comparing different-length {@link ArrayView} instances.
+     */
+    @Test
+    public void testCompareArrayViewVariableSize() {
+        testCompareBufferView(a -> a, new ByteArrayComparator()::compare);
+    }
+
+    /**
+     * Tests comparing different-length {@link BufferView} instances.
+     */
+    @Test
+    public void testCompareBufferViewVariableSize() {
+        testCompareBufferView(a -> {
+                    val builder = BufferView.builder();
+                    for (int i = 0; i < a.getLength(); i++) {
+                        builder.add(a.slice(i, 1));
+                    }
+                    return builder.build();
+                },
+                new ByteArrayComparator()::compare);
+    }
+
+    private <T extends BufferView> void testCompareBufferView(Function<ArrayView, T> toBufferView, BiFunction<T, T, Integer> comparator) {
+        val sortedData = generateSortedVariableData().stream()
+                .map(ByteArraySegment::new)
+                .map(toBufferView)
+                .collect(Collectors.toList());
+        sortedData.add(0, toBufferView.apply(new ByteArraySegment(new byte[0]))); // Empty.
+        test(sortedData, comparator);
+        for (val s : sortedData) {
+            int compareResult = comparator.apply(toBufferView.apply(new ByteArraySegment(ByteArrayComparator.getMinValue())), s);
+            if (compareResult == 0) {
+                // Only equal to itself.
+                Assert.assertTrue(s.getLength() == 1 && s.getBufferViewReader().readByte() == ByteArrayComparator.MIN_VALUE);
+            } else if (compareResult > 0) {
+                // Only empty array is smaller than it.
+                Assert.assertEquals(0, s.getLength());
+            }
+        }
+    }
+
+    /**
+     * Tests the {@link  ByteArrayComparator#getNextItemOfSameLength}.
+     */
+    @Test
+    public void testGetNextItemOfSameLength() {
+        val c = new ByteArrayComparator();
+        val max = ByteArrayComparator.MAX_VALUE;
+        val almostMax = (byte) (max - 1);
+        List<Map.Entry<byte[], byte[]>> tests = Arrays.asList(
+                new AbstractMap.SimpleImmutableEntry<>(new byte[]{}, null),
+                new AbstractMap.SimpleImmutableEntry<>(new byte[]{almostMax}, new byte[]{max}),
+                new AbstractMap.SimpleImmutableEntry<>(new byte[]{max}, null),
+                new AbstractMap.SimpleImmutableEntry<>(new byte[]{0, 1, 2, 3, 4}, new byte[]{0, 1, 2, 3, 5}),
+                new AbstractMap.SimpleImmutableEntry<>(new byte[]{max, almostMax}, new byte[]{max, max}),
+                new AbstractMap.SimpleImmutableEntry<>(new byte[]{max, max}, null));
+
+        for (val e : tests) {
+            val actual = ByteArrayComparator.getNextItemOfSameLength(new ByteArraySegment(e.getKey()));
+            if (e.getValue() == null) {
+                Assert.assertNull(actual);
+            } else {
+                val expected = new ByteArraySegment(e.getValue());
+                Assert.assertEquals(0, c.compare(expected, actual));
+            }
+        }
+    }
+
+    private ArrayList<byte[]> generateSortedData() {
+        val sortedData = new ArrayList<byte[]>();
+        int maxValue = COUNT / 2;
+        for (int i = -maxValue; i < maxValue; i++) {
+            byte[] data = new byte[Long.BYTES];
+            BitConverter.writeUnsignedLong(data, 0, i);
+            sortedData.add(data);
+        }
+        return sortedData;
+    }
+
+    private List<byte[]> generateSortedVariableData() {
+        // We use String sort to mimic our comparison, then parse the string putting each digit into its own byte array index.
+        return LongStream.range(0, COUNT)
+                .mapToObj(Long::toString)
+                .sorted()
+                .map(this::parseDigits)
+                .collect(Collectors.toList());
+    }
+
+    private byte[] parseDigits(String digits) {
+        byte[] result = new byte[digits.length()];
+        for (int i = 0; i < digits.length(); i++) {
+            result[i] = Byte.parseByte(digits.substring(i, i + 1));
+        }
+        return result;
+    }
+
+    private <T> void test(List<T> sortedData, BiFunction<T, T, Integer> comparator) {
+        for (int i = 0; i < sortedData.size(); i++) {
+            for (int j = 0; j < sortedData.size(); j++) {
+                int expectedResult = (int) Math.signum(Integer.compare(i, j));
+                int actualResult = (int) Math.signum(comparator.apply(sortedData.get(i), sortedData.get(j)));
+                Assert.assertEquals("Unexpected comparison value.", expectedResult, actualResult);
+            }
+        }
+    }
+}
---
+++ controller/src/main/java/io/pravega/controller/store/kvtable/AbstractKVTableBase.java
@@ -0,0 +1,194 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.controller.store.kvtable;
+
+import com.google.common.collect.ImmutableList;
+import io.pravega.client.tables.KeyValueTableConfiguration;
+import io.pravega.common.concurrent.Futures;
+import io.pravega.controller.store.Version;
+import io.pravega.controller.store.VersionedMetadata;
+import io.pravega.controller.store.kvtable.records.KVTEpochRecord;
+import io.pravega.controller.store.kvtable.records.KVTSegmentRecord;
+import io.pravega.controller.store.kvtable.records.KVTConfigurationRecord;
+import io.pravega.controller.store.kvtable.records.KVTStateRecord;
+import io.pravega.controller.store.stream.StoreException;
+import lombok.extern.slf4j.Slf4j;
+
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.CompletionStage;
+import java.util.stream.IntStream;
+
+@Slf4j
+public abstract class AbstractKVTableBase implements KeyValueTable {
+    protected final String scopeName;
+    protected final String name;
+
+    AbstractKVTableBase(final String scope, final String name) {
+        this.scopeName = scope;
+        this.name = name;
+    }
+
+    @Override
+    public String getScopeName() {
+        return this.scopeName;
+    }
+
+    @Override
+    public String getName() {
+        return this.name;
+    }
+
+    @Override
+    public CompletableFuture<Void> updateState(final KVTableState state) {
+        return getStateData(true)
+                .thenCompose(currState -> {
+                    VersionedMetadata<KVTableState> currentState = new VersionedMetadata<KVTableState>(currState.getObject().getState(), currState.getVersion());
+                    return Futures.toVoid(updateVersionedState(currentState, state));
+                });
+    }
+
+    @Override
+    public CompletableFuture<VersionedMetadata<KVTableState>> getVersionedState() {
+        return getStateData(true)
+                .thenApply(x -> new VersionedMetadata<>(x.getObject().getState(), x.getVersion()));
+    }
+
+    @Override
+    public CompletableFuture<VersionedMetadata<KVTableState>> updateVersionedState(final VersionedMetadata<KVTableState> previous, final KVTableState newState) {
+        if (KVTableState.isTransitionAllowed(previous.getObject(), newState)) {
+            return setStateData(new VersionedMetadata<>(KVTStateRecord.builder().state(newState).build(), previous.getVersion()))
+                    .thenApply(updatedVersion -> new VersionedMetadata<>(newState, updatedVersion));
+        } else {
+            return Futures.failedFuture(StoreException.create(
+                    StoreException.Type.OPERATION_NOT_ALLOWED,
+                    "KeyValueTable: " + getName() + " State: " + newState.name() + " current state = " +
+                            previous.getObject()));
+        }
+    }
+
+    @Override
+    public CompletableFuture<KVTableState> getState(boolean ignoreCached) {
+        return getStateData(ignoreCached)
+                .thenApply(x -> x.getObject().getState());
+    }
+
+    @Override
+    public CompletableFuture<CreateKVTableResponse> create(final KeyValueTableConfiguration configuration, long createTimestamp, int startingSegmentNumber) {
+        return checkKeyValueTableExists(configuration, createTimestamp, startingSegmentNumber)
+                .thenCompose(createKVTResponse -> createKVTableMetadata()
+                        .thenCompose((Void v) -> storeCreationTimeIfAbsent(createKVTResponse.getTimestamp()))
+                        .thenCompose((Void v) -> createConfigurationIfAbsent(KVTConfigurationRecord.builder()
+                                                .scope(scopeName).kvtName(name).kvtConfiguration(configuration).build()))
+                        .thenCompose((Void v) -> createStateIfAbsent(KVTStateRecord.builder().state(KVTableState.CREATING).build()))
+                        .thenCompose((Void v) -> createHistoryRecords(startingSegmentNumber, createKVTResponse))
+                        .thenApply((Void v) -> createKVTResponse));
+    }
+
+    private CompletionStage<Void> createHistoryRecords(int startingSegmentNumber, CreateKVTableResponse createKvtResponse) {
+        final int numSegments = createKvtResponse.getConfiguration().getPartitionCount();
+        // create epoch 0 record
+        final double keyRangeChunk = 1.0 / numSegments;
+
+        long creationTime = createKvtResponse.getTimestamp();
+        final ImmutableList.Builder<KVTSegmentRecord> builder = ImmutableList.builder();
+
+        IntStream.range(0, numSegments).boxed()
+                .forEach(x -> builder.add(newSegmentRecord(0, startingSegmentNumber + x, creationTime,
+                        x * keyRangeChunk, (x + 1) * keyRangeChunk)));
+
+        KVTEpochRecord epoch0 = new KVTEpochRecord(0, builder.build(), creationTime);
+
+        return createEpochRecord(epoch0).thenCompose(r -> createCurrentEpochRecordDataIfAbsent(epoch0));
+    }
+
+    private KVTSegmentRecord newSegmentRecord(int epoch, int segmentNumber, long time, Double low, Double high) {
+        return KVTSegmentRecord.builder().creationEpoch(epoch).segmentNumber(segmentNumber).creationTime(time)
+                .keyStart(low).keyEnd(high).build();
+    }
+
+    private CompletableFuture<Void> createEpochRecord(KVTEpochRecord epoch) {
+        return createEpochRecordDataIfAbsent(epoch.getEpoch(), epoch);
+    }
+
+    @Override
+    public CompletableFuture<List<KVTSegmentRecord>> getActiveSegments() {
+        // read current epoch record
+        return verifyLegalState()
+                .thenCompose(v -> getActiveEpochRecord(true).thenApply(epochRecord -> epochRecord.getSegments()));
+    }
+
+    @Override
+    public CompletableFuture<KVTEpochRecord> getActiveEpochRecord(boolean ignoreCached) {
+        return getCurrentEpochRecordData(ignoreCached).thenApply(VersionedMetadata::getObject);
+    }
+
+    private CompletableFuture<Void> verifyLegalState() {
+        return getState(false).thenApply(state -> {
+            if (state == null || state.equals(KVTableState.UNKNOWN) || state.equals(KVTableState.CREATING)) {
+                throw StoreException.create(StoreException.Type.ILLEGAL_STATE,
+                        "KeyValueTable: " + getName() + " State: " + state.name());
+            }
+            return null;
+        });
+    }
+
+    @Override
+    public CompletableFuture<KVTEpochRecord> getEpochRecord(int epoch) {
+        log.info("getEpochRecord():: epoch number = {}", epoch);
+        return getEpochRecordData(epoch).thenApply(VersionedMetadata::getObject);
+    }
+
+    /**
+     * Fetch configuration at configurationPath.
+     *
+     * @return Future of kvtable configuration
+     */
+    @Override
+    public CompletableFuture<KeyValueTableConfiguration> getConfiguration() {
+        return getConfigurationData(false).thenApply(x -> x.getObject().getKvtConfiguration());
+    }
+
+    @Override
+    public CompletableFuture<Set<Long>> getAllSegmentIds() {
+        return getActiveEpochRecord(true)
+                .thenApply(epochRecord -> epochRecord.getSegmentIds());
+    }
+
+    // region state
+    abstract public CompletableFuture<String> getId();
+
+    abstract CompletableFuture<Void> createStateIfAbsent(final KVTStateRecord state);
+
+    abstract CompletableFuture<Version> setStateData(final VersionedMetadata<KVTStateRecord> state);
+
+    abstract CompletableFuture<VersionedMetadata<KVTStateRecord>> getStateData(boolean ignoreCached);
+
+    abstract CompletableFuture<CreateKVTableResponse> checkKeyValueTableExists(final KeyValueTableConfiguration configuration,
+                                                                               final long creationTime,
+                                                                               final int startingSegmentNumber);
+
+    abstract CompletableFuture<Void> createKVTableMetadata();
+
+    abstract CompletableFuture<Void> storeCreationTimeIfAbsent(final long creationTime);
+
+    abstract CompletableFuture<VersionedMetadata<KVTConfigurationRecord>> getConfigurationData(boolean ignoreCached);
+
+    abstract CompletableFuture<Void> createConfigurationIfAbsent(final KVTConfigurationRecord data);
+
+    abstract CompletableFuture<Void> createCurrentEpochRecordDataIfAbsent(KVTEpochRecord data);
+
+    abstract CompletableFuture<Void> createEpochRecordDataIfAbsent(int epoch, KVTEpochRecord data);
+
+    abstract CompletableFuture<VersionedMetadata<KVTEpochRecord>> getCurrentEpochRecordData(boolean ignoreCached);
+
+    abstract CompletableFuture<VersionedMetadata<KVTEpochRecord>> getEpochRecordData(int epoch);
+}
