bda0b1fc901987a0bbb4df88b811f3a43974e991
Andrei Paduroiu <andreipaduroiu@users.noreply.github.com>
2020-07-10 15:15:24 -0700

    Issue 4933: (Key-Value Tables) Enforcing Key and Value serialization limits (#4935)
    Enforcing the maximum Key Length, maximum Value Length and maximum Key Family Length for all KeyValueTable operations.
    Max Key Length is 4KB, Max Value Length is 1MB-1KB, Max Key Family Length is 2KB.
    Ensuring no KeyValueTable wire command exceeds the maximum wire command size.
    
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- client/src/test/java/io/pravega/client/tables/impl/KeyValueTableMapImplTests.java
+++ client/src/test/java/io/pravega/client/tables/impl/KeyValueTableMapImplTests.java
@@ -11,6 +11,7 @@ package io.pravega.client.tables.impl;
 
 import com.google.common.util.concurrent.Runnables;
 import io.pravega.client.admin.KeyValueTableInfo;
+import io.pravega.client.stream.Serializer;
 import io.pravega.client.stream.mock.MockConnectionFactoryImpl;
 import io.pravega.client.stream.mock.MockController;
 import io.pravega.client.tables.KeyValueTable;
@@ -60,6 +61,11 @@ public class KeyValueTableMapImplTests extends KeyValueTableTestSetup {
         return this.keyValueTable;
     }
 
+    @Override
+    protected <K, V> KeyValueTable<K, V> createKeyValueTable(Serializer<K> keySerializer, Serializer<V> valueSerializer) {
+        return new KeyValueTableImpl<>(KVT, this.segmentFactory, this.controller, keySerializer, valueSerializer);
+    }
+
     @Before
     public void setup() throws Exception {
         super.setup();
@@ -69,7 +75,7 @@ public class KeyValueTableMapImplTests extends KeyValueTableTestSetup {
         this.controller.createKeyValueTable(KVT.getScope(), KVT.getKeyValueTableName(),
                 KeyValueTableConfiguration.builder().partitionCount(getSegmentCount()).build());
         this.segmentFactory = new MockTableSegmentFactory(getSegmentCount(), executorService());
-        this.keyValueTable = new KeyValueTableImpl<>(KVT, this.segmentFactory, this.controller, KEY_SERIALIZER, VALUE_SERIALIZER);
+        this.keyValueTable = createKeyValueTable(KEY_SERIALIZER, VALUE_SERIALIZER);
     }
 
     @After
