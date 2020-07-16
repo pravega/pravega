a54c078d0d54d049f2023537f61f5c68e8513a8c
Ravi Rajamani <ravi.rajamani@emc.com>
2020-07-16 12:19:40 -0700

    Issue 4940: Propagating Transaction Exceptions to the caller (#4941)
    Propagating Transaction Exceptions to the caller instead of wrapping in RuntimeException.
    
    Signed-off-by: Ravi Rajamani <ravi.rajamani@emc.com>
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- test/integration/src/test/java/io/pravega/test/integration/endtoendtest/EndToEndTxnWithTest.java
+++ test/integration/src/test/java/io/pravega/test/integration/endtoendtest/EndToEndTxnWithTest.java
@@ -219,9 +219,9 @@ public class EndToEndTxnWithTest extends ThreadPooledTestSuite {
                 e -> Exceptions.unwrap(e) instanceof IllegalArgumentException);
 
         EventWriterConfig highTimeoutConfig = EventWriterConfig.builder().transactionTimeoutTime(200 * 1000).build();
-        AssertExtensions.assertThrows("high timeout period not honoured",
+        AssertExtensions.assertThrows("lease value too large, max value is 120000",
                 () -> createTxn(clientFactory, highTimeoutConfig, "test"),
-                e -> Exceptions.unwrap(e.getCause()) instanceof IllegalArgumentException);
+                e -> e instanceof IllegalArgumentException);
     }
 
     @Test(timeout = 20000)
