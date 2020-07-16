a2405fcfb6b127ec69811886c9aca2bbf4a53170
Tom Kaitchuck <tom.kaitchuck@emc.com>
2020-07-16 09:31:07 -0700

    Adjust batching algorithm
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- client/src/main/java/io/pravega/client/connection/impl/AppendBatchSizeTrackerImpl.java
+++ client/src/main/java/io/pravega/client/connection/impl/AppendBatchSizeTrackerImpl.java
@@ -50,7 +50,7 @@ public class AppendBatchSizeTrackerImpl implements AppendBatchSizeTracker {
     private final AtomicLong lastAppendTime;
     private final AtomicLong lastAckNumber;
     private final ExponentialMovingAverage eventSize = new ExponentialMovingAverage(1024, 0.01, true);
-    private final ExponentialMovingAverage nanosBetweenAppends = new ExponentialMovingAverage(10 * NANOS_PER_MILLI, 0.001, true);
+    private final ExponentialMovingAverage nanosBetweenAppends = new ExponentialMovingAverage(10 * NANOS_PER_MILLI, 0.001, false);
     private final ExponentialMovingAverage appendsOutstanding = new ExponentialMovingAverage(20, 0.001, false);
 
     public AppendBatchSizeTrackerImpl() {
@@ -90,7 +90,7 @@ public class AppendBatchSizeTrackerImpl implements AppendBatchSizeTracker {
         double nanosPerAppend = nanosBetweenAppends.getCurrentValue();
         double appendsInMaxBatchTime = Math.max(1.0, (MAX_BATCH_TIME_MILLIS * NANOS_PER_MILLI) / nanosPerAppend);
         double appendsInTime = Math.max(1.0, BASE_TIME_NANOS / nanosPerAppend);
-        double appendsInBatch = MathHelpers.minMax(appendsOutstanding.getCurrentValue() * OUTSTANDING_FRACTION, appendsInTime, appendsInMaxBatchTime);
+        double appendsInBatch = MathHelpers.minMax(appendsOutstanding.getCurrentValue() * OUTSTANDING_FRACTION + appendsInTime, 1.0, appendsInMaxBatchTime);
         int size = (int) (appendsInBatch * eventSize.getCurrentValue()) + BASE_SIZE;
         return MathHelpers.minMax(size, 0, MAX_BATCH_SIZE);
     }
