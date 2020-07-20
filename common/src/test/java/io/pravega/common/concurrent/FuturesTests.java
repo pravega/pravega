3c7b4088758559d14ffe7085d236b24bf49a6e84
shivesh ranjan <13659022+shiveshr@users.noreply.github.com>
2020-07-20 20:16:09 +0530

    Issue 4915: Add timeout to add requests from Controller to Segment Store (#4916)
    Adds a timeout to requests being made to Segment Store using Segment Helper.
    
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- common/src/test/java/io/pravega/common/concurrent/FuturesTests.java
+++ common/src/test/java/io/pravega/common/concurrent/FuturesTests.java
@@ -9,9 +9,12 @@
  */
 package io.pravega.common.concurrent;
 
+import io.pravega.common.Exceptions;
 import io.pravega.common.hash.RandomFactory;
 import io.pravega.test.common.AssertExtensions;
 import io.pravega.test.common.IntentionalException;
+
+import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collection;
@@ -22,14 +25,16 @@ import java.util.Map;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ForkJoinPool;
-import java.util.concurrent.Executors;
-import java.util.concurrent.Executor;
+import java.util.concurrent.TimeoutException;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicReference;
 import java.util.function.Function;
 import java.util.function.Predicate;
+import java.util.function.Supplier;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
+
+import io.pravega.test.common.ThreadPooledTestSuite;
 import lombok.val;
 import org.junit.Assert;
 import org.junit.Test;
@@ -37,7 +42,12 @@ import org.junit.Test;
 /**
  * Unit tests for the Futures class.
  */
-public class FuturesTests {
+public class FuturesTests extends ThreadPooledTestSuite {
+    @Override
+    protected int getThreadPoolSize() {
+        return 1;
+    }
+
     /**
      * Tests the failedFuture() method.
      */
@@ -540,22 +550,27 @@ public class FuturesTests {
     }
 
     @Test
+    public void testTimeout() {
+        Supplier<CompletableFuture<Integer>> supplier = CompletableFuture::new;
+        CompletableFuture<Integer> f1 = Futures.futureWithTimeout(supplier, Duration.ofMillis(10), "", executorService());
+        AssertExtensions.assertFutureThrows("Future should have timedout. ", f1, e -> Exceptions.unwrap(e) instanceof TimeoutException);
+    }
+
     public void testCompleteOn() {
         val successfulFuture = new CompletableFuture<Integer>();
-        Executor executor = Executors.newSingleThreadExecutor();
-        CompletableFuture<Integer> result = Futures.completeOn(successfulFuture, executor);
+        CompletableFuture<Integer> result = Futures.completeOn(successfulFuture, executorService());
         successfulFuture.complete(1);
         Assert.assertEquals("Expected completion value for successful future.", Integer.valueOf(1), result.join());
 
         val failedFuture = new CompletableFuture<Integer>();
-        CompletableFuture<Integer> failedResult = Futures.completeOn(failedFuture, executor);
+        CompletableFuture<Integer> failedResult = Futures.completeOn(failedFuture, executorService());
         failedFuture.completeExceptionally(new IntentionalException());
         AssertExtensions.assertSuppliedFutureThrows(
                 "Failed future throws exception.",
                 () -> failedResult,
                 ex -> ex instanceof IntentionalException);
     }
-
+    
     private List<CompletableFuture<Integer>> createNumericFutures(int count) {
         ArrayList<CompletableFuture<Integer>> result = new ArrayList<>();
         for (int i = 0; i < count; i++) {
