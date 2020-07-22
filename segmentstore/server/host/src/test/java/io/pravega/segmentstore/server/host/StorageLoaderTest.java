54d71e184a464532a9547d3c3871164f3c8f7216
Sachin Joshi <sachin.joshi@emc.com>
2020-07-22 10:33:12 -0700

    Issue 4676: (PDP-34) Initial Implementation (Part 3 of 4) - Fix StorageLoaderTests.
    Signed-off-by: Sachin Joshi <sachin.joshi@emc.com>

--- segmentstore/server/host/src/test/java/io/pravega/segmentstore/server/host/StorageLoaderTest.java
+++ segmentstore/server/host/src/test/java/io/pravega/segmentstore/server/host/StorageLoaderTest.java
@@ -12,10 +12,11 @@ package io.pravega.segmentstore.server.host;
 import io.pravega.segmentstore.server.store.ServiceBuilder;
 import io.pravega.segmentstore.server.store.ServiceBuilderConfig;
 import io.pravega.segmentstore.server.store.ServiceConfig;
+import io.pravega.segmentstore.storage.ConfigSetup;
 import io.pravega.segmentstore.storage.DurableDataLogException;
 import io.pravega.segmentstore.storage.StorageFactory;
-import io.pravega.segmentstore.storage.StorageMetadataFormat;
 import io.pravega.segmentstore.storage.StorageLayoutType;
+import io.pravega.segmentstore.storage.StorageMetadataFormat;
 import io.pravega.segmentstore.storage.mocks.InMemoryStorageFactory;
 import io.pravega.segmentstore.storage.noop.NoOpStorageFactory;
 import io.pravega.segmentstore.storage.noop.StorageExtraConfig;
@@ -23,16 +24,20 @@ import io.pravega.storage.extendeds3.ExtendedS3SimpleStorageFactory;
 import io.pravega.storage.extendeds3.ExtendedS3StorageConfig;
 import io.pravega.storage.extendeds3.ExtendedS3StorageFactory;
 import io.pravega.storage.filesystem.FileSystemSimpleStorageFactory;
+import io.pravega.storage.filesystem.FileSystemStorageConfig;
 import io.pravega.storage.filesystem.FileSystemStorageFactory;
 import io.pravega.storage.hdfs.HDFSSimpleStorageFactory;
+import io.pravega.storage.hdfs.HDFSStorageConfig;
 import io.pravega.storage.hdfs.HDFSStorageFactory;
 import lombok.val;
-import org.junit.Ignore;
 import org.junit.Test;
 
 import java.util.concurrent.ScheduledThreadPoolExecutor;
 
 import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.when;
 
 public class StorageLoaderTest {
 
@@ -77,72 +82,93 @@ public class StorageLoaderTest {
     @Test
     public void testFileSystemStorage() throws Exception {
         val storageType = ServiceConfig.StorageType.FILESYSTEM;
-        ServiceBuilder builder = getStorageFactory(storageType, "FILESYSTEM", StorageLayoutType.ROLLING_STORAGE, StorageMetadataFormat.HEADER_BASED);
+        ConfigSetup configSetup = mock(ConfigSetup.class);
+        val extraConfig = StorageExtraConfig.builder()
+                .with(StorageExtraConfig.STORAGE_NO_OP_MODE, false)
+                .build();
+        when(configSetup.getConfig(any())).thenReturn(extraConfig, FileSystemStorageConfig.builder().build());
+        val factory  = getStorageFactory(configSetup, storageType, "FILESYSTEM", StorageLayoutType.ROLLING_STORAGE, StorageMetadataFormat.HEADER_BASED);
         assertTrue(expectedFactory instanceof FileSystemStorageFactory);
-        builder.close();
     }
 
-    @Ignore
     @Test
     public void testSimpleFileSystemStorage() throws Exception {
         val storageType = ServiceConfig.StorageType.FILESYSTEM;
-        ServiceBuilder builder = getStorageFactory(storageType, "FILESYSTEM", StorageLayoutType.CHUNKED_STORAGE, StorageMetadataFormat.TABLE_BASED);
+        ConfigSetup configSetup = mock(ConfigSetup.class);
+        val extraConfig = StorageExtraConfig.builder()
+                .with(StorageExtraConfig.STORAGE_NO_OP_MODE, false)
+                .build();
+        when(configSetup.getConfig(any())).thenReturn(extraConfig, FileSystemStorageConfig.builder().build());
+
+        val factory  = getStorageFactory(configSetup, storageType, "FILESYSTEM", StorageLayoutType.CHUNKED_STORAGE, StorageMetadataFormat.TABLE_BASED);
         assertTrue(expectedFactory instanceof FileSystemSimpleStorageFactory);
-        builder.close();
     }
 
     @Test
     public void testHDFSStorage() throws Exception {
         val storageType = ServiceConfig.StorageType.HDFS;
-        ServiceBuilder builder = getStorageFactory(storageType, "HDFS", StorageLayoutType.ROLLING_STORAGE, StorageMetadataFormat.HEADER_BASED);
-        assertTrue(expectedFactory instanceof HDFSStorageFactory);
-        builder.close();
+        ConfigSetup configSetup = mock(ConfigSetup.class);
+        val extraConfig = StorageExtraConfig.builder()
+                .with(StorageExtraConfig.STORAGE_NO_OP_MODE, false)
+                .build();
+        when(configSetup.getConfig(any())).thenReturn(extraConfig, HDFSStorageConfig.builder().build());
+        val factory  = getStorageFactory(configSetup, storageType, "HDFS", StorageLayoutType.ROLLING_STORAGE, StorageMetadataFormat.HEADER_BASED);
+        assertTrue(factory instanceof HDFSStorageFactory);
     }
 
-    @Ignore
     @Test
     public void testHDFSSimpleStorage() throws Exception {
         val storageType = ServiceConfig.StorageType.HDFS;
-        ServiceBuilder builder = getStorageFactory(storageType, "HDFS", StorageLayoutType.CHUNKED_STORAGE, StorageMetadataFormat.TABLE_BASED);
-        assertTrue(expectedFactory instanceof HDFSSimpleStorageFactory);
-        builder.close();
+        ConfigSetup configSetup = mock(ConfigSetup.class);
+        val extraConfig = StorageExtraConfig.builder()
+                .with(StorageExtraConfig.STORAGE_NO_OP_MODE, false)
+                .build();
+        when(configSetup.getConfig(any())).thenReturn(extraConfig, HDFSStorageConfig.builder().build());
+        val factory = getStorageFactory(configSetup, storageType, "HDFS", StorageLayoutType.CHUNKED_STORAGE, StorageMetadataFormat.TABLE_BASED);
+        assertTrue(factory instanceof HDFSSimpleStorageFactory);
     }
 
     @Test
     public void testExtendedS3Storage() throws Exception {
         val storageType = ServiceConfig.StorageType.EXTENDEDS3;
-        ServiceBuilder builder = getStorageFactory(storageType, "EXTENDEDS3", StorageLayoutType.ROLLING_STORAGE, StorageMetadataFormat.HEADER_BASED);
-        assertTrue(expectedFactory instanceof ExtendedS3StorageFactory);
-        builder.close();
+        ConfigSetup configSetup = mock(ConfigSetup.class);
+
+        val config = ExtendedS3StorageConfig.builder()
+                .with(ExtendedS3StorageConfig.CONFIGURI, "http://127.0.0.1?identity=x&secretKey=x")
+                .with(ExtendedS3StorageConfig.BUCKET, "bucket")
+                .with(ExtendedS3StorageConfig.PREFIX, "samplePrefix")
+                .build();
+        val extraConfig = StorageExtraConfig.builder()
+                .with(StorageExtraConfig.STORAGE_NO_OP_MODE, false)
+                .build();
+        when(configSetup.getConfig(any())).thenReturn(extraConfig, config);
+
+        val factory = getStorageFactory(configSetup, storageType, "EXTENDEDS3", StorageLayoutType.ROLLING_STORAGE, StorageMetadataFormat.HEADER_BASED);
+        assertTrue(factory instanceof ExtendedS3StorageFactory);
     }
 
-    @Ignore
     @Test
     public void testExtendedS3SimpleStorage() throws Exception {
         val storageType = ServiceConfig.StorageType.EXTENDEDS3;
-        ServiceBuilder builder = getStorageFactory(storageType, "EXTENDEDS3", StorageLayoutType.CHUNKED_STORAGE, StorageMetadataFormat.TABLE_BASED);
-        assertTrue(expectedFactory instanceof ExtendedS3SimpleStorageFactory);
-        builder.close();
+        ConfigSetup configSetup = mock(ConfigSetup.class);
+        val config = ExtendedS3StorageConfig.builder()
+                .with(ExtendedS3StorageConfig.CONFIGURI, "http://127.0.0.1?identity=x&secretKey=x")
+                .with(ExtendedS3StorageConfig.BUCKET, "bucket")
+                .with(ExtendedS3StorageConfig.PREFIX, "samplePrefix")
+                .build();
+        val extraConfig = StorageExtraConfig.builder()
+                .with(StorageExtraConfig.STORAGE_NO_OP_MODE, false)
+                .build();
+        when(configSetup.getConfig(any())).thenReturn(extraConfig, config);
+
+        val factory = getStorageFactory(configSetup, storageType, "EXTENDEDS3", StorageLayoutType.CHUNKED_STORAGE, StorageMetadataFormat.TABLE_BASED);
+        assertTrue(factory instanceof ExtendedS3SimpleStorageFactory);
     }
 
-    private ServiceBuilder getStorageFactory(ServiceConfig.StorageType storageType, String name, StorageLayoutType storageLayoutType, StorageMetadataFormat storageMetadataFormat) throws DurableDataLogException {
+    private StorageFactory getStorageFactory(ConfigSetup setup, ServiceConfig.StorageType storageType, String name, StorageLayoutType storageLayoutType, StorageMetadataFormat storageMetadataFormat) throws DurableDataLogException {
         ScheduledThreadPoolExecutor executor = new ScheduledThreadPoolExecutor(1);
-        ServiceBuilderConfig.Builder configBuilder = ServiceBuilderConfig
-                .builder()
-                .include(ServiceConfig.builder()
-                        .with(ServiceConfig.CONTAINER_COUNT, 1)
-                        .with(ServiceConfig.STORAGE_IMPLEMENTATION, storageType))
-                .include(ExtendedS3StorageConfig.builder()
-                        .with(ExtendedS3StorageConfig.CONFIGURI, "http://127.0.0.1?identity=x&secretKey=x")
-                        .with(ExtendedS3StorageConfig.BUCKET, "bucket"));
-
-        ServiceBuilder builder = ServiceBuilder.newInMemoryBuilder(configBuilder.build())
-                .withStorageFactory(setup -> {
-                    StorageLoader loader = new StorageLoader();
-                    expectedFactory = loader.load(setup, name, storageLayoutType, storageMetadataFormat, executor);
-                    return expectedFactory;
-                });
-        builder.initialize();
-        return builder;
+        StorageLoader loader = new StorageLoader();
+        expectedFactory = loader.load(setup, name, storageLayoutType, storageMetadataFormat, executor);
+        return expectedFactory;
     }
 }
