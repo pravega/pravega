3b7acb6c9a17ed212fde3cc946d1a402f33c63a7
Ravi Rajamani <ravi.rajamani@emc.com>
2020-07-23 10:45:31 -0700

    checkstyle
    Signed-off-by: Ravi Rajamani <ravi.rajamani@emc.com>

--- test/system/src/main/java/io/pravega/test/system/framework/services/docker/PravegaSegmentStoreDockerService.java
+++ test/system/src/main/java/io/pravega/test/system/framework/services/docker/PravegaSegmentStoreDockerService.java
@@ -23,7 +23,6 @@ import com.spotify.docker.client.messages.swarm.ServiceSpec;
 import com.spotify.docker.client.messages.swarm.TaskSpec;
 import java.net.URI;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
