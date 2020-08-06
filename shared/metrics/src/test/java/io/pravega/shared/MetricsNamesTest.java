28d9515731b75965a184c5257464fa08751599d3
Raúl Gracia <raul.gracia@emc.com>
2020-08-06 13:35:41 +0200

    Removing more warnings.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- shared/metrics/src/test/java/io/pravega/shared/MetricsNamesTest.java
+++ shared/metrics/src/test/java/io/pravega/shared/MetricsNamesTest.java
@@ -71,7 +71,7 @@ public class MetricsNamesTest {
 
     @Test
     public void testMetricKeyWithSingleNull() {
-        MetricsNames.MetricKey keys = MetricsNames.metricKey("metric", null);
+        MetricsNames.MetricKey keys = MetricsNames.metricKey("metric", (String) null);
         assertEquals("metric", keys.getCacheKey());
         assertEquals("metric", keys.getRegistryKey());
     }
