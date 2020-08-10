438c4443ab94bac56731109c1837f57f57d33eaa
co-jo <colin.hryniowski@dell.com>
2020-08-10 10:51:12 -0700

    * Add unit test to coverage new exception thrown.
    Signed-off-by: co-jo <colin.hryniowski@dell.com>

--- segmentstore/server/src/test/java/io/pravega/segmentstore/server/tables/TableEntryDeltaIteratorTests.java
+++ segmentstore/server/src/test/java/io/pravega/segmentstore/server/tables/TableEntryDeltaIteratorTests.java
@@ -76,11 +76,21 @@ public class TableEntryDeltaIteratorTests extends ThreadPooledTestSuite {
     }
 
     @Test
+    public void testUsingInvalidArgs() {
+        TableContext context = new TableContext(COMPACTION_SIZE, executorService());
+        val segment = context.ext.createSegment(SEGMENT_NAME, false, TIMEOUT).join();
+        AssertExtensions.assertSuppliedFutureThrows(
+                "entryDeltaIterator should throw an IllegalArgumentException if 'fromPosition' > segment length.",
+                () -> context.ext.entryDeltaIterator(SEGMENT_NAME, 1, TIMEOUT),
+                ex -> ex instanceof IllegalArgumentException);
+    }
+
+    @Test
     public void testSortedTableSegment() {
         TableContext context = new TableContext(COMPACTION_SIZE, executorService());
         val segment = context.ext.createSegment(SEGMENT_NAME, true, TIMEOUT).join();
         AssertExtensions.assertSuppliedFutureThrows(
-                "entryDeltaIterator threw an UnsupportedOperationException on a sorted TableSegment.",
+                "entryDeltaIterator should throw an UnsupportedOperationException on a sorted TableSegment.",
                 () -> context.ext.entryDeltaIterator(SEGMENT_NAME, 0, TIMEOUT),
                 ex -> ex instanceof UnsupportedOperationException);
     }
