a3722b4586b4044f6fcde0e3e42dd20fa817b357
Atharva Joshi <Atharva.Joshi@dell.com>
2020-08-18 19:54:41 +0530

    formatting
    Signed-off-by: Atharva Joshi <Atharva.Joshi@dell.com>

--- shared/protocol/src/test/java/io/pravega/shared/NameUtilsTest.java
+++ shared/protocol/src/test/java/io/pravega/shared/NameUtilsTest.java
@@ -1,21 +1,19 @@
 /**
  * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
- * <p>
+ *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  */
 package io.pravega.shared;
 
 import io.pravega.test.common.AssertExtensions;
-
 import java.util.Random;
 import java.util.UUID;
 import java.util.concurrent.TimeUnit;
 import java.util.function.Function;
-
 import lombok.val;
 import org.junit.Assert;
 import org.junit.Rule;
@@ -61,18 +59,15 @@ public class NameUtilsTest {
         Assert.assertEquals(2, tokens.size());
         Assert.assertEquals(scope, tokens.get(0));
         Assert.assertEquals(kvt, tokens.get(1));
-        AssertExtensions.assertThrows("", () -> NameUtils.extractScopedNameTokens(scope),
-                ex -> ex instanceof IllegalArgumentException);
-        AssertExtensions.assertThrows("", () -> NameUtils.extractScopedNameTokens("a/b/c"),
-                ex -> ex instanceof IllegalArgumentException);
+        AssertExtensions.assertThrows("", () -> NameUtils.extractScopedNameTokens(scope), ex -> ex instanceof IllegalArgumentException);
+        AssertExtensions.assertThrows("", () -> NameUtils.extractScopedNameTokens("a/b/c"), ex -> ex instanceof IllegalArgumentException);
     }
 
     @Test
     public void testStreamNameVerifier() {
         NameUtils.validateStreamName("_systemstream123");
         NameUtils.validateStreamName("stream123");
-        AssertExtensions.assertThrows(IllegalArgumentException.class,
-                () -> NameUtils.validateStreamName("system_stream123"));
+        AssertExtensions.assertThrows(IllegalArgumentException.class, () -> NameUtils.validateStreamName("system_stream123"));
         AssertExtensions.assertThrows(IllegalArgumentException.class, () -> NameUtils.validateStreamName("stream/123"));
         AssertExtensions.assertThrows(NullPointerException.class, () -> NameUtils.validateStreamName(null));
         NameUtils.validateStreamName("a-b-c");
@@ -108,10 +103,8 @@ public class NameUtilsTest {
 
         NameUtils.validateScopeName("_systemscope123");
         NameUtils.validateScopeName("userscope123");
-        AssertExtensions.assertThrows(IllegalArgumentException.class,
-                () -> NameUtils.validateScopeName("system_scope"));
-        AssertExtensions.assertThrows(IllegalArgumentException.class,
-                () -> NameUtils.validateScopeName("system/scope"));
+        AssertExtensions.assertThrows(IllegalArgumentException.class, () -> NameUtils.validateScopeName("system_scope"));
+        AssertExtensions.assertThrows(IllegalArgumentException.class, () -> NameUtils.validateScopeName("system/scope"));
         AssertExtensions.assertThrows(NullPointerException.class, () -> NameUtils.validateScopeName(null));
 
     }
@@ -119,8 +112,7 @@ public class NameUtilsTest {
     @Test
     public void testReaderGroupNameVerifier() {
         NameUtils.validateReaderGroupName("stream123");
-        AssertExtensions.assertThrows(IllegalArgumentException.class,
-                () -> NameUtils.validateReaderGroupName("_stream"));
+        AssertExtensions.assertThrows(IllegalArgumentException.class, () -> NameUtils.validateReaderGroupName("_stream"));
         AssertExtensions.assertThrows(NullPointerException.class, () -> NameUtils.validateReaderGroupName(null));
     }
 
