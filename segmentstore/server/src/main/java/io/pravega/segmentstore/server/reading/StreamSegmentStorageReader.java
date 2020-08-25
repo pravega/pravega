9a2e3ac97b3ae41f1a1186c7bf436140632a0241
Andrei Paduroiu <andreipaduroiu@users.noreply.github.com>
2020-08-25 12:52:27 -0700

    Issue 5102: (SegmentStore) TableSegment Rollover Size not properly set. (#5103)
    Fixed a bug where the Attributes.ROLLOVER_SIZE is not properly set for Table Segments.
    
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- segmentstore/server/src/main/java/io/pravega/segmentstore/server/reading/StreamSegmentStorageReader.java
+++ segmentstore/server/src/main/java/io/pravega/segmentstore/server/reading/StreamSegmentStorageReader.java
@@ -98,7 +98,7 @@ public final class StreamSegmentStorageReader {
         public CompletableReadResultEntry apply(Long readOffset, Integer readLength) {
             if (readOffset < this.segmentInfo.getStartOffset()) {
                 // We attempted to read from a truncated portion of the Segment.
-                return new TruncatedReadResultEntry(readOffset, readLength, this.segmentInfo.getStartOffset());
+                return new TruncatedReadResultEntry(readOffset, readLength, this.segmentInfo.getStartOffset(), this.segmentInfo.getName());
             } else if (readOffset >= this.segmentInfo.getLength()) {
                 // We've reached the end of a Sealed Segment.
                 return new EndOfStreamSegmentReadResultEntry(readOffset, readLength);
