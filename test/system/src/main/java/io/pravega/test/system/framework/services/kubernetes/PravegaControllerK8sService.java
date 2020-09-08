dfbb5e3b2008a073b7fc23ffc2e2bbe447c8058f
Ravi Rajamani <ravi.rajamani@emc.com>
2020-09-08 05:50:19 -0700

    Issue 4162: Add the ability to run system tests with TLS (and auth) enabled (#5059)
    Add the ability to run system tests with TLS (and auth) enabled.
    
    Signed-off-by: Ravi Rajamani <ravi.rajamani@emc.com>
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- test/system/src/main/java/io/pravega/test/system/framework/services/kubernetes/PravegaControllerK8sService.java
+++ test/system/src/main/java/io/pravega/test/system/framework/services/kubernetes/PravegaControllerK8sService.java
@@ -12,6 +12,7 @@ package io.pravega.test.system.framework.services.kubernetes;
 import com.google.common.collect.ImmutableMap;
 import io.pravega.common.concurrent.Futures;
 import io.pravega.test.system.framework.TestFrameworkException;
+import io.pravega.test.system.framework.Utils;
 import lombok.extern.slf4j.Slf4j;
 
 import java.net.URI;
@@ -32,7 +33,6 @@ public class PravegaControllerK8sService extends AbstractService {
         super(id);
         this.zkUri = zkUri;
         this.properties = properties;
-
     }
 
     @Override
@@ -76,10 +76,11 @@ public class PravegaControllerK8sService extends AbstractService {
     @Override
     public List<URI> getServiceDetails() {
         //fetch the URI.
+        String prefix = Utils.TLS_AND_AUTH_ENABLED ? TLS : TCP;
         return Futures.getAndHandleExceptions(k8sClient.getStatusOfPodWithLabel(NAMESPACE, "component", PRAVEGA_CONTROLLER_LABEL)
                                                        .thenApply(statuses -> statuses.stream()
-                                                                                     .flatMap(s -> Stream.of(URI.create(TCP + s.getPodIP() + ":" + CONTROLLER_GRPC_PORT),
-                                                                                                             URI.create(TCP + s.getPodIP() + ":" + CONTROLLER_REST_PORT)))
+                                                                                     .flatMap(s -> Stream.of(URI.create(prefix + s.getPodIP() + ":" + CONTROLLER_GRPC_PORT),
+                                                                                                             URI.create(prefix + s.getPodIP() + ":" + CONTROLLER_REST_PORT)))
                                                                                      .collect(Collectors.toList())),
                                               t -> new TestFrameworkException(RequestFailed, "Failed to fetch ServiceDetails for pravega-controller", t));
     }
