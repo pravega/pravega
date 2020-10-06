2e70f876c5eaeaf0ed51504174c5e6b13f0645a1
Raúl Gracia <raul.gracia@emc.com>
2020-10-06 21:14:08 +0200

    Issue 4803: Move Pravega CLI back into Pravega Core repository (#5065)
    Bring back Pravega CLI tools into the core repository. Separate user (based on former Interactive Demo Tool) and admin CLI (based on Pravega Tools repo) functionality into different tools.
    
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

---
+++ cli/admin/src/test/java/io/pravega/cli/admin/cluster/ClusterCommandsTest.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.cli.admin.cluster;
+
+import io.pravega.cli.admin.AbstractAdminCommandTest;
+import io.pravega.cli.admin.utils.TestUtils;
+import io.pravega.common.cluster.Host;
+import io.pravega.controller.store.host.ZKHostStore;
+import lombok.Cleanup;
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.CuratorFrameworkFactory;
+import org.apache.curator.retry.RetryOneTime;
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Map;
+import java.util.Set;
+
+public class ClusterCommandsTest extends AbstractAdminCommandTest {
+
+    @Test
+    public void testGetClusterNodesCommand() throws Exception {
+        String commandResult = TestUtils.executeCommand("cluster list-instances", STATE.get());
+        Assert.assertTrue(commandResult.contains("controllers"));
+        Assert.assertNotNull(GetClusterNodesCommand.descriptor());
+    }
+
+    @Test
+    public void testGetSegmentStoreByContainerCommand() throws Exception {
+        createDummyHostContainerAssignment();
+        String commandResult = TestUtils.executeCommand("cluster get-host-by-container 0", STATE.get());
+        Assert.assertTrue(commandResult.contains("owner_segment_store"));
+        Assert.assertNotNull(GetSegmentStoreByContainerCommand.descriptor());
+    }
+
+    @Test
+    public void testListContainersCommand() throws Exception {
+        createDummyHostContainerAssignment();
+        String commandResult = TestUtils.executeCommand("cluster list-containers", STATE.get());
+        Assert.assertTrue(commandResult.contains("segment_store_container_map"));
+        Assert.assertNotNull(ListContainersCommand.descriptor());
+    }
+
+    /**
+     * This method creates a dummy Host-Container mapping, given that it is not created in Pravega standalone.
+     */
+    private void createDummyHostContainerAssignment() {
+        @Cleanup
+        CuratorFramework curatorFramework = CuratorFrameworkFactory.builder().namespace("pravega/pravega-cluster")
+                .connectString(SETUP_UTILS.getZkTestServer().getConnectString())
+                .retryPolicy(new RetryOneTime(5000)).build();
+        curatorFramework.start();
+        ZKHostStore zkHostStore = new ZKHostStore(curatorFramework, 4);
+        Map<Host, Set<Integer>> dummyHostContainerAssignment = new HashMap<>();
+        dummyHostContainerAssignment.put(new Host("localhost", 1234, "localhost"), new HashSet<>(Arrays.asList(0, 1, 2, 3)));
+        zkHostStore.updateHostContainersMap(dummyHostContainerAssignment);
+    }
+}
