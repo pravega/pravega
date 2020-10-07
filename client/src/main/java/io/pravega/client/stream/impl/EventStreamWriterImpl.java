f661e9c6c7c376f159b291938119e1379e73ecf7
shivesh ranjan <13659022+shiveshr@users.noreply.github.com>
2020-10-07 14:27:48 +0530

    Issue 5072: Single routing key batch writes (#5084)
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- client/src/main/java/io/pravega/client/stream/impl/EventStreamWriterImpl.java
+++ client/src/main/java/io/pravega/client/stream/impl/EventStreamWriterImpl.java
@@ -117,19 +117,40 @@ public class EventStreamWriterImpl<Type> implements EventStreamWriter<Type> {
         ByteBuffer data = serializer.serialize(event);
         CompletableFuture<Void> ackFuture = new CompletableFuture<Void>();
         synchronized (writeFlushLock) {
-            synchronized (writeSealLock) {                
-                SegmentOutputStream segmentWriter = selector.getSegmentOutputStreamForKey(routingKey);
-                while (segmentWriter == null) {
-                    log.info("Don't have a writer for segment: {}", selector.getSegmentForEvent(routingKey));
-                    handleMissingLog();
-                    segmentWriter = selector.getSegmentOutputStreamForKey(routingKey);
-                }
+            synchronized (writeSealLock) {
+                SegmentOutputStream segmentWriter = getSegmentWriter(routingKey);
                 segmentWriter.write(PendingEvent.withHeader(routingKey, data, ackFuture));
             }
         }
         return ackFuture;
     }
-    
+
+    @Override
+    public CompletableFuture<Void> writeEvents(String routingKey, List<Type> events) {
+        Preconditions.checkNotNull(routingKey);
+        Preconditions.checkNotNull(events);
+        Exceptions.checkNotClosed(closed.get(), this);
+        List<ByteBuffer> data = events.stream().map(serializer::serialize).collect(Collectors.toList());
+        CompletableFuture<Void> ackFuture = new CompletableFuture<Void>();
+        synchronized (writeFlushLock) {
+            synchronized (writeSealLock) {
+                SegmentOutputStream segmentWriter = getSegmentWriter(routingKey);
+                segmentWriter.write(PendingEvent.withHeader(routingKey, data, ackFuture));
+            }
+        }
+        return ackFuture;
+    }
+
+    private SegmentOutputStream getSegmentWriter(String routingKey) {
+        SegmentOutputStream segmentWriter = selector.getSegmentOutputStreamForKey(routingKey);
+        while (segmentWriter == null) {
+            log.info("Don't have a writer for segment: {}", selector.getSegmentForEvent(routingKey));
+            handleMissingLog();
+            segmentWriter = selector.getSegmentOutputStreamForKey(routingKey);
+        }
+        return segmentWriter;
+    }
+
     @GuardedBy("writeSealLock")
     private void handleMissingLog() {
         List<PendingEvent> toResend = selector.refreshSegmentEventWriters(segmentSealedCallBack);
