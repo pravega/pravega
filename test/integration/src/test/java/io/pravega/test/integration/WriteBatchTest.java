373adf01dcc64144a9496ebe05c1f4b721ec7d31
shivesh ranjan <13659022+shiveshr@users.noreply.github.com>
2020-10-07 14:27:48 +0530

    Issue 5072: Single routing key batch writes (#5084)
    Signed-off-by: Shivesh Ranjan <shivesh.ranjan@gmail.com>
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

---
+++ test/integration/src/test/java/io/pravega/test/integration/WriteBatchTest.java
@@ -0,0 +1,265 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.test.integration;
+
+import io.pravega.client.ClientConfig;
+import io.pravega.client.EventStreamClientFactory;
+import io.pravega.client.admin.ReaderGroupManager;
+import io.pravega.client.admin.StreamManager;
+import io.pravega.client.admin.impl.ReaderGroupManagerImpl;
+import io.pravega.client.admin.impl.StreamManagerImpl;
+import io.pravega.client.connection.impl.ConnectionFactory;
+import io.pravega.client.connection.impl.ConnectionPool;
+import io.pravega.client.connection.impl.ConnectionPoolImpl;
+import io.pravega.client.connection.impl.SocketConnectionFactoryImpl;
+import io.pravega.client.control.impl.Controller;
+import io.pravega.client.stream.EventStreamReader;
+import io.pravega.client.stream.EventStreamWriter;
+import io.pravega.client.stream.EventWriterConfig;
+import io.pravega.client.stream.ReaderConfig;
+import io.pravega.client.stream.ReaderGroupConfig;
+import io.pravega.client.stream.ScalingPolicy;
+import io.pravega.client.stream.Stream;
+import io.pravega.client.stream.StreamConfiguration;
+import io.pravega.client.stream.impl.ClientFactoryImpl;
+import io.pravega.client.stream.impl.JavaSerializer;
+import io.pravega.common.concurrent.ExecutorServiceHelpers;
+import io.pravega.common.concurrent.Futures;
+import io.pravega.common.hash.RandomFactory;
+import io.pravega.segmentstore.contracts.StreamSegmentStore;
+import io.pravega.segmentstore.contracts.tables.TableStore;
+import io.pravega.segmentstore.server.host.handler.PravegaConnectionListener;
+import io.pravega.segmentstore.server.store.ServiceBuilder;
+import io.pravega.segmentstore.server.store.ServiceBuilderConfig;
+import io.pravega.test.common.TestUtils;
+import io.pravega.test.common.TestingServerStarter;
+import io.pravega.test.integration.demo.ControllerWrapper;
+import lombok.extern.slf4j.Slf4j;
+import org.apache.curator.test.TestingServer;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ConcurrentLinkedQueue;
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.atomic.AtomicBoolean;
+import java.util.concurrent.atomic.AtomicInteger;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.stream.Collectors;
+import java.util.stream.LongStream;
+
+import static java.util.concurrent.TimeUnit.SECONDS;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+
+@Slf4j
+public class WriteBatchTest {
+    private static final String STREAM_NAME = "testBatchWrite" + RandomFactory.create().nextInt(Integer.MAX_VALUE);
+    private static final int NUM_WRITERS = 20;
+    private static final int NUM_READERS = 20;
+    private static final int NUM_EVENT_ITERATIONS_BY_WRITER = 1000;
+    private TestingServer zkTestServer = null;
+    private PravegaConnectionListener server = null;
+    private ControllerWrapper controllerWrapper = null;
+    private Controller controller = null;
+    private ScheduledExecutorService writerPool;
+    private ScheduledExecutorService readerPool;
+    private final AtomicInteger totalNumberOfEvents = new AtomicInteger();
+    
+    @Before
+    public void setup() throws Exception {
+
+        final int controllerPort = TestUtils.getAvailableListenPort();
+        final String serviceHost = "localhost";
+        final int servicePort = TestUtils.getAvailableListenPort();
+        final int containerCount = 4;
+
+        // 1. Start ZK
+        this.zkTestServer = new TestingServerStarter().start();
+
+        // 2. Start Pravega SegmentStore service.
+        ServiceBuilder serviceBuilder = ServiceBuilder.newInMemoryBuilder(ServiceBuilderConfig.getDefaultConfig());
+        serviceBuilder.initialize();
+        StreamSegmentStore store = serviceBuilder.createStreamSegmentService();
+        TableStore tableStore = serviceBuilder.createTableStoreService();
+
+        this.server = new PravegaConnectionListener(false, servicePort, store, tableStore, serviceBuilder.getLowPriorityExecutor());
+        this.server.startListening();
+
+        // 3. Start Pravega Controller service
+        this.controllerWrapper = new ControllerWrapper(zkTestServer.getConnectString(), false,
+                controllerPort, serviceHost, servicePort, containerCount);
+        this.controllerWrapper.awaitRunning();
+        this.controller = controllerWrapper.getController();
+        this.writerPool = ExecutorServiceHelpers.newScheduledThreadPool(NUM_WRITERS, "WriterPool");
+        this.readerPool = ExecutorServiceHelpers.newScheduledThreadPool(NUM_READERS, "ReaderPool");
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+        if (this.controllerWrapper != null) {
+            this.controllerWrapper.close();
+            this.controllerWrapper = null;
+        }
+        if (this.server != null) {
+            this.server.close();
+            this.server = null;
+        }
+        if (this.zkTestServer != null) {
+            this.zkTestServer.close();
+            this.zkTestServer = null;
+        }
+    }
+
+    @Test(timeout = 60000)
+    public void readWriteTest() throws InterruptedException, ExecutionException {
+
+        String scope = "testBatchWrite";
+        String readerGroupName = "testBatchWriteRG";
+        //20  readers -> 20 stream segments ( to have max read parallelism)
+        ScalingPolicy scalingPolicy = ScalingPolicy.fixed(20);
+        StreamConfiguration config = StreamConfiguration.builder().scalingPolicy(scalingPolicy).build();
+
+        ConcurrentLinkedQueue<Long> eventsReadFromPravega = new ConcurrentLinkedQueue<>();
+        AtomicLong eventData = new AtomicLong();
+        AtomicLong eventReadCount = new AtomicLong();
+        AtomicBoolean stopReadFlag = new AtomicBoolean(false);
+        ClientConfig clientConfig = ClientConfig.builder().build();
+        try (ConnectionPool cp = new ConnectionPoolImpl(clientConfig, new SocketConnectionFactoryImpl(clientConfig));
+             StreamManager streamManager = new StreamManagerImpl(controller, cp)) {
+            //create a scope
+            Boolean createScopeStatus = streamManager.createScope(scope);
+            log.info("Create scope status {}", createScopeStatus);
+            //create a stream
+            Boolean createStreamStatus = streamManager.createStream(scope, STREAM_NAME, config);
+            log.info("Create stream status {}", createStreamStatus);
+        }
+
+        try (ConnectionFactory connectionFactory = new SocketConnectionFactoryImpl(ClientConfig.builder().build());
+             ClientFactoryImpl clientFactory = new ClientFactoryImpl(scope, controller, connectionFactory);
+             ReaderGroupManager readerGroupManager = new ReaderGroupManagerImpl(scope, controller, clientFactory)) {
+
+            //start writing events to the stream
+            log.info("Creating {} writers", NUM_WRITERS);
+            List<CompletableFuture<Void>> writerList = new ArrayList<>();
+            for (int i = 0; i < NUM_WRITERS; i++) {
+                log.info("Starting writer{}", i);
+                writerList.add(startNewWriter(eventData, clientFactory));
+            }
+
+            //create a reader group
+            log.info("Creating Reader group : {}", readerGroupName);
+
+            readerGroupManager.createReaderGroup(readerGroupName, ReaderGroupConfig.builder().stream(Stream.of(scope, STREAM_NAME)).build());
+            log.info("Reader group name {} ", readerGroupManager.getReaderGroup(readerGroupName).getGroupName());
+            log.info("Reader group scope {}", readerGroupManager.getReaderGroup(readerGroupName).getScope());
+
+            //create readers
+            log.info("Creating {} readers", NUM_READERS);
+            List<CompletableFuture<Void>> readerList = new ArrayList<>();
+            String readerName = "reader" + RandomFactory.create().nextInt(Integer.MAX_VALUE);
+            //start reading events
+            for (int i = 0; i < NUM_READERS; i++) {
+                log.info("Starting reader{}", i);
+                readerList.add(startNewReader(readerName + i, clientFactory, readerGroupName,
+                        eventsReadFromPravega, eventData, eventReadCount, stopReadFlag));
+            }
+
+            //wait for writers completion
+            Futures.allOf(writerList).get();
+            
+            //set stop read flag to true
+            stopReadFlag.set(true);
+            
+            //wait for readers completion
+            Futures.allOf(readerList).get();
+            ExecutorServiceHelpers.shutdown(writerPool);
+            ExecutorServiceHelpers.shutdown(readerPool);
+
+            //delete readergroup
+            log.info("Deleting readergroup {}", readerGroupName);
+            readerGroupManager.deleteReaderGroup(readerGroupName);
+        }
+        log.info("All writers have stopped. Setting Stop_Read_Flag. Event Written Count:{}, Event Read " +
+                "Count: {}", eventData.get(), eventsReadFromPravega.size());
+        assertEquals(totalNumberOfEvents.get(), eventsReadFromPravega.size());
+        assertEquals(totalNumberOfEvents.get(), new TreeSet<>(eventsReadFromPravega).size()); //check unique events.
+        //seal the stream
+        CompletableFuture<Boolean> sealStreamStatus = controller.sealStream(scope, STREAM_NAME);
+        log.info("Sealing stream {}", STREAM_NAME);
+        assertTrue(sealStreamStatus.get());
+        //delete the stream
+        CompletableFuture<Boolean> deleteStreamStatus = controller.deleteStream(scope, STREAM_NAME);
+        log.info("Deleting stream {}", STREAM_NAME);
+        assertTrue(deleteStreamStatus.get());
+        //delete the  scope
+        CompletableFuture<Boolean> deleteScopeStatus = controller.deleteScope(scope);
+        log.info("Deleting scope {}", scope);
+        assertTrue(deleteScopeStatus.get());
+        log.info("Read write test succeeds");
+    }
+
+    private CompletableFuture<Void> startNewWriter(final AtomicLong data,
+                                                   final EventStreamClientFactory clientFactory) {
+        return CompletableFuture.runAsync(() -> {
+            final EventStreamWriter<Long> writer = clientFactory.createEventWriter(STREAM_NAME,
+                    new JavaSerializer<Long>(),
+                    EventWriterConfig.builder().build());
+            for (int i = 0; i < NUM_EVENT_ITERATIONS_BY_WRITER; i++) {
+                // for every 20th event, create a batch of 10 events.
+                if (i % 20 == 0) {
+                    long l = data.addAndGet(10);
+                    List<Long> values = LongStream.range(l - 9, l + 1).boxed().collect(Collectors.toList());
+                    totalNumberOfEvents.addAndGet(10);
+                    log.info("Writing events {}", values);
+                    writer.writeEvents(String.valueOf(values.get(0)), values);
+                    writer.flush();
+                } else {
+                    long value = data.incrementAndGet();
+                    totalNumberOfEvents.incrementAndGet();
+                    log.info("Writing event {}", value);
+                    writer.writeEvent(String.valueOf(value), value);
+                    writer.flush();
+                }
+            }
+            log.info("Closing writer {}", writer);
+            writer.close();
+
+        }, writerPool);
+    }
+
+    private CompletableFuture<Void> startNewReader(final String id, final EventStreamClientFactory clientFactory, final String
+            readerGroupName, final ConcurrentLinkedQueue<Long> readResult, final AtomicLong writeCount, final
+                                                   AtomicLong readCount, final  AtomicBoolean exitFlag) {
+        return CompletableFuture.runAsync(() -> {
+            final EventStreamReader<Long> reader = clientFactory.createReader(id,
+                    readerGroupName,
+                    new JavaSerializer<Long>(),
+                    ReaderConfig.builder().build());
+            while (!(exitFlag.get() && readCount.get() == writeCount.get())) {
+                final Long longEvent = reader.readNextEvent(SECONDS.toMillis(2)).getEvent();
+                log.info("Reading event {}", longEvent);
+                if (longEvent != null) {
+                    //update if event read is not null.
+                    readResult.add(longEvent);
+                    readCount.incrementAndGet();
+                }
+            }
+            log.info("Closing reader {}", reader);
+            reader.close();
+        }, readerPool);
+    }
+}
