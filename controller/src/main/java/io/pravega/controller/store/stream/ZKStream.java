6d516a49a1ab31559fcf91016f698a248cab0a9c
pbelgundi <prajakta.belgundi@emc.com>
2020-10-19 06:20:06 -0700

    fixed ZKStream
    Signed-off-by: pbelgundi <prajakta.belgundi@emc.com>

--- controller/src/main/java/io/pravega/controller/store/stream/ZKStream.java
+++ controller/src/main/java/io/pravega/controller/store/stream/ZKStream.java
@@ -11,6 +11,7 @@ package io.pravega.controller.store.stream;
 
 import com.google.common.annotations.VisibleForTesting;
 import com.google.common.base.Strings;
+import com.google.common.collect.ImmutableList;
 import com.google.common.collect.ImmutableMap;
 import io.pravega.client.stream.StreamConfiguration;
 import io.pravega.common.Exceptions;
@@ -33,6 +34,7 @@ import io.pravega.controller.store.stream.records.StreamCutRecord;
 import io.pravega.controller.store.stream.records.StreamTruncationRecord;
 import io.pravega.controller.store.stream.records.WriterMark;
 import io.pravega.controller.store.stream.records.StreamSubscriber;
+import io.pravega.controller.store.stream.records.SubscriberSet;
 import lombok.AccessLevel;
 import lombok.Getter;
 import lombok.extern.slf4j.Slf4j;
@@ -445,7 +447,8 @@ class ZKStream extends PersistentStreamBase {
 
     @Override
     CompletableFuture<Void> createSubscribersRecordIfAbsent() {
-        throw new UnsupportedOperationException();
+        SubscriberSet subscribersSetRecord = new SubscriberSet(ImmutableList.of());
+        return Futures.toVoid(store.createZNodeIfNotExist(subscribersPath, subscribersSetRecord.toBytes()));
     }
 
     @Override
