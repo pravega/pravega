f2c0ddefd39db350bc6f20f2cd3a816908e24f6d
co-jo <colin.hryniowski@dell.com>
2020-10-23 11:00:33 -0700

    Look up property properly
    Signed-off-by: co-jo <colin.hryniowski@dell.com>

--- test/system/src/test/java/io/pravega/test/system/DynamicRestApiTest.java
+++ test/system/src/test/java/io/pravega/test/system/DynamicRestApiTest.java
@@ -63,8 +63,8 @@ public class DynamicRestApiTest extends AbstractSystemTest {
         clientConfig.register(JacksonJsonProvider.class);
 
         if (Utils.AUTH_ENABLED) {
-            HttpAuthenticationFeature feature = HttpAuthenticationFeature.basic(System.getProperty("pravega.client.auth.username"),
-                    System.getProperty("pravega.client.auth.password"));
+            HttpAuthenticationFeature feature = HttpAuthenticationFeature.basic(Utils.PRAVEGA_PROPERTIES.get("pravega.client.auth.username"),
+                    Utils.PRAVEGA_PROPERTIES.get("pravega.client.auth.password"));
             clientConfig.register(feature);
         }
 
