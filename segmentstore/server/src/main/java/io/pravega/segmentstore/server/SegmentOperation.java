6451bacf9005f80f93a2f6d030cef4768d50814a
Andrei Paduroiu <andreipaduroiu@users.noreply.github.com>
2020-11-22 10:58:24 -0800

    Issue 5345: (SegmentStore) Rewiring StorageWriter into DurableLog (#5355)
    Changed the way DurableLog holds Tier 1 Operations in memory and how they are read by the Storage Writer. Retired uses of SequencedItemList in this case and replaced with a BlockingDrainingQueue.
    
    Changed the internals of BlockingDrainingQueue and PriorityBlockingDrainingQueue to use a variation of ArrayDeque that is more efficient at polling (removing) items in bulk.
    
    Signed-off-by: Andrei Paduroiu <andrei.paduroiu@emc.com>
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- segmentstore/server/src/main/java/io/pravega/segmentstore/server/SegmentOperation.java
+++ segmentstore/server/src/main/java/io/pravega/segmentstore/server/SegmentOperation.java
@@ -9,14 +9,15 @@
  */
 package io.pravega.segmentstore.server;
 
-import io.pravega.common.util.SequencedItemList;
+import io.pravega.segmentstore.contracts.SequencedElement;
 
 /**
  * Defines a Log Operation that deals with a Segment.
  */
-public interface SegmentOperation extends SequencedItemList.Element {
+public interface SegmentOperation extends SequencedElement {
     /**
      * Gets a value indicating the Id of the StreamSegment this operation relates to.
+     *
      * @return StreamSegment Id.
      */
     long getStreamSegmentId();
