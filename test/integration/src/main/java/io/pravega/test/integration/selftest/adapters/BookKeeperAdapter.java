8c8a5eaf1f1d921d54d5158081db7f616b125deb
co-jo <colin.hryniowski@dell.com>
2020-12-03 10:29:04 -0800

    Remove improper Javadoc formatting.
    Signed-off-by: co-jo <colin.hryniowski@dell.com>

--- test/integration/src/main/java/io/pravega/test/integration/selftest/adapters/BookKeeperAdapter.java
+++ test/integration/src/main/java/io/pravega/test/integration/selftest/adapters/BookKeeperAdapter.java
@@ -4,8 +4,8 @@
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
- * <p>
- * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
  */
 
 package io.pravega.test.integration.selftest.adapters;
