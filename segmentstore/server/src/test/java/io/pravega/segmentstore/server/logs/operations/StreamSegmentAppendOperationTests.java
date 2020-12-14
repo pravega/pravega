c0f92eee7424ff793d3ef6ec02a1b44abe334639
ManishKumarKeshri <manish.keshri562@gmail.com>
2020-12-14 01:02:10 -0800

    Reverting back access specifier at one position.
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- segmentstore/server/src/test/java/io/pravega/segmentstore/server/logs/operations/StreamSegmentAppendOperationTests.java
+++ segmentstore/server/src/test/java/io/pravega/segmentstore/server/logs/operations/StreamSegmentAppendOperationTests.java
@@ -70,7 +70,7 @@ public class StreamSegmentAppendOperationTests extends OperationTestsBase<Stream
         Assert.assertEquals("Expected an invocation to BufferView.release().", 0, buf.refCount);
     }
 
-    public static class RefCountByteArraySegment extends ByteArraySegment {
+    private static class RefCountByteArraySegment extends ByteArraySegment {
         private int refCount = 0;
 
         RefCountByteArraySegment(byte[] array) {
