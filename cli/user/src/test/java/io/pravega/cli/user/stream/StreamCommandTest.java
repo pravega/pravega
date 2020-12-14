b5a809388a650e95ec2cd901a2b2f92af47b7081
anirudhkovuru <31472818+anirudhkovuru@users.noreply.github.com>
2020-12-14 20:47:30 +0530

    Issue 5221: Port CLI security changes from Pravega Tools to Pravega core (#5250)
    Porting the changes made in the pravega tools repo (PR 75) to allow for the admin CLI to function in security enabled environments.
    
    Signed-off-by: anirudhkovuru <anirudhkovuru@gmail.com>

--- cli/user/src/test/java/io/pravega/cli/user/stream/StreamCommandTest.java
+++ cli/user/src/test/java/io/pravega/cli/user/stream/StreamCommandTest.java
@@ -9,87 +9,140 @@
  */
 package io.pravega.cli.user.stream;
 
-import io.pravega.cli.user.AbstractUserCommandTest;
 import io.pravega.cli.user.CommandArgs;
 import io.pravega.cli.user.TestUtils;
+import io.pravega.cli.user.config.InteractiveConfig;
 import io.pravega.cli.user.scope.ScopeCommand;
 import io.pravega.shared.NameUtils;
+import io.pravega.test.integration.demo.ClusterWrapper;
+import lombok.SneakyThrows;
+import org.junit.AfterClass;
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 
 import java.util.Collections;
 
-public class StreamCommandTest extends AbstractUserCommandTest {
+import static io.pravega.cli.user.TestUtils.createPravegaCluster;
+import static io.pravega.cli.user.TestUtils.getCLIControllerUri;
+import static io.pravega.cli.user.TestUtils.createCLIConfig;
+
+public class StreamCommandTest {
+
+    private static final ClusterWrapper CLUSTER = createPravegaCluster(false, false);
+    private static final InteractiveConfig CONFIG = createCLIConfig(getCLIControllerUri(CLUSTER.controllerUri()), false, false);
+
+    protected InteractiveConfig cliConfig() {
+        return CONFIG;
+    }
+
+    @BeforeClass
+    public static void start() {
+        CLUSTER.start();
+    }
+
+    @AfterClass
+    public static void shutDown() {
+        if (CLUSTER != null) {
+            CLUSTER.close();
+        }
+    }
 
     @Test(timeout = 5000)
-    public void testCreateStream() throws Exception {
+    @SneakyThrows
+    public void testCreateStream() {
         String scope = "createStreamScope";
         String stream = NameUtils.getScopedStreamName(scope, "newStream");
-        CommandArgs commandArgs = new CommandArgs(Collections.singletonList(scope), CONFIG.get());
+        CommandArgs commandArgs = new CommandArgs(Collections.singletonList(scope), cliConfig());
         new ScopeCommand.Create(commandArgs).execute();
 
-        String commandResult = TestUtils.executeCommand("stream create " + stream, CONFIG.get());
+        String commandResult = TestUtils.executeCommand("stream create " + stream, cliConfig());
         Assert.assertTrue(commandResult.contains("created successfully"));
         Assert.assertNotNull(StreamCommand.Create.descriptor());
     }
 
     @Test(timeout = 5000)
-    public void testDeleteStream() throws Exception {
+    @SneakyThrows
+    public void testDeleteStream() {
         String scope = "deleteStreamScope";
         String stream = NameUtils.getScopedStreamName(scope, "deleteStream");
-        CommandArgs commandArgs = new CommandArgs(Collections.singletonList(scope), CONFIG.get());
+        CommandArgs commandArgs = new CommandArgs(Collections.singletonList(scope), cliConfig());
         new ScopeCommand.Create(commandArgs).execute();
 
-        commandArgs = new CommandArgs(Collections.singletonList(stream), CONFIG.get());
+        commandArgs = new CommandArgs(Collections.singletonList(stream), cliConfig());
         new StreamCommand.Create(commandArgs).execute();
 
-        String commandResult = TestUtils.executeCommand("stream delete " + stream, CONFIG.get());
+        String commandResult = TestUtils.executeCommand("stream delete " + stream, cliConfig());
         Assert.assertTrue(commandResult.contains("deleted successfully"));
         Assert.assertNotNull(StreamCommand.Delete.descriptor());
     }
 
     @Test(timeout = 5000)
-    public void testListStream() throws Exception {
+    @SneakyThrows
+    public void testListStream() {
         String scope = "listStreamScope";
         String stream = NameUtils.getScopedStreamName(scope, "theStream");
-        CommandArgs commandArgsScope = new CommandArgs(Collections.singletonList(scope), CONFIG.get());
+        CommandArgs commandArgsScope = new CommandArgs(Collections.singletonList(scope), cliConfig());
         new ScopeCommand.Create(commandArgsScope).execute();
 
         // List Streams in scope when it is empty.
-        String commandResult = TestUtils.executeCommand("stream list " + scope, CONFIG.get());
+        String commandResult = TestUtils.executeCommand("stream list " + scope, cliConfig());
         Assert.assertFalse(commandResult.contains("theStream"));
 
-        CommandArgs commandArgsStream = new CommandArgs(Collections.singletonList(stream), CONFIG.get());
+        CommandArgs commandArgsStream = new CommandArgs(Collections.singletonList(stream), cliConfig());
         new StreamCommand.Create(commandArgsStream).execute();
 
         // List Streams in scope when we have one.
-        commandResult = TestUtils.executeCommand("stream list " + scope, CONFIG.get());
+        commandResult = TestUtils.executeCommand("stream list " + scope, cliConfig());
         Assert.assertTrue(commandResult.contains("theStream"));
         Assert.assertNotNull(StreamCommand.List.descriptor());
     }
 
     @Test(timeout = 20000)
-    public void testAppendAndReadStream() throws Exception {
+    @SneakyThrows
+    public void testAppendAndReadStream() {
         String scope = "appendAndReadStreamScope";
         String stream = NameUtils.getScopedStreamName(scope, "appendAndReadStream");
-        CommandArgs commandArgs = new CommandArgs(Collections.singletonList(scope), CONFIG.get());
+        CommandArgs commandArgs = new CommandArgs(Collections.singletonList(scope), cliConfig());
         new ScopeCommand.Create(commandArgs).execute();
 
-        CommandArgs commandArgsStream = new CommandArgs(Collections.singletonList(stream), CONFIG.get());
+        CommandArgs commandArgsStream = new CommandArgs(Collections.singletonList(stream), cliConfig());
         new StreamCommand.Create(commandArgsStream).execute();
 
-        String commandResult = TestUtils.executeCommand("stream append " + stream + " 100", CONFIG.get());
+        String commandResult = TestUtils.executeCommand("stream append " + stream + " 100", cliConfig());
         Assert.assertTrue(commandResult.contains("Done"));
         Assert.assertNotNull(StreamCommand.Append.descriptor());
 
         // Need to use a timeout for readers, otherwise the test never completes.
-        commandResult = TestUtils.executeCommand("stream read " + stream + " true 5", CONFIG.get());
+        commandResult = TestUtils.executeCommand("stream read " + stream + " true 5", cliConfig());
         Assert.assertTrue(commandResult.contains("Done"));
 
-        commandResult = TestUtils.executeCommand("stream append " + stream + " key 100", CONFIG.get());
+        commandResult = TestUtils.executeCommand("stream append " + stream + " key 100", cliConfig());
         Assert.assertTrue(commandResult.contains("Done"));
-        commandResult = TestUtils.executeCommand("stream read " + stream + " 5", CONFIG.get());
+        commandResult = TestUtils.executeCommand("stream read " + stream + " 5", cliConfig());
         Assert.assertTrue(commandResult.contains("Done"));
         Assert.assertNotNull(StreamCommand.Read.descriptor());
+    }
+
+    public static class SecureStreamCommandsTest extends StreamCommandTest {
+        private static final ClusterWrapper CLUSTER = createPravegaCluster(true, true);
+        private static final InteractiveConfig CONFIG = createCLIConfig(getCLIControllerUri(CLUSTER.controllerUri()), true, true);
+
+        @Override
+        protected InteractiveConfig cliConfig() {
+            return CONFIG;
+        }
+
+        @BeforeClass
+        public static void start() {
+            CLUSTER.start();
+        }
+
+        @AfterClass
+        public static void shutDown() {
+            if (CLUSTER != null) {
+                CLUSTER.close();
+            }
+        }
     }
 }
