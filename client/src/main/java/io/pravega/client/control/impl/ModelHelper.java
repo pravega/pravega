d4b5cdffb87f0db00bbe019857b84e231693c432
pbelgundi <prajakta.belgundi@emc.com>
2020-12-16 20:08:29 -0800

    checkstyle fix
    Signed-off-by: pbelgundi <prajakta.belgundi@emc.com>

--- client/src/main/java/io/pravega/client/control/impl/ModelHelper.java
+++ client/src/main/java/io/pravega/client/control/impl/ModelHelper.java
@@ -390,6 +390,7 @@ public final class ModelHelper {
      * @param scope the stream's scope
      * @param streamName The Stream Name
      * @param subscriber subscriber for this stream.
+     * @param generation subscriber generation.
      * @param streamCut truncationStreamCut for this subscriber for this stream.
      * @return SubscriberStreamCut instance.
      */
