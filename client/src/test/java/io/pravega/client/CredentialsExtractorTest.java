3138c61bb96dac55f9c1fa220f6e67ec50d3e3d9
Ravi Sharda <16592107+ravisharda@users.noreply.github.com>
2020-12-17 22:55:53 -0800

    Make DefaultCredentials a subtype of both old and new Credentials interface (#5436)
    * Make DefaultCredentials implement the legacy io.pravega.client.stream.impl.Credentials interface instead of io.pravega.shared.security.auth.Credentials
    
    Signed-off-by: rsharda <ravi.sharda@emc.com>
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- client/src/test/java/io/pravega/client/CredentialsExtractorTest.java
+++ client/src/test/java/io/pravega/client/CredentialsExtractorTest.java
@@ -9,6 +9,7 @@
  */
 package io.pravega.client;
 
+import io.pravega.client.stream.impl.DefaultCredentials;
 import io.pravega.shared.security.auth.Credentials;
 import java.util.HashMap;
 import java.util.Map;
@@ -234,6 +235,12 @@ public class CredentialsExtractorTest {
         assertNotNull(LegacyCredentials2.class.getName(), credentials.getClass());
         assertEquals("Expected a different authentication type",
                 LegacyCredentials2.AUTHENTICATION_METHOD, credentials.getAuthenticationType());
+    }
+
+    @Test
+    public void defaultCredentialsIsAssignableToEitherCredsInterface() {
+        Credentials credentials1 = new DefaultCredentials("pwd", "username");
+        io.pravega.client.stream.impl.Credentials credentials2 = new DefaultCredentials("pwd", "username");
     }
 
     /**
