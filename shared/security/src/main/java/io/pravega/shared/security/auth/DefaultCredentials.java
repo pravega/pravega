8e0fdb555cb760538e786d061ae5c50eecf700eb
rsharda <ravi.sharda@emc.com>
2020-12-21 23:38:43 -0800

    Add new implementation
    Signed-off-by: rsharda <ravi.sharda@emc.com>

---
+++ shared/security/src/main/java/io/pravega/shared/security/auth/DefaultCredentials.java
@@ -0,0 +1,37 @@
+/**
+ * Copyright (c) Dell Inc., or its subsidiaries. All Rights Reserved.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ */
+package io.pravega.shared.security.auth;
+
+import io.pravega.auth.AuthConstants;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Base64;
+
+public class DefaultCredentials implements Credentials {
+
+    private static final long serialVersionUID = 1L;
+
+    private final String token;
+
+    public DefaultCredentials(String password, String userName) {
+        String decoded = userName + ":" + password;
+        this.token = Base64.getEncoder().encodeToString(decoded.getBytes(StandardCharsets.UTF_8));
+    }
+
+    @Override
+    public String getAuthenticationType() {
+        return AuthConstants.BASIC;
+    }
+
+    @Override
+    public String getAuthenticationToken() {
+        return token;
+    }
+}
