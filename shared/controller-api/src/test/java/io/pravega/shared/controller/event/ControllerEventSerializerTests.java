83b63ba9065cefcec62fd8227a77021d261ca19e
pbelgundi <prajakta.belgundi@emc.com>
2021-01-06 02:35:22 -0800

    test fix
    Signed-off-by: pbelgundi <prajakta.belgundi@emc.com>

--- shared/controller-api/src/test/java/io/pravega/shared/controller/event/ControllerEventSerializerTests.java
+++ shared/controller-api/src/test/java/io/pravega/shared/controller/event/ControllerEventSerializerTests.java
@@ -14,6 +14,7 @@ import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.Map;
+import java.util.HashMap;
 
 import java.util.function.Supplier;
 import com.google.common.collect.ImmutableSet;
@@ -87,7 +88,7 @@ public class ControllerEventSerializerTests {
 
     @Test
     public void testCreateReaderGroupEvent() {
-        Map<String, RGStreamCutRecord> testMap = Map.of();
+        Map<String, RGStreamCutRecord> testMap = new HashMap<String, RGStreamCutRecord>(1);
         testClass(() ->
                 new CreateReaderGroupEvent(SCOPE, READER_GROUP,
                         123L, 456L, 10,
