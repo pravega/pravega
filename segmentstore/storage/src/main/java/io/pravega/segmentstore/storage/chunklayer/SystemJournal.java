a4c4f81e09fd5df9c88e791292c1acb33de705cc
Sachin Joshi <sachin.joshi@emc.com>
2021-01-13 15:18:37 -0800

    Issue 4967: (SLTS) Remove redundant check.
    Signed-off-by: Sachin Joshi <sachin.joshi@emc.com>

--- segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/chunklayer/SystemJournal.java
+++ segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/chunklayer/SystemJournal.java
@@ -172,7 +172,6 @@ public class SystemJournal {
     public CompletableFuture<Void> bootstrap(long epoch) throws Exception {
         this.epoch = epoch;
         Preconditions.checkState(!reentryGuard.getAndSet(true), "bootstrap called multiple times.");
-        Preconditions.checkState(!chunkStorage.exists(getSystemJournalChunkName()).get());
         try (val txn = metadataStore.beginTransaction(false, getSystemSegments())) {
             // Keep track of offsets at which chunks were added to the system segments.
             val chunkStartOffsets = new HashMap<String, Long>();
