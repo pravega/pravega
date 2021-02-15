1d6844bd94353f960444ed365ea9ba29d4678f03
Sachin Joshi <sachin.joshi@emc.com>
2021-02-15 15:58:24 -0800

    Issue 5460: Change default value of read index block to 1MB.
    Signed-off-by: Sachin Joshi <sachin.joshi@emc.com>

--- segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/chunklayer/ChunkedSegmentStorageConfig.java
+++ segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/chunklayer/ChunkedSegmentStorageConfig.java
@@ -33,7 +33,7 @@ public class ChunkedSegmentStorageConfig {
     public static final Property<Integer> MAX_INDEXED_SEGMENTS = Property.named("readindex.segments.max", 1024);
     public static final Property<Integer> MAX_INDEXED_CHUNKS_PER_SEGMENTS = Property.named("readindex.chunksPerSegment.max", 1024);
     public static final Property<Integer> MAX_INDEXED_CHUNKS = Property.named("readindex.chunks.max", 16 * 1024);
-    public static final Property<Long> READ_INDEX_BLOCK_SIZE = Property.named("readindex.block.size", 64 * 1024 * 1024L);
+    public static final Property<Long> READ_INDEX_BLOCK_SIZE = Property.named("readindex.block.size", 1024 * 1024L);
     public static final Property<Boolean> APPENDS_ENABLED = Property.named("appends.enable", true);
     public static final Property<Boolean> LAZY_COMMIT_ENABLED = Property.named("commit.lazy.enable", true);
     public static final Property<Boolean> INLINE_DEFRAG_ENABLED = Property.named("defrag.inline.enable", true);
@@ -66,7 +66,7 @@ public class ChunkedSegmentStorageConfig {
             .garbageCollectionMaxQueueSize(16 * 1024)
             .garbageCollectionSleep(Duration.ofMillis(10))
             .garbageCollectionMaxAttempts(3)
-            .indexBlockSize(64 * 1024 * 1024)
+            .indexBlockSize(1024 * 1024)
             .build();
 
     static final String COMPONENT_CODE = "storage";
