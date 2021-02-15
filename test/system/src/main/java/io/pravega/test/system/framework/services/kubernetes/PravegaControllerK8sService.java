ce735f03a930dd3eccb4d67743b1d842f3d79795
Raúl <raul.gracia@emc.com>
2021-02-15 11:43:29 +0100

    Make sure that tests start when Controller is ready (instead of just running)
    Signed-off-by: Raúl <raul.gracia@emc.com>

--- test/system/src/main/java/io/pravega/test/system/framework/services/kubernetes/PravegaControllerK8sService.java
+++ test/system/src/main/java/io/pravega/test/system/framework/services/kubernetes/PravegaControllerK8sService.java
@@ -10,6 +10,7 @@
 package io.pravega.test.system.framework.services.kubernetes;
 
 import com.google.common.collect.ImmutableMap;
+import io.kubernetes.client.openapi.models.V1ContainerStatus;
 import io.pravega.common.concurrent.Futures;
 import io.pravega.test.system.framework.TestFrameworkException;
 import io.pravega.test.system.framework.Utils;
@@ -63,7 +64,7 @@ public class PravegaControllerK8sService extends AbstractService {
                         .thenApply(statuses -> statuses.stream()
                                                        .filter(podStatus -> podStatus.getContainerStatuses()
                                                                                      .stream()
-                                                                                     .allMatch(st -> st.getState().getRunning() != null))
+                                                                                     .allMatch(V1ContainerStatus::getReady))
                                                        .count())
                         .thenApply(runCount -> runCount >= DEFAULT_CONTROLLER_COUNT)
                         .exceptionally(t -> {
