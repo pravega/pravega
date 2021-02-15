d2bf66fa9c2883b1fc3c1c796f2ae0e87ec622e9
Sandeep <sandeep.shridhar@emc.com>
2021-02-15 23:18:14 +0530

    Checkstyle fix.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

--- test/integration/src/test/java/io/pravega/test/integration/ReadTest.java
+++ test/integration/src/test/java/io/pravega/test/integration/ReadTest.java
@@ -92,7 +92,6 @@ import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertNull;
 import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
 
 @Slf4j
 public class ReadTest extends LeakDetectorTestSuite {
