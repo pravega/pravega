508d9a679425b7c60e640ea54df03f00d2fd33f8
wenqi <452787782@qq.com>
2021-03-01 17:39:25 -0800

    minor
    Signed-off-by: wenqi <452787782@qq.com>

--- client/src/test/java/io/pravega/client/connection/impl/RawClientTest.java
+++ client/src/test/java/io/pravega/client/connection/impl/RawClientTest.java
@@ -31,7 +31,9 @@ import org.junit.Test;
 import org.mockito.Mockito;
 
 import static io.pravega.test.common.AssertExtensions.assertFutureThrows;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 
 public class RawClientTest {
     
