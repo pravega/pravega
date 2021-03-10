b350fa78e6c04e6b8d03849efcfdaa830175da6b
Sachin Joshi <sachin.joshi@emc.com>
2021-03-10 14:06:49 -0800

    Issue 5830: SLTS - Robust thread safety related changes.
    Signed-off-by: Sachin Joshi <sachin.joshi@emc.com>

--- segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/metadata/SegmentMetadata.java
+++ segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/metadata/SegmentMetadata.java
@@ -229,7 +229,7 @@ public class SegmentMetadata extends StorageMetadata {
     /**
      * Sets the given bit for given mask.
      */
-    synchronized private SegmentMetadata setFlag(int mask, boolean value) {
+    private SegmentMetadata setFlag(int mask, boolean value) {
         status = value ? (status | mask) : (status & (~mask));
         return this;
     }
@@ -237,7 +237,7 @@ public class SegmentMetadata extends StorageMetadata {
     /**
      * Gets the status of the bit for given mask.
      */
-    synchronized private boolean getFlag(int mask) {
+    private boolean getFlag(int mask) {
         return (status & mask) != 0;
     }
 
