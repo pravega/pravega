b898f2fe6e10fa7b6dd9cda1ecca482b39ad10f7
Derek Moore <derek.moore@dell.com>
2021-03-23 22:47:02 -0500

    improve prepare-docs.sh, add latest submodules

--- documentation/src/prepare-docs.sh
+++ documentation/src/prepare-docs.sh
@@ -3,4 +3,8 @@ git submodule init
 
 find flink-connectors -name "*.md" -\! -wholename "*/.github/*" -print0 | xargs -0 cp --parents -t docs
 
-find pravega-operator -name "*.md" -\! -wholename "*/.github/*" -print0 | xargs -0 cp --parents -t docs
+find pravega-operator -name "*.md" -\! -wholename "*/.github/*" -or -wholename "*/deploy/certificate.yaml" -print0 | xargs -0 cp --parents -t docs
+
+find schema-registry -name "*.md" -\! -wholename "*/.github/*" -print0 | xargs -0 cp --parents -t docs
+
+find spark-connectors -name "*.md" -\! -wholename "*/.github/*" -print0 | xargs -0 cp --parents -t docs
