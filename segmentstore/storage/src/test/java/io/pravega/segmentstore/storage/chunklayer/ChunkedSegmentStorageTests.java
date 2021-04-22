8a37513db0111e2c92603142dee4bd1ed66aecf6
Sachin Joshi <sachin.joshi@emc.com>
2021-04-22 12:57:55 -0700

    Issue 5698: SLTS - SystemJournal should create new snapshots periodically (#5904)
    SystemJournal should creates new snapshots periodically
    
    Signed-off-by: Sachin Joshi <sachin.joshi@emc.com>
    # Conflicts:
    #	segmentstore/storage/src/main/java/io/pravega/segmentstore/storage/chunklayer/SystemJournal.java

--- segmentstore/storage/src/test/java/io/pravega/segmentstore/storage/chunklayer/ChunkedSegmentStorageTests.java
+++ segmentstore/storage/src/test/java/io/pravega/segmentstore/storage/chunklayer/ChunkedSegmentStorageTests.java
@@ -146,7 +146,6 @@ public class ChunkedSegmentStorageTests extends ThreadPooledTestSuite {
         val config = ChunkedSegmentStorageConfig.DEFAULT_CONFIG;
         @Cleanup
         val chunkedSegmentStorage = new ChunkedSegmentStorage(CONTAINER_ID, chunkStorage, metadataStore, executorService(), config);
-        val systemJournal = new SystemJournal(CONTAINER_ID, chunkStorage, metadataStore, config);
 
         testUninitialized(chunkedSegmentStorage);
 
@@ -157,12 +156,11 @@ public class ChunkedSegmentStorageTests extends ThreadPooledTestSuite {
         Assert.assertEquals(policy, chunkedSegmentStorage.getConfig().getDefaultRollingPolicy());
         Assert.assertEquals(1, chunkedSegmentStorage.getEpoch());
 
-        chunkedSegmentStorage.bootstrap().join();
         Assert.assertEquals(metadataStore, chunkedSegmentStorage.getMetadataStore());
         Assert.assertEquals(chunkStorage, chunkedSegmentStorage.getChunkStorage());
         Assert.assertEquals(policy, chunkedSegmentStorage.getConfig().getDefaultRollingPolicy());
         Assert.assertNotNull(chunkedSegmentStorage.getSystemJournal());
-        Assert.assertEquals(systemJournal.getConfig().getDefaultRollingPolicy(), policy);
+        Assert.assertEquals(chunkedSegmentStorage.getSystemJournal().getConfig().getDefaultRollingPolicy(), policy);
         Assert.assertEquals(1, chunkedSegmentStorage.getEpoch());
         Assert.assertEquals(CONTAINER_ID, chunkedSegmentStorage.getContainerId());
         Assert.assertEquals(0, chunkedSegmentStorage.getConfig().getMinSizeLimitForConcat());
@@ -170,7 +168,6 @@ public class ChunkedSegmentStorageTests extends ThreadPooledTestSuite {
         chunkedSegmentStorage.close();
 
         testUninitialized(chunkedSegmentStorage);
-
     }
 
     private void testUninitialized(ChunkedSegmentStorage chunkedSegmentStorage) {
