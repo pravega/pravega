536752d8337a241a599421f446d8e9672851c735
ManishKumarKeshri <manish.keshri562@gmail.com>
2021-05-25 13:27:55 -0700

    Removing unnecessary logging.
    Signed-off-by: ManishKumarKeshri <manish.keshri562@gmail.com>

--- controller/src/main/java/io/pravega/controller/server/SegmentHelper.java
+++ controller/src/main/java/io/pravega/controller/server/SegmentHelper.java
@@ -642,7 +642,6 @@ public class SegmentHelper implements AutoCloseable {
 
         WireCommands.FlushToStorage request = new WireCommands.FlushToStorage(delegationToken,
                 requestId);
-        log.info("Request: {}", request);
         return sendRequest(connection, requestId, request)
                 .thenApply(r -> {
                     handleReply(requestId, r, connection, null, WireCommands.FlushToStorage.class, type);
