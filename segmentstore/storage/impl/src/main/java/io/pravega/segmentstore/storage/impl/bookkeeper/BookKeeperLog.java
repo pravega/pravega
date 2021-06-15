97d38578c7a2ad01f869b36bf14d52efe01f1189
Raúl Gracia <raul.gracia@emc.com>
2021-06-15 09:54:39 +0200

    Fix checkstyle
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperLog.java
+++ segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperLog.java
@@ -44,7 +44,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
