6e3bc42898f421f6f7f8ec26da7b3dbafd8811cf
SaiCharan <kotlasaicharan@yahoo.com>
2021-06-21 13:52:22 +0530

    Issue 5946: Speedup controller Unit tests (#5988)
    Signed-off-by: SaiCharan <kotlasaicharan@yahoo.com>

--- controller/src/test/java/io/pravega/controller/server/eventProcessor/requesthandlers/StreamRequestProcessorWithZkStore.java
+++ controller/src/test/java/io/pravega/controller/server/eventProcessor/requesthandlers/StreamRequestProcessorWithZkStore.java
@@ -15,36 +15,27 @@
  */
 package io.pravega.controller.server.eventProcessor.requesthandlers;
 
+import io.pravega.controller.PravegaZkCuratorResource;
 import io.pravega.controller.store.stream.StreamMetadataStore;
 import io.pravega.controller.store.stream.StreamStoreFactory;
-import io.pravega.test.common.TestingServerStarter;
-import org.apache.curator.framework.CuratorFramework;
-import org.apache.curator.framework.CuratorFrameworkFactory;
-import org.apache.curator.retry.ExponentialBackoffRetry;
-import org.apache.curator.test.TestingServer;
 import org.junit.After;
 import org.junit.Before;
+import org.junit.ClassRule;
 
 public class StreamRequestProcessorWithZkStore extends StreamRequestProcessorTest {
+    @ClassRule
+    public static final PravegaZkCuratorResource PRAVEGA_ZK_CURATOR_RESOURCE = new PravegaZkCuratorResource();
     private StreamMetadataStore store;
-    private CuratorFramework client;
-    private TestingServer zkServer;
 
     @Before
     public void setUp() throws Exception {
-        zkServer = new TestingServerStarter().start();
-        client = CuratorFrameworkFactory.newClient(zkServer.getConnectString(),
-            new ExponentialBackoffRetry(200, 10, 5000));
-        client.start();
 
-        store = StreamStoreFactory.createZKStore(client, executorService());
+        store = StreamStoreFactory.createZKStore(PRAVEGA_ZK_CURATOR_RESOURCE.client, executorService());
     }
 
     @After
     public void tearDown() throws Exception {
         store.close();
-        client.close();
-        zkServer.close();
     }
 
     @Override
