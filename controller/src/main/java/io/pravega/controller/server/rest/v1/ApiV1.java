7cd6b95f1a7b9c2e045d74deaf96f1454995420e
co-jo <colin.hryniowski@dell.com>
2021-06-22 15:52:46 -0700

    Continue merge process.
    Signed-off-by: Colin Hryniowski <colin.hryniowski@dell.com>

--- controller/src/main/java/io/pravega/controller/server/rest/v1/ApiV1.java
+++ controller/src/main/java/io/pravega/controller/server/rest/v1/ApiV1.java
@@ -27,7 +27,6 @@ import io.pravega.controller.server.rest.generated.model.StreamState;
 import io.pravega.controller.server.rest.generated.model.StreamsList;
 import io.pravega.controller.server.rest.generated.model.UpdateStreamRequest;
 import io.pravega.controller.server.rest.generated.model.HealthResult;
-import io.pravega.controller.server.rest.generated.model.HealthDependencies;
 import io.pravega.controller.server.rest.generated.model.HealthDetails;
 import io.pravega.controller.server.rest.generated.model.HealthStatus;
 import io.pravega.controller.server.rest.generated.api.NotFoundException;
@@ -324,19 +323,33 @@ public final class ApiV1 {
     @Path("/v1/health")
     public interface HealthApi {
         @GET
+        @Path("/details/{id}")
+
+        @Produces({ "application/json" })
+        @io.swagger.annotations.ApiOperation(value = "", notes = "Fetch the details of a specific health contributor.", response = HealthDetails.class, tags = { "Health", })
+        @io.swagger.annotations.ApiResponses(value = {
+                @io.swagger.annotations.ApiResponse(code = 200, message = "The list of details for the health contributor with a given id.", response = HealthDetails.class),
+
+                @io.swagger.annotations.ApiResponse(code = 404, message = "The health details for the contributor with given id was not found.", response = HealthDetails.class),
+
+                @io.swagger.annotations.ApiResponse(code = 500, message = "Internal server error while fetching the health details for a given health contributor.", response = HealthDetails.class) })
+        void getContributorDetails(@ApiParam(value = "The id of an existing health contributor.", required = true) @PathParam("id") String id,
+                                   @Context SecurityContext securityContext, @Suspended final AsyncResponse asyncResponse)
+        throws NotFoundException;
+
+        @GET
         @Path("/{id}")
 
         @Produces({ "application/json" })
         @io.swagger.annotations.ApiOperation(value = "", notes = "Return the Health of a health contributor with a given id.", response = HealthResult.class, tags = { "Health", })
-
         @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The Health result of the Controller.", response = HealthResult.class),
+                @io.swagger.annotations.ApiResponse(code = 200, message = "The Health result of the Controller.", response = HealthResult.class),
 
-                @io.swagger.annotations.ApiResponse(code = 405, message = "A health provider for the given id could not be found.", response = HealthResult.class),
+                @io.swagger.annotations.ApiResponse(code = 404, message = "A health provider for the given id could not be found.", response = HealthResult.class),
 
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the health for a given contributor.", response = HealthResult.class) })
-        void getContributorHealth(@ApiParam(value = "The id of an existing health contributor.", required = true) @PathParam("id") String id, @QueryParam("details") Boolean details,
-                                  @Context SecurityContext securityContext, @Suspended AsyncResponse asyncResponse)
+                @io.swagger.annotations.ApiResponse(code = 500, message = "Internal server error while fetching the health for a given contributor.", response = HealthResult.class) })
+        void getContributorHealth(@ApiParam(value = "The id of an existing health contributor.", required = true) @PathParam("id") String id,
+                                  @Context SecurityContext securityContext, @Suspended final AsyncResponse asyncResponse)
                 throws NotFoundException;
 
         @GET
@@ -345,55 +358,13 @@ public final class ApiV1 {
         @Produces({ "application/json" })
         @io.swagger.annotations.ApiOperation(value = "", notes = "Fetch the liveness state of the specified health contributor.", response = Boolean.class, tags = { "Health", })
         @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The alive status for the specified health contributor.", response = Boolean.class),
+                @io.swagger.annotations.ApiResponse(code = 200, message = "The alive status for the specified health contributor.", response = Boolean.class),
 
-                @io.swagger.annotations.ApiResponse(code = 405, message = "The liveness status for the contributor with given id was not found.", response = Boolean.class),
+                @io.swagger.annotations.ApiResponse(code = 404, message = "The liveness status for the contributor with given id was not found.", response = Boolean.class),
 
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the liveness state for a given health contributor.", response = Boolean.class) })
+                @io.swagger.annotations.ApiResponse(code = 500, message = "Internal server error while fetching the liveness state for a given health contributor.", response = Boolean.class) })
         void getContributorLiveness(@ApiParam(value = "The id of an existing health contributor.", required = true) @PathParam("id") String id,
-                                    @Context SecurityContext securityContext, @Suspended AsyncResponse asyncResponse)
-                throws NotFoundException;
-
-        @GET
-        @Path("/components")
-
-        @Produces({ "application/json" })
-        @io.swagger.annotations.ApiOperation(value = "", notes = "Fetch the details of the Controller service.", response = HealthDependencies.class, tags = { "Health", })
-        @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The list of health dependencies of the Controller.", response = HealthDependencies.class),
-
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the health dependencies of the Controller.", response = HealthDependencies.class) })
-        void getDependencies(@Context SecurityContext securityContext, @Suspended AsyncResponse asyncResponse)
-                throws NotFoundException;
-
-        @GET
-        @Path("/components/{id}")
-
-        @Produces({ "application/json" })
-        @io.swagger.annotations.ApiOperation(value = "", notes = "Fetch the health dependencies for a specific health contributor.", response = HealthDependencies.class, tags = { "Health", })
-        @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The list of health dependencies for a specific health contributor.", response = HealthDependencies.class),
-
-                @io.swagger.annotations.ApiResponse(code = 405, message = "The health dependencies for the contributor with given id was not found.", response = HealthDependencies.class),
-
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the health dependencies of a given health contributor.", response = HealthDependencies.class) })
-        void getContributorDependencies(@ApiParam(value = "The id of an existing health contributor.", required = true) @PathParam("id") String id,
-                                        @Context SecurityContext securityContext, @Suspended AsyncResponse asyncResponse)
-                throws NotFoundException;
-
-        @GET
-        @Path("/details/{id}")
-
-        @Produces({ "application/json" })
-        @io.swagger.annotations.ApiOperation(value = "", notes = "Fetch the details of a specific health contributor.", response = HealthDetails.class, tags = { "Health", })
-        @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The list of details for the health contributor with a given id.", response = HealthDetails.class),
-
-                @io.swagger.annotations.ApiResponse(code = 405, message = "The health details for the contributor with given id was not found.", response = HealthDetails.class),
-
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the health details for a given health contributor.", response = HealthDetails.class) })
-        void getContributorDetails(@ApiParam(value = "The id of an existing health contributor.", required = true) @PathParam("id") String id,
-                                   @Context SecurityContext securityContext, @Suspended AsyncResponse asyncResponse)
+                                    @Context SecurityContext securityContext, @Suspended final AsyncResponse asyncResponse)
                 throws NotFoundException;
 
         @GET
@@ -402,13 +373,13 @@ public final class ApiV1 {
         @Produces({ "application/json" })
         @io.swagger.annotations.ApiOperation(value = "", notes = "Fetch the ready state of the health contributor.", response = Boolean.class, tags = { "Health", })
         @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The readiness status for the health contributor with given id.", response = Boolean.class),
+                @io.swagger.annotations.ApiResponse(code = 200, message = "The readiness status for the health contributor with given id.", response = Boolean.class),
 
-                @io.swagger.annotations.ApiResponse(code = 405, message = "The readiness status for the contributor with given id was not found.", response = Boolean.class),
+                @io.swagger.annotations.ApiResponse(code = 404, message = "The readiness status for the contributor with given id was not found.", response = Boolean.class),
 
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the ready state for a given health contributor.", response = Boolean.class) })
+                @io.swagger.annotations.ApiResponse(code = 500, message = "Internal server error while fetching the ready state for a given health contributor.", response = Boolean.class) })
         void getContributorReadiness(@ApiParam(value = "The id of an existing health contributor.", required = true) @PathParam("id") String id,
-                                     @Context SecurityContext securityContext, @Suspended AsyncResponse asyncResponse)
+                                     @Context SecurityContext securityContext, @Suspended final AsyncResponse asyncResponse)
                 throws NotFoundException;
 
         @GET
@@ -417,11 +388,11 @@ public final class ApiV1 {
         @Produces({ "application/json" })
         @io.swagger.annotations.ApiOperation(value = "", notes = "Fetch the status of a specific health contributor.", response = HealthStatus.class, tags = { "Health", })
         @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The health status of the Controller.", response = HealthStatus.class),
+                @io.swagger.annotations.ApiResponse(code = 200, message = "The health status of the Controller.", response = HealthStatus.class),
 
-                @io.swagger.annotations.ApiResponse(code = 405, message = "The health status for the contributor with given id was not found.", response = HealthStatus.class),
+                @io.swagger.annotations.ApiResponse(code = 404, message = "The health status for the contributor with given id was not found.", response = HealthStatus.class),
 
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the health status of a given health contributor.", response = HealthStatus.class) })
+                @io.swagger.annotations.ApiResponse(code = 500, message = "Internal server error while fetching the health status of a given health contributor.", response = HealthStatus.class) })
         void getContributorStatus(@ApiParam(value = "The id of an existing health contributor.", required = true) @PathParam("id") String id,
                                   @Context SecurityContext securityContext, @Suspended AsyncResponse asyncResponse)
                 throws NotFoundException;
@@ -432,59 +403,53 @@ public final class ApiV1 {
         @Produces({ "application/json" })
         @io.swagger.annotations.ApiOperation(value = "", notes = "Fetch the details of the Controller service.", response = HealthDetails.class, tags = { "Health", })
         @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The list of details.", response = HealthDetails.class),
+                @io.swagger.annotations.ApiResponse(code = 200, message = "The list of details.", response = HealthDetails.class),
 
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the health details of the Controller.", response = HealthDetails.class) })
-        void getDetails(@Context SecurityContext securityContext, @Suspended AsyncResponse asyncResponse)
+                @io.swagger.annotations.ApiResponse(code = 500, message = "Internal server error while fetching the health details of the Controller.", response = HealthDetails.class) })
+        void getDetails(@Context SecurityContext securityContext, @Suspended final AsyncResponse asyncResponse)
                 throws NotFoundException;
 
         @GET
-
-
         @Produces({ "application/json" })
         @io.swagger.annotations.ApiOperation(value = "", notes = "Return the Health of the Controller service.", response = HealthResult.class, tags = { "Health", })
         @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The Health result of the Controller.", response = HealthResult.class),
+                @io.swagger.annotations.ApiResponse(code = 200, message = "The Health result of the Controller.", response = HealthResult.class),
 
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the Health.", response = HealthResult.class) })
-        void getHealth(@ApiParam(value = "Whether or not to provide the details in the health response.", defaultValue = "false") @QueryParam("details") Boolean details,
-                       @Context SecurityContext securityContext, @Suspended AsyncResponse asyncResponse)
+                @io.swagger.annotations.ApiResponse(code = 500, message = "Internal server error while fetching the Health.", response = HealthResult.class) })
+        void getHealth(@Context SecurityContext securityContext, @Suspended final AsyncResponse asyncResponse)
                 throws NotFoundException;
 
         @GET
         @Path("/liveness")
-
         @Produces({ "application/json" })
         @io.swagger.annotations.ApiOperation(value = "", notes = "Fetch the liveness state of the Controller service.", response = Boolean.class, tags = { "Health", })
         @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The alive status.", response = Boolean.class),
+                @io.swagger.annotations.ApiResponse(code = 200, message = "The alive status.", response = Boolean.class),
 
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the liveness state of the Controller.", response = Boolean.class) })
-        void getLiveness(@Context SecurityContext securityContext, @Suspended AsyncResponse asyncResponse)
+                @io.swagger.annotations.ApiResponse(code = 500, message = "Internal server error while fetching the liveness state of the Controller.", response = Boolean.class) })
+        void getLiveness(@Context SecurityContext securityContext, @Suspended final AsyncResponse asyncResponse)
                 throws NotFoundException;
 
         @GET
         @Path("/readiness")
-
         @Produces({ "application/json" })
         @io.swagger.annotations.ApiOperation(value = "", notes = "Fetch the ready state of the Controller service.", response = Boolean.class, tags = { "Health", })
         @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The ready status.", response = Boolean.class),
+                @io.swagger.annotations.ApiResponse(code = 200, message = "The ready status.", response = Boolean.class),
 
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the ready state of the Controller.", response = Boolean.class) })
-        void getReadiness(@Context SecurityContext securityContext, @Suspended AsyncResponse asyncResponse)
+                @io.swagger.annotations.ApiResponse(code = 500, message = "Internal server error while fetching the ready state of the Controller.", response = Boolean.class) })
+        void getReadiness(@Context SecurityContext securityContext, @Suspended final AsyncResponse asyncResponse)
                 throws NotFoundException;
 
         @GET
         @Path("/status")
-
         @Produces({ "application/json" })
         @io.swagger.annotations.ApiOperation(value = "", notes = "Fetch the status of the Controller service.", response = HealthStatus.class, tags = { "Health", })
         @io.swagger.annotations.ApiResponses(value = {
-                @io.swagger.annotations.ApiResponse(code = 201, message = "The health status of the Controller.", response = HealthStatus.class),
+                @io.swagger.annotations.ApiResponse(code = 200, message = "The health status of the Controller.", response = HealthStatus.class),
 
-                @io.swagger.annotations.ApiResponse(code = 501, message = "Internal server error while fetching the health status of the Controller.", response = HealthStatus.class) })
-        void getStatus(@Context SecurityContext securityContext, @Suspended  AsyncResponse asyncResponse)
+                @io.swagger.annotations.ApiResponse(code = 500, message = "Internal server error while fetching the health status of the Controller.", response = HealthStatus.class) })
+        void getStatus(@Context SecurityContext securityContext, @Suspended final AsyncResponse asyncResponse)
                 throws NotFoundException;
     }
 }
