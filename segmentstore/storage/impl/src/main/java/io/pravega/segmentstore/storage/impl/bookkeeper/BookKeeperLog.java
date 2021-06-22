67e91f5f1af48b3aa79bf4c3066ca9537aebff48
Raúl Gracia <raul.gracia@emc.com>
2021-06-22 16:00:47 +0200

    Issue 6021: OperationProcessor queue may be left closed while the container is in RUNNING state (#6085)
    Fixes a bug in which the BookkeeperLog could get closed throwing a non-fatal exception from the OperationProcessor viewpoint, leaving the latter one in an inconsistent state.
    
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>
    
    Co-authored-by: Andrei Paduroiu <andreipaduroiu@users.noreply.github.com>

--- segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperLog.java
+++ segmentstore/storage/impl/src/main/java/io/pravega/segmentstore/storage/impl/bookkeeper/BookKeeperLog.java
@@ -44,7 +44,6 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.ScheduledFuture;
@@ -182,7 +181,7 @@ class BookKeeperLog implements DurableDataLog {
             }
 
             // Close the write queue and cancel the pending writes.
-            this.writes.close().forEach(w -> w.fail(new CancellationException("BookKeeperLog has been closed."), true));
+            this.writes.close().forEach(w -> w.fail(new ObjectClosedException("BookKeeperLog has been closed."), true));
 
             if (writeLedger != null) {
                 try {
