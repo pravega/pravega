7c874c9c4a934a0cc49afdc01ae7c1ba94449fd9
Colin Hryniowski <13041044+co-jo@users.noreply.github.com>
2021-07-02 07:03:17 -0700

    Issue 5098: Controller/SegmentStore HealthCheck Integration (#6102)
    Incorporates both the refactored shared:rest project as well the shared:health project.
    Sets up the rest endpoint and generated required models to server Health data externally.
    Adds the HealthServiceManager (the top level component) to the ControllerServiceStarter.
    
    Signed-off-by: Colin Hryniowski <colin.hryniowski@dell.com>
    Co-authored-by: Raúl Gracia <raul.gracia@emc.com>
    Co-authored-by: Andrei Paduroiu <andreipaduroiu@users.noreply.github.com>

--- shared/health/src/main/java/io/pravega/shared/health/HealthServiceManager.java
+++ shared/health/src/main/java/io/pravega/shared/health/HealthServiceManager.java
@@ -67,14 +67,24 @@ public class HealthServiceManager implements AutoCloseable {
 
     @VisibleForTesting
     HealthServiceUpdater getHealthServiceUpdater() {
-        return updater;
+        return this.updater;
+    }
+
+    /**
+     * Synchronously starts the {@link HealthServiceUpdater}.
+     */
+    public void start() {
+        this.updater.startAsync();
+        this.updater.awaitRunning();
     }
 
     @Override
     public void close() {
         if (!this.closed.getAndSet(true)) {
-            this.updater.close();
             this.root.close();
+            this.updater.close();
+            this.updater.stopAsync();
+            this.updater.awaitTerminated();
         }
     }
 
