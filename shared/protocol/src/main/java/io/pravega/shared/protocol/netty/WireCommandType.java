699e3c347d95d1c7e10892890d923cc1d6eddf83
pbelgundi <prajakta.belgundi@emc.com>
2021-07-05 07:02:32 -0700

    first draft
    Signed-off-by: pbelgundi <prajakta.belgundi@emc.com>

--- shared/protocol/src/main/java/io/pravega/shared/protocol/netty/WireCommandType.java
+++ shared/protocol/src/main/java/io/pravega/shared/protocol/netty/WireCommandType.java
@@ -85,6 +85,9 @@ public enum WireCommandType {
     AUTH_TOKEN_CHECK_FAILED(60, WireCommands.AuthTokenCheckFailed::readFrom),
     ERROR_MESSAGE(61, WireCommands.ErrorMessage::readFrom),
 
+    GET_TABLE_SEGMENT_INFO(68, WireCommands.GetTableSegmentInfo::readFrom),
+    TABLE_SEGMENT_INFO(69, WireCommands.TableSegmentInfo::readFrom),
+
     CREATE_TABLE_SEGMENT(70, WireCommands.CreateTableSegment::readFrom),
     DELETE_TABLE_SEGMENT(71, WireCommands.DeleteTableSegment::readFrom),
     MERGE_TABLE_SEGMENTS(72, WireCommands.MergeTableSegments::readFrom),
