b382123b9f5b5daa665c6e27f01982ee7ff523ea
Atharva <Atharva.Joshi@dell.com>
2021-07-14 08:19:02 -0700

    redone
    Signed-off-by: Atharva <Atharva.Joshi@dell.com>

--- controller/src/main/java/io/pravega/controller/server/rest/generated/api/JacksonJsonProvider.java
+++ controller/src/main/java/io/pravega/controller/server/rest/generated/api/JacksonJsonProvider.java
@@ -1,29 +1,18 @@
 package io.pravega.controller.server.rest.generated.api;
 
-import com.fasterxml.jackson.databind.DeserializationFeature;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.SerializationFeature;
-
-import com.fasterxml.jackson.datatype.joda.*;
-
-import com.fasterxml.jackson.jaxrs.json.JacksonJaxbJsonProvider;
-
+import io.swagger.util.Json;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.ext.Provider;
+import org.glassfish.jersey.jackson.internal.jackson.jaxrs.json.JacksonJaxbJsonProvider;
 
 @Provider
 @Produces({MediaType.APPLICATION_JSON})
 public class JacksonJsonProvider extends JacksonJaxbJsonProvider {
+    private static ObjectMapper commonMapper = Json.mapper();
 
     public JacksonJsonProvider() {
-
-        ObjectMapper objectMapper = new ObjectMapper()
-            .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-            .disable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS)
-            .registerModule(new JodaModule())
-            .setDateFormat(new RFC3339DateFormat());
-
-        setMapper(objectMapper);
+        super.setMapper(commonMapper);
     }
 }
