e4c7912aad91e2a3a23bcc519c66555a48c426e7
Atharva <Atharva.Joshi@dell.com>
2021-07-29 01:01:33 -0700

    reverting import statements in ScaleMetadata
    Signed-off-by: Atharva <Atharva.Joshi@dell.com>

--- controller/src/main/java/io/pravega/controller/server/rest/generated/model/ScaleMetadata.java
+++ controller/src/main/java/io/pravega/controller/server/rest/generated/model/ScaleMetadata.java
@@ -15,9 +15,13 @@ package io.pravega.controller.server.rest.generated.model;
 
 import java.util.Objects;
 import com.fasterxml.jackson.annotation.JsonProperty;
+import com.fasterxml.jackson.annotation.JsonCreator;
+import io.pravega.controller.server.rest.generated.model.Segment;
+import io.swagger.annotations.ApiModel;
 import io.swagger.annotations.ApiModelProperty;
 import java.util.ArrayList;
 import java.util.List;
+import javax.validation.constraints.*;
 
 /**
  * ScaleMetadata
