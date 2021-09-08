dabd03874d608c6138bdbdd7602b876706d2cf98
Shashwat Sharma <shashwat_sharma@dell.com>
2021-09-08 04:33:20 -0700

    rebuild PR
    Signed-off-by: Shashwat Sharma <shashwat_sharma@dell.com>

--- controller/src/main/java/io/pravega/controller/server/eventProcessor/ControllerEventProcessors.java
+++ controller/src/main/java/io/pravega/controller/server/eventProcessor/ControllerEventProcessors.java
@@ -209,7 +209,7 @@ public class ControllerEventProcessors extends AbstractIdleService implements Fa
      * @return true if zookeeper is connected and bootstrap is completed.
      */
     public boolean isReady() {
-        log.info("inside isReady method, values of isMetadataServiceConnected is {} and isBootstrapCompleted is {}",
+        log.info("inside CEP isReady method, values of isMetadataServiceConnected is {} and isBootstrapCompleted is {}",
                 isMetadataServiceConnected(), isBootstrapCompleted());
         return isMetadataServiceConnected() && isBootstrapCompleted();
     }
