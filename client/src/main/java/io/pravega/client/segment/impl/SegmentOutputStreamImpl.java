ea27e30215cad2c12a8291da75a08da03b5771a0
Sandeep <sandeep.shridhar@emc.com>
2021-09-20 13:13:28 +0530

    Improve EventStreamWriter.flush() error handling.
    Signed-off-by: Sandeep <sandeep.shridhar@emc.com>

--- client/src/main/java/io/pravega/client/segment/impl/SegmentOutputStreamImpl.java
+++ client/src/main/java/io/pravega/client/segment/impl/SegmentOutputStreamImpl.java
@@ -574,6 +574,7 @@ class SegmentOutputStreamImpl implements SegmentOutputStream {
             } catch (Exception e) {
                 failConnection(e);
                 if (e instanceof RetriesExhaustedException) {
+                    log.error("Flush on segment {} by writer {} failed after all retries", segmentName, writerId);
                     //throw an exception to the external world that the flush failed due to RetriesExhaustedException
                     throw Exceptions.sneakyThrow(e);
                 }
@@ -591,6 +592,7 @@ class SegmentOutputStreamImpl implements SegmentOutputStream {
         } else if (state.exception instanceof RetriesExhaustedException) {
             // All attempts to connect with SSS have failed.
             // The number of retry attempts is based on EventWriterConfig
+            log.error("Flush on segment {} by writer {} failed after all retries", segmentName, writerId);
             throw Exceptions.sneakyThrow(state.exception);
         }
     }
