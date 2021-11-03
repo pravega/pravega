7d4558ff4a2297f31e5eff99c9052efdc20dd79b
SrishT <Srishti.Thakkar@emc.com>
2021-11-03 06:13:21 -0700

    Issue 5501: Fixing checkstyle errors
    Signed-off-by: SrishT <Srishti.Thakkar@emc.com>

--- cli/admin/src/main/java/io/pravega/cli/admin/readerGroup/ParseReaderGroupStreamCommand.java
+++ cli/admin/src/main/java/io/pravega/cli/admin/readerGroup/ParseReaderGroupStreamCommand.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright Pravega Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.pravega.cli.admin.readerGroup;
 
 import io.pravega.cli.admin.AdminCommand;
@@ -13,16 +28,19 @@ import lombok.Cleanup;
 import lombok.val;
 import org.apache.curator.framework.CuratorFramework;
 
-import java.io.*;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.TimeUnit;
 
 public class ParseReaderGroupStreamCommand extends AdminCommand {
 
-    protected final GrpcAuthHelper authHelper;
     private static final int REQUEST_TIMEOUT_SECONDS = 10;
     private static final int READ_WRITE_BUFFER_SIZE = 2 * 1024 * 1024;
+    private final GrpcAuthHelper authHelper;
 
     public ParseReaderGroupStreamCommand(CommandArgs args) {
         super(args);
