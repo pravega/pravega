ea5a0a4f4e89910fa3473af2e59d724aa2775227
Shashwat Sharma <shashwat_sharma@dell.com>
2021-11-08 22:12:38 -0800

    Remove unused import
    Signed-off-by: Shashwat Sharma <shashwat_sharma@dell.com>

--- controller/src/test/java/io/pravega/controller/store/PravegaTablesScopeTest.java
+++ controller/src/test/java/io/pravega/controller/store/PravegaTablesScopeTest.java
@@ -35,8 +35,6 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.ArgumentMatchers.eq;
-import static org.mockito.Mockito.doAnswer;
-import static org.mockito.Mockito.doNothing;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.spy;
