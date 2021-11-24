03391a2b4c41ce9a58dd1d605551463b6ecac742
Bhupender-Y <Bhupender.Y@dell.com>
2021-11-24 19:49:48 +0530

    Resolved checkstyle issue.
    Signed-off-by: Bhupender-Y <Bhupender.Y@dell.com>

--- controller/src/main/java/io/pravega/controller/server/ControllerService.java
+++ controller/src/main/java/io/pravega/controller/server/ControllerService.java
@@ -383,7 +383,7 @@ public class ControllerService {
                                   return CreateStreamStatus.newBuilder().setStatus(status).build();
                               }, executor);
                           } else {
-                              log.info("Stream {} already exists ", NameUtils.getScopedStreamName(scope,stream));
+                              log.info("Stream {} already exists ", NameUtils.getScopedStreamName(scope, stream));
                               return CompletableFuture.completedFuture(
                                       CreateStreamStatus.newBuilder().setStatus(CreateStreamStatus.Status.STREAM_EXISTS).build());
                           }
