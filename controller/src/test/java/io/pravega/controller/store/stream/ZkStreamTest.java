3aedb957352afd6d1e027f24a41d85c3a488d610
pbelgundi <prajakta.belgundi@emc.com>
2021-12-06 01:51:56 -0800

    increased test timeout
    Signed-off-by: pbelgundi <prajakta.belgundi@emc.com>

--- controller/src/test/java/io/pravega/controller/store/stream/ZkStreamTest.java
+++ controller/src/test/java/io/pravega/controller/store/stream/ZkStreamTest.java
@@ -74,7 +74,7 @@ import static org.mockito.Mockito.spy;
 public class ZkStreamTest {
     private static final String SCOPE = "scope";
     @Rule
-    public Timeout globalTimeout = new Timeout(30, TimeUnit.SECONDS);
+    public Timeout globalTimeout = new Timeout(90, TimeUnit.SECONDS);
     private TestingServer zkTestServer;
     private CuratorFramework cli;
     private StreamMetadataStore storePartialMock;
