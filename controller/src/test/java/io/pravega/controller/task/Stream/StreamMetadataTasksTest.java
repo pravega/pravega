8585d54d503e4eb45aff89be16441507cc895c3c
Shashwat Sharma <shashwat_sharma@dell.com>
2021-12-17 08:03:42 -0800

    checkstyle
    Signed-off-by: Shashwat Sharma <shashwat_sharma@dell.com>

--- controller/src/test/java/io/pravega/controller/task/Stream/StreamMetadataTasksTest.java
+++ controller/src/test/java/io/pravega/controller/task/Stream/StreamMetadataTasksTest.java
@@ -160,8 +160,6 @@ import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.spy;
-import static org.mockito.Mockito.times;
-import static org.mockito.Mockito.verify;
 
 public abstract class StreamMetadataTasksTest {
 
