aa95f9684b786d5f4949f8f87c137cad7bf4456f
Raúl Gracia <raul.gracia@emc.com>
2022-03-11 17:12:47 +0100

    Issue 6638: Cherry-pick PR 6517 to r0.11 (#6517) (#6639)
    Add a new metric to keep track of Table Segment used credits.
    
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- segmentstore/server/src/test/java/io/pravega/segmentstore/server/SegmentStoreMetricsTests.java
+++ segmentstore/server/src/test/java/io/pravega/segmentstore/server/SegmentStoreMetricsTests.java
@@ -41,8 +41,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
-import static io.pravega.shared.MetricsTags.containerTag;
-import static io.pravega.shared.MetricsTags.throttlerTag;
+import static io.pravega.shared.MetricsTags.*;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNull;
 
@@ -235,6 +234,13 @@ public class SegmentStoreMetricsTests {
         AssertExtensions.assertEventuallyEquals(true, () -> MetricRegistryUtils.getGauge(MetricsNames.OPERATION_LOG_SIZE, containerTag(containerId)).value() == 1000, 2000);
         op.close();
         assertNull(MetricRegistryUtils.getGauge(MetricsNames.OPERATION_LOG_SIZE, containerTag));
+    }
+
+    @Test
+    public void testTableSegmentCreditsMetrics() throws Exception {
+        String segmentName = "_system/test/segment";
+        SegmentStoreMetrics.tableSegmentUsedCredits(segmentName, 100);
+        AssertExtensions.assertEventuallyEquals(true, () -> MetricRegistryUtils.getGauge(MetricsNames.TABLE_SEGMENT_USED_CREDITS, TAG_SEGMENT, segmentName).value() == 100, 2000);
     }
 
     private static class TestCompletableOperation extends CompletableOperation {
