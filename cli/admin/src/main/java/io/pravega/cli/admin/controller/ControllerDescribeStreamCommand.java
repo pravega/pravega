81439f44ecf26726c6101091883b1fe2cf157857
Kuldeep Kumar <kuldeep.kumar3@dell.com>
2022-03-28 10:22:24 -0700

    Replacing cleanup annotation with try-with-resources
    Signed-off-by: Kuldeep Kumar <kuldeep.kumar3@dell.com>

--- cli/admin/src/main/java/io/pravega/cli/admin/controller/ControllerDescribeStreamCommand.java
+++ cli/admin/src/main/java/io/pravega/cli/admin/controller/ControllerDescribeStreamCommand.java
@@ -39,10 +39,9 @@ public class ControllerDescribeStreamCommand extends ControllerCommand {
         final String scope = getArg(0);
         final String stream = getArg(1);
 
-        @Cleanup
-        val context = createContext();
-        // Print the response sent by the Controller.
-        prettyJSONOutput(executeRESTCall(context, "/v1/scopes/" + scope + "/streams/" + stream));
+        try (val context = createContext()){
+            prettyJSONOutput(executeRESTCall(context, "/v1/scopes/" + scope + "/streams/" + stream));
+        }
     }
 
     public static CommandDescriptor descriptor() {
