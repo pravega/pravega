7b834b5e6c1f85e47375290801981969edb66af6
Shashwat Sharma <shashwat_sharma@dell.com>
2022-05-19 12:36:26 +0530

    Issue_6745 Handle Cntroller connectivity with ZK
    Signed-off-by: Shashwat Sharma <shashwat_sharma@dell.com>

--- controller/src/main/java/io/pravega/controller/store/client/StoreClientFactory.java
+++ controller/src/main/java/io/pravega/controller/store/client/StoreClientFactory.java
@@ -121,37 +121,15 @@ public class StoreClientFactory {
         @Override
         @Synchronized
         public ZooKeeper newZooKeeper(String connectString, int sessionTimeout, Watcher watcher, boolean canBeReadOnly) throws Exception {
-            // prevent creating a new client, stick to the same client created earlier
-            // this trick prevents curator from re-creating ZK client on session expiry
             if (client == null) {
                 Exceptions.checkNotNullOrEmpty(connectString, "connectString");
                 Preconditions.checkArgument(sessionTimeout > 0, "sessionTimeout should be a positive integer");
                 this.connectString = connectString;
                 this.sessionTimeout = sessionTimeout;
                 this.canBeReadOnly = canBeReadOnly;
-                this.client = new ZooKeeper(connectString, sessionTimeout, watcher, canBeReadOnly);
-            } else {
-                try {
-                    Preconditions.checkArgument(this.connectString.equals(connectString), "connectString differs");
-                    Preconditions.checkArgument(this.sessionTimeout == sessionTimeout, "sessionTimeout differs");
-                    Preconditions.checkArgument(this.canBeReadOnly == canBeReadOnly, "canBeReadOnly differs");
-                    this.client.register(watcher);
-                } catch (IllegalArgumentException e) {
-                    log.warn("Input argument for new ZooKeeper client ({}, {}, {}) changed with respect to existing client ({}, {}, {}).",
-                        connectString, sessionTimeout, canBeReadOnly, this.connectString, this.sessionTimeout, this.canBeReadOnly);
-                    closeClient(client);
-                }
             }
+            this.client = new ZooKeeper(this.connectString, this.sessionTimeout, watcher, this.canBeReadOnly);
             return this.client;
-        }
-
-        private void closeClient(ZooKeeper client) {
-            try {
-                client.close();
-            } catch (Exception e) {
-                // We prevent throwing uncontrolled exceptions here, which may lead Curator to retry indefinitely.
-                log.warn("Exception when closing ZooKeeper client. {}", e.getMessage());
-            }
         }
     }
 
