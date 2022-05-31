b96f9c5b8bce36333723c0ba30b321c273718058
Munish <munish.kumar2@dell.com>
2022-05-31 14:43:25 +0530

    Adding delete and seal even fail metrics
    Signed-off-by: Munish <munish.kumar2@dell.com>

--- controller/src/main/java/io/pravega/controller/server/eventProcessor/requesthandlers/SealStreamTask.java
+++ controller/src/main/java/io/pravega/controller/server/eventProcessor/requesthandlers/SealStreamTask.java
@@ -36,7 +36,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.CompletionException;
 import java.util.concurrent.CompletionStage;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.stream.Collectors;
