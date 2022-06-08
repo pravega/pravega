8400a844c4294a78c9df6bf5c2d715ca73de5539
Amit-Singh40 <amit.singh30@dell.com>
2022-06-08 09:53:27 +0530

    Adding white space after typecasting
    Signed-off-by: Amit-Singh40 <amit.singh30@dell.com>

--- segmentstore/server/src/test/java/io/pravega/segmentstore/server/writer/WriterConfigTest.java
+++ segmentstore/server/src/test/java/io/pravega/segmentstore/server/writer/WriterConfigTest.java
@@ -10,7 +10,7 @@ public class WriterConfigTest {
 
     @Test
     public void testMaxRolloverSize() {
-        Assert.assertEquals(134217728L, (long)WriterConfig.MAX_ROLLOVER_SIZE.getDefaultValue());
+        Assert.assertEquals(134217728L, (long) WriterConfig.MAX_ROLLOVER_SIZE.getDefaultValue());
         Assert.assertEquals("rollover.size.bytes.max", WriterConfig.MAX_ROLLOVER_SIZE.getName());
     }
 }
