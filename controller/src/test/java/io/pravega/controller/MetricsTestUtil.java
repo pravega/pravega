a3f8997e9a7e4aa75f0d42f334331cc0b0515f68
Munish <munish.kumar2@dell.com>
2022-06-13 16:32:00 +0530

    Moved common logic to different class
    Signed-off-by: Munish <munish.kumar2@dell.com>

--- controller/src/test/java/io/pravega/controller/MetricsTestUtil.java
+++ controller/src/test/java/io/pravega/controller/MetricsTestUtil.java
@@ -1,3 +1,18 @@
+/**
+ * Copyright Pravega Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package io.pravega.controller;
 
 import io.pravega.shared.metrics.MetricRegistryUtils;
@@ -10,11 +25,8 @@ import lombok.val;
 
 public class MetricsTestUtil {
 
-    /**
-     * Start Metrics service
-     * @return StatsProvider
-     */
-    public static  StatsProvider getInitialisedStatsProvider(){
+    public static  StatsProvider getInitialisedStatsProvider() {
+        //Start Metrics service
         StatsProvider statsProvider = null;
         MetricsConfig metricsConfig = MetricsConfig.builder()
                                                    .with(MetricsConfig.ENABLE_STATISTICS, true)
@@ -24,7 +36,6 @@ public class MetricsTestUtil {
         MetricsProvider.initialize(metricsConfig);
         statsProvider = MetricsProvider.getMetricsProvider();
         statsProvider.startWithoutExporting();
-
         return statsProvider;
     }
 
