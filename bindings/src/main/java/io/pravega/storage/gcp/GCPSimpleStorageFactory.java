701241afe5c29c14575987bccdc4da64e84bc2fb
Amit-Singh40 <amit.singh30@dell.com>
2022-07-01 09:40:10 +0530

    Commiting to re trigger the build
    Signed-off-by: Amit-Singh40 <amit.singh30@dell.com>

--- bindings/src/main/java/io/pravega/storage/gcp/GCPSimpleStorageFactory.java
+++ bindings/src/main/java/io/pravega/storage/gcp/GCPSimpleStorageFactory.java
@@ -109,7 +109,6 @@ public class GCPSimpleStorageFactory implements SimpleStorageFactory {
         jsonObject.addProperty("private_key", GCPStorageConfig.PRIVATE_KEY.getDefaultValue());
         jsonObject.addProperty("client_email", GCPStorageConfig.CLIENT_EMAIL.getDefaultValue());
         jsonObject.addProperty("client_id", GCPStorageConfig.CLIENT_ID.getDefaultValue());
-
         return jsonObject;
     }
 }
