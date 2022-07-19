b132dea8d651910831929ef3a9f8a100bc2204e5
Aditi Jadhav <aditi.jadhav@dell.com>
2022-07-19 14:59:14 -0700

    Adding new tests. Do not push.
    Signed-off-by: Aditi Jadhav <Aditi.Jadhav@dell.com>

--- bindings/src/test/java/io/pravega/storage/azure/AzureBlobClientImplTestsWithMock.java
+++ bindings/src/test/java/io/pravega/storage/azure/AzureBlobClientImplTestsWithMock.java
@@ -18,6 +18,7 @@ package io.pravega.storage.azure;
 import com.azure.storage.blob.BlobClient;
 import com.azure.storage.blob.BlobContainerClient;
 import com.azure.storage.blob.models.AppendBlobItem;
+import com.azure.storage.blob.models.AppendBlobRequestConditions;
 import com.azure.storage.blob.specialized.AppendBlobClient;
 import com.azure.storage.blob.specialized.BlobClientBase;
 import lombok.val;
@@ -93,21 +94,57 @@ public class AzureBlobClientImplTestsWithMock {
 
     @Test
     public void testGetInputStream() {
+        String blobName = "testBlobName";
+        val blobContainerClient = mock(BlobContainerClient.class);
+        val blobClient = mock(BlobClient.class);
+        val blobClientBase = mock(BlobClientBase.class);
+        when(blobContainerClient.getBlobClient(any())).thenReturn(blobClient);
+        when(blobClientBase.getContainerClient()).thenReturn(blobContainerClient);
+
+        val azureClient = new AzureBlobClientImpl(AzureStorageConfig.builder()
+                .with(AzureStorageConfig.CONTAINER, "test")
+                .build(), blobContainerClient);
+
+        azureClient.getInputStream(blobName, 0, 10);
+        blobContainerClient.getBlobClient(blobName);
+        blobClientBase.openInputStream();
+        blobClientBase.downloadContent();
+
+        verify(blobClientBase).openInputStream();
+    }
+
+    @Test
+    public void testAppendBlock() {
         val blobContainerClient = mock(BlobContainerClient.class);
         val blobClient = mock(BlobClient.class);
         val appendBlobClient = mock(AppendBlobClient.class);
-        val blobClientBase = mock(BlobClientBase.class);
-        when(blobClientBase.getContainerClient()).thenReturn(blobContainerClient);
         when(blobContainerClient.getBlobClient(any())).thenReturn(blobClient);
         when(blobClient.getAppendBlobClient()).thenReturn(appendBlobClient);
 
         val azureClient = new AzureBlobClientImpl(AzureStorageConfig.builder()
                 .with(AzureStorageConfig.CONTAINER, "test")
                 .build(), blobContainerClient);
+        val inputStream = azureClient.getInputStream("testBlob", 0, 10);
+        val conditions = mock(AppendBlobRequestConditions.class);
 
-        azureClient.create("testBlobName");
-        azureClient.getInputStream("testBlobName", 0, 10);
-        blobClientBase.openInputStream(any(), any());
+        azureClient.appendBlock("testBlob", 0, 10, inputStream);
+        conditions.setAppendPosition(5L);
+
+        verify(appendBlobClient).appendBlock(inputStream, 10);
     }
 
+    @Test
+    public void testGetBlobProperties() {
+        val blobContainerClient = mock(BlobContainerClient.class);
+        val blobClient = mock(BlobClient.class);
+        when(blobContainerClient.getBlobClient(any())).thenReturn(blobClient);
+
+        val azureClient = new AzureBlobClientImpl(AzureStorageConfig.builder()
+                .with(AzureStorageConfig.CONTAINER, "test")
+                .build(), blobContainerClient);
+
+        azureClient.getBlobProperties("testBlob");
+
+        verify(blobClient).getProperties();
+    }
 }
