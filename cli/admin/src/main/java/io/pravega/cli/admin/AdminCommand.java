4d73e96e6b9c37a38dd2a304df10abf9fbb8c8de
Munish <munish.kumar2@dell.com>
2022-08-22 19:21:21 +0530

    Issue 6497: Adding admin cli for viewing pending events (#6828)
    Add a new controller-metadata command in admin-cli to view pending events for a particular Controller host.
    
    Signed-off-by: Munish <munish.kumar2@dell.com>

--- cli/admin/src/main/java/io/pravega/cli/admin/AdminCommand.java
+++ cli/admin/src/main/java/io/pravega/cli/admin/AdminCommand.java
@@ -43,6 +43,7 @@ import io.pravega.cli.admin.controller.metadata.ControllerMetadataGetEntryComman
 import io.pravega.cli.admin.controller.metadata.ControllerMetadataListEntriesCommand;
 import io.pravega.cli.admin.controller.metadata.ControllerMetadataListKeysCommand;
 import io.pravega.cli.admin.controller.metadata.ControllerMetadataUpdateEntryCommand;
+import io.pravega.cli.admin.controller.metadata.ControllerMetadataViewPendingEventsCommand;
 import io.pravega.cli.admin.controller.metadata.ControllerMetadataViewReaderInfoCommand;
 import io.pravega.cli.admin.dataRecovery.DurableLogInspectCommand;
 import io.pravega.cli.admin.dataRecovery.DurableLogRecoveryCommand;
@@ -410,6 +411,7 @@ public abstract class AdminCommand {
                         .put(ControllerMetadataUpdateEntryCommand::descriptor, ControllerMetadataUpdateEntryCommand::new)
                         .put(ControllerMetadataViewReaderInfoCommand::descriptor, ControllerMetadataViewReaderInfoCommand::new)
                         .put(ListChunksCommand::descriptor, ListChunksCommand::new)
+                        .put(ControllerMetadataViewPendingEventsCommand::descriptor, ControllerMetadataViewPendingEventsCommand::new)
                         .build());
 
         /**
