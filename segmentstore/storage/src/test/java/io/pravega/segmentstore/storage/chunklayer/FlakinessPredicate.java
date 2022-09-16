9b6d2712fb61ba6603eb4591f74b1e41a8211665
Sachin Joshi <sachin.joshi@emc.com>
2022-09-16 15:37:39 -0700

    Implement ReplicatedChunkStorage
    Signed-off-by: Sachin Joshi <sachin.joshi@emc.com>

---
+++ segmentstore/storage/src/test/java/io/pravega/segmentstore/storage/chunklayer/FlakinessPredicate.java
@@ -0,0 +1,41 @@
+/**
+ * Copyright Pravega Authors.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package io.pravega.segmentstore.storage.chunklayer;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.NonNull;
+import lombok.RequiredArgsConstructor;
+
+import java.util.concurrent.Callable;
+import java.util.function.Function;
+
+/**
+ * Defines the flaky behavior matching given Predicate.
+ */
+@Builder
+@Data
+@RequiredArgsConstructor
+public class FlakinessPredicate {
+    @NonNull
+    final String matchRegEx;
+    @NonNull
+    final String method;
+    @NonNull
+    final Function<Integer, Boolean> matchPredicate;
+    @NonNull
+    final Callable action;
+}
