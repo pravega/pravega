18ef82404211a89c51fe99a4473a1fff01fe972a
Shwetha N <shwetha.n1@dell.com>
2023-01-30 23:05:24 -0800

    added testcases
    Signed-off-by: Shwetha N <shwetha.n1@dell.com>

--- client/src/test/java/io/pravega/client/stream/impl/ReaderGroupImplTest.java
+++ client/src/test/java/io/pravega/client/stream/impl/ReaderGroupImplTest.java
@@ -31,12 +31,18 @@ import io.pravega.client.state.Update;
 import io.pravega.client.stream.Checkpoint;
 import io.pravega.client.stream.ReaderGroupConfig;
 import io.pravega.client.stream.ReaderSegmentDistribution;
+import io.pravega.client.stream.ScalingPolicy;
 import io.pravega.client.stream.Serializer;
 import io.pravega.client.stream.Stream;
+import io.pravega.client.stream.StreamConfiguration;
 import io.pravega.client.stream.StreamCut;
 import io.pravega.client.stream.impl.ReaderGroupState.ClearCheckpointsBefore;
+import io.pravega.client.stream.mock.MockConnectionFactoryImpl;
+import io.pravega.client.stream.mock.MockController;
+import io.pravega.client.stream.mock.MockSegmentStreamFactory;
 import io.pravega.common.concurrent.Futures;
 import io.pravega.shared.NameUtils;
+import io.pravega.shared.protocol.netty.PravegaNodeUri;
 import io.pravega.test.common.AssertExtensions;
 import io.pravega.test.common.InlineExecutor;
 import java.util.Arrays;
@@ -44,8 +50,10 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.HashSet;
 import java.util.Map;
+import java.util.NavigableMap;
 import java.util.Optional;
 import java.util.Set;
+import java.util.TreeMap;
 import java.util.UUID;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ExecutionException;
@@ -57,6 +65,7 @@ import java.util.concurrent.atomic.AtomicReference;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import lombok.Cleanup;
+import lombok.val;
 import org.junit.Before;
 import org.junit.After;
 import org.junit.Test;
@@ -104,7 +113,9 @@ public class ReaderGroupImplTest {
 
     private Serializer<InitialUpdate<ReaderGroupState>> initSerializer = new ReaderGroupStateInitSerializer();
     private Serializer<Update<ReaderGroupState>> updateSerializer = new ReaderGroupStateUpdatesSerializer();
-
+    private final StreamConfiguration configStream = StreamConfiguration.builder()
+                                                                  .scalingPolicy(ScalingPolicy.fixed(1))
+                                                                  .build();
     @SuppressWarnings("unchecked")
     @Before
     public void setUp() throws Exception {
@@ -484,6 +495,46 @@ public class ReaderGroupImplTest {
         assertEquals(Long.MAX_VALUE, endSegmentMap.get(new Segment(SCOPE, "s1", 0)).longValue());
     }
 
+        @Test(timeout = 10000)
+    public void initiateCheckpointOutstandingCheckPoint() throws Exception {
+        PravegaNodeUri endpoint = new PravegaNodeUri("localhost", 12345);
+        MockConnectionFactoryImpl connectionFactory = new MockConnectionFactoryImpl();
+        MockController mkController = new MockController(endpoint.getEndpoint(), endpoint.getPort(), connectionFactory, false);
+        createScopeAndStream("scope", "stream", mkController);
+        MockSegmentStreamFactory streamFactory = new MockSegmentStreamFactory();
+
+        @Cleanup
+        SynchronizerClientFactory syncClientFactory = new ClientFactoryImpl("scope", mkController, connectionFactory, streamFactory,
+                streamFactory, streamFactory, streamFactory);
+        SynchronizerConfig syncConfig = SynchronizerConfig.builder().build();
+        Map<SegmentWithRange, Long> segments = new HashMap<>();
+        Segment s1 = new Segment("scope", "stream", 1);
+        Segment s2 = new Segment("scope", "stream", 2);
+        segments.put(new SegmentWithRange(s1, 0.0, 0.5), 1L);
+        segments.put(new SegmentWithRange(s2, 0.5, 1.0), 2L);
+        createScopeAndStream("scope", NameUtils.getStreamForReaderGroup(GROUP_NAME), mkController);
+        readerGroup = new ReaderGroupImpl("scope", GROUP_NAME, syncConfig, initSerializer,
+                updateSerializer, syncClientFactory, mkController, connectionPool);
+
+        @Cleanup("shutdown")
+        InlineExecutor executor = new InlineExecutor();
+        StateSynchronizer<ReaderGroupState> rgStateSynchronizer = readerGroup.getSynchronizer();
+        rgStateSynchronizer.initialize(new ReaderGroupState.ReaderGroupStateInit(
+                ReaderGroupConfig.builder().stream(Stream.of("scope", "stream")).maxOutstandingCheckpointRequest(1).build(), segments, Collections.emptyMap(), false));
+        CheckpointState rgState = rgStateSynchronizer.getState().getCheckpointState();
+        rgState.beginNewCheckpoint("1", ImmutableSet.of("a", "b"), Collections.emptyMap());
+        CompletableFuture<Checkpoint> result = readerGroup.initiateCheckpoint("test", executor);
+        assertTrue("expecting a checkpoint failure", result.isCompletedExceptionally());
+        try {
+            result.get();
+        } catch (InterruptedException | ExecutionException e) {
+            assertTrue("expecting MaxNumberOfCheckpointsExceededException", e.getCause() instanceof MaxNumberOfCheckpointsExceededException);
+        }
+    }
+    private void createScopeAndStream(String scope, String stream, MockController controller) {
+        controller.createScope(scope).join();
+        controller.createStream(scope, stream, configStream).join();
+    }
     private StreamCut createStreamCut(String streamName, int numberOfSegments) {
         Map<Segment, Long> positions = new HashMap<>();
         IntStream.of(numberOfSegments).forEach(segNum -> positions.put(new Segment(SCOPE, streamName, segNum), 10L));
