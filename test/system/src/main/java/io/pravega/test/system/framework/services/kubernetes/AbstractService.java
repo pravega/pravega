0a5b7cc7123e50e5ffeb68be7f90452f407fe86e
AJadhav29 <aditi.jadhav@dell.com>
2023-05-17 17:25:38 -0700

    Adding the log.info and changing the path for json input file. Signed-off-by: AJadhav29 <aditi.jadhav@dell.com>
    Signed-off-by: AJadhav29 <aditi.jadhav@dell.com>

--- test/system/src/main/java/io/pravega/test/system/framework/services/kubernetes/AbstractService.java
+++ test/system/src/main/java/io/pravega/test/system/framework/services/kubernetes/AbstractService.java
@@ -53,7 +53,7 @@ public abstract class AbstractService implements Service {
 
     public static final int CONTROLLER_GRPC_PORT = 9090;
     public static final int CONTROLLER_REST_PORT = 10080;
-    public static final String SYSTEMTESTPROPERTIES = "systemTestConfig.json";
+    public static final String SYSTEMTESTPROPERTIES = "test/system/src/test/resources/systemTestConfig.json";
     protected static final String DOCKER_REGISTRY =  System.getProperty("dockerRegistryUrl", "");
     protected static final String PREFIX = System.getProperty("imagePrefix", "pravega");
     protected static final String TCP = "tcp://";
@@ -444,6 +444,7 @@ public abstract class AbstractService implements Service {
         try {
             file.getAbsolutePath();
             resourceWrapper = objectMapper.readValue(file, ResourceWrapper.class);
+            log.info("*******" + resourceWrapper.getControllerProperties().getControllerResources().getRequests().get("cpu"));
         } catch (IOException e) {
             log.error("Input json file not available", e);
         }
