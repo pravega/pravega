5f18d71fa6635569758679cb96564ce24bead2b2
AnkurAnand11 <122247323+AnkurAnand11@users.noreply.github.com>
2023-05-24 20:59:46 +0530

    Issue 6939: handle ctrl c signal for controller (#7068)
    Pravega Controller process does not handle properly Cntrl+C signal when the SegmentStore instance is down
    
    Signed-off-by: Ankur_Anand <Ankur.Anand@dell.com>
    
    ---------
    
    Signed-off-by: Ankur_Anand <Ankur.Anand@dell.com>
    Co-authored-by: Tom Kaitchuck <tkaitchuck@users.noreply.github.com>

--- controller/src/main/java/io/pravega/controller/eventProcessor/impl/EventProcessorGroupImpl.java
+++ controller/src/main/java/io/pravega/controller/eventProcessor/impl/EventProcessorGroupImpl.java
@@ -214,7 +214,7 @@ public final class EventProcessorGroupImpl<T extends ControllerEvent> extends Ab
                 // Initiate stop on all event processor cells
                 for (EventProcessorCell<T> cell : eventProcessorMap.values()) {
                     log.info("Terminating event processor cell: {}", cell);
-                    cell.stopAsync();
+                    cell.stopAsync(true);
                 }
                 // Await termination of cells
                 for (EventProcessorCell<T> cell : eventProcessorMap.values()) {
@@ -229,7 +229,7 @@ public final class EventProcessorGroupImpl<T extends ControllerEvent> extends Ab
                 // Finally, clean up Reader Group data from Checkpoint store.
                 try {
                     cleanupReaderGroup(actorSystem.getProcess(), readerPositions);
-                } catch (CheckpointStoreException e) {
+                } catch (Exception e) {
                     log.warn("Error removing data for Reader Group {} from checkpoint store. ", readerGroup.getGroupName(), e);
                 }
                 readerGroup.close();
@@ -377,7 +377,7 @@ public final class EventProcessorGroupImpl<T extends ControllerEvent> extends Ab
             EventProcessorCell<T> cell = eventProcessorMap.get(readerId);
             log.info("Stopping event processor cell: {}", cell);
             try {
-                cell.stopAsync();
+                cell.stopAsync(false);
                 cell.awaitTerminated();
                 checkpointStore.removeReader(cell.getProcess(), readerGroup.getGroupName(), readerId);
                 eventProcessorMap.remove(readerId);
