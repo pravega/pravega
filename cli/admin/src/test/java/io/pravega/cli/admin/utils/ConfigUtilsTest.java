658edf661ae20ed89c6c801e6e3560e2105b6ee9
Amit-Singh40 <amit.singh30@dell.com>
2023-06-20 16:50:28 +0530

    Adding the lts_recovery_backup.sh file
    Signed-off-by: Amit-Singh40 <amit.singh30@dell.com>

--- cli/admin/src/test/java/io/pravega/cli/admin/utils/ConfigUtilsTest.java
+++ cli/admin/src/test/java/io/pravega/cli/admin/utils/ConfigUtilsTest.java
@@ -25,15 +25,16 @@ public class ConfigUtilsTest {
 
     @Test
     public void testConfigUtilsWithValidFile() throws IOException {
+        System.setProperty("pravega.configurationFile", "../../config/admin-cli.properties");
         @Cleanup
         AdminCommandState commandState = new AdminCommandState();
-        System.setProperty("pravega.configurationFile", "../../config/admin-cli.properties");
         System.setProperty("pravegaservice", "pravegaservice");
         ConfigUtils.loadProperties(commandState);
     }
 
     @Test
     public void testConfigUtilsWithInValidFile() throws IOException {
+        System.setProperty("pravega.configurationFile", "../../config/admin-cli.properties");
         @Cleanup
         AdminCommandState commandState = new AdminCommandState();
         System.setProperty("pravega.configurationFile", "dummy");
