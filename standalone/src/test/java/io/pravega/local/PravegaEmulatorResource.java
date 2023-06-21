233d92164708ad1b48c5a7103ab9b71a7d1f0680
Tom Kaitchuck <tom.kaitchuck@emc.com>
2023-06-20 17:44:51 -0700

    Add clarifying stack trace for when standalone fails to start.
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- standalone/src/test/java/io/pravega/local/PravegaEmulatorResource.java
+++ standalone/src/test/java/io/pravega/local/PravegaEmulatorResource.java
@@ -166,8 +166,12 @@ public class PravegaEmulatorResource extends ExternalResource {
         assertNotNull(streamManager);
         // Try creating a scope. This will retry based on the provided retry configuration.
         // If all the retries fail a RetriesExhaustedException will be thrown failing the tests.
-        boolean isScopeCreated = streamManager.createScope("healthCheck-scope");
-        log.debug("Health check scope creation is successful {}", isScopeCreated);
+        try {
+            boolean isScopeCreated = streamManager.createScope("healthCheck-scope");
+            log.debug("Health check scope creation is successful {}", isScopeCreated);
+        } catch (Exception e) {
+            throw new RuntimeException("Startup failed", e);
+        }
     }
 }
 
