271a5b715326fb931f2bb9e66ec578a70a8221e6
Amit-Singh40 <amit.singh30@dell.com>
2023-06-21 17:48:36 +0530

    Addressing the review comments
    Signed-off-by: Amit-Singh40 <amit.singh30@dell.com>

--- docker/pravega/scripts/lts_recovery_backup.sh
+++ docker/pravega/scripts/lts_recovery_backup.sh
@@ -1,82 +1,78 @@
-      max_attempt=3
-      home_dir=/opt/pravega
-      set_configuration() {
+max_attempt=3
+home_dir=/opt/pravega
 
-          line=$(ps -aef | more | grep -o '\Dpravegaservice.clusterName=.*D')
-          line=$(echo $line | cut -d ' ' -f1)
-          if [[ $line == "" ]]
-          then
-          	echo "Failed to get PravegaCluster"
-          	exit 1
-          fi
+validate() {
+  if [ "$#" -eq 2 ]
+  then
+    return
+  fi
+  [[ "$#" -ne 3 ]] && name=$1 || name=$3
+  if [ "$#" -ne 3 ] || [ $2 == "$1" ]
+  then
+    echo "Failed to get " $name
+    exit 1
+  fi
+}
 
-          pravega_cluster=$(echo $line | cut -d '=' -f2)
-          if [[ $pravega_cluster == "" ]]
-          then
-            echo "Failed to get PravegaCluster"
-            exit 1
-          fi
+val=''
 
-          line=$(ps -aef | more | grep -o '\Dpravegaservice.zk.connect.uri=.*D')
-          line=$(echo $line | cut -d ' ' -f1)
+get_val() {
+  cmd=$1
+  output=$(ps -aef | more | grep -o $cmd)
+  validate "$output" "${cmd:1}" "$2"
+  output=$(echo $output | cut -d ' ' -f1)
+  empty=""
+  validate "$output" $empty "$2"
+  val=$(echo $output | cut -d '=' -f2)
+  validate $val $empty "$2"
+}
 
-          if [[ $line == "" ]]
-          then
-              echo "Failed to get zookeeper client"
-              exit 1
-          fi
+set_configuration() {
+  get_val '\Dpravegaservice.clusterName=.*D' "Pravega cluster"
+  pravega_cluster=$val
+  get_val '\Dpravegaservice.zk.connect.uri=.*D' "zookeeper client"
+  zookeeper_client=$val
 
-          zookeeper_client=$(echo $line | cut -d '=' -f2)
-          if [[ $zookeeper_client == "" ]]
-          then
-              echo "Failed to get zookeeper client"
-              exit 1
-          fi
+  echo "Pravega cluster: $pravega_cluster"
+  echo "Zookeeper client: $zookeeper_client"
+  echo "pravegaservice.clusterName=pravega/$pravega_cluster"
+  echo "bookkeeper.ledger.path=pravega/$pravega_cluster/bookkeeper/ledgers"
+  echo "pravegaservice.zk.connect.uri=$zookeeper_client"
 
-          echo "Pravega cluster: $pravega_cluster"
-          echo "Zookeeper client: $zookeeper_client"
-          echo "pravegaservice.clusterName=pravega/$pravega_cluster"
-          echo "bookkeeper.ledger.path=pravega/$pravega_cluster/bookkeeper/ledgers"
-          echo "pravegaservice.zk.connect.uri=$zookeeper_client"
+  sed -i "s|pravegaservice.clusterName=.*|pravegaservice.clusterName=pravega/$pravega_cluster|g" $home_dir/conf/admin-cli.properties
+  sed -i "s|bookkeeper.ledger.path=.*|bookkeeper.ledger.path=pravega/$pravega_cluster/bookkeeper/ledgers|g" $home_dir/conf/admin-cli.properties
+  sed -i "s|pravegaservice.zk.connect.uri=.*|pravegaservice.zk.connect.uri=$zookeeper_client|g" $home_dir/conf/admin-cli.properties
+}
 
-          sed -i "s|pravegaservice.clusterName=.*|pravegaservice.clusterName=pravega/$pravega_cluster|g" $home_dir/conf/admin-cli.properties
-          sed -i "s|bookkeeper.ledger.path=.*|bookkeeper.ledger.path=pravega/$pravega_cluster/bookkeeper/ledgers|g" $home_dir/conf/admin-cli.properties
-          sed -i "s|pravegaservice.zk.connect.uri=.*|pravegaservice.zk.connect.uri=$zookeeper_client|g" $home_dir/conf/admin-cli.properties
-      }
+flush_container() {
+  cd $home_dir
+  output=$(./bin/pravega-admin container flush-to-storage all)
+  message="Flushed all the given segment container to storage."
+  if [[ "$output" != *"$message"* ]]
+  then
+    return 0;
+  fi
+  return 1;
+}
 
+set_configuration
+container_flushed=0
+retry=0
 
-      flush_container() {
-          cd $home_dir
-          output=$(./bin/pravega-admin container flush-to-storage all)
-          message="Flushed all the given segment container to storage."
-          if [[ "$output" != *"$message"* ]]
-          then
-              return 0;
-          fi
+while [ $container_flushed -eq 0 ] && [ $retry -lt $max_attempt ]
+do
+  echo ' '
+  echo "Calling flush to storage"
+  flush_container
+  container_flushed=$?
+  retry=$((retry + 1))
+done
 
-          return 1;
-      }
-
-
-      set_configuration
-      flush_container
-      container_flushed=$?
-      retry=1
-
-      while [ $container_flushed -eq 0 ] && [ $retry -lt $max_attempt ]
-      do
-          echo "Retrying flush to storage"
-          flush_container
-          container_flushed=$?
-          retry=$((retry + 1))
-      done
-
-      if [ $container_flushed -eq 1 ]
-      then
-          echo "Container flushed successfully."
-          exit 0
-      else
-          echo "Failed to flush container."
-          exit 1
-      fi
-
+if [ $container_flushed -eq 1 ]
+then
+  echo "Container flushed successfully."
+  exit 0
+else
+  echo "Failed to flush container."
+  exit 1
+fi
