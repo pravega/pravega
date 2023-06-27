cec0bdba79a57f7b0e8d35d58caa0654c64d9e43
amit-kumar59 <amit.kumar59@dell.com>
2023-06-27 15:52:55 +0530

    commiting the changes for checkstyle.
    Signed-off-by: amit-kumar59 <amit.kumar59@dell.com>

--- common/src/main/java/io/pravega/common/util/NewtonianSearch.java
+++ common/src/main/java/io/pravega/common/util/NewtonianSearch.java
@@ -20,27 +20,30 @@ import java.util.function.Function;
  * TO-DO.
  */
 public class NewtonianSearch {
+    @SuppressWarnings("unchecked")
     public static <T, R extends Comparable<R>> int newtonianSearch(Function<T, R> valueExtractor, R targetValue,
                                                                    int lowerBoundIndex, int upperBoundIndex) {
         int low = lowerBoundIndex;
         int high = upperBoundIndex;
 
         while (low <= high) {
-            int mid = interpolate(low, high, valueExtractor.apply((T) Integer.valueOf(low)),
-                    valueExtractor.apply((T) Integer.valueOf(high)), targetValue);
-
+            int mid = interpolate(low, high, valueExtractor.apply((T) createInteger(low)),
+                    valueExtractor.apply((T) createInteger(high)), targetValue);
             R midValue = valueExtractor.apply((T) Integer.valueOf(mid));
             int cmp = midValue.compareTo(targetValue);
-
-            if (cmp < 0)
+            if (cmp < 0) {
                 low = mid + 1;
-            else if (cmp > 0)
+            } else if (cmp > 0) {
                 high = mid - 1;
-            else
+            } else {
                 return mid;
+            }
         }
-
         return -(low + 1);
+    }
+
+    private static Integer createInteger(int value) {
+        return value;
     }
 
     private static <T, R extends Comparable<R>> int interpolate(int low, int high, R lowValue, R highValue,
