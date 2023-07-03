8782e541779e29d24a0625975722f6037c12bf80
Abhin Balu <Abhin_Balur@dell.com>
2023-07-03 20:27:25 +0530

    log for storagelength

--- segmentstore/contracts/src/main/java/io/pravega/segmentstore/contracts/StreamSegmentInformation.java
+++ segmentstore/contracts/src/main/java/io/pravega/segmentstore/contracts/StreamSegmentInformation.java
@@ -76,7 +76,7 @@ public class StreamSegmentInformation implements SegmentProperties {
                                      boolean sealedInStorage, boolean deletedInStorage, Map<AttributeId, Long> attributes, ImmutableDate lastModified) {
         Preconditions.checkArgument(startOffset >= 0, "startOffset must be a non-negative number.");
         Preconditions.checkArgument(length >= startOffset, "length must be a non-negative number and greater than startOffset.");
-        Preconditions.checkArgument(length >= storageLength, "storageLength must be less than or equal to length.");
+        Preconditions.checkArgument(length >= storageLength, "storageLength {} must be less than or equal to length {} for {}.",storageLength, length, name);
         if (deletedInStorage) {
             Preconditions.checkArgument(deleted, "deleted must be set if deletedInStorage is set.");
         }
