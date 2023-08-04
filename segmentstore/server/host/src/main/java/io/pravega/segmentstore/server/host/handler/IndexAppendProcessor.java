872835af4bc2be3f2ed4ca8b623b6719699e0389
Bhupender-Y <Bhupender.Y@dell.com>
2023-08-04 19:04:11 +0530

    Create index segment on StreamDoesNotExist exception.
    Signed-off-by: Bhupender-Y <Bhupender.Y@dell.com>

--- segmentstore/server/host/src/main/java/io/pravega/segmentstore/server/host/handler/IndexAppendProcessor.java
+++ segmentstore/server/host/src/main/java/io/pravega/segmentstore/server/host/handler/IndexAppendProcessor.java
@@ -17,16 +17,24 @@
 package io.pravega.segmentstore.server.host.handler;
 
 import io.netty.buffer.Unpooled;
+import io.pravega.common.Exceptions;
 import io.pravega.common.concurrent.LatestItemSequentialProcessor;
 import io.pravega.segmentstore.contracts.AttributeUpdate;
 import io.pravega.segmentstore.contracts.AttributeUpdateCollection;
 import io.pravega.segmentstore.contracts.AttributeUpdateType;
+import io.pravega.segmentstore.contracts.SegmentType;
+import io.pravega.segmentstore.contracts.StreamSegmentNotExistsException;
 import io.pravega.segmentstore.contracts.StreamSegmentStore;
 import io.pravega.shared.protocol.netty.ByteBufWrapper;
 import java.time.Duration;
+import java.util.Arrays;
+import java.util.Collection;
 import java.util.concurrent.ScheduledExecutorService;
 import lombok.extern.slf4j.Slf4j;
 
+import static io.pravega.segmentstore.contracts.Attributes.ALLOWED_INDEX_SEG_EVENT_SIZE;
+import static io.pravega.segmentstore.contracts.Attributes.ATTRIBUTE_SEGMENT_TYPE;
+import static io.pravega.segmentstore.contracts.Attributes.CREATION_TIME;
 import static io.pravega.segmentstore.contracts.Attributes.EVENT_COUNT;
 import static io.pravega.shared.NameUtils.getIndexSegmentName;
 import static io.pravega.shared.NameUtils.isTransactionSegment;
@@ -69,15 +77,42 @@ public class IndexAppendProcessor {
              .whenComplete((length, ex) -> {
                  if (ex == null) {
                      log.trace("Index segment append successful for Segment : {} with length {} ", segmentName, length);
+                 } else if (Exceptions.unwrap(ex) instanceof StreamSegmentNotExistsException) {
+                     logWarningMessage(segmentName, ex);
+                     createIndexSegment(segmentName);
                  } else {
-                     log.warn("Index segment append failed for segment {} due to ", segmentName, ex);
+                     logWarningMessage(segmentName, ex);
                  }
              });
+
+    }
+
+    private void logWarningMessage(String segmentName, Throwable ex) {
+        log.warn("Index segment append failed for segment {} due to ", segmentName, ex);
     }
 
     private ByteBufWrapper getIndexAppendBuf(Long eventLength, Long eventCount) {
         IndexEntry indexEntry = new IndexEntry(eventLength, eventCount, System.currentTimeMillis());
         return new ByteBufWrapper(Unpooled.wrappedBuffer( indexEntry.toBytes().getCopy()));
+    }
+
+    private void createIndexSegment(final String segmentName) {
+        String indexSegmentName = getIndexSegmentName(segmentName);
+        log.info("Creating index segment {}.", indexSegmentName);
+        final long maxEventSize = 24L;
+        Collection<AttributeUpdate> attributes = Arrays.asList(
+                new AttributeUpdate(CREATION_TIME, AttributeUpdateType.None, System.currentTimeMillis()),
+                new AttributeUpdate(ATTRIBUTE_SEGMENT_TYPE, AttributeUpdateType.None, SegmentType.INDEX_SEGMENT.getValue()),
+                new AttributeUpdate(ALLOWED_INDEX_SEG_EVENT_SIZE, AttributeUpdateType.None, maxEventSize)
+        );
+        store.createStreamSegment(getIndexSegmentName(segmentName), SegmentType.INDEX_SEGMENT,
+                attributes, TIMEOUT).whenComplete((v, e) -> {
+                    if (e == null) {
+                        log.info("Index segment {} created successfully.", indexSegmentName);
+                    } else {
+                        log.warn("Unable to create index segment {} due to ", indexSegmentName, e);
+                    }
+        });
     }
 
 }
