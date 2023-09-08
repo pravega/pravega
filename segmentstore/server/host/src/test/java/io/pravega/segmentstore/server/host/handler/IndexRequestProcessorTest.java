88b176012584b035f616070bc6cecc9de6f60ce6
Tom Kaitchuck <tom.kaitchuck@emc.com>
2023-09-07 17:25:43 -0700

    Increase code coverage
    Signed-off-by: Tom Kaitchuck <tom.kaitchuck@emc.com>

--- segmentstore/server/host/src/test/java/io/pravega/segmentstore/server/host/handler/IndexRequestProcessorTest.java
+++ segmentstore/server/host/src/test/java/io/pravega/segmentstore/server/host/handler/IndexRequestProcessorTest.java
@@ -87,6 +87,60 @@ public class IndexRequestProcessorTest {
     public void locateOffsetForSegmentWithFuture() {
         String segmentName = "test";
         String indexSegmentName = getIndexSegmentName(segmentName);
+        SegmentProperties segmentProperties = StreamSegmentInformation.builder()
+                                                                      .name(indexSegmentName)
+                                                                      .length(12)
+                                                                      .startOffset(0)
+                                                                      .attributes(Map.of(EVENT_COUNT, 1L))
+                                                                      .build();
+        SegmentProperties indexSegmentProperties = StreamSegmentInformation.builder()
+                                                                           .name(indexSegmentName)
+                                                                           .length(24)
+                                                                           .startOffset(0)
+                                                                           .attributes(Map.of(EVENT_COUNT, 0L))
+                                                                           .build();
+        ReadResultEntry readResultEntry = mock(ReadResultEntry.class);
+        doNothing().when(readResultEntry).requestContent(any());
+        doReturn(ReadResultEntryType.Future).when(readResultEntry).getType();
+        doReturn(CompletableFuture.completedFuture(new IndexEntry(12, 1, 1234).toBytes())).when(readResultEntry).getContent();
+        ReadResult result = mock(ReadResult.class);
+        doReturn(true).when(result).hasNext();
+        doReturn(readResultEntry).when(result).next();
+        doReturn(CompletableFuture.completedFuture(segmentProperties)).when(store).getStreamSegmentInfo(eq(segmentName), any());
+        doReturn(CompletableFuture.completedFuture(indexSegmentProperties)).when(store).getStreamSegmentInfo(eq(indexSegmentName), any());
+        doReturn(CompletableFuture.completedFuture(result)).when(store).read(anyString(), anyLong(), anyInt(), any());
+        assertEquals(0, IndexRequestProcessor.locateOffsetForSegment(store, segmentName, 0L, false));
+        assertEquals(12, IndexRequestProcessor.locateOffsetForSegment(store, segmentName, 10L, true));
+        assertEquals(12, IndexRequestProcessor.locateOffsetForSegment(store, segmentName, 20L, true));
+    }
+
+    @Test(timeout = 5000)
+    public void locateTruncateOffsetForSegment() {
+        String segmentName = "test";
+        String indexSegmentName = getIndexSegmentName(segmentName);
+        SegmentProperties segmentProperties = StreamSegmentInformation.builder()
+                                                                      .name(segmentName)
+                                                                      .length(1234)
+                                                                      .startOffset(123)
+                                                                      .attributes(Map.of(EVENT_COUNT, 30L))
+                                                                      .build();
+        SegmentProperties indexSegmentProperties = StreamSegmentInformation.builder()
+                                                                      .name(indexSegmentName)
+                                                                      .length(0)
+                                                                      .startOffset(0)
+                                                                      .attributes(Map.of(EVENT_COUNT, 0L))
+                                                                      .build();
+
+        doReturn(CompletableFuture.completedFuture(indexSegmentProperties)).when(store).getStreamSegmentInfo(indexSegmentName, timeout);
+        doReturn(CompletableFuture.completedFuture(segmentProperties)).when(store).getStreamSegmentInfo(segmentName, timeout);
+
+        assertEquals(0, IndexRequestProcessor.locateTruncateOffsetInIndexSegment(store, segmentName, 10L));
+    }
+    
+    @Test(timeout = 5000)
+    public void locateTruncateOffsetForSegmentWithFuture() {
+        String segmentName = "test";
+        String indexSegmentName = getIndexSegmentName(segmentName);
 
         SegmentProperties indexSegmentProperties = StreamSegmentInformation.builder()
                                                                            .name(indexSegmentName)
@@ -103,7 +157,8 @@ public class IndexRequestProcessorTest {
         doReturn(readResultEntry).when(result).next();
         doReturn(CompletableFuture.completedFuture(indexSegmentProperties)).when(store).getStreamSegmentInfo(eq(indexSegmentName), any());
         doReturn(CompletableFuture.completedFuture(result)).when(store).read(anyString(), anyLong(), anyInt(), any());
-        assertEquals(12, IndexRequestProcessor.locateOffsetForSegment(store, segmentName, 10L, true));
+        assertEquals(0, IndexRequestProcessor.locateTruncateOffsetInIndexSegment(store, segmentName, 10L));
+        assertEquals(24, IndexRequestProcessor.locateTruncateOffsetInIndexSegment(store, segmentName, 12L));
     }
-
+    
 }
