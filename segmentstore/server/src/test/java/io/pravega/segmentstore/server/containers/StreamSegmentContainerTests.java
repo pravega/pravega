aa843e0026a9b91436823a07de6dee4d51da2f9d
dada-dell-emc <119924591+dada-dell-emc@users.noreply.github.com>
2023-09-14 17:26:14 +0530

    Issue 7258: Handling evicted operations before flushing to storage. (#7251)
    Issue 7258: Handling operations of evicted segments before flushing to storage. (#7251)
    
    ---------
    Signed-off-by: dada-dell-emc <dadasaheb.patil@dell.com>
    Signed-off-by: Abhin Balur <Abhin_Balur@dell.com>
    Co-authored-by: Abhin Balur <Abhin_Balur@dell.com>
    Co-authored-by: abhinb <88840194+abhinb@users.noreply.github.com>

--- segmentstore/server/src/test/java/io/pravega/segmentstore/server/containers/StreamSegmentContainerTests.java
+++ segmentstore/server/src/test/java/io/pravega/segmentstore/server/containers/StreamSegmentContainerTests.java
@@ -813,6 +813,67 @@ public class StreamSegmentContainerTests extends ThreadPooledTestSuite {
     }
 
     /**
+     * The below test validates a case where the container does not bail out
+     * post container recovery where the StorageWriter has to deal with an Operation
+     * on a segment that was originally evicted out(not present in metadata) yet
+     * the Operation still exists in the BK Log and read during recovery and hence
+     * processed by StorageWriter.
+     * @throws Exception in case of exception.
+     */
+    @Test(timeout = 10000)
+    public void testWritingEvictedSegmentOperations() throws Exception {
+        final String segmentName = "segment";
+        final ByteArraySegment appendData = new ByteArraySegment("hello".getBytes());
+
+        final TestContainerConfig containerConfig = new TestContainerConfig();
+        containerConfig.setSegmentMetadataExpiration(Duration.ofMillis(EVICTION_SEGMENT_EXPIRATION_MILLIS_SHORT));
+
+        @Cleanup
+        TestContext context = createContext(containerConfig);
+        val localDurableLogFactory = new DurableLogFactory(DEFAULT_DURABLE_LOG_CONFIG, context.dataLogFactory, executorService());
+        AtomicReference<OperationLog> durableLog = new AtomicReference<>();
+
+        val watchableOperationLogFactory = new WatchableOperationLogFactory(localDurableLogFactory, durableLog::set);
+        try (val container1 = new MetadataCleanupContainer(CONTAINER_ID, containerConfig, watchableOperationLogFactory,
+                context.readIndexFactory, context.attributeIndexFactory, context.writerFactory, context.storageFactory,
+                context.getDefaultExtensions(), executorService())) {
+            container1.startAsync().awaitRunning();
+
+            // Create segment and make one append to it.
+            container1.createStreamSegment(segmentName, getSegmentType(segmentName), null, TIMEOUT).get(TIMEOUT.toMillis(), TimeUnit.MILLISECONDS);
+            container1.append(segmentName, appendData, null, TIMEOUT).get(TIMEOUT.toMillis(), TimeUnit.MILLISECONDS);
+            val metadataId = container1.metadata.getStreamSegmentId(segmentName, true);
+            // Wait until the segment is forgotten.
+            container1.triggerMetadataCleanup(Collections.singleton(segmentName)).get(TIMEOUT.toMillis(), TimeUnit.MILLISECONDS);
+
+            // Add a read so the segment is re-instated.
+            container1.read(segmentName, 0, 5, TIMEOUT).join();
+
+            container1.deleteStreamSegment(segmentName, TIMEOUT).join();
+
+            // Simulate segment eviction in metadata
+            StreamSegmentMetadata met = new TestStreamSegmentMetadata(segmentName, metadataId, container1.getId());
+            val segmentMetadata = container1.metadata.getStreamSegmentMetadata(metadataId);
+            met.copyFrom(segmentMetadata);
+            met.setLastUsed(0);
+            Collection<SegmentMetadata> evictSegment = List.of(met);
+            container1.metadata.cleanup(evictSegment, container1.metadata.getOperationSequenceNumber());
+            // Generate a checkpoint without the segment metadata.
+            durableLog.get().checkpoint(TIMEOUT).join();
+            container1.stopAsync().awaitTerminated();
+        }
+
+        // Restart container and verify it started successfully.
+        @Cleanup
+        val container2 = new MetadataCleanupContainer(CONTAINER_ID, containerConfig, localDurableLogFactory,
+                context.readIndexFactory, context.attributeIndexFactory, context.writerFactory, context.storageFactory,
+                context.getDefaultExtensions(), executorService());
+        container2.startAsync().awaitRunning();
+        container2.flushToStorage(TIMEOUT).join();
+        container2.stopAsync().awaitTerminated();
+    }
+
+    /**
      * Test conditional updates for Extended Attributes when they are not loaded in memory (i.e., they will need to be
      * auto-fetched from the AttributeIndex so that the operation may succeed).
      */
@@ -3342,6 +3403,56 @@ public class StreamSegmentContainerTests extends ThreadPooledTestSuite {
             OperationLog log = this.wrappedFactory.createDurableLog(containerMetadata, readIndex);
             this.onCreateLog.accept(log);
             return log;
+        }
+    }
+
+    static class TestStreamSegmentMetadata extends StreamSegmentMetadata {
+        /**
+         * Creates a new instance of the StreamSegmentMetadata class for a StreamSegment.
+         *
+         * @param streamSegmentName The name of the StreamSegment.
+         * @param streamSegmentId   The Id of the StreamSegment.
+         * @param containerId       The Id of the Container this StreamSegment belongs to.
+         * @throws IllegalArgumentException If either of the arguments are invalid.
+         */
+        public TestStreamSegmentMetadata(String streamSegmentName, long streamSegmentId, int containerId) {
+            super(streamSegmentName, streamSegmentId, containerId);
+        }
+
+        public synchronized void copyFrom(SegmentMetadata base) {
+            Exceptions.checkArgument(this.getId() == base.getId(), "base", "Given SegmentMetadata refers to a different StreamSegment than this one (SegmentId).");
+            Exceptions.checkArgument(this.getName().equals(base.getName()), "base", "Given SegmentMetadata refers to a different StreamSegment than this one (SegmentName).");
+
+            setStorageLength(base.getStorageLength());
+            setLength(base.getLength());
+
+            // Update StartOffset after (potentially) updating the length, since he Start Offset must be less than or equal to Length.
+            setStartOffset(base.getStartOffset());
+            setLastModified(base.getLastModified());
+            updateAttributes(base.getAttributes());
+            refreshDerivedProperties();
+
+            if (base.isSealed()) {
+                markSealed();
+                if (base.isSealedInStorage()) {
+                    markSealedInStorage();
+                }
+            }
+
+            if (base.isMerged()) {
+                markMerged();
+            }
+
+            if (base.isDeleted()) {
+                markDeleted();
+                if (base.isDeletedInStorage()) {
+                    markDeletedInStorage();
+                }
+            }
+
+            if (base.isPinned()) {
+                markPinned();
+            }
         }
     }
 
