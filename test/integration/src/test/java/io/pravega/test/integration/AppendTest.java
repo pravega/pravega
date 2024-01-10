80b9710f21b61d7ed30c7a6b68545e2b81f266ea
Abhin Balur <Abhin_Balur@dell.com>
2024-01-09 20:06:12 -0800

    test pr
    Signed-off-by: Abhin Balur <Abhin_Balur@dell.com>

--- test/integration/src/test/java/io/pravega/test/integration/AppendTest.java
+++ test/integration/src/test/java/io/pravega/test/integration/AppendTest.java
@@ -512,7 +512,7 @@ public class AppendTest extends LeakDetectorTestSuite {
         streamManager.createStream("Scope", streamName, null);
         @Cleanup
         EventStreamWriter<ByteBuffer> producer = clientFactory.createEventWriter(streamName, new ByteBufferSerializer(), EventWriterConfig.builder().build());
-        long blockingTime = timeWrites(testPayload, 3000, producer, true);
+        long blockingTime = timeWrites(testPayload, 2000, producer, true);
         long nonBlockingTime = timeWrites(testPayload, 60000, producer, false);
         System.out.println("Blocking took: " + blockingTime + "ms.");
         System.out.println("Non blocking took: " + nonBlockingTime + "ms.");
