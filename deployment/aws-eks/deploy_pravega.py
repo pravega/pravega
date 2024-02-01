7684e5d515f68271cfa232e3dc9b4ea9f3e38ed4
Raúl Gracia <raul.gracia@emc.com>
2024-02-01 15:12:03 +0100

    Added instructions to add permissiosn for creating EBS volumes.
    Signed-off-by: Raúl Gracia <raul.gracia@emc.com>

--- deployment/aws-eks/deploy_pravega.py
+++ deployment/aws-eks/deploy_pravega.py
@@ -32,8 +32,15 @@ from kubernetes import client, config
 # you are using another type of i3 instance, it may happen that the name of the drives to mount change across instances.
 # Take that into account when setting the journal_nvme_device and ledger_nvme_device variables in the main() method.
 #
-# 3) Attach the AmazonEBSCSIDriverPolicy to the EKS Cluster role, so we can create EBS volumes:
-# > aws iam attach-role-policy --role-name AmazonEBSCSIDriverPolicy --policy-arn arn:aws:iam::aws:policy/YourPolicyName
+# 3) Attach the AmazonEBSCSIDriverPolicy to the EKS Cluster role, so we can create EBS volumes (replace
+# YourClusterNameHere and YourRegion by the appropriate values):
+# > eksctl utils associate-iam-oidc-provider --region=YourRegion --cluster=YourClusterNameHere --approve
+# > eksctl create iamserviceaccount --region YourRegion -name ebs-csi-controller-sa --namespace kube-system \
+#   --cluster YourClusterNameHere --attach-policy-arn arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy \
+#   --approve --role-only -role-name AmazonEKS_EBS_CSI_DriverRole
+# > eksctl create addon --name aws-ebs-csi-driver --cluster YourClusterNameHere \
+#   --service-account-role-arn arn:aws:iam::$(aws sts get-caller-identity --query Account --output text):role/AmazonEKS_EBS_CSI_DriverRole \
+#   --force
 #
 # 4) Run the deployment script for Pravega.
 # > python deploy_pravega.py
